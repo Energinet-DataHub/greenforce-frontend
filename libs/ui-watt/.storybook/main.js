18bf82e50b0a0fa1c66a6feb873bfacfd3bb7406
frontend-specialisten.dk <alexander@frontend-specialisten.dk>
2021-08-25 12:22:06 +0200

    feat(storybook): add a11y addon

--- libs/ui-watt/.storybook/main.js
+++ libs/ui-watt/.storybook/main.js
@@ -5,5 +5,5 @@ module.exports = {
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ["@storybook/addon-links", '@storybook/addon-essentials']
+  addons: ['@storybook/addon-links', '@storybook/addon-a11y', '@storybook/addon-essentials']
 };
