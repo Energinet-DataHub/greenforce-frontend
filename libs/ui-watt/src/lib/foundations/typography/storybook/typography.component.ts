6c9142cff94564009e9d869c29623f3a32326890
Dzhavat Ushev <xdzus@energinet.dk>
2021-08-31 15:04:44 +0200

    update typography component

--- libs/ui-watt/src/lib/foundations/typography/storybook/typography.component.ts
+++ libs/ui-watt/src/lib/foundations/typography/storybook/typography.component.ts
@@ -2,59 +2,113 @@ import { Component } from '@angular/core';
 
 interface Typography {
   name: string;
+  html: string;
   size: string;
   weight: string;
   letterCase: string;
   letterSpacing: string;
   lineHeight: string;
-  tag: string;
 }
 
 const typeScale: Typography[] = [
   {
     name: 'Headline 1',
+    html: '<h1>Headline 1</h1>',
     size: '36px',
     weight: 'Medium',
     letterCase: 'All caps',
     letterSpacing: '0',
     lineHeight: '54',
-    tag: 'h1',
   },
   {
     name: 'Headline 2',
+    html: '<h2>Headline 2</h2>',
     size: '32px',
     weight: 'Medium',
     letterCase: 'All caps',
     letterSpacing: '0',
     lineHeight: '48',
-    tag: 'h2',
   },
   {
     name: 'Headline 3',
+    html: '<h3>Headline 3</h3>',
     size: '28px',
     weight: 'Medium',
     letterCase: 'All caps',
     letterSpacing: '0',
     lineHeight: '42',
-    tag: 'h3',
   },
   {
     name: 'Headline 4',
+    html: '<h4>Headline 4</h4>',
     size: '24px',
     weight: 'Medium',
     letterCase: 'All caps',
     letterSpacing: '0',
     lineHeight: '36',
-    tag: 'h4',
   },
   {
     name: 'Headline 5',
+    html: '<h5>Headline 5</h5>',
     size: '20px',
     weight: 'Medium',
     letterCase: 'All caps',
     letterSpacing: '0',
     lineHeight: '30',
-    tag: 'h5',
+  },
+  {
+    name: 'Lead (text-l)',
+    html: '<p class="watt-text-l">Lead (text-l)</p>',
+    size: '18px',
+    weight: 'Medium',
+    letterCase: 'Sentence',
+    letterSpacing: '0',
+    lineHeight: '27',
+  },
+  {
+    name: 'Body (text-m)',
+    html: '<p>Body (text-m)</p>',
+    size: '16px',
+    weight: 'Regular',
+    letterCase: 'Sentence',
+    letterSpacing: '0',
+    lineHeight: '24',
+  },
+  {
+    name: 'Small (text-s)',
+    html: '<p><small>Body (text-s)</small></p>',
+    size: '14px',
+    weight: 'Regular',
+    letterCase: 'Sentence',
+    letterSpacing: '0',
+    lineHeight: '21',
+  },
+  {
+    name: 'Extra small (text-xs)',
+    html: '<p class="watt-text-xs">Extra small (text-xs)</p>',
+    size: '12px',
+    weight: 'Regular',
+    letterCase: 'Sentence',
+    letterSpacing: '0',
+    lineHeight: '18',
+  },
+  {
+    name: 'Button',
+    html: '<p class="watt-button">Button</p>',
+    size: '16px',
+    weight: 'Medium',
+    letterCase: 'All caps',
+    letterSpacing: '1.25px',
+    lineHeight: '16',
+  },
+  {
+    name: 'Label',
+    html: '<p class="watt-label">Label</p>',
+    size: '12px',
+    weight: 'Medium',
+    letterCase: 'All caps',
+    letterSpacing: '1.5px',
+    lineHeight: '16',
   },
 ];
 
