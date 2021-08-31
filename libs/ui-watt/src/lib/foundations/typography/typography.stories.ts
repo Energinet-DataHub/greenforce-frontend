55761cb19524b3c07dc41aa9bd9869285929c6e6
Dzhavat Ushev <xdzus@energinet.dk>
2021-08-31 09:51:58 +0200

    add story to typography component

--- libs/ui-watt/src/lib/foundations/typography/typography.stories.ts
+++ libs/ui-watt/src/lib/foundations/typography/typography.stories.ts
@@ -21,3 +21,64 @@ const Template: Story<TypographyComponent> = (args) => ({
 
 //👇 Each story then reuses that template
 export const Typography = Template.bind({});
+Typography.parameters = {
+  docs: {
+    page: null,
+  },
+};
+
+export const TypographyUsage = () => ({
+  template: `
+    <h1>
+      Headline 1
+    </h1>
+
+    <h2>
+      Headline 2
+    </h2>
+
+    <h3>
+      Headline 3
+    </h3>
+
+    <h4>
+      Headline 4
+    </h4>
+
+    <h5>
+      Headline 5
+    </h5>
+
+    <p class="watt-text-l">
+      Lead (text-l)
+    </p>
+
+    <p>
+      Body (text-m)
+    </p>
+
+    <p>
+      <small>
+        Small (text-s)
+      </small>
+    </p>
+
+    <p class="watt-text-xs">
+      Extra small (text-xs)
+    </p>
+
+    <p class="watt-button">
+      Button
+    </p>
+
+    <p class="watt-overline">
+      Label
+    </p>
+  `,
+});
+TypographyUsage.storyName = 'Typography - Usage';
+TypographyUsage.parameters = {
+  docs: {
+    page: null,
+  },
+};
