452009e426be142f53253453e44f11d768008cf7
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-20 14:42:28 +0200

    test(ett): remove generated test setup

--- apps/ett-app-e2e/src/integration/app.spec.ts
+++ apps/ett-app-e2e/src/integration/app.spec.ts
@@ -1,13 +1,9 @@
 import { getTitle } from '../support/app.po';
 
-describe('ett-app', () => {
+describe('Energy Track and Trace app', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    // cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
+  it('displays a title', () => {
     getTitle().contains('Energy Track and Trace');
   });
 });
