f56a70efe950b92930a74b1040145830663915a7
Dzhavat Ushev <xdzus@energinet.dk>
2021-09-21 17:10:08 +0200

    fix(test): remove unnecessary test command

--- apps/dh3-admin-e2e/src/integration/app.spec.ts
+++ apps/dh3-admin-e2e/src/integration/app.spec.ts
@@ -4,9 +4,6 @@ describe('dh3-admin', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('THIS IS THE APP');
   });
