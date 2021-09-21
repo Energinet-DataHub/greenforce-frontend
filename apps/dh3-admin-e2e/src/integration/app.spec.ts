9de4f29382c1b340ab2925aca622c3fdc998f342
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-21 15:45:38 +0200

    test(dh3): fix end-to-end test

--- apps/dh3-admin-e2e/src/integration/app.spec.ts
+++ apps/dh3-admin-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('dh3-admin', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to dh3-admin!');
+    getGreeting().contains('THIS IS THE APP');
   });
 });
