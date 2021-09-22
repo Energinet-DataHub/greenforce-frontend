453ae35e71ffa29ee4b27936e99def5b51b190f4
Dan Stenrøjl <XDAST@energinet.dk>
2021-09-22 16:38:03 +0200

    Updated StyleCop and editorconfig rules

--- libs/api-dh-app/source/DataHub.WebApi/WeatherForecast.cs
+++ libs/api-dh-app/source/DataHub.WebApi/WeatherForecast.cs
@@ -1,6 +1,20 @@
-﻿using System;
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-namespace DataHub.WebApi
+using System;
+
+namespace Energinet.DataHub.WebApiS
 {
     public class WeatherForecast
     {
@@ -11,5 +25,6 @@ namespace DataHub.WebApi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+            = string.Empty;
     }
 }
