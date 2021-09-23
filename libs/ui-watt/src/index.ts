40d745638f063be44146183becd6215331c577a8
Dzhavat Ushev <xdzus@energinet.dk>
2021-09-23 13:58:01 +0200

    feature: export shell component artifacts

--- libs/ui-watt/src/index.ts
+++ libs/ui-watt/src/index.ts
@@ -1,3 +1,7 @@
 export { WattModule } from './lib/ui-watt.module';
+
 export { AutocompleteComponent } from './lib/components/autocomplete/autocomplete.component';
 export { AutocompleteModule } from './lib/components/autocomplete/autocomplete.module';
+
+export { ShellComponent } from './lib/components/shell/shell.component';
+export { ShellModule } from './lib/components/shell/shell.module';
