ec353b9743a1691d9b4018b9da4832612b8fba7b
Dzhavat Ushev <xdzus@energinet.dk>
2021-09-23 13:57:43 +0200

    feature: generate shell component

---
+++ libs/ui-watt/src/lib/components/shell/shell.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'watt-shell',
+  styleUrls: ['./shell.component.scss'],
+  templateUrl: './shell.component.html',
+})
+export class ShellComponent {}
---
+++ libs/ui-watt/src/lib/components/shell/shell.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ShellComponent } from './shell.component';
+
+@NgModule({
+  declarations: [ShellComponent],
+  exports: [ShellComponent],
+})
+export class ShellModule {}
