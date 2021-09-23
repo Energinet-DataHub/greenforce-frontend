1afc07f59747f23df72b0545c3f6063afc74eddc
Dzhavat Ushev <xdzus@energinet.dk>
2021-09-23 13:56:40 +0200

    chore: format file

--- libs/ui-watt/src/lib/components/autocomplete/autocomplete.module.ts
+++ libs/ui-watt/src/lib/components/autocomplete/autocomplete.module.ts
@@ -1,14 +1,21 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { AutocompleteComponent } from "./autocomplete.component";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { AutocompleteComponent } from './autocomplete.component';
 
 @NgModule({
-    imports: [CommonModule, ReactiveFormsModule, MatAutocompleteModule, MatFormFieldModule, MatInputModule],
-    declarations: [AutocompleteComponent],
-    exports: [AutocompleteComponent],
+  declarations: [AutocompleteComponent],
+  exports: [AutocompleteComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class AutocompleteModule {}
