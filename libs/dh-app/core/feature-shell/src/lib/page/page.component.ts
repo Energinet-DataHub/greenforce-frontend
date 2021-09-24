3930eba3fb64fda51e81616c8b054de834345f35
Dzhavat Ushev <xdzus@energinet.dk>
2021-09-24 08:56:08 +0200

    feature: generate page component in DataHub app

---
+++ libs/dh-app/core/feature-shell/src/lib/page/page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'energinet-page',
+  styleUrls: ['./page.component.scss'],
+  templateUrl: './page.component.html',
+})
+export class PageComponent {}
---
+++ libs/dh-app/core/feature-shell/src/lib/page/page.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PageComponent } from './page.component';
+
+@NgModule({
+  declarations: [PageComponent],
+  imports: [CommonModule],
+})
+export class PageModule {}
