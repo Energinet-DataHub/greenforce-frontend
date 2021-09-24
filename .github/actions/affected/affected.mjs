d7bbb7c562ecf6ccf8c11ba4ccd05182fb112b4b
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-24 13:45:49 +0200

    ci(ett): build and publish host (#35)
    Energy Track and Trace app workflow:
    - Build the app when the app is affected by a PR or merge
    - Build, tag, and publish the host container image to GitHub Container Registry when the app is affected by a PR or merge
    - Host container image is tagged as `pr-<pull-request-number>` or `latest` for PRs and merge runs, respectively
    
    Energy Track and Trace host:
     - Nginx server with port 80 exposed
     - Gzip compression
     - SPA routing
    
    Affected action:
    - Licensed by ngworkers
    - Determines whether a project is affected by changes in a pull request or merge commit

---
+++ .github/actions/affected/affected.mjs
@@ -0,0 +1,98 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2021 ngworkers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * Terms
+ */
+import { execSync } from 'child_process';
+import { readFileSync } from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+function readAffectedApps(baseBranch, baseCommit) {
+  const affected = execSync(
+    `yarn nx affected:apps --plain --base=origin/${baseBranch} --head=${baseCommit}`,
+    {
+      encoding: 'utf-8',
+      stdio: 'pipe',
+    }
+  );
+
+  return sanitizeAffectedOutput(affected);
+}
+
+function readAffectedLibs(baseBranch, baseCommit) {
+  const affected = execSync(
+    `yarn nx affected:libs --plain --base=origin/${baseBranch} --head=${baseCommit}`,
+    {
+      encoding: 'utf-8',
+      stdio: 'pipe',
+    }
+  );
+
+  return sanitizeAffectedOutput(affected);
+}
+
+function readAffectedProjects(baseBranch, baseCommit) {
+  const affectedApps = readAffectedApps(baseBranch, baseCommit);
+  const affectedLibs = readAffectedLibs(baseBranch, baseCommit);
+
+  return affectedApps.concat(affectedLibs);
+}
+
+function sanitizeAffectedOutput(affectedOutput) {
+  return affectedOutput
+    .trim()
+    .split(' ')
+    .filter((project) => project !== '');
+}
+
+function validateProjectParameter(projectName) {
+  if (!projectName) {
+    console.error('No project argument passed.');
+
+    process.exit(1);
+  }
+
+  const workspacePath = path.resolve(__dirname, '../../../angular.json');
+  const workspace = JSON.parse(readFileSync(workspacePath).toString());
+  const isProjectFound = workspace.projects[projectName] !== undefined;
+
+  if (!isProjectFound) {
+    console.error(
+      `"${projectName}" is not the name of a project in this workspace.`
+    );
+
+    process.exit(1);
+  }
+}
+
+// Not available in an ES Module as of Node.js 12.x
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const [, , project, baseBranch, baseCommit] = process.argv;
+
+validateProjectParameter(project);
+
+const affectedProjects = readAffectedProjects(baseBranch, baseCommit);
+const isAffected = affectedProjects.includes(project);
+
+console.log(isAffected);
