3a3ddb5753e871e00c84f83d468c2393a94321f7
Dzhavat Ushev <xdzus@energinet.dk>
2021-09-24 11:56:33 +0200

    refactor: change library prefix

--- libs/dh-app/core/feature-shell/src/lib/page/page.component.ts
+++ libs/dh-app/core/feature-shell/src/lib/page/page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'energinet-page',
+  selector: 'dh-page',
   styleUrls: ['./page.component.scss'],
   templateUrl: './page.component.html',
 })
