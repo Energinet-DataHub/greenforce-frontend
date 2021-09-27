80f03d9da42f1f080a2527ccbb36f702cfcd54db
Dzhavat Ushev <xdzus@energinet.dk>
2021-09-27 10:27:02 +0200

    test(dh-app): simplify test setup

--- apps/dh-app/src/app/datahub-app.module.spec.ts
+++ apps/dh-app/src/app/datahub-app.module.spec.ts
@@ -1,52 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { DataHubAppComponent } from './datahub-app.component';
 import { DataHubAppModule } from './datahub-app.module';
 
-function setup() {
-  TestBed.configureTestingModule({
-    imports: [DataHubAppModule, RouterTestingModule],
-  }).compileComponents();
+describe('Application smoke test', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [DataHubAppModule, NoopAnimationsModule, RouterTestingModule],
+    }).compileComponents();
 
-  let rootFixture: ComponentFixture<DataHubAppComponent>;
-  const initializeRootFixture = () => {
-    if (rootFixture == null) {
-      rootFixture = TestBed.createComponent(DataHubAppComponent);
-    }
-  };
+    rootFixture = TestBed.createComponent(DataHubAppComponent);
+    router = TestBed.inject(Router);
 
-  return {
-    get router() {
-      initializeRootFixture();
-
-      return TestBed.inject(Router);
-    },
-    run<TResult>(task: () => TResult) {
-      initializeRootFixture();
-
-      return rootFixture.ngZone == null ? task() : rootFixture.ngZone.run(task);
-    },
-  };
-}
-
-describe('Applicaton smoke test', () => {
-  it('the application boots up', () => {
-    const bootApplication = () => {
-      const { router, run } = setup();
-
-      run(() => router.initialNavigation());
-    };
-
-    expect(bootApplication).not.toThrow();
+    rootFixture.autoDetectChanges(true);
   });
 
+  let rootFixture: ComponentFixture<DataHubAppComponent>;
+  let router: Router;
+
   it('navigation works', async () => {
-    const { router, run } = setup();
+    expect.assertions(1);
 
-    const canNavigate = await run(() => router.navigateByUrl('/'));
+    const whenNavigatedToDefaultRoute = rootFixture.ngZone?.run(() =>
+      router.navigateByUrl('/')
+    );
 
-    expect(canNavigate).toBe(true);
+    await expect(whenNavigatedToDefaultRoute).resolves.toBe(true);
   });
 });
