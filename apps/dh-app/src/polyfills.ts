3cd530c6577bef255a2c15e7f8ec987d60a83165
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-29 12:23:40 +0200

    Restore polyfills.ts

--- apps/dh-app/src/polyfills.ts
+++ apps/dh-app/src/polyfills.ts
@@ -1,19 +1,19 @@
 /**
- * @license
- * Copyright 2021 Energinet DataHub A/S
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
  *
- * Licensed under the Apache License, Version 2.0 (the "License2");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This file is divided into 2 sections:
+ *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
+ *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
+ *      file.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
+ * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
+ * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Learn more in https://angular.io/guide/browser-support
  */
+
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
