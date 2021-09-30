816d3007906e19597ef8a171f28fd815a999e475
Dzhavat Ushev <xdzus@energinet.dk>
2021-09-30 10:53:42 +0200

    feat: generate button component

---
+++ libs/ui-watt/src/lib/components/button/button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'watt-button',
+  styleUrls: ['./button.component.scss'],
+  templateUrl: './button.component.html',
+})
+export class WattButtonComponent {}
---
+++ libs/ui-watt/src/lib/components/button/button.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { WattButtonComponent } from './button.component';
+
+@NgModule({
+  declarations: [WattButtonComponent],
+  exports: [WattButtonComponent],
+})
+export class WattButtonModule {}
