a4ab3e99d65850c338378792f565eedd64a66504
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-30 11:19:10 +0200

    refactor: use re-export

--- libs/ett/shared/test-util-angular/src/index.ts
+++ libs/ett/shared/test-util-angular/src/index.ts
@@ -14,4 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { setUpTestbed } from './lib/set-up-testbed';
+export * from './lib/set-up-testbed';
