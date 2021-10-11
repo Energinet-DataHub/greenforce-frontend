b1571562f8d6452fe699a56fa4d77543a18758ec
Dzhavat Ushev <xdzus@energinet.dk>
2021-10-11 10:39:17 +0200

    fix(watt): remove unnecessary directives

--- libs/ui-watt/src/lib/components/form-field/form-field.module.ts
+++ libs/ui-watt/src/lib/components/form-field/form-field.module.ts
@@ -21,24 +21,18 @@ import { MatInputModule } from '@angular/material/input';
 import { WattErrorComponent } from './components/error.component';
 import { WattHintComponent } from './components/hint.component';
 import { WattLabelComponent } from './components/label.component';
-import { WattPrefixDirective } from './directives/prefix.directive';
-import { WattSuffixDirective } from './directives/suffix.directive';
 import { FormFieldComponent } from './form-field.component';
 
 @NgModule({
   imports: [CommonModule, MatFormFieldModule, MatInputModule],
   declarations: [
     FormFieldComponent,
-    WattPrefixDirective,
-    WattSuffixDirective,
     WattErrorComponent,
     WattLabelComponent,
     WattHintComponent,
   ],
   exports: [
     FormFieldComponent,
-    WattPrefixDirective,
-    WattSuffixDirective,
     WattErrorComponent,
     WattLabelComponent,
     WattHintComponent,
