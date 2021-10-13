f40d5e4eb449467d1cbc5e337920ac25920d76a2
Alexander HafstadFrontend Speciliasten <alexander@frontend-specialisten.dk>
2021-10-13 12:36:05 +0200

    refactor(typografy): make mixin snippet funtion

--- libs/ui-watt/src/lib/foundations/typography/+storybook/shared/typography-html-snippets.ts
+++ libs/ui-watt/src/lib/foundations/typography/+storybook/shared/typography-html-snippets.ts
@@ -14,61 +14,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const mixinImport = `@use '@energinet-datahub/watt' as watt
-`;
+function mixinSnippet(name: string) {
+  return `@use '@energinet-datahub/watt' as watt
+@include watt.typography-watt-${name}
+  `;
+}
 
 export const typographyHtmlSnippets = {
   h1: {
     tag: '<h1>Headline 1</h1>',
     class: '<p class="watt-headline-1">Headline 1</p>',
-    mixin: mixinImport + '@include watt.typography-watt-headline-1();',
+    mixin: mixinSnippet('headline-1'),
   },
   h2: {
     tag: '<h2>Headline 2</h2>',
     class: '<p class="watt-headline-2">Headline 2</p>',
-    mixin: mixinImport + '@include watt.typography-watt-headline-2();',
+    mixin: mixinSnippet('headline-2'),
   },
   h3: {
     tag: '<h3>Headline 3</h3>',
     class: '<p class="watt-headline-3">Headline 3</p>',
-    mixin: mixinImport + '@include watt.typography-watt-headline-3();',
+    mixin: mixinSnippet('headline-3'),
   },
   h4: {
     tag: '<h4>Headline 4</h4>',
     class: '<p class="watt-headline-4">Headline 4</p>',
-    mixin: mixinImport + '@include watt.typography-watt-headline-4();',
+    mixin: mixinSnippet('headline-4'),
   },
   h5: {
     tag: '<h5>Headline 5</h5>',
     class: '<p class="watt-headline-5">Headline 5</p>',
-    mixin: mixinImport + '@include watt.typography-watt-headline-5();',
+    mixin: mixinSnippet('headline-5'),
   },
   textL: {
     class: '<p class="watt-text-l">Lead (text-l)</p>',
-    mixin: mixinImport + '@include watt.typography-watt-text-l();',
+    mixin: mixinSnippet('text-l'),
   },
   bodyTextM: {
     tag: '<p>Body (text-m)</p>',
     class: '<div class="watt-text-m">Body (text-m)</div>',
-    mixin: mixinImport + '@include watt.typography-watt-text-m();',
+    mixin: mixinSnippet('text-m'),
   },
   textS: {
     tag: '<p><small>Body (text-s)</small></p>',
-    mixin: mixinImport + '@include watt.typography-watt-text-s();',
+    mixin: mixinSnippet('text-s'),
   },
   textXs: {
     class: '<p class="watt-text-xs">Extra small (text-xs)</p>',
-    mixin: mixinImport + '@include watt.typography-watt-text-xs();',
+    mixin: mixinSnippet('text-xs'),
   },
   button: {
     storybook: '<p class="watt-button">Button</p>',
     tag: '<button type="button">Button</button>',
     class: '<a class="watt-button" href="#">Button</a>',
-    mixin: mixinImport + '@include watt.typography-watt-button();',
+    mixin: mixinSnippet('button'),
   },
   label: {
     tag: '<label>Label</label>',
     class: '<p class="watt-label">Label</p>',
-    mixin: mixinImport + '@include watt.typography-watt-label();',
+    mixin: mixinSnippet('watt-label'),
   },
 };
