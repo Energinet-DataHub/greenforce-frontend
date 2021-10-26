4346a0fe2d4c8e9a08ecc2da3cb67257affc1bf1
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-10-26 11:40:13 +0200

    refactor(ett): move oidc to api folder

--- libs/ett/auth/data-access/src/index.ts
+++ libs/ett/auth/data-access/src/index.ts
@@ -14,5 +14,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// OIDC
-export * from './lib/oidc/auth-oidc-http.service';
+// API
+export * from './lib/api/oidc/auth-oidc-http.service';
