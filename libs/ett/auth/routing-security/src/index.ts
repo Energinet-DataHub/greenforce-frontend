35ad323ffe2d29d7899b110dbd5f6da702020669
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-08 15:26:22 +0100

    refactor(ett): normalize exports

--- libs/ett/auth/routing-security/src/index.ts
+++ libs/ett/auth/routing-security/src/index.ts
@@ -14,5 +14,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { ettAuthRoutePath } from './lib/ett-auth-route-path';
-export { EttAuthenticationGuard } from './lib/ett-authentication.guard';
+export * from './lib/ett-auth-route-path';
+export * from './lib/ett-authentication.guard';
--- libs/ett/core/shell-core/src/index.ts
+++ libs/ett/core/shell-core/src/index.ts
@@ -14,4 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { EttCoreShellModule } from './lib/ett-core-shell.module';
+export * from './lib/ett-core-shell.module';
