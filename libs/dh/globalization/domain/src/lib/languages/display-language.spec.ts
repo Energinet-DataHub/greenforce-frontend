183e4b130d6798940a1f2f2fc191b88b3c0877fe
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-09 11:57:47 +0100

    test: format test descriptions

--- libs/dh/globalization/domain/src/lib/languages/display-language.spec.ts
+++ libs/dh/globalization/domain/src/lib/languages/display-language.spec.ts
@@ -1,16 +1,14 @@
 import { DisplayLanguage, toDisplayLanguage } from './display-language';
 
 describe('Display language', () => {
-  it(`
-    When an unknown language is specified
+  it(`When an unknown language is specified
     Then an error is thrown`, () => {
     expect(() => {
       toDisplayLanguage('test');
     }).toThrowError(/unknown display language/i);
   });
 
-  it(`
-    When a supported language is specified
+  it(`When a supported language is specified
     Then it is accepted`, () => {
     const expectedDisplayLanguage = 'da';
 
