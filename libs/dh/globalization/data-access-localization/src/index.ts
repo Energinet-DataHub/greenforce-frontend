394fd38ede4664e1b1955ec25b921868bc0dff34
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-09 12:07:16 +0100

    refactor(dh): export all symbols

--- libs/dh/globalization/data-access-localization/src/index.ts
+++ libs/dh/globalization/data-access-localization/src/index.ts
@@ -14,4 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { dhTranslocoHttpLoaderProvider } from './lib/dh-transloco-http-loader.service';
+export * from './lib/dh-transloco-http-loader.service';
