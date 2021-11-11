294706fc49fb4460c3cf170c12604eca498c5226
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-11 22:57:44 +0100

    test(dh): cover metering point API

--- libs/dh/shared/data-access-api/src/lib/metering-point-api-v1.spec.ts
+++ libs/dh/shared/data-access-api/src/lib/metering-point-api-v1.spec.ts
@@ -1,14 +1,28 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule, HttpStatusCode } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { lastValueFrom } from 'rxjs';
 
 import { DhApiModule } from './dh-api.module';
-import { MeteringPointHttp } from './generated/v1';
+import { MeteringPointDto, MeteringPointHttp } from './generated/v1';
+
+const nullGsrn = '000000000000000000';
+const testMeteringPointGsrns = [
+  '571313180400014077',
+  '573830327917661875',
+  '572618486737161170',
+  '575990512432508407',
+  '576198124843533632',
+  '572865215148321165',
+  '576874126135722710',
+  '576811034603915520',
+  '571313180400014169',
+  '579548851016417698',
+];
 
 describe('Metering Point API v1', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, DhApiModule.forRoot()],
+      imports: [HttpClientModule, DhApiModule.forRoot()],
     });
 
     http = TestBed.inject(MeteringPointHttp);
@@ -18,8 +32,29 @@ describe('Metering Point API v1', () => {
 
   it(`When an existing metering point is looked up by GSRN
     Then a successful response is received`, async () => {
-    const response = await lastValueFrom(http.v1MeteringPointGetByGsrnGet(''));
+    const [expectedGsrn] = testMeteringPointGsrns;
 
-    expect(response).not.toBeNull();
+    const whenResponse = lastValueFrom(
+      http.v1MeteringPointGetByGsrnGet(expectedGsrn)
+    );
+
+    await expect(whenResponse).resolves.toEqual(
+      expect.objectContaining<MeteringPointDto>({
+        gsrnNumber: expectedGsrn,
+      })
+    );
+  });
+
+  it(`When an unknown metering point is looked up by GSRN
+    Then the response has status code 404`, async () => {
+    const whenResponse = lastValueFrom(
+      http.v1MeteringPointGetByGsrnGet(nullGsrn)
+    );
+
+    await expect(whenResponse).rejects.toEqual(
+      expect.objectContaining({
+        status: HttpStatusCode.NotFound,
+      })
+    );
   });
 });
