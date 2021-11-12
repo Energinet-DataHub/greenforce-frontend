a8cd885ff1222e2765a64b0012f395a29d75884f
Dzhavat Ushev <xdzus@energinet.dk>
2021-11-12 16:47:25 +0100

    feat(dh): add function to validate metering point id

--- libs/dh/metering-point/domain/src/index.ts
+++ libs/dh/metering-point/domain/src/index.ts
@@ -14,3 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+export * from './lib/is-metering-point-id';
---
+++ libs/dh/metering-point/domain/src/lib/metering-point-id-pattern.ts
@@ -0,0 +1,1 @@
+export const meteringPointIdPattern = /^\d{18}$/;
