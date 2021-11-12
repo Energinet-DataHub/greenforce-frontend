30ff277bd3abd0d2a9020bdf982828abba338565
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-12 12:49:51 +0100

    chore: don't add license headers to generated files

--- tools/executors/license/impl.js
+++ tools/executors/license/impl.js
@@ -1,19 +1,3 @@
-/**
- * @license
- * Copyright 2021 Energinet DataHub A/S
- *
- * Licensed under the Apache License, Version 2.0 (the "License2");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 'use strict';
 var __awaiter =
   (this && this.__awaiter) ||
@@ -173,13 +157,15 @@ function addLicenseExecutor(options) {
   return __awaiter(this, void 0, void 0, function () {
     var globs, success, files;
     return __generator(this, function (_a) {
-      globs = Object.keys(config);
+      globs = Object.keys(config).filter(function (glob) {
+        return glob !== 'ignore';
+      });
       success = true;
       console.info('Adding licenses...');
       files = glob.sync(
         '{,!(node_modules|dist)/**/*}*{' + globs.join(',') + '}',
         {
-          ignore: ['**/api-*/**/*'],
+          ignore: config.ignore,
         }
       );
       files.forEach(function (file) {
