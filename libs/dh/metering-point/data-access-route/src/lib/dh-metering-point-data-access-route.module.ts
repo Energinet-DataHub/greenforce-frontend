96277cfba6f9c05ac3b70f55383a8a93c2787dbe
Dzhavat Ushev <xdzus@energinet.dk>
2021-11-16 14:56:05 +0100

    feat(dh): rename data-access route library

--- libs/dh/metering-point/data-access-route/src/lib/dh-metering-point-data-access-route.module.ts
+++ libs/dh/metering-point/data-access-route/src/lib/dh-metering-point-data-access-route.module.ts
@@ -4,4 +4,4 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [CommonModule],
 })
-export class DhSharedDataAccessRouteModule {}
+export class DhMeteringPointDataAccessRouteModule {}
