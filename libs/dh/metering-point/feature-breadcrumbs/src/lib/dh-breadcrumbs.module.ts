f516a207b565ecb0fad5669b7355a5b54047865b
Dzhavat Ushev <xdzus@energinet.dk>
2021-11-17 15:00:46 +0100

    feat(dh): move hard-coded string to translations

--- libs/dh/metering-point/feature-breadcrumbs/src/lib/dh-breadcrumbs.module.ts
+++ libs/dh/metering-point/feature-breadcrumbs/src/lib/dh-breadcrumbs.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
+
 import { WattIconModule } from '@energinet-datahub/watt';
 
 import { DhBreadcrumbsComponent } from './dh-breadcrumbs.component';
@@ -8,6 +10,6 @@ import { DhBreadcrumbsComponent } from './dh-breadcrumbs.component';
 @NgModule({
   declarations: [DhBreadcrumbsComponent],
   exports: [DhBreadcrumbsComponent],
-  imports: [CommonModule, RouterModule, WattIconModule],
+  imports: [CommonModule, RouterModule, TranslocoModule, WattIconModule],
 })
 export class DhBreadcrumbsModule {}
