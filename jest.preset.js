842039f9ef8c0fd5165684902aab8643be7215d6
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-17 13:34:57 +0100

    refactor(gf): move JSDOM environment to scripts folder

--- jest.preset.js
+++ jest.preset.js
@@ -5,7 +5,7 @@ module.exports = {
   ...nxPreset,
   testEnvironment: path.join(
     __dirname,
-    'tools/gf/test-util',
+    'scripts/gf/test-util',
     'jsdom-lax-ssl-environment.ts'
   ),
   testURL: 'https://localhost:5001',
