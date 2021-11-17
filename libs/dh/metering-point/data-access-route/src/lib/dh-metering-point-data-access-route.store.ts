c17f842ca2832411ff9f026bfb53f1a660b5087e
Dzhavat Ushev <xdzus@energinet.dk>
2021-11-17 10:03:15 +0100

    refactor(dh): remove component store setup from service

--- libs/dh/metering-point/data-access-route/src/lib/dh-metering-point-data-access-route.store.ts
+++ libs/dh/metering-point/data-access-route/src/lib/dh-metering-point-data-access-route.store.ts
@@ -1,24 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ComponentStore } from '@ngrx/component-store';
 import { map, Observable } from 'rxjs';
 import { dhMeteringPointIdParam } from '@energinet-datahub/dh/metering-point/routing';
 
-interface RouterState {
-  readonly meteringPointId: string;
-}
-
-const initialState: RouterState = {
-  meteringPointId: '',
-};
-
 @Injectable()
-export class DhMeteringPointDataAccessRouteStore extends ComponentStore<RouterState> {
+export class DhMeteringPointDataAccessRouteStore {
   meteringPointId$: Observable<string> = this.route.params.pipe(
     map((params) => params[dhMeteringPointIdParam])
   );
 
-  constructor(private route: ActivatedRoute) {
-    super(initialState);
-  }
+  constructor(private route: ActivatedRoute) {}
 }
