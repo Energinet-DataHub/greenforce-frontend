3ae111b27ce1000c5458caa84f68a9727dcc36c1
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-17 13:25:05 +0100

    refactor: move JSDOM environment

--- jest.preset.js
+++ jest.preset.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 module.exports = {
   ...nxPreset,
-  testEnvironment: path.join(__dirname, 'jsdom-lax-ssl-environment.ts'),
+  testEnvironment: path.join(
+    __dirname,
+    'tools/gf/test-util',
+    'jsdom-lax-ssl-environment.ts'
+  ),
   testURL: 'https://localhost:5001',
 };
