d0df10d3214149e6890bc1d5d8798bf2a3efe2ac
Jakob Kristensen <jakob@jakobk.dk>
2021-11-29 12:42:15 +0100

    Fixed broken testcase

--- libs/ett/core/shell/src/lib/ett-shell.component.spec.ts
+++ libs/ett/core/shell/src/lib/ett-shell.component.spec.ts
@@ -17,19 +17,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { WattShellComponent } from '@energinet-datahub/watt';
+import { AuthOidcHttp, GetProfileResponse, UserProfile } from '@energinet-datahub/ett/auth/data-access-api';
+import { of } from 'rxjs';
+import { MockService } from 'ng-mocks';
 
 import { EttShellComponent, EttShellScam } from './ett-shell.component';
 
+
 describe(EttShellComponent.name, () => {
   beforeEach(() => {
+    const profile = {
+      id: '123',
+      name: 'Mock',
+      company: 'Energinet'
+    } as UserProfile;
+
+    const response = {
+      success: true,
+      profile: profile,
+    } as GetProfileResponse;
+
+    mockAuthOidcHttp = MockService(AuthOidcHttp);
+
+    (mockAuthOidcHttp.getProfile as jest.Mock).mockReturnValue(of(response));
+
     TestBed.configureTestingModule({
       imports: [EttShellScam],
+      providers: [
+        {
+          provide: AuthOidcHttp,
+          useValue: mockAuthOidcHttp,
+        }
+      ]
     });
 
     fixture = TestBed.createComponent(EttShellComponent);
   });
 
   let fixture: ComponentFixture<EttShellComponent>;
+  let mockAuthOidcHttp: AuthOidcHttp;
 
   it('displays the Watt shell', () => {
     const wattShell = fixture.debugElement.query(
--- libs/ett/core/shell/src/test-setup.ts
+++ libs/ett/core/shell/src/test-setup.ts
@@ -17,7 +17,8 @@
 import 'jest-preset-angular/setup-jest';
 
 import { addNgModuleMatchers } from '@energinet-datahub/gf/test-util-matchers';
-import { setUpTestbed } from '@energinet-datahub/gf/test-util-staging';
+import { setUpTestbed, setUpNgMocks } from '@energinet-datahub/gf/test-util-staging';
 
 setUpTestbed();
+setUpNgMocks();
 addNgModuleMatchers();
