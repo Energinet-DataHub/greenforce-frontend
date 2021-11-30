92be0ce9e010a9fdb82f7f4ba65c1b64ff4ba867
Dzhavat Ushev <xdzus@energinet.dk>
2021-11-30 11:40:09 +0100

    fix(dh): remove usage of push pipe

--- libs/dh/metering-point/feature-overview/src/lib/dh-metering-point-overview.component.ts
+++ libs/dh/metering-point/feature-overview/src/lib/dh-metering-point-overview.component.ts
@@ -89,7 +89,6 @@ export class DhMeteringPointOverviewComponent implements OnDestroy {
   imports: [
     CommonModule,
     LetModule,
-    PushModule,
     TranslocoModule,
     DhBreadcrumbScam,
     DhMeteringPointNotFoundScam,
