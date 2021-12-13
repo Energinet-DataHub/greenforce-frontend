3651fcbd490fc2abec6e59a60588a71d6ee48b5b
Peter Tandrup <PTA@energinet.dk>
2021-12-13 08:25:19 +0100

    Remove usings

--- apps/dh/api-dh/source/DataHub.WebApi.Tests/Integration/Controllers/ChargeLinksControllerTests.cs
+++ apps/dh/api-dh/source/DataHub.WebApi.Tests/Integration/Controllers/ChargeLinksControllerTests.cs
@@ -12,12 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using AutoFixture;
 using Energinet.Charges.Contracts;
