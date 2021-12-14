88639890cfddc95e517666c49ea0e8b82f004a08
Jonas Michelsen <jvm@energinet.dk>
2021-12-14 14:47:34 +0100

    Use MeteringPointCimDto istead of custom interface

--- libs/dh/metering-point/feature-overview/src/lib/ui-secondary-master-data/ui-secondary-master-data.component.ts
+++ libs/dh/metering-point/feature-overview/src/lib/ui-secondary-master-data/ui-secondary-master-data.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, NgModule } from '@angular/core';
+import { MeteringPointCimDto } from '@energinet-datahub/dh/shared/data-access-api';
 import { WattExpansionModule, WattIconModule } from '@energinet-datahub/watt';
 import { TranslocoModule } from '@ngneat/transloco';
-import { SecondaryMasterData } from './secondary-master-data';
 
 @Component({
   selector: 'dh-ui-secondary-master-data',
@@ -10,22 +10,7 @@ import { SecondaryMasterData } from './secondary-master-data';
   styleUrls: ['./ui-secondary-master-data.component.scss']
 })
 export class UiSecondaryMasterDataComponent{
-  @Input() secondaryMasterData: SecondaryMasterData = {
-    netSettlementGroup: undefined,
-    disconnectionType: undefined,
-    connectionType: undefined,
-    gridAreaName: undefined,
-    gridAreaCode: undefined,
-    capacity: undefined,
-    assetType: undefined,
-    ratedCapacity: undefined,
-    ratedCurrent: undefined,
-    gsrnNumber: undefined,
-    productId: undefined,
-    streetCode: undefined,
-    municipalityCode: undefined,
-    countryCode: undefined
-  };
+  @Input() secondaryMasterData: MeteringPointCimDto = {};
 
 }
 
