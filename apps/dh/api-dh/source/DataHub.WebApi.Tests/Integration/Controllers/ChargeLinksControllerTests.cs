b06273227c0310e0ddf62cf5e4b7fc0c0625d2a3
Peter Tandrup <PTA@energinet.dk>
2021-12-15 09:39:36 +0100

    Update packages Clients and Registration

--- apps/dh/api-dh/source/DataHub.WebApi.Tests/Integration/Controllers/ChargeLinksControllerTests.cs
+++ apps/dh/api-dh/source/DataHub.WebApi.Tests/Integration/Controllers/ChargeLinksControllerTests.cs
@@ -18,7 +18,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using AutoFixture;
 using Energinet.Charges.Contracts;
-using Energinet.DataHub.Charges.Clients;
+using Energinet.DataHub.Charges.Clients.ChargeLinks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
