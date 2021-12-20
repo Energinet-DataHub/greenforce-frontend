1a67fb0993b36fd4e46fb5ab056f91ed6d871906
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-12-20 12:30:59 +0100

    chore: add production API configuration

--- apps/ett/app-ett/src/configuration/load-eo-api-environment.ts
+++ apps/ett/app-ett/src/configuration/load-eo-api-environment.ts
@@ -15,12 +15,15 @@
  * limitations under the License.
  */
 import {
+  environment,
   EoApiEnvironment,
   loadEoApiEnvironment as _loadEoApiEnvironment,
 } from '@energinet-datahub/eo/shared/environments';
 
 export function loadEoApiEnvironment(): Promise<EoApiEnvironment> {
-  return _loadEoApiEnvironment('eo-api-environment.json').catch(() =>
-    _loadEoApiEnvironment('eo-api-environment.local.json')
-  );
+  const configurationFilename = environment.production
+    ? 'eo-api-environment.json'
+    : 'eo-api-environment.local.json';
+
+  return _loadEoApiEnvironment(configurationFilename);
 }
