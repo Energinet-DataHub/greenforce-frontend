a8b48463919a38dd2a9f2e82f0c7063d036eaf7d
Jonas Michelsen <jvm@energinet.dk>
2021-12-22 11:52:52 +0100

    Add Sorting

--- libs/dh/metering-point/feature-overview/src/lib/child-metering-point/dh-child-metering-point.component.ts
+++ libs/dh/metering-point/feature-overview/src/lib/child-metering-point/dh-child-metering-point.component.ts
@@ -1,55 +1,63 @@
-import {LiveAnnouncer} from '@angular/cdk/a11y';
-import { AfterViewInit, Component, Input, NgModule, ViewChild } from '@angular/core';
-import { MatSort, Sort } from '@angular/material/sort';
+import { Component, Input, NgModule, ViewChild, AfterViewInit } from '@angular/core';
+import { MatSort, MatSortable, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { WattIconModule, WattIconSize } from '@energinet-datahub/watt';
 import { ConnectionState, MeteringPointSimpleCimDto, MeteringPointType } from '@energinet-datahub/dh/shared/data-access-api';
 import { TranslocoModule } from '@ngneat/transloco';
 import { DhMeteringPointStatusBadgeScam } from '../status-badge/dh-metering-point-status-badge.component';
 
+const TestData: MeteringPointSimpleCimDto[] = [
+  {gsrnNumber: '123', effectiveDate: '2020-01-02T00:00:00Z', connectionState: ConnectionState.D03, meteringPointId: '5678', meteringPointType: MeteringPointType.D01},
+  {gsrnNumber: '910', effectiveDate: '2020-04-01T00:00:00Z', connectionState: ConnectionState.D02, meteringPointId: '546', meteringPointType: MeteringPointType.D02},
+  {gsrnNumber: '678', effectiveDate: '2020-01-03T00:00:00Z', connectionState: ConnectionState.E22, meteringPointId: '125', meteringPointType: MeteringPointType.D09},
+  {gsrnNumber: '345', effectiveDate: '2020-02-02T00:00:00Z', connectionState: ConnectionState.E23, meteringPointId: '558', meteringPointType: MeteringPointType.D13}
+]
+
 @Component({
   selector: 'dh-child-metering-point',
   templateUrl: './dh-child-metering-point.component.html',
   styleUrls: ['./dh-child-metering-point.component.scss']
 })
-export class DhChildMeteringPointComponent{
+export class DhChildMeteringPointComponent implements AfterViewInit{
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   iconSize = WattIconSize
+  sortedData: Array<MeteringPointSimpleCimDto> = []
   @Input()
-  childMeteringPoints: Array<MeteringPointSimpleCimDto> = this.testData()
-  // constructor(private _liveAnnouncer: LiveAnnouncer) {}
-  
-  // @ViewChild(MatSort) sort: MatSort;
-  
-  // ngAfterViewInit() {
-  //   this.childMeteringPoints.sort = this.sort;
-  // }
+  childMeteringPoints: Array<MeteringPointSimpleCimDto> = TestData
 
-  // announceSortChange(sortState: Sort) {
-  //   // This example uses English messages. If your application supports
-  //   // multiple language, you would internationalize these strings.
-  //   // Furthermore, you can customize the message to add additional
-  //   // details about the values being sorted.
-  //   if (sortState.direction) {
-  //     this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-  //   } else {
-  //     this._liveAnnouncer.announce('Sorting cleared');
-  //   }
-  // }
+  @ViewChild(MatSort) matSort!: MatSort;
 
-  private testData(){
-    const td: Array<MeteringPointSimpleCimDto> = []
-    const mpscd: MeteringPointSimpleCimDto = {
-      gsrnNumber: '1234',
-      effectiveDate: '2020-01-01T00:00:00Z',
-      connectionState: ConnectionState.D03,
-      meteringPointId: '5678',
-      meteringPointType: MeteringPointType.D01
+  ngAfterViewInit(): void {
+    this.setDefaultSorting();
+  }
+
+  sortData(sort: Sort) {
+    const data = this.childMeteringPoints.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      this.setDefaultSorting();
+      return;
     }
-    td.push(mpscd)
-    td.push(mpscd)
-    td.push(mpscd)
-    return td
+    
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'childMeteringPoint':
+          return this.compare(a.gsrnNumber, b.gsrnNumber, isAsc);
+        case 'effectivePeriod':
+          return this.compare(a.effectiveDate, b.effectiveDate, isAsc);
+        case 'status':
+          return this.compare(a.connectionState, b.connectionState, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+  setDefaultSorting(){
+    this.matSort.sort(this.matSort.sortables.get('childMeteringPoint') as MatSortable);
+  }
+  compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 }
 
@@ -59,7 +67,8 @@ export class DhChildMeteringPointComponent{
     MatTableModule,
     TranslocoModule,
     DhMeteringPointStatusBadgeScam,
-    WattIconModule
+    WattIconModule,
+    MatSortModule
   ],
   exports: [DhChildMeteringPointComponent],
 })
