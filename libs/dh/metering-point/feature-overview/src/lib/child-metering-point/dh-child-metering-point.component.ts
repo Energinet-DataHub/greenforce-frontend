984defa7a9ed5cc3d36556a6d4c7f2256d94b6c7
Jonas Michelsen <jvm@energinet.dk>
2021-12-22 09:49:07 +0100

    styling

--- libs/dh/metering-point/feature-overview/src/lib/child-metering-point/dh-child-metering-point.component.ts
+++ libs/dh/metering-point/feature-overview/src/lib/child-metering-point/dh-child-metering-point.component.ts
@@ -1,6 +1,11 @@
-import { Component, Input, NgModule } from '@angular/core';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import { AfterViewInit, Component, Input, NgModule, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
-import { MeteringPointSimpleCimDto } from '@energinet-datahub/dh/shared/data-access-api';
+import { WattIconModule, WattIconSize } from '@energinet-datahub/watt';
+import { ConnectionState, MeteringPointSimpleCimDto, MeteringPointType } from '@energinet-datahub/dh/shared/data-access-api';
+import { TranslocoModule } from '@ngneat/transloco';
+import { DhMeteringPointStatusBadgeScam } from '../status-badge/dh-metering-point-status-badge.component';
 
 @Component({
   selector: 'dh-child-metering-point',
@@ -8,14 +13,53 @@ import { MeteringPointSimpleCimDto } from '@energinet-datahub/dh/shared/data-acc
   styleUrls: ['./dh-child-metering-point.component.scss']
 })
 export class DhChildMeteringPointComponent{
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  iconSize = WattIconSize
   @Input()
-  childMeteringPoints: Array<MeteringPointSimpleCimDto> = []
+  childMeteringPoints: Array<MeteringPointSimpleCimDto> = this.testData()
+  // constructor(private _liveAnnouncer: LiveAnnouncer) {}
+  
+  // @ViewChild(MatSort) sort: MatSort;
+  
+  // ngAfterViewInit() {
+  //   this.childMeteringPoints.sort = this.sort;
+  // }
+
+  // announceSortChange(sortState: Sort) {
+  //   // This example uses English messages. If your application supports
+  //   // multiple language, you would internationalize these strings.
+  //   // Furthermore, you can customize the message to add additional
+  //   // details about the values being sorted.
+  //   if (sortState.direction) {
+  //     this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+  //   } else {
+  //     this._liveAnnouncer.announce('Sorting cleared');
+  //   }
+  // }
+
+  private testData(){
+    const td: Array<MeteringPointSimpleCimDto> = []
+    const mpscd: MeteringPointSimpleCimDto = {
+      gsrnNumber: '1234',
+      effectiveDate: '2020-01-01T00:00:00Z',
+      connectionState: ConnectionState.D03,
+      meteringPointId: '5678',
+      meteringPointType: MeteringPointType.D01
+    }
+    td.push(mpscd)
+    td.push(mpscd)
+    td.push(mpscd)
+    return td
+  }
 }
 
 @NgModule({
   declarations: [DhChildMeteringPointComponent],
   imports: [
-    MatTableModule
+    MatTableModule,
+    TranslocoModule,
+    DhMeteringPointStatusBadgeScam,
+    WattIconModule
   ],
   exports: [DhChildMeteringPointComponent],
 })
