bfa4c8ed8d4470bcf2034754585408fcedd319f4
Lasse Klitgaard <lki@energinet.dk>
2022-01-04 15:14:25 +0100

    lint

--- libs/dh/metering-point/feature-overview/src/lib/charges/dh-charges.component.spec.ts
+++ libs/dh/metering-point/feature-overview/src/lib/charges/dh-charges.component.spec.ts
@@ -16,7 +16,7 @@
  */
 // import { render, screen } from '@testing-library/angular';
 
-import { DhChargesComponent, DhChargesScam } from './dh-charges.component';
+import { DhChargesComponent } from './dh-charges.component';
 // import { getTranslocoTestingModule } from '@energinet-datahub/dh/shared/test-util-i18n';
 // import { runOnPushChangeDetection } from '@energinet-datahub/dh/shared/test-util-metering-point';
 
