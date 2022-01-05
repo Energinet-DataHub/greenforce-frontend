fbbd4c73c6a50be9621468cc95157c86cf1d8f52
Alexander HafstadFrontend Speciliasten <alexander@frontend-specialisten.dk>
2022-01-05 09:42:06 +0100

    debug: failing e2e test in CI

--- apps/dh/e2e-dh/src/integrations/language-selection.spec.ts
+++ apps/dh/e2e-dh/src/integrations/language-selection.spec.ts
@@ -32,6 +32,12 @@ test.describe('Language selection', () => {
 
   test(`Given no language is selected
         Then Danish translations are displayed`, async ({ page }) => {
+    test.slow();
+    await page.waitForTimeout(5000);
+    console.log('-------------------------------------');
+    console.log('the application title is displayed')
+    console.log(await page.innerHTML('dh-metering-point-search'));
+    console.log('-------------------------------------');
     await expect(appShell.getTitle(page)).toHaveText('Målepunkter');
   });
 
