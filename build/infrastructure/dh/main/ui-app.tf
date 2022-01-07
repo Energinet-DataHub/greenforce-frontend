2c0c0b1b785cd6a58486786bb587e885c690d320
René Nielsen <renetdev@gmail.com>
2022-01-07 13:33:21 +0100

    changed name from bff to webapi

--- build/infrastructure/dh/main/ui-app.tf
+++ build/infrastructure/dh/main/ui-app.tf
@@ -26,3 +26,13 @@ resource "azurerm_static_site" "ui" {
     ]
   }
 }
+
+module "kvs_stapp_ui_web_app_api_key" {
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
+
+  name          = "stapp-ui-web-app-api-key"
+  value         = azurerm_static_site.ui.api_key
+  key_vault_id  = data.azurerm_key_vault.kv_shared_resources.id
+
+  tags          = azurerm_resource_group.this.tags
+}
