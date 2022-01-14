5099ae95e1f62786ff1e01174e9417072d68e6a7
Dzhavat Ushev <xdzus@energinet.dk>
2022-01-14 09:20:37 +0100

    refactor(dh): move identity component to another library

--- libs/dh/metering-point/feature-identity-and-master-data/src/lib/dh-metering-point-feature-identity-and-master-data.module.ts
+++ libs/dh/metering-point/feature-identity-and-master-data/src/lib/dh-metering-point-feature-identity-and-master-data.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DhMeteringPointIdentityScam } from './identity/dh-metering-point-identity.component';
+
 @NgModule({
+  exports: [DhMeteringPointIdentityScam],
   imports: [CommonModule],
 })
 export class DhMeteringPointFeatureIdentityAndMasterDataModule {}
