419da6cd1e6497eff6f54926edfcf4f819eebcc2
Jan Klemmensen <jank50@hotmail.com>
2022-01-21 15:38:24 +0100

    Removed duplicate headers | Moved the header component to the same level as the landing page shell

--- apps/ett/e2e-ett/src/support/landing-page.po.ts
+++ apps/ett/e2e-ett/src/support/landing-page.po.ts
@@ -14,24 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * @license
- * Copyright 2021 Energinet DataHub A/S
- *
- * Licensed under the Apache License, Version 2.0 (the "License2");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// export const getLogoInHeader = () => cy.get('img').should('have.attr', 'class', 'eo-landingpage-shell-header__toolbar');
 export const getLogoInHeader = () =>
-  cy.get('eo-landingpage-shell-header').find('img');
+  cy.get('eo-landing-page-header').find('img');
 export const getLogInLinkInHeader = () =>
-  cy.get('eo-landingpage-shell-header').find('a');
+  cy.get('eo-landing-page-header').find('a');
--- libs/eo/landing-page/shell/README.md
+++ libs/eo/landing-page/shell/README.md
@@ -1,7 +1,1 @@
 # EnergyOrigin Landing page shell
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test eo-landing-page-shell` to execute the unit tests.
