e4557b4042ec49513922a40b3ab0acb5c2e797f4
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-01-27 12:54:12 +0100

    chore: correct transform patterns for Jest Angular preset (#327)
    Correct the transform patterns used with `jest-preset-angular` so that the `.` character is only used to match the dot preceeding a file extension rather than *any* character.

--- libs/dh/metering-point/data-access-api/jest.config.js
+++ libs/dh/metering-point/data-access-api/jest.config.js
@@ -27,9 +27,9 @@ module.exports = {
   coverageDirectory:
     '../../../../coverage/libs/dh/metering-point/data-access-api',
   transform: {
-    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
-  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
