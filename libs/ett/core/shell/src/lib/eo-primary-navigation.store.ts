a57fbcb3db1dadde6bf0842e280ce729d68cc34a
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-01-28 12:10:37 +0100

    feat(eo): navigate to landing page after successful log out

--- libs/ett/core/shell/src/lib/eo-primary-navigation.store.ts
+++ libs/ett/core/shell/src/lib/eo-primary-navigation.store.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { eoLandingPageRelativeUrl } from '@energinet-datahub/eo/landing-page/routing';
 import { AuthHttp } from '@energinet-datahub/ett/auth/data-access-api';
-import { ComponentStore } from '@ngrx/component-store';
-import { exhaustMap, map } from 'rxjs';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { exhaustMap, mergeMap, of, throwError } from 'rxjs';
 
 // No internal state needed
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -9,14 +11,28 @@ interface EoPrimaryNavigationState {}
 
 @Injectable()
 export class EoPrimaryNavigationStore extends ComponentStore<EoPrimaryNavigationState> {
-  constructor(private authHttp: AuthHttp) {
+  constructor(private authHttp: AuthHttp, private router: Router) {
     super({});
   }
 
   onLogOut = this.effect<void>((origin$) =>
     origin$.pipe(
-      exhaustMap(() => this.authHttp.getLogout()),
-      map((response) => response.success)
+      exhaustMap(() =>
+        this.authHttp.getLogout().pipe(
+          mergeMap((response) =>
+            response.success === true
+              ? of(undefined)
+              : throwError(() => new Error('Log out failed'))
+          ),
+          tapResponse(
+            () => this.router.navigateByUrl(eoLandingPageRelativeUrl),
+            (error) => {
+              // We only support the happy path for now
+              throw error;
+            }
+          )
+        )
+      )
     )
   );
 }
