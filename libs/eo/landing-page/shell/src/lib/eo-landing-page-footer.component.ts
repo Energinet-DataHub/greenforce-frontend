0f29fc91f46c0851efc469f12bbbb64201c4a821
Jan Klemmensen <jank50@hotmail.com>
2022-01-31 11:52:42 +0100

    Added BEM styles to elements in the footer

--- libs/eo/landing-page/shell/src/lib/eo-landing-page-footer.component.ts
+++ libs/eo/landing-page/shell/src/lib/eo-landing-page-footer.component.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2021 Energinet DataHub A/S
+ * Copyright 2020 Energinet DataHub A/S
  *
  * Licensed under the Apache License, Version 2.0 (the "License2");
  * you may not use this file except in compliance with the License.
@@ -30,93 +30,58 @@ const selector = 'eo-landing-page-footer';
   styles: [
     `
     @use '@energinet-datahub/watt/utils' as watt;
-    // @todo: Get the line-height we should use - Overrides are used multiple places below in order to match with what is in Figma
     ${selector} {
-
-      // General styles for the 'eo-landing-page-footer' tag
       display: grid;
-      // @todo: Get the column sizes from Kenneth
       grid-template-columns: repeat(3, 1fr);
       width: 100%;
-      position: absolute;
-      bottom: 0;
-      // filter: drop-shadow(0px 0px 15px rgba(0, 0, 0, 0.1)); // -> Figma style - Which one to use(?)
-      box-shadow: 0px 2px 16px rgba(0, 0, 0, 0.3); // Equivalent to Watt "shadow-s" style - Which one to use(?)
+      box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
       background: var(--watt-color-neutral-white);
-      padding: var(--watt-space-l) var(--watt-space-m); // No mixing exists for "inset-stretch-l", see: C:lib/foundations/spacing/_spacing.import.scss
+      padding: var(--watt-space-l) var(--watt-space-m); // No mixing exists for "space-inset-stretch-l", see: C:lib/foundations/spacing/_spacing.import.scss
 
-      // Column 1
-      section:first-of-type {
-        p {
-          @include watt.space-stack-xs;
-          @include watt.typography-watt-text-s; // Adds line-height: 1.5;
-          // line-height: 24px; // Override: This line-height corresponds to what is added in Figma(?)
-        }
-        img {
-          // @todo: Get the image dimensions from Kenneth
-          display: block;
-          width: 360px;
-          height: 48px;
-          @include watt.space-stack-l;
-        }
-        a {
-          @include watt.typography-watt-text-s; // Adds line-height: 1.5;
-          // line-height: 24px; // Override: This line-height corresponds to what is added in Figma(?)
-          color: var(--watt-color-primary);
-        }
+      .${selector}__a {
+        text-decoration: underline; // This overrides WATT styles (which removes the underline on a tags)
+        color: var(--watt-color-primary); // This overrides the '--watt-color-primary-dark' color which is added by a class on the element
       }
 
-      // Column 2
-      section:nth-child(2) {
-        p {
-          @include watt.typography-watt-text-s; // Adds line-height: 1.5;
-          // line-height: 24px; // Override: This line-height corresponds to what is added in Figma(?)
-          color: var(--watt-color-neutral-black);
-          margin: 0; // This resets a margin-bottom which is applied from Angular Material on the P tag
-        }
+      .${selector}__p--black {
+        color: var(--watt-color-neutral-black); // This overrides the default '--watt-color-primary-dark' color
       }
 
-      // Column 3
-      section:last-of-type {
-        a {
-          @include watt.typography-watt-text-s; // Adds line-height: 1.5;
-          // line-height: 24px; // Override: This line-height corresponds to what is added in Figma(?)
-          color: var(--watt-color-primary);
-          display: block;
-        }
+      .${selector}__p--stack-xs {
+        @include watt.space-stack-xs; // This adds the spacing between the text and the Energinet logo
       }
 
-      // Column 2 & Column 3 shared
-      section:nth-child(2), section:last-of-type {
-        h5 {
-          @include watt.typography-watt-text-m;
-          font-weight: 600; // Overrides the font-weight from 'watt.typography-watt-text-m', in order to match the Figma styles
+      .${selector}__h5 {
+        @include watt.typography-watt-headline-5; // This overrides the styles applied from Angular Material on h5 tags
+      }
 
-          // This is the mixin available in watt, for level 5 headings -> Does not match with the styles in Figma((font-size & line-height))...
-          // ... But should be the one we can use for level 5 headings(?)
-          // @include watt.typography-watt-headline-5;
-        }
+      .${selector}__img {
+        display: block;
+        width: calc(90 * var(--watt-space-xs));
+        height: calc(12 * var(--watt-space-xs));
       }
     }
     `,
   ],
   template: `
       <section>
-        <p>Powered by</p>
-        <img src="assets/energinet-logo.svg" alt="EnergyOrigin" />
-        <a href="#">Privacy Policy</a>
+        <p class="${selector}__p--stack-xs watt-text-s">Powered by</p>
+        <img src="assets/energinet-logo.svg" alt="EnergiNet" class="${selector}__img watt-space-stack-l" />
+        <a href="#" class="${selector}__a watt-text-s">Privacy Policy</a>
       </section>
       <section>
-        <h5>Address</h5>
-        <p>Tonne Kjærsvej 65</p>
-        <p>7000 Fredericia</p>
-        <p>Danmark</p>
-        <p>CVR: 28980671</p>
+        <h5 class="${selector}__h5">Address</h5>
+        <p class="${selector}__p--black watt-text-s">
+          Tonne Kjærsvej 65<br>
+          7000 Fredericia<br>
+          Danmark<br>
+          CVR: 28980671
+        </p>
       </section>
       <section>
-        <h5>Contact</h5>
-        <a href="tel:+4588446633">+45 88 44  66 33</a>
-        <a href="mailto:datahub@energinet.dk">datahub@energinet.dk</a>
+        <h5 class="${selector}__h5">Contact</h5>
+        <a href="tel:+4588446633" class="${selector}__a watt-text-s">+45 88 44  66 33</a><br>
+        <a href="mailto:datahub@energinet.dk" class="${selector}__a watt-text-s">datahub@energinet.dk</a>
       </section>
   `,
 })
