b95dd67bda9a0b53e7fc7bd5d16340a9143bd4bc
Jan Klemmensen <jank50@hotmail.com>
2022-02-01 15:25:49 +0100

    Added integration.spec.ts for the privacy policy page

---
+++ libs/eo/privacy-page/shell/src/lib/eo-privacy-page.integration.spec.ts
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ import {
+  SpectacularAppComponent,
+  SpectacularFeatureTestingModule,
+} from '@ngworker/spectacular';
+import { render, screen } from '@testing-library/angular';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { EoPrivacyPageShellModule } from './eo-privacy-page-shell.module';
+
+describe('EnergyOrigin privacy page', () => {
+
+  it('*** Enter a good description of the first test here ***', async () => {
+    const { navigate } = await render(SpectacularAppComponent, {
+      imports: [
+        SpectacularFeatureTestingModule.withFeature({
+          featureModule: EoPrivacyPageShellModule,
+          featurePath: '', // @todo: Should this also be the "/privacy-page" url
+        }),
+        HttpClientTestingModule,
+      ],
+    });
+    await navigate('/privacy-policy');
+  });
+});
