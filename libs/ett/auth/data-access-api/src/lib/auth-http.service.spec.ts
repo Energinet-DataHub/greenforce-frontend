d57d9f71cc5bbab499eb114180ca9d8c7425e33e
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-02-02 11:34:42 +0100

    test(eo): remove duplicate license header

--- libs/ett/auth/data-access-api/src/lib/auth-http.service.spec.ts
+++ libs/ett/auth/data-access-api/src/lib/auth-http.service.spec.ts
@@ -31,22 +31,6 @@ import {
 } from './auth-http.service';
 import { AuthOidcQueryParameterName } from './auth-oidc-query-parameter-name';
 
-/**
- * @license
- * Copyright 2020 Energinet DataHub A/S
- *
- * Licensed under the Apache License, Version 2.0 (the "License2");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 function setup() {
   TestBed.configureTestingModule({
     imports: [HttpClientTestingModule],
