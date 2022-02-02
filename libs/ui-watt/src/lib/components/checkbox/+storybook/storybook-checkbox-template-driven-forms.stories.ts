a81b9c26e322a7340b3dcc0bf7696c12885d2127
Dzhavat Ushev <xdzus@energinet.dk>
2022-02-02 08:45:34 +0100

    feat(watt): add checkbox component (#333)
    * feat(watt): create checkbox component
    
    * feat(watt): implement ControlValueAccessor interface in checkbox component
    
    * test(watt): add tests to checkbox component
    
    * feat(watt): style checkbox
    
    * feat(watt): set color to checkbox component
    
    * feat(watt): set touched on checkbox
    
    * feat(watt): showcase checkbox component in Storybook
    
    * chore: add license
    
    * feat(watt): add ignore tag to component properties
    
    * feat(watt): add more stories to checkbox component
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ libs/ui-watt/src/lib/components/checkbox/+storybook/storybook-checkbox-template-driven-forms.stories.ts
@@ -0,0 +1,94 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { FormsModule } from '@angular/forms';
+
+import { WattCheckboxModule } from '../watt-checkbox.module';
+import { WattCheckboxComponent } from '../watt-checkbox.component';
+
+export default {
+  title: 'Components/Checkbox/Template-Driven Forms',
+  component: WattCheckboxComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [FormsModule, WattCheckboxModule],
+    }),
+  ],
+} as Meta<WattCheckboxComponent>;
+
+const howToUseGuideBasic = `
+How to use
+
+1. Import ${WattCheckboxModule.name} in a module
+
+import { ${WattCheckboxModule.name} } from '@energinet-datahub/watt';
+
+2. Create model in a component
+
+exampleModel = true;
+
+3. Assign the model to the checkbox component
+
+<watt-checkbox [(ngModel)]="exampleModel">Keep me signed in</watt-checkbox>`;
+
+export const withModel: Story<WattCheckboxComponent> = () => ({
+  props: {
+    exampleModel: true,
+  },
+  template: `<watt-checkbox [(ngModel)]="exampleModel">Keep me signed in</watt-checkbox>`,
+});
+withModel.parameters = {
+  docs: {
+    source: {
+      code: howToUseGuideBasic,
+    },
+  },
+};
+
+const howToUseGuideDisabled = `
+How to use
+
+1. Import ${WattCheckboxModule.name} in a module
+
+import { ${WattCheckboxModule.name} } from '@energinet-datahub/watt';
+
+2. Create model in a component
+
+exampleModel = true;
+
+3. Create property to track the disabled state in a component
+
+isDisabled = true;
+
+4. Assign the model to the checkbox component
+
+<watt-checkbox [(ngModel)]="exampleModel" [disabled]="isDisabled">Keep me signed in</watt-checkbox>`;
+
+export const Disabled: Story<WattCheckboxComponent> = () => ({
+  props: {
+    exampleModel: true,
+    isDisabled: true,
+  },
+  template: `<watt-checkbox [(ngModel)]="exampleModel" [disabled]="isDisabled">Keep me signed in</watt-checkbox>`,
+});
+Disabled.parameters = {
+  docs: {
+    source: {
+      code: howToUseGuideDisabled,
+    },
+  },
+};
---
+++ libs/ui-watt/src/lib/components/checkbox/watt-checkbox.module.spec.ts
@@ -0,0 +1,110 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { render, screen } from '@testing-library/angular';
+import user from '@testing-library/user-event';
+
+import { WattCheckboxModule } from './watt-checkbox.module';
+
+describe(WattCheckboxModule.name, () => {
+  it('exports shared Watt Design System checkbox', async () => {
+    const labelText = 'Text';
+
+    const view = await render(`<watt-checkbox>${labelText}</watt-checkbox>`, {
+      imports: [WattCheckboxModule],
+    });
+
+    expect(view.queryByLabelText(labelText)).not.toBeNull();
+  });
+
+  // eslint-disable-next-line sonarjs/cognitive-complexity
+  describe('Reactive forms', () => {
+    async function setup(initialState: { value: boolean; disabled?: boolean }) {
+      const labelText = 'Are you awesome?';
+
+      @Component({
+        template: `<watt-checkbox [formControl]="checkboxControl"
+          >${labelText}</watt-checkbox
+        >`,
+      })
+      class TestComponent {
+        checkboxControl = new FormControl(initialState);
+      }
+
+      const { fixture } = await render(TestComponent, {
+        imports: [WattCheckboxModule, ReactiveFormsModule],
+      });
+      const checkboxLabel = screen.queryByLabelText(labelText);
+
+      return {
+        fixture,
+        checkboxLabel,
+      };
+    }
+
+    it('toggles checkbox when clicked', async () => {
+      const initialState = { value: true };
+      const { fixture, checkboxLabel } = await setup(initialState);
+
+      if (checkboxLabel) {
+        user.click(checkboxLabel);
+      }
+
+      expect(fixture.componentInstance.checkboxControl.value).toBeFalsy();
+
+      if (checkboxLabel) {
+        user.click(checkboxLabel);
+      }
+
+      expect(fixture.componentInstance.checkboxControl.value).toBeTruthy();
+    });
+
+    it('prevents clicking on disabled checkbox', async () => {
+      const initialState = { value: true, disabled: true };
+      const { fixture, checkboxLabel } = await setup(initialState);
+
+      if (checkboxLabel) {
+        user.click(checkboxLabel);
+      }
+
+      const actualValue = fixture.componentInstance.checkboxControl.value;
+      expect(actualValue).toBeTruthy();
+    });
+
+    it('can click on checkbox after enabling it', async () => {
+      const initialState = { value: true, disabled: true };
+      const { fixture, checkboxLabel } = await setup(initialState);
+
+      if (checkboxLabel) {
+        user.click(checkboxLabel);
+      }
+
+      let actualValue = fixture.componentInstance.checkboxControl.value;
+      expect(actualValue).toBeTruthy();
+
+      fixture.componentInstance.checkboxControl.enable();
+
+      if (checkboxLabel) {
+        user.click(checkboxLabel);
+      }
+
+      actualValue = fixture.componentInstance.checkboxControl.value;
+      expect(actualValue).toBeFalsy();
+    });
+  });
+});
