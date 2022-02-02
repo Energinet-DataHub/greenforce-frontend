fb35175b5a014128351ed97c23fbef3172e67c1b
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-02-02 08:27:44 +0100

    test(eo): cover AuthHttp#postLogout

--- libs/ett/auth/data-access-api/src/test-setup.ts
+++ libs/ett/auth/data-access-api/src/test-setup.ts
@@ -16,6 +16,7 @@
  */
 import 'jest-preset-angular/setup-jest';
 
-import { setUpTestbed } from '@energinet-datahub/gf/test-util-staging';
+import { setUpNgMocks, setUpTestbed } from '@energinet-datahub/gf/test-util-staging';
 
 setUpTestbed();
+setUpNgMocks();
