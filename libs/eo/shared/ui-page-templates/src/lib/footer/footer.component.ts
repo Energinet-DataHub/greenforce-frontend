04949ae6eb89c0c9699fe8adc9b9139797b93744
Jan Klemmensen <jank50@hotmail.com>
2022-02-08 10:01:50 +0100

    Added new shared UI library for page templates | Added header component to new lib | Added footer component to lib

---
+++ libs/eo/shared/ui-page-templates/src/lib/footer/footer.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eo-footer',
+  template: `
+    <p>
+      footer works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ libs/eo/shared/ui-page-templates/src/lib/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eo-header',
+  template: `
+    <p>
+      header works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
