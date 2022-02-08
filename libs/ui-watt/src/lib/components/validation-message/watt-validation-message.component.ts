2455a476e14fdc6b8572aa2dc30b71e490231c99
Dzhavat Ushev <xdzus@energinet.dk>
2022-02-08 08:29:44 +0100

    feat(watt): add validation message component (#336)
    * feat(watt): add validation message component
    
    * feat(watt): export validation component from Watt
    
    * feat(watt): rename validation message stories
    
    * feat(watt): adjust validation message styling
    
    * chore: add license
    
    * feat(watt): remove text color from validation message component
    
    * test(watt): add test to validation message component
    
    * feat(watt): add usage comment to validation message component
    
    * refactor(watt): set class to use modifier
    
    * test(watt): update imports
    
    * docs(watt): use correct Watt name in Storybook
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ libs/ui-watt/src/lib/components/validation-message/watt-validation-message.component.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+} from '@angular/core';
+
+export type WattValidationMessageType =
+  | 'info'
+  | 'warning'
+  | 'success'
+  | 'danger';
+
+/**
+ * Usage:
+ * `import { WattValidationMessageModule } from '@energinet-datahub/watt';`
+ */
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'watt-validation-message',
+  styleUrls: ['./watt-validation-message.component.scss'],
+  templateUrl: './watt-validation-message.component.html',
+})
+export class WattValidationMessageComponent {
+  @Input() label = '';
+  @Input() message = '';
+  @Input() type: WattValidationMessageType = 'info';
+
+  /**
+   * @ignore
+   */
+  @HostBinding('class') get cssClass() {
+    return `watt-validation-message--${this.type}`;
+  }
+}
