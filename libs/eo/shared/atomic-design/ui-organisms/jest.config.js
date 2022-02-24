d2ec1dc4b80730e92c02062906916f1f9a1d1050
Jan Klemmensen <jank50@hotmail.com>
2022-02-24 13:57:03 +0100

    Removed libraries from atomic design we do not need yet

--- libs/eo/shared/atomic-design/ui-organisms/jest.config.js
+++ libs/eo/shared/atomic-design/ui-organisms/jest.config.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 module.exports = {
-  displayName: 'eo-shared-atomic-design-organisms',
+  displayName: 'eo-shared-atomic-design-ui-organisms',
   preset: '../../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -25,7 +25,7 @@ module.exports = {
     },
   },
   coverageDirectory:
-    '../../../../../coverage/libs/eo/shared/atomic-design/organisms',
+    '../../../../../coverage/libs/eo/shared/atomic-design/ui-organisms',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
