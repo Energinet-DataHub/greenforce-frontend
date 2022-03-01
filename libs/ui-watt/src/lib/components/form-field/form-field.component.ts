9e11a2c8970c051f9726f3739c9854f48ac3956f
Dzhavat Ushev <xdzus@energinet.dk>
2022-03-01 15:47:32 +0100

    feat(watt): support select component from Material inside watt form field

--- libs/ui-watt/src/lib/components/form-field/form-field.component.ts
+++ libs/ui-watt/src/lib/components/form-field/form-field.component.ts
@@ -27,6 +27,7 @@ import {
   MatFormField,
   MatFormFieldControl,
 } from '@angular/material/form-field';
+import { MatSelect } from '@angular/material/select';
 
 import { WattInputDirective } from '../input/input.directive';
 
@@ -46,14 +47,19 @@ export class FormFieldComponent implements AfterViewInit {
   beforeViewInit = true; // Used to remove placeholder control
 
   @ContentChild(WattInputDirective)
-  control!: MatFormFieldControl<unknown>;
+  inputControl!: MatFormFieldControl<unknown>;
+
+  @ContentChild(MatSelect)
+  selectControl!: MatFormFieldControl<unknown>;
 
   @ViewChild(MatFormField)
   matFormField!: MatFormField;
 
   ngAfterViewInit() {
+    const control = this.inputControl || this.selectControl;
+
     if (this.beforeViewInit) {
-      this.matFormField._control = this.control;
+      this.matFormField._control = control;
       this.matFormField.ngAfterContentInit();
       this.beforeViewInit = false;
     }
