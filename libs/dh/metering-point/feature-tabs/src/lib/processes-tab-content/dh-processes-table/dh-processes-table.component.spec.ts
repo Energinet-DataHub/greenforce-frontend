452347e7f2620fc7e41a08b7adcad71af73b70e3
Daniel Christensen <daniel@dchr.dk>
2022-03-08 13:15:27 +0100

    Fixed import path

--- libs/dh/metering-point/feature-tabs/src/lib/processes-tab-content/dh-processes-table/dh-processes-table.component.spec.ts
+++ libs/dh/metering-point/feature-tabs/src/lib/processes-tab-content/dh-processes-table/dh-processes-table.component.spec.ts
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { DhProcess } from '../../../../../domain/src/model/dh-process';
+import { DhProcess } from '@energinet-datahub/dh/metering-point/domain';
 import {
   DhProcessesTableComponent,
   DhProcessesTableScam,
