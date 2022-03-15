bc86a47f35a29e8022cd7680fc0c0a1859475fbf
Jan Klemmensen <jank50@hotmail.com>
2022-03-15 15:23:27 +0100

    Updated description in unit test for the landing page

--- libs/eo/landing-page/shell/src/lib/eo-landing-page-store.spec.ts
+++ libs/eo/landing-page/shell/src/lib/eo-landing-page-store.spec.ts
@@ -52,7 +52,7 @@ describe(EoLandingPageStore.name, () => {
       expect(actualUrl.origin + actualUrl.pathname).toBe(authenticationUrl);
     });
 
-    it(`Then the specified return url is equal to the dashboard page`, async () => {
+    it(`Then the specified return url is equal to the metering points page`, async () => {
       const baseHref = TestBed.inject(APP_BASE_HREF);
 
       expect(
