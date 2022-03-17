f012d99eec15bccfa0fcb7573651ef41c33ffca9
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-17 11:31:29 +0100

    test(eo): cover document title

--- libs/ett/shared/util-browser/src/lib/title/eo-title.store.spec.ts
+++ libs/ett/shared/util-browser/src/lib/title/eo-title.store.spec.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { firstValueFrom } from 'rxjs';
@@ -45,5 +46,40 @@ describe(EoTitleStore.name, () => {
         'Metering point detail for: 123456789012345'
       );
     });
+  });
+
+  it('the document title is updated when a route with a title is activated', async () => {
+    // Arrange
+    TestBed.configureTestingModule({
+      declarations: [TestNestedComponent],
+      imports: [
+        RouterTestingModule.withRoutes([
+          {
+            path: 'metering-points',
+            data: { title: 'Metering points' },
+            children: [
+              {
+                path: ':id',
+                data: { title: 'Metering point detail for: 123456789012345' },
+                component: TestNestedComponent,
+              },
+            ],
+          },
+        ]),
+      ],
+    });
+    const router = TestBed.inject(Router);
+    const documentTitle = TestBed.inject(Title);
+    const setTitleSpy = jest.spyOn(documentTitle, 'setTitle');
+    // We need an instance to kick off effects
+    TestBed.inject(EoTitleStore);
+
+    // Act
+    await router.navigateByUrl('metering-points/123456789012345');
+
+    // Assert
+    expect(setTitleSpy).toHaveBeenCalledWith(
+      'Metering point detail for: 123456789012345 | Energy Origin'
+    );
   });
 });
