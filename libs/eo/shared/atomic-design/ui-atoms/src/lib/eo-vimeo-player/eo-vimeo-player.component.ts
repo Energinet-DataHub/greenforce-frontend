5e012cce981b7ec50ee11139f26ea3e134437d97
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-24 14:18:32 +0100

    style(eo): organize styles

--- libs/eo/shared/atomic-design/ui-atoms/src/lib/eo-vimeo-player/eo-vimeo-player.component.ts
+++ libs/eo/shared/atomic-design/ui-atoms/src/lib/eo-vimeo-player/eo-vimeo-player.component.ts
@@ -46,16 +46,18 @@ const wrapperClass = 'embed-container';
 
       .${wrapperClass} {
         position: relative;
-        padding-bottom: 56.25%;
-        height: 0;
         overflow: hidden;
+
         max-width: 100%;
+        height: 0;
+        padding-bottom: 56.25%;
 
         > iframe {
-          border: 0;
           position: absolute;
           top: 0;
           left: 0;
+
+          border: 0;
           width: 100%;
           height: 100%;
         }
