d5a9f269f270817f5f502442f8a85403b3fb16b6
Klemmensen <jank50@hotmail.com>
2022-03-25 14:51:27 +0100

    Adds support for external links in the watt nav list item component (#427)
    ## Description
    
    - Adds support for external links in the watt nav list item component
    - Updates test for the same
    
    ## References
    
    https://github.com/Energinet-DataHub/energy-origin/issues/420

--- libs/ui-watt/src/lib/components/shell/nav-list/watt-nav-list.component.spec.ts
+++ libs/ui-watt/src/lib/components/shell/nav-list/watt-nav-list.component.spec.ts
@@ -14,9 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component } from '@angular/core';
-import { render } from '@testing-library/angular';
 
+import { render, screen } from '@testing-library/angular';
+
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { WattNavListModule } from './watt-nav-list.component';
 
 describe(WattNavListModule.name, () => {
@@ -45,51 +47,157 @@ describe(WattNavListModule.name, () => {
     })
     class TestPageComponent {}
 
-    @Component({
-      selector: 'watt-main',
-      template: `<watt-nav-list>
-          <watt-nav-list-item link="/page-1">Page 1</watt-nav-list-item>
-          <watt-nav-list-item link="/page-2">Page 2</watt-nav-list-item>
-        </watt-nav-list>
+    const view = await render(
+      `
+      <watt-nav-list>
+        <watt-nav-list-item link="/default-page">Default page</watt-nav-list-item>
+        <watt-nav-list-item link="/other-page">Other page</watt-nav-list-item>
+      </watt-nav-list>
+      <router-outlet></router-outlet>
+    `,
+      {
+        declarations: [TestPageComponent],
+        imports: [WattNavListModule, RouterModule],
+        routes: [
+          {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'default-page',
+          },
+          {
+            path: 'default-page',
+            component: TestPageComponent,
+          },
+          {
+            path: 'other-page',
+            component: TestPageComponent,
+          },
+        ],
+      }
+    );
 
-        <router-outlet></router-outlet>`,
-    })
-    class TestAppComponent {}
-
-    const view = await render(TestAppComponent, {
-      declarations: [TestPageComponent],
-      imports: [WattNavListModule],
-      routes: [
-        {
-          path: '',
-          children: [
-            {
-              path: 'page-1',
-              component: TestPageComponent,
-            },
-            {
-              path: 'page-2',
-              component: TestPageComponent,
-            },
-          ],
-        },
-      ],
+    const activeClass = 'active';
+    const otherPageLink = await screen.findByRole('link', {
+      name: /other page/i,
     });
 
-    const expectedClass = 'active';
+    await view.navigate('/');
 
-    let link = view.getByRole('link', {
-      name: /page 2/i,
-    }) as HTMLAnchorElement;
+    expect(otherPageLink).not.toHaveClass(activeClass);
 
-    expect(Object.values(link.classList)).not.toContain(expectedClass);
+    await view.navigate(otherPageLink);
 
-    await view.navigate(link);
+    expect(otherPageLink).toHaveClass(activeClass);
+  });
 
-    link = view.getByRole('link', {
-      name: /page 2/i,
-    }) as HTMLAnchorElement;
+  it('Supports external links', async () => {
+    // Arrange
+    await render(
+      `
+      <watt-nav-list>
+        <watt-nav-list-item link="https://energinet.dk">
+          Energinet
+        </watt-nav-list-item>
+      </watt-nav-list>
+    `,
+      {
+        imports: [WattNavListModule],
+      }
+    );
 
-    expect(Object.values(link.classList)).toContain(expectedClass);
+    // Act
+
+    // Assert
+    expect(
+      await screen.findByRole('link', {
+        name: /energinet/i,
+      })
+    ).toHaveAttribute('href', 'https://energinet.dk');
+  });
+
+  describe(`${WattNavListModule.name} - Ensures external links are specified with the expected protocol`, () => {
+    const setup = async (link: string) => {
+      // Arrange
+      await render(
+        `
+        <watt-nav-list>
+          <watt-nav-list-item [link]="link">
+            Energinet
+          </watt-nav-list-item>
+        </watt-nav-list>
+      `,
+        {
+          imports: [WattNavListModule],
+          componentProperties: {
+            link,
+          },
+        }
+      );
+    };
+
+    it('Ensures external links are specified with the https protocol', async () => {
+      // Arrange
+      await setup('https://energinet.dk');
+      // Act
+      // Assert
+      const link = await screen.findByRole('link', {
+        name: /energinet/i,
+      });
+      expect(link).toHaveAttribute('href', 'https://energinet.dk');
+    });
+
+    it('Ensures external links are specified with the http protocol', async () => {
+      // Arrange
+      await setup('http://energinet.dk');
+      // Act
+      // Assert
+      const link = await screen.findByRole('link', {
+        name: /energinet/i,
+      });
+      expect(link).toHaveAttribute('href', 'http://energinet.dk');
+    });
+  });
+
+  describe(`${WattNavListModule.name} - Verify links added with href attribute opens in expected windows`, () => {
+    const setup = async (target: string | null) => {
+      // Arrange
+      await render(
+        `
+        <watt-nav-list>
+          <watt-nav-list-item link="https://energinet.dk" [target]="target">
+            Energinet
+          </watt-nav-list-item>
+        </watt-nav-list>
+      `,
+        {
+          imports: [WattNavListModule],
+          componentProperties: {
+            target,
+          },
+        }
+      );
+    };
+
+    it('Opens external links in a new browser tab, when specified', async () => {
+      // Arrange
+      await setup('_blank');
+      // Act
+      // Assert
+      const link = await screen.findByRole('link', {
+        name: /energinet/i,
+      });
+      expect(link).toHaveAttribute('target', '_blank');
+    });
+
+    it('Opens external links in the same browser tab by default', async () => {
+      // Arrange
+      await setup(null);
+      // Act
+      // Assert
+      const link = await screen.findByRole('link', {
+        name: /energinet/i,
+      });
+      expect(link).not.toHaveAttribute('target');
+    });
   });
 });
