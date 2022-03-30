9a4f5c429072e47f2d91045cb6ca6a6834389bee
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-30 22:29:05 +0200

    docs(eo): remove duplicate license header

--- libs/eo/landing-page/shell/src/lib/eo-landing-page-shell.component.ts
+++ libs/eo/landing-page/shell/src/lib/eo-landing-page-shell.component.ts
@@ -14,12 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {
-  ChangeDetectionStrategy,
-  Component,
-  HostBinding,
-  NgModule,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, NgModule } from '@angular/core';
 import { EoFooterScam } from '@energinet-datahub/eo/shared/atomic-design/ui-organisms';
 
 import { EoLandingPageCallToActionScam } from './eo-landing-page-call-to-action.component';
@@ -34,22 +29,6 @@ import { EoLandingPagePresenter } from './eo-landing-page.presenter';
 import { EoLandingPageStore } from './eo-landing-page.store';
 import { EoLandingPageAudienceScam } from './eo-landinge-page-audience.component';
 
-/**
- * @license
- * Copyright 2020 Energinet DataHub A/S
- *
- * Licensed under the Apache License, Version 2.0 (the "License2");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'eo-landing-page-shell',
