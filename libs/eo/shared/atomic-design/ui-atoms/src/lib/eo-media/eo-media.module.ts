61a4dd369c6dac2e46f912ba3199048358be95f7
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-31 13:53:04 +0200

    docs(eo): add readme section on the Media atom

--- libs/eo/shared/atomic-design/ui-atoms/src/lib/eo-media/eo-media.module.ts
+++ libs/eo/shared/atomic-design/ui-atoms/src/lib/eo-media/eo-media.module.ts
@@ -24,24 +24,18 @@ import { EoMediaScam } from './eo-media.component';
  * object.
  *
  * @example
- * <eo-media
- *   [eoMediaGapPixels]="40"
- *   [eoMediaMaxWidthPixels]="960"
- * >
- *   <!-- Any content can be added as siblings to the element with an -->
- *   <!-- `eoMediaImage` directive -->
- *   <h1>Example title</h1>
- *
- *   <p>
- *     Example copy
- *   </p>
- *
+ * <eo-media [eoMediaGapPixels]="40" [eoMediaMaxWidthPixels]="960">
  *   <img
  *     eoMediaImage
  *     eoMediaImageAlign="end"
  *     [eoMediaImageMaxWidthPixels]="540"
  *     alt="Example image"
  *   />
+ *   <!-- Any content can be added as siblings to the element with an -->
+ *   <!-- `eoMediaImage` directive -->
+ *   <h1>Example title</h1>
+ *
+ *   <p>Example copy</p>
  * </eo-media>
  */
 @NgModule({
