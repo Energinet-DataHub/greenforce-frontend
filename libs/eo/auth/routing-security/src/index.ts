e91977dd60164717e94f702f12e91783ef453864
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-04-04 08:22:23 +0200

    fix(eo): remove reference to deleted module from barrel file

--- libs/eo/auth/routing-security/src/index.ts
+++ libs/eo/auth/routing-security/src/index.ts
@@ -14,5 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './lib/eo-auth-route-path';
 export * from './lib/eo-authentication.guard';
