62bdeaed36d5edc4702a8dc6b47794177147014c
Dzhavat Ushev <xdzus@energinet.dk>
2022-04-07 12:53:19 +0200

    refactor(watt): split logic into methods

--- libs/ui-watt/src/lib/components/time-range-input/watt-time-range-input.component.ts
+++ libs/ui-watt/src/lib/components/time-range-input/watt-time-range-input.component.ts
@@ -23,6 +23,9 @@ import {
 import { MatDateRangeInput } from '@angular/material/datepicker';
 import Inputmask from 'inputmask';
 
+import { WattColorHelperService } from '../../foundations/color/color-helper.service';
+import { WattColor } from '../../foundations/color/colors';
+
 export type WattTimeRange = { start: string; end: string };
 
 /**
@@ -83,7 +86,8 @@ export class WattTimeRangeInputComponent
 
   constructor(
     private renderer: Renderer2,
-    @Host() private parentControlDirective: NgControl
+    @Host() private parentControlDirective: NgControl,
+    private wattColorService: WattColorHelperService
   ) {
     this.parentControlDirective.valueAccessor = this;
   }
@@ -225,36 +229,48 @@ export class WattTimeRangeInputComponent
     inputMask: Inputmask.Instance
   ) {
     const emptyMask = inputMask.getemptymask();
-    const val = inputElement.value;
+    const inputValue = inputElement.value;
 
+    const gradient = this.buildGradient(emptyMask, inputValue);
+
+    this.renderer.setStyle(
+      inputElement,
+      'background-image',
+      `linear-gradient(90deg, ${gradient})`
+    );
+  }
+
+  /**
+   * @ignore
+   */
+  private buildGradient(emptyMask: string, inputValue: string): string {
     const splittedEmptyMask = emptyMask.split('');
-    const splittedVal = val.split('');
+    const splittedValue = inputValue.split('');
 
+    // Note: The number 9 is based on experimenting with different values
+    // but it is influenced by the monospace font set on the component
     const charWidth = 9;
-    const gradient = splittedEmptyMask.map((char, index) => {
+
+    const gradientParts = splittedEmptyMask.map((char, index) => {
       const charHasChanged =
-        char !== splittedVal[index] && splittedVal[index] !== undefined;
+        char !== splittedValue[index] && splittedValue[index] !== undefined;
+
       const color = charHasChanged
-        ? 'var(--watt-color-neutral-black)'
-        : 'var(--watt-color-neutral-grey-500)';
+        ? this.wattColorService.getColor(WattColor.black)
+        : this.wattColorService.getColor(WattColor.grey500);
+
       const gradientStart =
         index === 0 ? `${charWidth}px` : `${charWidth * index}px`;
       const gradientEnd =
         index === 0 ? `${charWidth}px` : `${charWidth * (index + 1)}px`;
 
       if (index === 0) {
-        return `${color} ${gradientStart},`;
-      } else {
-        return `${color} ${gradientStart}, ${color} ${gradientEnd}${
-          index !== splittedEmptyMask.length - 1 ? ',' : ''
-        }`;
+        return `${color} ${gradientStart}`;
       }
+
+      return `${color} ${gradientStart}, ${color} ${gradientEnd}`;
     });
 
-    this.renderer.setStyle(
-      inputElement,
-      'background-image',
-      `linear-gradient(90deg, ${gradient.join('')})`
-    );
+    return gradientParts.join(',');
   }
 }
