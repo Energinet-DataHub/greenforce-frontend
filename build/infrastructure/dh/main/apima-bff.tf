583ba6c649665d3e1fcd769dc589f7c1e51275af
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-04-08 15:13:25 +0200

    add draft of oauth authorization server - adjustments needed to reflect correct open id configuration

--- build/infrastructure/dh/main/apima-bff.tf
+++ build/infrastructure/dh/main/apima-bff.tf
@@ -21,16 +21,74 @@ module "apima_bff" {
   api_management_name         = data.azurerm_key_vault_secret.apim_instance_name.value
   resource_group_name         = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
   display_name                = "BFF Api"
+  authorization_server_name   = azurerm_api_management_authorization_server.oauth_server_bff.name
   apim_logger_id              = data.azurerm_key_vault_secret.apim_logger_id.value
   logger_sampling_percentage  = 100.0
   path                        = "bff"
   backend_service_url         = "https://${azurerm_app_service.bff.default_site_hostname}"
   import                      =  {
-      content_format          = "openapi+json"
-      content_value           = data.local_file.swagger_file.content
+    content_format          = "openapi+json"
+    content_value           = data.local_file.swagger_file.content
+  }
+  policies                    = [
+    {
+      xml_content = <<XML
+        <policies>
+          <inbound>
+            <base />
+            <validate-jwt header-name="Authorization" failed-validation-httpcode="401" failed-validation-error-message="Unauthorized. Failed policy requirements, or token is invalid or missing.">
+                <openid-config url="https://login.microsoftonline.com/${var.apim_b2c_tenant_id}/v2.0/.well-known/openid-configuration" />
+                <required-claims>
+                    <claim name="aud" match="any">
+                        <value>${var.frontend_app_id}</value>
+                    </claim>
+                </required-claims>
+            </validate-jwt>
+            <set-header name="Correlation-ID" exists-action="override">
+                <value>@($"{context.RequestId}")</value>
+            </set-header>
+            <set-header name="RequestTime" exists-action="override">
+                <value>@(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"))</value>
+            </set-header>
+          </inbound>
+          <backend>
+              <base />
+          </backend>
+          <outbound>
+              <base />
+          </outbound>
+          <on-error>
+              <base />
+          </on-error>
+        </policies>
+      XML
     }
+  ]
 }
 
 data "local_file" "swagger_file" {
     filename = "${path.module}/../../swagger/swagger.json"
+}
+
+resource "azurerm_api_management_authorization_server" "oauth_server_bff" {
+  name                         = "oauthserver_bff"
+  api_management_name          = data.azurerm_key_vault_secret.apim_instance_name.value
+  resource_group_name          = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
+  display_name                 = "BFF: OAuth client credentials server"
+  client_registration_endpoint = "http://localhost/"
+  grant_types = [
+    "implicit",
+  ]
+  authorization_endpoint       = "https://login.microsoftonline.com/${var.apim_b2c_tenant_id}/oauth2/v2.0/authorize"
+  authorization_methods        =  [
+    "GET",
+  ]
+  token_endpoint               = "https://login.microsoftonline.com/${var.apim_b2c_tenant_id}/oauth2/v2.0/token"
+  client_authentication_method = [
+    "Body",
+  ]
+  bearer_token_sending_methods = [
+    "authorizationHeader",
+  ]
+  client_id                    = var.frontend_app_id
 }
