c9dd6e8448e4ceb4c45371cd61fdd2963e4bc47a
Dan Stenrøjl <XDAST@energinet.dk>
2022-04-19 11:21:58 +0200

    Missed _w in name

--- build/infrastructure/dh/main/app-bff.tf
+++ build/infrastructure/dh/main/app-bff.tf
@@ -122,7 +122,7 @@ module "kvs_app_bff_base_url" {
   source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
 
   name          = "app-bff-base-url"
-  value         = "https://${module.bff_w.default_site_hostname}"
+  value         = "https://${module.bff.default_site_hostname}"
   key_vault_id  = data.azurerm_key_vault.kv_shared_resources.id
 
   tags          = azurerm_resource_group.this.tags
