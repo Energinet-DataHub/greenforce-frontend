b4e222a685c3c0ea4b7d17efde4a79786ae51d74
Mads Heimdal Thy <mads@heimdal-thy.dk>
2022-04-20 13:08:25 +0200

    Add API endpoint test

--- libs/eo/metering-points/shell/src/lib/eo-metering-points.service.spec.ts
+++ libs/eo/metering-points/shell/src/lib/eo-metering-points.service.spec.ts
@@ -15,18 +15,52 @@
  * limitations under the License.
  */
 import { TestBed } from '@angular/core/testing';
+import { eoApiEnvironmentToken } from '@energinet-datahub/eo/shared/environments';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import {
+  EoMeteringPointsService,
+  MeteringPointsResponse,
+} from './eo-metering-points.service';
+import { lastValueFrom } from 'rxjs';
 
-import { EoMeteringPointsService } from './eo-metering-points.service';
-
-describe('EoMeteringPointsService', () => {
-  let service: EoMeteringPointsService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EoMeteringPointsService);
+function setup() {
+  TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  const server = TestBed.inject(HttpTestingController);
+
+  return {
+    apiEnvironment: TestBed.inject(eoApiEnvironmentToken),
+    client: TestBed.inject(EoMeteringPointsService),
+    server,
+    teardown: () => {
+      server.verify();
+    },
+  };
+}
+
+describe('EoMeteringPointsService', () => {
+  describe('getMeteringPoints', () => {
+    it('emits API response with metering points', async () => {
+      const { apiEnvironment, client, server, teardown } = setup();
+      const fakeResponse: MeteringPointsResponse = {
+        meteringpoints: [{ gsrn: '1234-meter-id' }],
+      };
+
+      const whenResponse = lastValueFrom(client.getMeteringPoints());
+      const response = server.expectOne(
+        (request) =>
+          request.url === `${apiEnvironment.apiBase}/meteringpoints/list` &&
+          request.method === 'GET'
+      );
+      response.flush(fakeResponse);
+
+      expect(await whenResponse).toEqual(fakeResponse);
+      teardown();
+    });
   });
 });
