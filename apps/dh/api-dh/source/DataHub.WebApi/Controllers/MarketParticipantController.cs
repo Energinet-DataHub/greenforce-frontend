112fde62309dd2a3c85160551e297abe97bb7656
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2022-04-20 09:56:35 +0200

    Handle market participant validation exceptions

--- apps/dh/api-dh/source/DataHub.WebApi/Controllers/MarketParticipantController.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Controllers/MarketParticipantController.cs
@@ -36,102 +36,124 @@ namespace Energinet.DataHub.WebApi.Controllers
         /// Retrieves all organizations
         /// </summary>
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<OrganizationDto>>> GetAllOrganizationsAsync()
+        public Task<ActionResult<IEnumerable<OrganizationDto>>> GetAllOrganizationsAsync()
         {
-            return Ok(await _client.GetOrganizationsAsync().ConfigureAwait(false));
+            return HandleExceptionAsync(() => _client.GetOrganizationsAsync());
         }
 
         /// <summary>
         /// Retrieves a single organization
         /// </summary>
         [HttpGet("{orgId:guid}/")]
-        public async Task<ActionResult<OrganizationDto>> GetOrganizationAsync(Guid orgId)
+        public Task<ActionResult<OrganizationDto>> GetOrganizationAsync(Guid orgId)
         {
-            return Ok(await _client.GetOrganizationAsync(orgId).ConfigureAwait(false));
+            return HandleExceptionAsync(() => _client.GetOrganizationAsync(orgId));
         }
 
         /// <summary>
         /// Creates an organization
         /// </summary>
         [HttpPost]
-        public async Task<ActionResult<Guid>> CreateOrganizationAsync(ChangeOrganizationDto organizationDto)
+        public Task<ActionResult<Guid>> CreateOrganizationAsync(ChangeOrganizationDto organizationDto)
         {
-            return Ok(await _client.CreateOrganizationAsync(organizationDto).ConfigureAwait(false));
+            return HandleExceptionAsync(() => _client.CreateOrganizationAsync(organizationDto));
         }
 
         /// <summary>
         /// Updates an organization
         /// </summary>
         [HttpPut]
-        public async Task<ActionResult> UpdateOrganizationAsync(Guid orgId, ChangeOrganizationDto organizationDto)
+        public Task<ActionResult> UpdateOrganizationAsync(Guid orgId, ChangeOrganizationDto organizationDto)
         {
-            await _client.UpdateOrganizationAsync(orgId, organizationDto).ConfigureAwait(false);
-            return Ok();
+            return HandleExceptionAsync(() => _client.UpdateOrganizationAsync(orgId, organizationDto));
         }
 
         /// <summary>
         /// Retrieves all actors to a single organization
         /// </summary>
         [HttpGet("{orgId:guid}/actor")]
-        public async Task<ActionResult<IEnumerable<ActorDto>>> GetActorsAsync(Guid orgId)
+        public Task<ActionResult<IEnumerable<ActorDto>>> GetActorsAsync(Guid orgId)
         {
-            return Ok(await _client.GetActorsAsync(orgId).ConfigureAwait(false));
+            return HandleExceptionAsync(() => _client.GetActorsAsync(orgId));
         }
 
         /// <summary>
         /// Retrieves a single actor to a specific organization
         /// </summary>
         [HttpGet("{orgId:guid}/actor/{actorId:guid}")]
-        public async Task<ActionResult<ActorDto>> GetActorAsync(Guid orgId, Guid actorId)
+        public Task<ActionResult<ActorDto>> GetActorAsync(Guid orgId, Guid actorId)
         {
-            return Ok(await _client.GetActorAsync(orgId, actorId).ConfigureAwait(false));
+            return HandleExceptionAsync(() => _client.GetActorAsync(orgId, actorId));
         }
 
         /// <summary>
         /// Updates an Actor in an organization
         /// </summary>
         [HttpPost("{orgId:guid}/actor")]
-        public async Task<ActionResult<Guid>> CreateActorAsync(Guid orgId, CreateActorDto actorDto)
+        public Task<ActionResult<Guid>> CreateActorAsync(Guid orgId, CreateActorDto actorDto)
         {
-            return Ok(await _client.CreateActorAsync(orgId, actorDto).ConfigureAwait(false));
+            return HandleExceptionAsync(() => _client.CreateActorAsync(orgId, actorDto));
         }
 
         /// <summary>
         /// Updates an Actor in an organization
         /// </summary>
         [HttpPut("{orgId:guid}/actor/{actorId:guid}")]
-        public async Task<ActionResult> UpdateActorAsync(Guid orgId, Guid actorId, ChangeActorDto actorDto)
+        public Task<ActionResult> UpdateActorAsync(Guid orgId, Guid actorId, ChangeActorDto actorDto)
         {
-            await _client.UpdateActorAsync(orgId, actorId, actorDto).ConfigureAwait(false);
-            return Ok();
+            return HandleExceptionAsync(() => _client.UpdateActorAsync(orgId, actorId, actorDto));
         }
 
         /// <summary>
         /// Gets all the contacts in an organization.
         /// </summary>
         [HttpGet("{orgId:guid}/contact")]
-        public async Task<ActionResult<IEnumerable<ContactDto>>> GetContactsAsync(Guid orgId)
+        public Task<ActionResult<IEnumerable<ContactDto>>> GetContactsAsync(Guid orgId)
         {
-            return Ok(await _client.GetContactsAsync(orgId).ConfigureAwait(false));
+            return HandleExceptionAsync(() => _client.GetContactsAsync(orgId));
         }
 
         /// <summary>
         /// Creates a contact in an organization.
         /// </summary>
         [HttpPost("{orgId:guid}/contact")]
-        public async Task<ActionResult<Guid>> CreateContactAsync(Guid orgId, CreateContactDto createDto)
+        public Task<ActionResult<Guid>> CreateContactAsync(Guid orgId, CreateContactDto createDto)
         {
-            return Ok(await _client.CreateContactAsync(orgId, createDto).ConfigureAwait(false));
+            return HandleExceptionAsync(() => _client.CreateContactAsync(orgId, createDto));
         }
 
         /// <summary>
         /// Removes a contact from an organization.
         /// </summary>
         [HttpDelete("{orgId:guid}/contact/{contactId:guid}")]
-        public async Task<ActionResult> DeleteContactAsync(Guid orgId, Guid contactId)
+        public Task<ActionResult> DeleteContactAsync(Guid orgId, Guid contactId)
         {
-            await _client.DeleteContactAsync(orgId, contactId).ConfigureAwait(false);
-            return Ok();
+            return HandleExceptionAsync(() => _client.DeleteContactAsync(orgId, contactId));
+        }
+
+        private async Task<ActionResult<T>> HandleExceptionAsync<T>(Func<Task<T>> func)
+        {
+            try
+            {
+                return Ok(await func().ConfigureAwait(false));
+            }
+            catch (MarketParticipantException e)
+            {
+                return StatusCode(e.StatusCode, e.Message);
+            }
+        }
+
+        private async Task<ActionResult> HandleExceptionAsync(Func<Task> func)
+        {
+            try
+            {
+                await func().ConfigureAwait(false);
+                return Ok();
+            }
+            catch (MarketParticipantException e)
+            {
+                return StatusCode(e.StatusCode, e.Message);
+            }
         }
     }
 }
