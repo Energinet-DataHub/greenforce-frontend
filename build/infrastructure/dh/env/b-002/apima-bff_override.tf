c186d307d532cc085bc822844d96f3b8e2df5499
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-04-22 13:06:05 +0200

    extract domain name to locals and use it twice

--- build/infrastructure/dh/env/b-002/apima-bff_override.tf
+++ build/infrastructure/dh/env/b-002/apima-bff_override.tf
@@ -13,4 +13,53 @@
 # limitations under the License.
 module "apima_bff" {
   logger_sampling_percentage  = 20.0
+  policies                    = [
+    {
+      xml_content = <<XML
+        <policies>
+          <inbound>
+            <base />
+            <validate-jwt header-name="Authorization" failed-validation-httpcode="401" failed-validation-error-message="Unauthorized. Failed policy requirements, or token is invalid or missing.">
+                <openid-config url="${data.azurerm_key_vault_secret.frontend_open_id_url.value}" />
+                <required-claims>
+                    <claim name="aud" match="any">
+                        <value>${data.azurerm_key_vault_secret.frontend_service_app_id.value}</value>
+                    </claim>
+                </required-claims>
+            </validate-jwt>
+            <set-header name="Correlation-ID" exists-action="override">
+                <value>@($"{context.RequestId}")</value>
+            </set-header>
+            <set-header name="RequestTime" exists-action="override">
+                <value>@(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"))</value>
+            </set-header>
+            <cors allow-credentials="true">
+                <allowed-origins>
+                    <origin>https://${azurerm_static_site.ui.default_host_name}</origin>
+                    <origin>https://${local.B_002_CUSTOM_DOMAIN_NAME}</origin>
+                </allowed-origins>
+                <allowed-methods preflight-result-max-age="300">
+                    <method>*</method>
+                </allowed-methods>
+                <allowed-headers>
+                    <header>*</header>
+                </allowed-headers>
+                <expose-headers>
+                    <header>*</header>
+                </expose-headers>
+            </cors>
+          </inbound>
+          <backend>
+              <base />
+          </backend>
+          <outbound>
+              <base />
+          </outbound>
+          <on-error>
+              <base />
+          </on-error>
+        </policies>
+      XML
+    }
+  ]
 }
