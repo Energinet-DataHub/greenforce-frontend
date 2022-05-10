2bac9dac7943b351d3a1fe1222ffb6765d766938
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2022-05-10 16:49:45 +0200

    test(ui-watt): add initial tests

--- libs/ui-watt/src/lib/components/chips/watt-chips.component.spec.ts
+++ libs/ui-watt/src/lib/components/chips/watt-chips.component.spec.ts
@@ -14,3 +14,63 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import { render, screen } from '@testing-library/angular';
+import user from '@testing-library/user-event';
+
+import { WattChipsModule } from './watt-chips.module';
+import { WattChipsComponent, WattChipsSelection } from './watt-chips.component';
+import { EventEmitter } from '@angular/core';
+
+describe(WattChipsComponent.name, () => {
+  it('renders a single chip', async () => {
+    await render(WattChipsComponent, {
+      imports: [WattChipsModule],
+      componentProperties: {
+        options: [{ label: 'Chip 1', value: '1' }],
+      },
+    });
+
+    expect(screen.getByText('Chip 1')).toBeInTheDocument();
+  });
+
+  it('renders multiple chips', async () => {
+    await render(WattChipsComponent, {
+      imports: [WattChipsModule],
+      componentProperties: {
+        options: [
+          { label: 'Chip 1', value: '1' },
+          { label: 'Chip 2', value: '2' },
+          { label: 'Chip 3', value: '3' },
+          { label: 'Chip 4', value: '4' },
+        ],
+      },
+    });
+
+    expect(screen.queryAllByText('Chip', { exact: false })).toHaveLength(4);
+  });
+
+  it('selects chip on click', async () => {
+    const spy = jest.fn();
+    const options = [
+      { label: 'Chip 1', value: '1' },
+      { label: 'Chip 2', value: '2' },
+      { label: 'Chip 3', value: '3' },
+      { label: 'Chip 4', value: '4' },
+    ];
+
+    await render(WattChipsComponent, {
+      imports: [WattChipsModule],
+      componentProperties: {
+        selectionChange: {
+          emit: (option) => spy(option),
+        } as EventEmitter<WattChipsSelection>,
+        options,
+      },
+    });
+
+    user.click(screen.getByText('Chip 1'));
+
+    expect(spy).toHaveBeenCalledWith(options[0]);
+  });
+});
