884438b1e8e0b64a3dfbe61c30c30855fcf090c7
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2022-05-11 12:04:56 +0200

    test(ui-watt): add more tests

--- libs/ui-watt/src/lib/components/chips/watt-chips.component.spec.ts
+++ libs/ui-watt/src/lib/components/chips/watt-chips.component.spec.ts
@@ -15,19 +15,28 @@
  * limitations under the License.
  */
 
+import { EventEmitter } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { take, toArray } from 'rxjs/operators';
 import { render, screen } from '@testing-library/angular';
 import user from '@testing-library/user-event';
 
 import { WattChipsModule } from './watt-chips.module';
 import { WattChipsComponent, WattChipsSelection } from './watt-chips.component';
-import { EventEmitter } from '@angular/core';
+
+const options = [
+  { label: 'Chip 1', value: '1' },
+  { label: 'Chip 2', value: '2' },
+  { label: 'Chip 3', value: '3' },
+  { label: 'Chip 4', value: '4' },
+];
 
 describe(WattChipsComponent.name, () => {
   it('renders a single chip', async () => {
     await render(WattChipsComponent, {
       imports: [WattChipsModule],
       componentProperties: {
-        options: [{ label: 'Chip 1', value: '1' }],
+        options: options.slice(0, 1),
       },
     });
 
@@ -38,39 +47,87 @@ describe(WattChipsComponent.name, () => {
     await render(WattChipsComponent, {
       imports: [WattChipsModule],
       componentProperties: {
-        options: [
-          { label: 'Chip 1', value: '1' },
-          { label: 'Chip 2', value: '2' },
-          { label: 'Chip 3', value: '3' },
-          { label: 'Chip 4', value: '4' },
-        ],
-      },
-    });
-
-    expect(screen.queryAllByText('Chip', { exact: false })).toHaveLength(4);
-  });
-
-  it('selects chip on click', async () => {
-    const spy = jest.fn();
-    const options = [
-      { label: 'Chip 1', value: '1' },
-      { label: 'Chip 2', value: '2' },
-      { label: 'Chip 3', value: '3' },
-      { label: 'Chip 4', value: '4' },
-    ];
-
-    await render(WattChipsComponent, {
-      imports: [WattChipsModule],
-      componentProperties: {
-        selectionChange: {
-          emit: (option) => spy(option),
-        } as EventEmitter<WattChipsSelection>,
         options,
       },
     });
 
+    expect(screen.queryAllByRole('option')).toHaveLength(4);
+    expect(
+      screen.queryByRole('option', { selected: true })
+    ).not.toBeInTheDocument();
+  });
+
+  it('renders with default chip', async () => {
+    await render(WattChipsComponent, {
+      imports: [WattChipsModule],
+      componentProperties: {
+        selection: '3',
+        options,
+      },
+    });
+
+    expect(screen.getByRole('option', { selected: true })).toHaveTextContent(
+      'Chip 3'
+    );
+  });
+
+  it('selects chip on click', async () => {
+    const selectionChange = new EventEmitter<WattChipsSelection>();
+    await render(WattChipsComponent, {
+      imports: [WattChipsModule],
+      componentProperties: {
+        selectionChange,
+        options,
+      },
+    });
+
+    const selection = firstValueFrom(selectionChange);
     user.click(screen.getByText('Chip 1'));
 
-    expect(spy).toHaveBeenCalledWith(options[0]);
+    expect(await selection).toBe('1');
+    expect(screen.getByRole('option', { selected: true })).toHaveTextContent(
+      'Chip 1'
+    );
+  });
+
+  it('selects at most one chip', async () => {
+    const selectionChange = new EventEmitter<WattChipsSelection>();
+    await render(WattChipsComponent, {
+      imports: [WattChipsModule],
+      componentProperties: {
+        selectionChange,
+        options,
+      },
+    });
+
+    const selections = firstValueFrom(selectionChange.pipe(take(2), toArray()));
+    user.click(screen.getByText('Chip 2'));
+    user.click(screen.getByText('Chip 4'));
+
+    expect(await selections).toStrictEqual(['2', '4']);
+    expect(screen.getByRole('option', { selected: true })).toHaveTextContent(
+      'Chip 4'
+    );
+  });
+
+  it('deselects chip', async () => {
+    const selectionChange = new EventEmitter<WattChipsSelection>();
+    await render(WattChipsComponent, {
+      imports: [WattChipsModule],
+      componentProperties: {
+        selectionChange,
+        options,
+      },
+    });
+
+    const selections = firstValueFrom(selectionChange.pipe(take(3), toArray()));
+    user.click(screen.getByText('Chip 1'));
+    user.click(screen.getByText('Chip 3'));
+    user.click(screen.getByText('Chip 3'));
+
+    expect(await selections).toStrictEqual(['1', '3', null]);
+    expect(
+      screen.queryByRole('option', { selected: true })
+    ).not.toBeInTheDocument();
   });
 });
