d8e0638b67c3635ea43542124ae8b3908f6c62e6
Alexander HafstadFrontend Speciliasten <alexander@frontend-specialisten.dk>
2022-05-20 09:40:11 +0200

    fix(e2e-dh): adjust b2c healthchecks

--- apps/dh/e2e-dh/src/integration/b2c.spec.ts
+++ apps/dh/e2e-dh/src/integration/b2c.spec.ts
@@ -14,38 +14,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { test, expect } from '@playwright/test';
+ import { test, expect } from '@playwright/test';
 
-const environments = [
-  {
-    name: 'U001',
-    url: 'https://jolly-sand-03f839703.azurestaticapps.net',
-  },
-  // TODO: Re-add this website once it has been fixed and returns 200 again
-  /*   {
-    name: 'U002',
-    url: 'https://ambitious-coast-027d0aa03.azurestaticapps.net',
-  }, */
-  {
-    name: 'T001',
-    url: 'https://lively-river-0f22ad403.azurestaticapps.net',
-  },
-  {
-    name: 'B001',
-    url: 'https://blue-rock-05b7e5e03.azurestaticapps.net',
-  },
-  {
-    name: 'B002',
-    url: 'https://purple-forest-07e41fb03.azurestaticapps.net',
-  },
-];
+ const environments = [
+   {
+     name: 'U001',
+     url: 'https://jolly-sand-03f839703.azurestaticapps.net',
+   },
+   {
+     name: 'U002',
+     url: 'https://wonderful-field-057109603.1.azurestaticapps.net',
+   },
+   {
+     name: 'T001',
+     url: 'https://lively-river-0f22ad403.azurestaticapps.net',
+   },
+   {
+     name: 'B001',
+     url: 'https://blue-rock-05b7e5e03.azurestaticapps.net',
+   },
+   {
+     name: 'B002',
+     url: 'https://purple-forest-07e41fb03.azurestaticapps.net',
+   }
+ ];
 
-environments.forEach((env) => {
-  test(`[B2C Healthcheck] ${env.name} should have correct redirect_uri, after redirected to B2C login page`, async ({
-    page,
-  }) => {
-    await page.goto(env.url).then((resp) => expect(resp?.status()).toBe(200));
-    await page.waitForNavigation();
-    expect(page.url()).toContain(`redirect_uri=${encodeURIComponent(env.url)}`);
-  });
-});
+ environments.forEach((env) => {
+   test(`[B2C Healthcheck] ${env.name} should have correct redirect_uri, after redirected to B2C login page`, async ({
+     page,
+   }) => {
+     await page.goto(env.url).then((resp) => expect(resp?.status()).toBe(200));
+     await page.waitForNavigation();
+     expect(page.url()).toContain(`redirect_uri=${encodeURIComponent(env.url)}`);
+   });
+ });
