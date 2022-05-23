72a0743dd5fee6588bcbbd443300ae3dada97833
KarstenM <xnetsrak@gmail.com>
2022-05-23 14:41:07 +0200

    removed test mocking inport

--- libs/dh/market-participant/feature-edit-actor/src/test-setup.ts
+++ libs/dh/market-participant/feature-edit-actor/src/test-setup.ts
@@ -21,9 +21,7 @@ import {
   setUpAngularTestingLibrary,
 } from '@energinet-datahub/gf/test-util-staging';
 import { addDomMatchers } from '@energinet-datahub/gf/test-util-matchers';
-import { setupMSW } from '@energinet-datahub/dh/shared/test-util-msw';
 
-setupMSW();
 addDomMatchers();
 setUpTestbed();
 setUpAngularTestingLibrary();
