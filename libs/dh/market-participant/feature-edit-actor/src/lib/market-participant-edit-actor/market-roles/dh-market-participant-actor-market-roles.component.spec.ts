4e5e13cf6528f8c81fb713429afa6f19384ea5cc
KarstenM <xnetsrak@gmail.com>
2022-05-24 13:23:21 +0200

    changed test to use userEvent, added setup method.

--- libs/dh/market-participant/feature-edit-actor/src/lib/market-participant-edit-actor/market-roles/dh-market-participant-actor-market-roles.component.spec.ts
+++ libs/dh/market-participant/feature-edit-actor/src/lib/market-participant-edit-actor/market-roles/dh-market-participant-actor-market-roles.component.spec.ts
@@ -16,53 +16,24 @@
  */
 
 import { HttpClientModule } from '@angular/common/http';
-import { fireEvent, render, screen } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event'
 import { DhMarketParticipantActorMarketRolesComponent } from './dh-market-participant-actor-market-roles.component';
 import { getTranslocoTestingModule } from '@energinet-datahub/dh/shared/test-util-i18n';
 import { DhApiModule } from '@energinet-datahub/dh/shared/data-access-api';
 import { MarketRoleService } from './market-role.service';
-import { EicFunction } from '@energinet-datahub/dh/shared/domain';
+import { EicFunction, MarketRoleDto } from '@energinet-datahub/dh/shared/domain';
 
 describe('MarketRolesChk', () => {
-  test('should render checkbox list', async () => {
-    await render(DhMarketParticipantActorMarketRolesComponent, {
-      imports: [
-        HttpClientModule,
-        DhApiModule.forRoot(),
-        getTranslocoTestingModule(),
-      ],
-    });
-
-    const elm = screen.getByRole('listbox');
-    expect(elm).toBeInTheDocument();
-  });
-
-  test('should render checkbox options', async () => {
-    await render(DhMarketParticipantActorMarketRolesComponent, {
-      imports: [
-        HttpClientModule,
-        DhApiModule.forRoot(),
-        getTranslocoTestingModule(),
-      ],
-    });
-
-    const marketRoleService = new MarketRoleService();
-    const availableMarketRolesCount =
-      marketRoleService.getAvailableMarketRoles.length;
-
-    const elms = screen.getAllByRole('option');
-    expect(elms).toHaveLength(availableMarketRolesCount);
-  });
-
-  test('should render checkbox options, preselected', async () => {
-    const view = await render(DhMarketParticipantActorMarketRolesComponent, {
+  async function setup(marketRolesArrToTest: MarketRoleDto[]) {
+    return await render(DhMarketParticipantActorMarketRolesComponent, {
       componentProperties: {
         actor: {
           actorId: '123',
           externalActorId: '1234',
           gln: { value: '13' },
           gridAreas: [],
-          marketRoles: [{ eicFunction: EicFunction.GridAccessProvider }],
+          marketRoles: marketRolesArrToTest,
           meteringPointTypes: [],
           status: 'New',
         },
@@ -73,12 +44,36 @@ describe('MarketRolesChk', () => {
         getTranslocoTestingModule(),
       ],
     });
+  };
+
+  function getAllOptions() { return screen.getAllByRole('option') };
+
+  test('should render checkbox list', async () => {
+    await setup([]);
+
+    const elm = screen.getByRole('listbox');
+    expect(elm).toBeInTheDocument();
+  });
+
+  test('should render checkbox options', async () => {
+    await setup([]);
+
+    const marketRoleService = new MarketRoleService();
+    const availableMarketRolesCount =
+      marketRoleService.getAvailableMarketRoles.length;
+
+      const allOptions = getAllOptions();
+      expect(allOptions).toHaveLength(availableMarketRolesCount);
+  });
+
+  test('should render checkbox options, preselected', async () => {
+    const view = await setup([{ eicFunction: EicFunction.GridAccessProvider }]);
 
     await view.fixture.whenStable();
 
     view.fixture.detectChanges();
 
-    const allOptions = screen.getAllByRole('option');
+    const allOptions = getAllOptions();
     const selectedRoles = allOptions.filter(
       (e) => e.attributes.getNamedItem('aria-selected')?.value == 'true'
     );
@@ -87,37 +82,20 @@ describe('MarketRolesChk', () => {
 
   test('should render checkbox options, on selection', async () => {
     // Arrange
-    const view = await render(DhMarketParticipantActorMarketRolesComponent, {
-      componentProperties: {
-        actor: {
-          actorId: '123',
-          externalActorId: '1234',
-          gln: { value: '13' },
-          gridAreas: [],
-          marketRoles: [],
-          meteringPointTypes: [],
-          status: 'New',
-        },
-      },
-      imports: [
-        HttpClientModule,
-        DhApiModule.forRoot(),
-        getTranslocoTestingModule(),
-      ],
-    });
+    const view = await setup([]);
 
     await view.fixture.whenStable();
 
-    const allOptions = screen.getAllByRole('option');
+    const allOptions = getAllOptions();
 
     const elemToSelect1 = allOptions[0] as HTMLOptionElement;
     const elemToSelect2 = allOptions[1] as HTMLOptionElement;
     const elemNotSelectable = allOptions[5] as HTMLOptionElement;
 
     // Act
-    fireEvent.click(elemToSelect1);
-    fireEvent.click(elemToSelect2);
-    fireEvent.click(elemNotSelectable); // not selectable
+    userEvent.click(elemToSelect1);
+    userEvent.click(elemToSelect2);
+    userEvent.click(elemNotSelectable, undefined, { skipPointerEventsCheck: true }); // not selectable by disabled validation
 
     view.fixture.detectChanges();
 
