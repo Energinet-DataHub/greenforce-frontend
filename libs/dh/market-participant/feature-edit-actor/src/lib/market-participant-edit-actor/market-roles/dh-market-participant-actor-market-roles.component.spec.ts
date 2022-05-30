b2a5d62ab3080dd574f20bfa34213b7706550eb0
Dzhavat Ushev <dzu@energinet.dk>
2022-05-30 14:57:51 +0200

    Refactor tests

--- libs/dh/market-participant/feature-edit-actor/src/lib/market-participant-edit-actor/market-roles/dh-market-participant-actor-market-roles.component.spec.ts
+++ libs/dh/market-participant/feature-edit-actor/src/lib/market-participant-edit-actor/market-roles/dh-market-participant-actor-market-roles.component.spec.ts
@@ -15,50 +15,64 @@
  * limitations under the License.
  */
 
-import { HttpClientModule } from '@angular/common/http';
 import { render, screen } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
-import { DhMarketParticipantActorMarketRolesComponent } from './dh-market-participant-actor-market-roles.component';
+import {
+  DhMarketParticipantActorMarketRolesComponent,
+  DhMarketParticipantActorMarketRolesComponentScam,
+} from './dh-market-participant-actor-market-roles.component';
 import { getTranslocoTestingModule } from '@energinet-datahub/dh/shared/test-util-i18n';
-import { DhApiModule } from '@energinet-datahub/dh/shared/data-access-api';
+import { EventEmitter } from '@angular/core';
 import { MarketRoleService } from './market-role.service';
 import { EicFunction } from '@energinet-datahub/dh/shared/domain';
-import {HarnessLoader, parallel} from '@angular/cdk/testing';
-import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
-import {ListItemHarnessFilters, ListOptionHarnessFilters, MatSelectionListHarness} from '@angular/material/list/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-let loader: HarnessLoader;
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatSelectionListHarness } from '@angular/material/list/testing';
+import { en as enTranslations } from '@energinet-datahub/dh/globalization/assets-localization';
+import { runOnPushChangeDetection } from '@energinet-datahub/dh/shared/test-util-metering-point';
 
-describe('MarketRolesComponent', () => {
-  async function setup(marketRolesEicFunctionsArrToTest: EicFunction[]) {
+let loader: HarnessLoader;
+const marketRoles = enTranslations.marketParticipant.marketRoles;
+
+describe(DhMarketParticipantActorMarketRolesComponent.name, () => {
+  async function setup(marketRolesEicFunctionsToTest: EicFunction[] = []) {
+    const outputFn = jest.fn();
+
     const view = await render(DhMarketParticipantActorMarketRolesComponent, {
       componentProperties: {
-        marketRolesEicFunctions: marketRolesEicFunctionsArrToTest as EicFunction[]
+        marketRolesEicFunctions: marketRolesEicFunctionsToTest,
+        marketRolesEicFunctionsChange: {
+          emit: outputFn,
+        } as unknown as EventEmitter<EicFunction[]>,
       },
       imports: [
-        NoopAnimationsModule,
-        HttpClientModule,
-        DhApiModule.forRoot(),
+        DhMarketParticipantActorMarketRolesComponentScam,
         getTranslocoTestingModule(),
       ],
     });
+
     loader = TestbedHarnessEnvironment.loader(view.fixture);
-    return view;
+
+    await runOnPushChangeDetection(view.fixture);
+
+    return {
+      view,
+      outputFn,
+    };
   }
 
   function getAllOptions() {
     return screen.getAllByRole('option');
   }
 
-  xtest('should render checkbox list', async () => {
-    await setup([]);
+  test('should render checkbox list', async () => {
+    await setup();
 
-    const elm = screen.getByRole('listbox');
-    expect(elm).toBeInTheDocument();
+    expect(screen.getByRole('listbox')).toBeInTheDocument();
   });
 
-  xtest('should render checkbox options', async () => {
-    await setup([]);
+  test('should render checkbox options', async () => {
+    await setup();
 
     const marketRoleService = new MarketRoleService();
     const availableMarketRolesCount =
@@ -68,80 +82,52 @@ describe('MarketRolesComponent', () => {
     expect(allOptions).toHaveLength(availableMarketRolesCount);
   });
 
-  xtest('should render checkbox options, preselected', async () => {
-    const view = await setup([ EicFunction.GridAccessProvider ]);
+  test('should render checkbox options, preselected', async () => {
+    await setup([EicFunction.GridAccessProvider]);
 
-    await view.fixture.whenStable();
+    const matSelectionList = await loader.getHarness(MatSelectionListHarness);
 
-    view.fixture.detectChanges();
-
-    const compo = await loader.getHarness(MatSelectionListHarness)
-
-    const filter123 = { selected: true };
-
-    const result = await compo.getItems(filter123);
+    const result = await matSelectionList.getItems({ selected: true });
 
     expect(result).toHaveLength(1);
   });
 
   test('should render checkbox options, on selection output', async () => {
     // Arrange
-    const view = await setup([EicFunction.GridAccessProvider, EicFunction.MeterAdministrator]);
+    const { outputFn } = await setup([
+      EicFunction.GridAccessProvider,
+      EicFunction.MeterAdministrator,
+    ]);
 
-    await view.fixture.whenStable();
+    const gridAccessProviderOption = await screen.findByText(
+      new RegExp(marketRoles.GridAccessProvider, 'i')
+    );
 
-    const allOptions = getAllOptions();
+    userEvent.click(gridAccessProviderOption);
 
-    const ngReflectValue = 'ng-reflect-value';
-    const gridAccessProviderOption = allOptions.find(e => e.attributes.getNamedItem(ngReflectValue)?.value == EicFunction.GridAccessProvider) as HTMLOptionElement;
-    const meterAdministratorOption = allOptions.find(e => e.attributes.getNamedItem(ngReflectValue)?.value == EicFunction.MeterAdministrator) as HTMLOptionElement;
-    const systemOperatorOption = allOptions.find(e => e.attributes.getNamedItem(ngReflectValue)?.value == EicFunction.SystemOperator) as HTMLOptionElement;
+    expect(outputFn).toHaveBeenCalledWith([EicFunction.MeterAdministrator]);
+  });
 
-    /*let outputSelectionResult: EicFunction[] = [];
-    view.fixture.componentInstance.marketRolesEicFunctionsChange.subscribe(
-      (value) => outputSelectionResult = value
-    );*/
+  test('should render checkbox options, disabled option click', async () => {
+    // Arrange
+    const { outputFn } = await setup([EicFunction.GridAccessProvider]);
 
-    // Act
-    /*userEvent.click(gridAccessProviderOption);
-    await view.fixture.whenStable();
+    const systemOperatorOptionDisabled = await screen.findByText(
+      new RegExp(marketRoles.SystemOperator, 'i')
+    );
+
+    expect(() => userEvent.click(systemOperatorOptionDisabled)).toThrow();
+    expect(outputFn).not.toHaveBeenCalled();
+
+    const meterAdministratorOption = await screen.findByText(
+      new RegExp(marketRoles.MeterAdministrator, 'i')
+    );
 
     userEvent.click(meterAdministratorOption);
-    view.fixture.detectChanges();
 
-    await view.fixture.whenStable();
-*/
-    /*userEvent.click(systemOperatorOption, undefined,
-      { skipPointerEventsCheck: true,
-    }); // not selectable by disabled validation
-*/
-    // Assert
-    const compo = await loader.getHarness(MatSelectionListHarness)
-
-    const filter123 = { selected: true };
-
-    const items = await compo.getItems();
-
-    //await items[0].toggle();
-    //await items[1].toggle();
-
-    console.log("selected0", await items[0].isSelected())
-    console.log("selected2", await items[1].isSelected())
-
-    view.fixture.detectChanges();
-
-    await view.fixture.whenStable();
-
-    console.log("disabled5", await items[5].isDisabled())
-
-    expect(items.length).toBe(16);
-    expect(await items[0].isDisabled()).toBe(false);
-    expect(await items[5].isDisabled()).toBe(true);
-
-    const selectedRoles = await compo.getItems(filter123);
-    const disabledOptions =  await parallel(() => items.filter((e) => e.isDisabled()));
-
-    expect(selectedRoles).toHaveLength([gridAccessProviderOption, meterAdministratorOption].length);
-    expect(await disabledOptions).toHaveLength(10);
+    expect(outputFn).toHaveBeenCalledWith([
+      EicFunction.GridAccessProvider,
+      EicFunction.MeterAdministrator,
+    ]);
   });
 });
