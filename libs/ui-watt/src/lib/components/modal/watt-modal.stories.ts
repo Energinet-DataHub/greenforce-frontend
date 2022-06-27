20a2902146f89fffd1ef8cecd85b5aa2c0bbc022
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2022-06-27 14:41:16 +0200

    fix: story header levels

--- libs/ui-watt/src/lib/components/modal/watt-modal.stories.ts
+++ libs/ui-watt/src/lib/components/modal/watt-modal.stories.ts
@@ -35,17 +35,17 @@ export const Overview: Story<WattModalComponent> = (args) => ({
   template: `
     <watt-button (click)="modal.open()">Open Modal</watt-button>
     <watt-modal #modal [title]="title" [size]="size" [disableClose]="disableClose" closeLabel="Close modal">
-      <h3>Develop across all platforms</h3>
+      <h4>Develop across all platforms</h4>
       <p>Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.</p>
-      <h3>Speed &amp; Performance</h3>
+      <h4>Speed &amp; Performance</h4>
       <p>Achieve the maximum speed possible on the Web Platform today, and take it further, via Web Workers and server-side rendering. Angular puts you in control over scalability. Meet huge data requirements by building data models on RxJS, Immutable.js or another push-model.</p>
-      <h3>Incredible tooling</h3>
+      <h4>Incredible tooling</h4>
       <p>Build features quickly with simple, declarative templates. Extend the template language with your own components and use a wide array of existing components. Get immediate Angular-specific help and feedback with nearly every IDE and editor. All this comes together so you can focus on building amazing apps rather than trying to make the code work.</p>
-      <h3>Loved by millions</h3>
+      <h4>Loved by millions</h4>
       <p>From prototype through global deployment, Angular delivers the productivity and scalable infrastructure that supports Google's largest applications.</p>
-      <h3>What is Angular?</h3>
+      <h4>What is Angular?</h4>
       <p>Angular is a platform that makes it easy to build applications with the web. Angular combines declarative templates, dependency injection, end to end tooling, and integrated best practices to solve development challenges. Angular empowers developers to build applications that live on the web, mobile, or the desktop</p>
-      <h3>Architecture overview</h3>
+      <h4>Architecture overview</h4>
       <p>Angular is a platform and framework for building client applications in HTML and TypeScript. Angular is itself written in TypeScript. It implements core and optional functionality as a set of TypeScript libraries that you import into your apps.</p>
       <p>The basic building blocks of an Angular application are NgModules, which provide a compilation context for components. NgModules collect related code into functional sets; an Angular app is defined by a set of NgModules. An app always has at least a root module that enables bootstrapping, and typically has many more feature modules.</p>
       <p>Components define views, which are sets of screen elements that Angular can choose among and modify according to your program logic and data. Every app has at least a root component.</p>
