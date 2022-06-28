4d3141f4380692382f0a5453e919976c8ba249ca
frontend-specialisten <alexander@frontend-specialisten.dk>
2022-06-28 08:57:29 +0200

    Feature flag service and config (#628)
    * feat(dh): add feature flags configuration
    
    * style: format
    
    * feat(dh): add feature flag configuration files for all environments
    
    * refactor: change feature flag environment to app environment
    
    * fix(dh): rename feature flag env to app env
    
    * revert .gitignore
    
    * feat(dh): add feature flag service
    
    * refactor(dh): adjust configs for feature-flags lib
    
    * style: format
    
    * chore: add license
    
    * refactor(dh): update filename for app environment
    
    * refactor(dh): adjust environments to reflect CD
    
    * style: format
    
    * Update apps/dh/app-dh/src/main.ts
    
    Co-authored-by: Bjørn Hoffmann <ManBearTM@users.noreply.github.com>
    
    * resolve comments
    
    * style: format
    
    * fix(dh): specs
    
    * refactor(dh): simplify test
    
    * fix(dh): specs for feature flags
    
    * style: format
    
    * resolve comments
    
    * fix(dh): specs
    
    * style: format
    
    * refactor(dh): remove as unknown
    
    * feat(dh): add no-dupe-keys for feature flags
    
    * Update libs/dh/shared/feature-flags/src/lib/feature-flags.spec.ts
    
    Co-authored-by: Dzhavat Ushev <dzu@energinet.dk>
    
    * docs(dh): add example of feature-flag
    
    * doc(dh): adjust example
    
    * adjust docs
    
    * adjust example with more spacing
    
    * chore: move comment block
    
    * chore: rename variable for consistency
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Bjørn Hoffmann <ManBearTM@users.noreply.github.com>
    Co-authored-by: Dzhavat Ushev <dzu@energinet.dk>

--- libs/dh/shared/assets/src/index.ts
+++ libs/dh/shared/assets/src/index.ts
@@ -16,5 +16,6 @@
  */
 import * as dhLocalApiEnvironment from './assets/configuration/dh-api-environment.local.json';
 import * as dhLocalB2CEnvironment from './assets/configuration/dh-b2c-environment.local.json';
+import * as dhLocalAppEnvironment from './assets/configuration/dh-app-environment.local.json';
 
-export { dhLocalApiEnvironment, dhLocalB2CEnvironment };
+export { dhLocalApiEnvironment, dhLocalB2CEnvironment, dhLocalAppEnvironment };
--- libs/dh/shared/environments/src/lib/app-environment/load-dh-app-environment.ts
+++ libs/dh/shared/environments/src/lib/app-environment/load-dh-app-environment.ts
@@ -14,17 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// API environment
-export * from './lib/api-environment/dh-api-environment';
-export * from './lib/api-environment/load-dh-api-environment';
+import { DhAppEnvironmentConfig } from './dh-app-environment';
 
-// B2C environment
-export * from './lib/b2c-environment/dh-b2c-environment';
-export * from './lib/b2c-environment/load-dh-b2c-environment';
-
-// App environment
-export * from './lib/app-environment/dh-app-environment';
-export * from './lib/app-environment/load-dh-app-environment';
-
-// Application environment
-export * from './lib/environment';
+export function loadDhAppEnvironment(
+  configurationFilename: string
+): Promise<DhAppEnvironmentConfig> {
+  return fetch(`/assets/configuration/${configurationFilename}`).then(
+    (response) => response.json()
+  );
+}
