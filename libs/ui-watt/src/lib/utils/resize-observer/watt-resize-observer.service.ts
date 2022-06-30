cba27f239e076a51520449e307fc4ff2ed704a57
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2022-06-30 12:27:05 +0200

    fix(ui-watt): make resize observer optional

--- libs/ui-watt/src/lib/utils/resize-observer/watt-resize-observer.service.ts
+++ libs/ui-watt/src/lib/utils/resize-observer/watt-resize-observer.service.ts
@@ -19,24 +19,26 @@ import { filter, finalize, Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class WattResizeObserverService {
-  private resizeObserver: ResizeObserver;
+  private resizeObserver?: ResizeObserver;
   private entrySubject = new Subject<ResizeObserverEntry>();
 
   constructor(private ngZone: NgZone) {
-    this.resizeObserver = new ResizeObserver((entries) => {
-      this.ngZone.run(() => {
-        for (const entry of entries) {
-          this.entrySubject.next(entry);
-        }
+    if (window.ResizeObserver) {
+      this.resizeObserver = new ResizeObserver((entries) => {
+        this.ngZone.run(() => {
+          for (const entry of entries) {
+            this.entrySubject.next(entry);
+          }
+        });
       });
-    });
+    }
   }
 
   observe(element: Element) {
-    this.resizeObserver.observe(element);
+    this.resizeObserver?.observe(element);
     return this.entrySubject.asObservable().pipe(
       filter((entry) => entry.target === element),
-      finalize(() => this.resizeObserver.unobserve(element))
+      finalize(() => this.resizeObserver?.unobserve(element))
     );
   }
 }
