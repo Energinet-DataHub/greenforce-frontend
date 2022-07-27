f3805b04dfc9df510babfa3fca0fccc7a4f98590
KarstenM <xnetsrak@gmail.com>
2022-07-27 10:32:44 +0200

    updated status models for organization

--- libs/dh/shared/domain/src/lib/generated/v1/model/organization-dto.ts
+++ libs/dh/shared/domain/src/lib/generated/v1/model/organization-dto.ts
@@ -10,6 +10,7 @@
  * Do not edit the class manually.
  */
 import { ActorDto } from './actor-dto';
+import { OrganizationStatus } from './organization-status';
 import { AddressDto } from './address-dto';
 
 
@@ -18,7 +19,7 @@ export interface OrganizationDto {
     name: string;
     businessRegisterIdentifier: string;
     comment: string;
-    status: string;
+    status: OrganizationStatus;
     address: AddressDto;
     actors: Array<ActorDto>;
 }
