0adecab70018083da958cf30a27aeb1be596c246
Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
2022-08-03 11:13:46 +0200

    Use OIDC (#747)

--- build/infrastructure/dh/main/backend.tf
+++ build/infrastructure/dh/main/backend.tf
@@ -17,5 +17,8 @@ terraform {
     storage_account_name  = "@storage_account_name"
     container_name        = "tfstate"
     key                   = "terraform_infra.tfstate"
-  }
+    use_oidc              = true
+    subscription_id       = "@azure_subscription_id"
+    tenant_id             = "@azure_tenant_id"
+}
 }
