c9647eed5ab7a10a6b4c54f8fa0fbf0b02de6276
Mads Heimdal Thy <mads@heimdal-thy.dk>
2022-08-04 23:14:17 +0200

    Production page (#753)

---
+++ libs/eo/production/shell/src/lib/eo-production-chart-card.component.ts
@@ -0,0 +1,85 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { EoLineChartScam } from '@energinet-datahub/eo/shared/atomic-design/ui-atoms';
+import { WattSpinnerModule } from '@energinet-datahub/watt';
+import { take } from 'rxjs';
+import { EoMeasurementData, EoProductionStore } from './eo-production.store';
+
+@Component({
+  selector: 'eo-production-line-chart',
+  template: ` <mat-card class="chart-card watt-space-inline-l">
+    <h3 class="watt-space-stack-s">kWh</h3>
+    <ng-container>
+      <div *ngIf="(loadingDone$ | async) === false" class="loadingObfuscator">
+        <watt-spinner [diameter]="100"></watt-spinner>
+      </div>
+      <eo-line-chart [data]="getDataInKWH()"></eo-line-chart>
+    </ng-container>
+  </mat-card>`,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+
+      .chart-card {
+        width: 375px; /* Magic UX number */
+      }
+
+      .loadingObfuscator {
+        position: absolute;
+        height: calc(100% - 64px);
+        width: calc(100% - 32px);
+        background-color: var(--watt-on-dark-high-emphasis);
+        padding-top: 32px;
+        padding-left: 120px;
+      }
+    `,
+  ],
+})
+export class EoProductionLineChartComponent {
+  loadingDone$ = this.store.loadingDone$;
+
+  constructor(private store: EoProductionStore) {}
+
+  getDataInKWH() {
+    const measurementList: EoMeasurementData[] = [];
+
+    this.store.measurements$.pipe(take(1)).subscribe((measurements) => {
+      measurements?.map((dataPoint) =>
+        measurementList.push({
+          ...dataPoint,
+          value: Number((dataPoint.value / 1000).toFixed(0)),
+        })
+      );
+    });
+
+    return measurementList ?? [];
+  }
+}
+
+@NgModule({
+  providers: [EoProductionStore],
+  declarations: [EoProductionLineChartComponent],
+  exports: [EoProductionLineChartComponent],
+  imports: [MatCardModule, EoLineChartScam, CommonModule, WattSpinnerModule],
+})
+export class EoProductionLineChartScam {}
