0547c0c5ed99928314dc673d5e379db3814415ae
Dzhavat Ushev <dzu@energinet.dk>
2022-08-12 17:16:58 +0200

    test(dh): add test setup

--- libs/dh/shared/util-application-insights/src/test-setup.ts
+++ libs/dh/shared/util-application-insights/src/test-setup.ts
@@ -15,3 +15,14 @@
  * limitations under the License.
  */
 import 'jest-preset-angular/setup-jest';
+
+import { addNgModuleMatchers } from '@energinet-datahub/gf/test-util-matchers';
+import {
+  setUpNgMocks,
+  setUpTestbed,
+} from '@energinet-datahub/gf/test-util-staging';
+
+addNgModuleMatchers();
+
+setUpTestbed();
+setUpNgMocks();
