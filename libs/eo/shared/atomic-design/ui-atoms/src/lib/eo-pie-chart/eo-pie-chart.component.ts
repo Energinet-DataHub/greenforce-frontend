d7c06f2dcfe760a44128a3c351365b02e35e0880
Mads Heimdal Thy <mads@heimdal-thy.dk>
2022-09-02 12:43:22 +0200

    Fix background color for pie chart

--- libs/eo/shared/atomic-design/ui-atoms/src/lib/eo-pie-chart/eo-pie-chart.component.ts
+++ libs/eo/shared/atomic-design/ui-atoms/src/lib/eo-pie-chart/eo-pie-chart.component.ts
@@ -79,21 +79,7 @@ export class EoPieChartComponent implements DoCheck {
   };
 
   public chartData: ChartData<'pie', number[], string | string[]> = {
-    datasets: [
-      {
-        rotation: 180,
-        data: [],
-        backgroundColor:
-          this.data.length > 0
-            ? [this.#colorGreen, this.#colorGrey]
-            : 'lightgrey',
-        hoverBackgroundColor:
-          this.data.length > 0
-            ? [this.#colorGreen, this.#colorGrey]
-            : 'lightgrey',
-        borderWidth: 0,
-      },
-    ],
+    datasets: [{ rotation: 180, data: [], borderWidth: 0 }],
   };
   public chartType: ChartType = 'pie';
   public chartPlugins = [DatalabelsPlugin];
@@ -102,6 +88,14 @@ export class EoPieChartComponent implements DoCheck {
     if (this.data !== this.chartData.datasets[0].data) {
       this.chartData.labels = this.data.length > 0 ? this.labels : [];
       this.chartData.datasets[0].data = this.data.length > 0 ? this.data : [1];
+      this.chartData.datasets[0].backgroundColor =
+        this.data.length > 0
+          ? [this.#colorGreen, this.#colorGrey]
+          : 'lightgrey';
+      this.chartData.datasets[0].hoverBackgroundColor =
+        this.data.length > 0
+          ? [this.#colorGreen, this.#colorGrey]
+          : 'lightgrey';
       this.chart?.update();
     }
   }
