25bd1f6c077202683cb7ae08496fc4833f7540c4
Dzhavat Ushev <dzu@energinet.dk>
2022-09-19 13:10:50 +0200

    test(ui-watt): remove config from test setup

--- libs/ui-watt/src/test-setup.ts
+++ libs/ui-watt/src/test-setup.ts
@@ -18,11 +18,7 @@
 import 'jest-preset-angular/setup-jest';
 
 import { addDomMatchers } from '@energinet-datahub/gf/test-util-matchers';
-import {
-  setUpAngularTestingLibrary,
-  setUpTestbed,
-} from '@energinet-datahub/gf/test-util-staging';
+import { setUpTestbed } from '@energinet-datahub/gf/test-util-staging';
 
 addDomMatchers();
 setUpTestbed();
-setUpAngularTestingLibrary();
