1f3b105fc3ec9e9c611bd45bbd93e3f0c1ec3afc
Morten Kjærgård <MKQ@energinet.dk>
2022-09-20 09:53:00 +0200

    added search container

---
+++ libs/dh/charges/domain/src/lib/chargeTypes.ts
@@ -0,0 +1,6 @@
+export enum ChargeTypes {
+  Unknown = '0',
+  Subscription = '1',
+  Fee = '2',
+  Tariff = '3',
+}
--- libs/dh/charges/feature-prices/src/lib/dh-charges-prices.component.ts
+++ libs/dh/charges/feature-prices/src/lib/dh-charges-prices.component.ts
@@ -14,18 +14,72 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, NgModule } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  NgModule,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco';
+
+import { ChargeTypes } from '@energinet-datahub/dh/charges/domain';
+
+import {
+  WattButtonModule,
+  WattFormFieldModule,
+  WattInputModule,
+  WattCheckboxModule,
+  WattBadgeModule,
+  WattDropdownModule,
+  WattSpinnerModule,
+  WattDropdownOptions,
+} from '@energinet-datahub/watt';
 
 @Component({
   selector: 'dh-charges-prices',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './dh-charges-prices.component.html',
   styleUrls: ['./dh-charges-prices.component.scss'],
 })
-export class DhChargesPricesComponent {}
+export class DhChargesPricesComponent implements OnInit {
+  chargeTypeOptions: WattDropdownOptions = this.buildChargeTypeOptions();
+  searchCriteria: any = {
+    chargeTypes: '',
+  };
+
+  constructor() {}
+
+  private buildChargeTypeOptions() {
+    return Object.entries(ChargeTypes).map((entry) => {
+      return {
+        value: entry[0],
+        displayValue: entry[0],
+      };
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {}
+
+  resetSearchCriteria() {}
+}
 
 @NgModule({
   declarations: [DhChargesPricesComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    WattButtonModule,
+    WattFormFieldModule,
+    WattInputModule,
+    WattCheckboxModule,
+    WattBadgeModule,
+    WattDropdownModule,
+    WattSpinnerModule,
+    TranslocoModule,
+    FormsModule,
+  ],
 })
 export class DhChargesPricesScam {}
