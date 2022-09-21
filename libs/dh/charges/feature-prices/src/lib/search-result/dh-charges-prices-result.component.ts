8424963b275d88f4e8d7ce9b429e43b6e46c9b17
Morten Kjærgård <MKQ@energinet.dk>
2022-09-21 11:09:51 +0200

    added pagination

--- libs/dh/charges/feature-prices/src/lib/search-result/dh-charges-prices-result.component.ts
+++ libs/dh/charges/feature-prices/src/lib/search-result/dh-charges-prices-result.component.ts
@@ -1,26 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, NgModule, OnInit } from '@angular/core';
+import {
+  Component,
+  NgModule,
+  OnInit,
+  OnDestroy,
+  OnChanges,
+  ViewChild
+} from '@angular/core';
 
-import { MatTableModule } from '@angular/material/table';
-import { TranslocoModule } from '@ngneat/transloco';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
 import { LetModule } from '@rx-angular/template';
 
 import {
-  WattIcon,
   WattIconModule,
   WattIconSize,
+  WattButtonModule,
+  WattEmptyStateModule
 } from '@energinet-datahub/watt';
+import {
+  MatPaginator,
+  MatPaginatorIntl,
+  MatPaginatorModule,
+} from '@angular/material/paginator';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'dh-charges-prices-result',
   templateUrl: './dh-charges-prices-result.component.html',
   styleUrls: ['./dh-charges-prices-result.component.scss']
 })
-export class DhChargesPricesResultComponent implements OnInit {
+export class DhChargesPricesResultComponent implements OnInit, OnDestroy, OnChanges {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  private destroy$ = new Subject<void>();
+
   iconSizes = WattIconSize;
   searchResult: Array<any> = [
     {
-      id: 1,
+      id: 2,
       name: "test name",
       owner: "test owner",
       isTax: true,
@@ -42,11 +60,52 @@ export class DhChargesPricesResultComponent implements OnInit {
     'validToDate'
   ];
 
-  constructor() { }
+  readonly dataSource: MatTableDataSource<any> =
+    new MatTableDataSource<any>(this.searchResult);
 
-  ngOnInit(): void {
+  constructor(
+    private translocoService: TranslocoService,
+    private matPaginatorIntl: MatPaginatorIntl
+  ) {}
+
+  ngOnInit() {
+    // this.dataSource.data = this.searchResult;
+    this.dataSource.paginator = this.paginator;
+    this.setupPaginatiorTranslation();
   }
 
+  ngOnChanges() {
+    this.dataSource.data = this.searchResult;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+  }
+
+  private readonly setupPaginatiorTranslation = () => {
+    const temp = this.matPaginatorIntl.getRangeLabel;
+    this.matPaginatorIntl.getRangeLabel = (page, pageSize, length) =>
+      temp(page, pageSize, length).replace(
+        'of',
+        this.translocoService.translate(
+          'charges.prices.result.paginator.of'
+        )
+      );
+
+    this.translocoService
+      .selectTranslateObject('charges.prices.result.paginator')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((value) => {
+        this.matPaginatorIntl.itemsPerPageLabel = value.itemsPerPageLabel;
+        this.matPaginatorIntl.nextPageLabel = value.next;
+        this.matPaginatorIntl.previousPageLabel = value.previous;
+        this.matPaginatorIntl.firstPageLabel = value.first;
+        this.matPaginatorIntl.lastPageLabel = value.last;
+        this.dataSource.paginator = this.paginator;
+      });
+  }
 }
 
 @NgModule({
@@ -58,6 +117,9 @@ export class DhChargesPricesResultComponent implements OnInit {
     TranslocoModule,
     LetModule,
     WattIconModule,
+    MatPaginatorModule,
+    WattButtonModule,
+    WattEmptyStateModule
   ],
 })
 export class DhChargesPricesResultScam {}
