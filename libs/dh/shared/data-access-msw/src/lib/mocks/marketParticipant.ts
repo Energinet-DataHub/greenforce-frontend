2caad03b46a2f630e7dbd760e246c1d2c3174ac6
Morten Vestergaard Hansen <mimo@mimo-design.com>
2022-10-11 14:40:20 +0200

    Added mocks for more market participant data (#1020)
    Co-authored-by: Dzhavat Ushev <dzu@energinet.dk>

--- libs/dh/shared/data-access-msw/src/lib/mocks/marketParticipant.ts
+++ libs/dh/shared/data-access-msw/src/lib/mocks/marketParticipant.ts
@@ -16,28 +16,50 @@
  */
 import { rest } from 'msw';
 
-import organizationData from './data/marketParticipant.json';
+import organizationsData from './data/marketParticipantOrganizations.json';
 import gridAreaData from './data/marketParticipantGridArea.json';
 import gridAreaOverviewData from './data/marketParticipantGridAreaOverview.json';
+import actorData from './data/marketPaticipantActor.json';
+import actorContactsData from './data/marketPaticipantActorContacts.json';
+import organizationData from './data/marketPaticipantOrganization.json';
+
+const port = 5001;
 
 export const marketParticipantMocks = [
   getOrganizations(),
   getMarketParticipantGridArea(),
   getMarketParticipantGridAreaOverview(),
+  getActor(),
+  getActorContact(),
+  getOrganization(),
 ];
 
 function getOrganizations() {
   return rest.get(
-    'https://localhost:5001/v1/MarketParticipant/organization',
+    `https://localhost:${port}/v1/MarketParticipant/organization`,
     (req, res, ctx) => {
-      return res(ctx.json(organizationData));
+      return res(ctx.json(organizationsData));
+    }
+  );
+}
+
+function getOrganization() {
+  return rest.get(
+    `https://localhost:${port}/v1/MarketParticipant/organization/:organizationId`,
+    (req, res, ctx) => {
+      const { organizationId } = req.params;
+      const organizationDataWithUpdatedId = {
+        ...organizationData,
+        organizationId,
+      };
+      return res(ctx.json(organizationDataWithUpdatedId));
     }
   );
 }
 
 function getMarketParticipantGridArea() {
   return rest.get(
-    'https://localhost:5001/v1/MarketParticipantGridArea',
+    `https://localhost:${port}/v1/MarketParticipantGridArea`,
     (req, res, ctx) => {
       return res(ctx.json(gridAreaData));
     }
@@ -46,9 +68,32 @@ function getMarketParticipantGridArea() {
 
 function getMarketParticipantGridAreaOverview() {
   return rest.get(
-    'https://localhost:5001/v1/MarketParticipantGridAreaOverview',
+    `https://localhost:${port}/v1/MarketParticipantGridAreaOverview`,
     (req, res, ctx) => {
       return res(ctx.json(gridAreaOverviewData));
+    }
+  );
+}
+
+function getActor() {
+  return rest.get(
+    `https://localhost:${port}/v1/MarketParticipant/organization/:organizationId/actor/:actorId`,
+    (req, res, ctx) => {
+      const { actorId } = req.params;
+      const actorDataWithUpdatedId = {
+        ...actorData,
+        actorId,
+      };
+      return res(ctx.json(actorDataWithUpdatedId));
+    }
+  );
+}
+
+function getActorContact() {
+  return rest.get(
+    `https://localhost:${port}/v1/MarketParticipant/organization/:organizationId/actor/:actorId/contact`,
+    (req, res, ctx) => {
+      return res(ctx.json(actorContactsData));
     }
   );
 }
