914028c8e7b34793bd686c60185d0218f99e710b
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2022-10-17 14:46:23 +0200

    fixed autogenerated file in permission-guard

--- libs/dh/shared/permission-guard/jest.config.ts
+++ libs/dh/shared/permission-guard/jest.config.ts
@@ -18,14 +18,21 @@
 export default {
   displayName: 'dh-shared-permission-guard',
   preset: '../../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/dh/shared/permission-guard',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
 };
