0d1d8cdfbf4321a9cdaf65c8cd1cb92fbba9adc4
Henrik Sommer <XHESO@energinet.dk>
2022-10-17 09:16:02 +0200

    Show empty list

--- libs/dh/charges/feature-prices/src/lib/drawer/message-tab/dh-charges-message-tab.component.ts
+++ libs/dh/charges/feature-prices/src/lib/drawer/message-tab/dh-charges-message-tab.component.ts
@@ -14,20 +14,129 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, NgModule } from '@angular/core';
-//import { DhDrawerDatepickerScam } from '../drawer-datepicker/dh-drawer-datepicker.component';
+ import {
+  Component,
+  NgModule,
+  AfterViewInit,
+  OnDestroy,
+  OnChanges,
+  ViewChild,
+  Input,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
+import { LetModule } from '@rx-angular/template';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { Subject, takeUntil } from 'rxjs';
+import {
+  WattButtonModule,
+  WattEmptyStateModule,
+  WattTooltipModule,
+  WattSpinnerModule,
+} from '@energinet-datahub/watt';
+import {
+  MatPaginator,
+  MatPaginatorIntl,
+  MatPaginatorModule,
+} from '@angular/material/paginator';
+import { DhSharedUiDateTimeModule } from '@energinet-datahub/dh/shared/ui-date-time';
 
 @Component({
-  selector: 'dh-charges-prices-message-tab',
+  selector: 'dh-charges-message-tab',
   templateUrl: './dh-charges-message-tab.component.html',
   styleUrls: ['./dh-charges-message-tab.component.scss']
 })
-export class DhChargesMessageTabComponent {}
+
+export class DhChargesMessageTabComponent
+  implements AfterViewInit, OnDestroy, OnChanges
+{
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+
+  @Input() result?: Array<string>;
+  @Input() isLoading = false;
+  @Input() isInit = false;
+  @Input() hasLoadingError = false;
+
+  private destroy$ = new Subject<void>();
+
+  displayedColumns = [
+    'chargeId',
+    'chargeName',
+    'chargeOwnerName',
+  ];
+
+  readonly dataSource: MatTableDataSource<string> =
+    new MatTableDataSource<string>();
+
+  constructor(
+    private translocoService: TranslocoService,
+    private matPaginatorIntl: MatPaginatorIntl
+  ) {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.setupPaginatorTranslation();
+  }
+
+  ngOnChanges() {
+    if (this.result) this.dataSource.data = this.result;
+
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.matSort;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private readonly setupPaginatorTranslation = () => {
+    const temp = this.matPaginatorIntl.getRangeLabel;
+    this.matPaginatorIntl.getRangeLabel = (page, pageSize, length) =>
+      temp(page, pageSize, length).replace(
+        'of',
+        this.translocoService.translate('charges.prices.paginator.of')
+      );
+
+    this.translocoService
+      .selectTranslateObject('charges.prices.paginator')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((value) => {
+        this.matPaginatorIntl.itemsPerPageLabel = value.itemsPerPageLabel;
+        this.matPaginatorIntl.nextPageLabel = value.next;
+        this.matPaginatorIntl.previousPageLabel = value.previous;
+        this.matPaginatorIntl.firstPageLabel = value.first;
+        this.matPaginatorIntl.lastPageLabel = value.last;
+        this.dataSource.paginator = this.paginator;
+      });
+  };
+
+  rowClicked(message: string) {
+    this.openMessage(message);
+  }
+
+  openMessage(message: string) {
+    console.log(message);
+  }
+}
 
 @NgModule({
   declarations: [DhChargesMessageTabComponent],
   exports: [DhChargesMessageTabComponent],
-  //imports: [DhDrawerDatepickerScam],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    TranslocoModule,
+    LetModule,
+    MatPaginatorModule,
+    WattButtonModule,
+    WattEmptyStateModule,
+    WattTooltipModule,
+    WattSpinnerModule,
+    DhSharedUiDateTimeModule,
+    MatSortModule,
+  ],
 })
-
 export class DhChargesMessageTabScam {}
