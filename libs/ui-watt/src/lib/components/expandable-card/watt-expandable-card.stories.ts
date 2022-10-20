1ba4349864ba47f3a1fe2bee2549858d572d1793
Bjørn Hoffmann <ManBearTM@users.noreply.github.com>
2022-10-20 13:06:07 +0200

    feat(ui-watt): expandable card (#1043)
    * Add ExpandableCard component
    
    * chore: bump eo
    
    * Satisfy my OCD
    
    * Minor fixes
    
    * Use watt-badge + make it standalone
    
    * chore: bump eo
    
    * Force app build for argoCD (#1058)
    
    * chore: bump eo
    
    * Party like it is 99
    
    Co-authored-by: frontend-specialisten <alexander@frontend-specialisten.dk>
    Co-authored-by: Mads Heimdal Thy <mads@heimdal-thy.dk>

---
+++ libs/ui-watt/src/lib/components/expandable-card/watt-expandable-card.stories.ts
@@ -0,0 +1,58 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { WattBadgeModule } from '../badge';
+import {
+  WattExpandableCardComponent,
+  WATT_EXPANDABLE_CARD_COMPONENTS,
+} from './watt-expandable-card.component';
+
+export default {
+  title: 'Components/Expandable Card',
+  component: WattExpandableCardComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [
+        BrowserAnimationsModule,
+        WattBadgeModule,
+        WATT_EXPANDABLE_CARD_COMPONENTS,
+      ],
+    }),
+  ],
+} as Meta;
+
+export const Overview: Story<WattExpandableCardComponent> = (args) => ({
+  props: args,
+  template: `
+    <watt-expandable-card [expanded]="expanded">
+      <watt-badge>02</watt-badge>
+      <watt-expandable-card-title>The Cosmos Awaits</watt-expandable-card-title>
+      <p>
+        The sky calls to us preserve and cherish that pale blue dot citizens of
+        distant epochs rich in heavy atoms the only home we've ever known cosmic
+        fugue. Vanquish the impossible a mote of dust suspended in a sunbeam
+        Sea of Tranquility Rig Veda invent the universe another world.
+      </p>
+    </watt-expandable-card>
+  `,
+});
+
+Overview.args = {
+  expanded: false,
+};
