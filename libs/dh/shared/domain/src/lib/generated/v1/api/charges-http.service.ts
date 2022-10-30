be74d153da57090f9667afecefdd30130e1d735a
Henrik Sommer <XHESO@energinet.dk>
2022-10-30 10:07:48 +0100

    Update frontend client

--- libs/dh/shared/domain/src/lib/generated/v1/api/charges-http.service.ts
+++ libs/dh/shared/domain/src/lib/generated/v1/api/charges-http.service.ts
@@ -19,6 +19,10 @@ import { CustomHttpParameterCodec }                          from '../encoder';
 import { Observable }                                        from 'rxjs';
 
 // @ts-ignore
+import { ChargeMessageV1Dto } from '../model/charge-message-v1-dto';
+// @ts-ignore
+import { ChargeMessagesSearchCriteriaV1Dto } from '../model/charge-messages-search-criteria-v1-dto';
+// @ts-ignore
 import { ChargePricesSearchCriteriaV1Dto } from '../model/charge-prices-search-criteria-v1-dto';
 // @ts-ignore
 import { ChargePricesV1Dto } from '../model/charge-prices-v1-dto';
@@ -289,6 +293,81 @@ export class ChargesHttp {
             {
                 context: localVarHttpContext,
                 body: chargeSearchCriteriaV1Dto,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * @param chargeMessagesSearchCriteriaV1Dto 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public v1ChargesSearchChargeMessagesAsyncPost(chargeMessagesSearchCriteriaV1Dto?: ChargeMessagesSearchCriteriaV1Dto, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'text/plain' | 'application/json' | 'text/json', context?: HttpContext}): Observable<Array<ChargeMessageV1Dto>>;
+    public v1ChargesSearchChargeMessagesAsyncPost(chargeMessagesSearchCriteriaV1Dto?: ChargeMessagesSearchCriteriaV1Dto, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'text/plain' | 'application/json' | 'text/json', context?: HttpContext}): Observable<HttpResponse<Array<ChargeMessageV1Dto>>>;
+    public v1ChargesSearchChargeMessagesAsyncPost(chargeMessagesSearchCriteriaV1Dto?: ChargeMessagesSearchCriteriaV1Dto, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'text/plain' | 'application/json' | 'text/json', context?: HttpContext}): Observable<HttpEvent<Array<ChargeMessageV1Dto>>>;
+    public v1ChargesSearchChargeMessagesAsyncPost(chargeMessagesSearchCriteriaV1Dto?: ChargeMessagesSearchCriteriaV1Dto, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'text/plain' | 'application/json' | 'text/json', context?: HttpContext}): Observable<any> {
+
+        let localVarHeaders = this.defaultHeaders;
+
+        let localVarCredential: string | undefined;
+        // authentication (Bearer) required
+        localVarCredential = this.configuration.lookupCredential('Bearer');
+        if (localVarCredential) {
+            localVarHeaders = localVarHeaders.set('Authorization', 'Bearer ' + localVarCredential);
+        }
+
+        let localVarHttpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
+        if (localVarHttpHeaderAcceptSelected === undefined) {
+            // to determine the Accept header
+            const httpHeaderAccepts: string[] = [
+                'text/plain',
+                'application/json',
+                'text/json'
+            ];
+            localVarHttpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        }
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        let localVarHttpContext: HttpContext | undefined = options && options.context;
+        if (localVarHttpContext === undefined) {
+            localVarHttpContext = new HttpContext();
+        }
+
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json',
+            'text/json',
+            'application/*+json'
+        ];
+        const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
+        if (httpContentTypeSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Content-Type', httpContentTypeSelected);
+        }
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        let localVarPath = `/v1/Charges/SearchChargeMessagesAsync`;
+        return this.httpClient.request<Array<ChargeMessageV1Dto>>('post', `${this.configuration.basePath}${localVarPath}`,
+            {
+                context: localVarHttpContext,
+                body: chargeMessagesSearchCriteriaV1Dto,
                 responseType: <any>responseType_,
                 withCredentials: this.configuration.withCredentials,
                 headers: localVarHeaders,
