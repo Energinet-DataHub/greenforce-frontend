bff95beb95bd1d9dd743cfaa787047c061add5fc
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2022-11-02 12:08:36 +0100

    fix

--- libs/dh/shared/test-util-auth/src/lib/local-storage-fake.service.ts
+++ libs/dh/shared/test-util-auth/src/lib/local-storage-fake.service.ts
@@ -16,6 +16,7 @@
  */
 
 export class LocalStorageFake implements Storage {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [name: string]: any;
 
   private internalLength = 0;
