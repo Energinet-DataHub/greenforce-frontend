69ff31e7d3cc29400be287ac730044a23e9d9543
Mads Heimdal Thy <mads@heimdal-thy.dk>
2022-11-03 09:06:19 +0100

    Simplify EO app

--- libs/eo/shared/utilities/src/index.ts
+++ libs/eo/shared/utilities/src/index.ts
@@ -14,4 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const eoDashboardRoutePath = 'dashboard';
+export * from './lib/absolute-url-generator.service';
+export * from './lib/eo-routes';
+export * from './lib/title/eo-title.store';
+
