99c8f761c95bffc8bde86a208bfe1d110991efcc
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2022-11-09 12:45:02 +0000

    Make table story responsive

--- libs/ui-watt/src/lib/components/table/+storybook/storybook-table-overview.component.ts
+++ libs/ui-watt/src/lib/components/table/+storybook/storybook-table-overview.component.ts
@@ -28,12 +28,28 @@ export const periodicElements: PeriodicElement[] = [
   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
 ];
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'storybook-table-overview',
   templateUrl: 'storybook-table-overview.component.html',
+  styles: [
+    `
+      :host {
+        display: grid;
+        height: calc(100vh - 2rem);
+        grid-template-rows: min-content minmax(10rem, min-content);
+      }
+
+      :host > * {
+        height: 100%;
+      }
+    `,
+  ],
 })
 export class StorybookTableOverviewComponent {
   dataSource = new WattTableDataSource(periodicElements);
