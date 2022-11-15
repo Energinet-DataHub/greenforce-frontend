1ebbac478f7dc2235089a4ac47f63386521b5022
Bjørn Hoffmann <ManBearTM@users.noreply.github.com>
2022-11-15 18:30:48 +0100

    feat(ui-watt): watt logo (#1144)
    * Add custom brand image
    
    * chore: bump eo
    
    * chore: bump eo
    
    * Add dark version + favicon
    
    * Add brandTitle

--- libs/ui-watt/.storybook/manager.js
+++ libs/ui-watt/.storybook/manager.js
@@ -4,3 +4,9 @@ import wattTheme from './theme';
 addons.setConfig({
   theme: wattTheme,
 });
+
+const link = document.createElement('link');
+link.setAttribute('rel', 'icon');
+link.setAttribute('type', 'image/svg+xml');
+link.setAttribute('href', '/assets/ui-watt/watt-icon.svg');
+document.head.appendChild(link);
