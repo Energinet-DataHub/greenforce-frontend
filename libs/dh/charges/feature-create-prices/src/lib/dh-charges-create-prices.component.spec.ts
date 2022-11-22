a2f9b2e9c83da2b69c60ad2477a26afeab537747
Morten Kjærgård <MKQ@energinet.dk>
2022-11-22 10:52:31 +0100

    try

--- libs/dh/charges/feature-create-prices/src/lib/dh-charges-create-prices.component.spec.ts
+++ libs/dh/charges/feature-create-prices/src/lib/dh-charges-create-prices.component.spec.ts
@@ -15,24 +15,22 @@
  * limitations under the License.
  */
 import { DhApiModule } from '@energinet-datahub/dh/shared/data-access-api';
-import { MatNativeDateModule } from '@angular/material/core';
 import { getTranslocoTestingModule } from '@energinet-datahub/dh/shared/test-util-i18n';
 import { HttpClientModule } from '@angular/common/http';
 import { render } from '@testing-library/angular';
-import {
-  DhChargesCreatePricesComponent,
-  DhChargesCreatePricesScam,
-} from './dh-charges-create-prices.component';
+import { DhChargesCreatePricesComponent } from './dh-charges-create-prices.component';
+import { WattDanishDatetimeModule } from '@energinet-datahub/watt/danish-date-time';
+import { DanishLocaleModule } from '@energinet-datahub/gf/configuration-danish-locale';
 
 describe('DhChargesCreatePrices', () => {
   async function setup() {
     const { fixture } = await render(DhChargesCreatePricesComponent, {
       imports: [
         getTranslocoTestingModule(),
-        MatNativeDateModule,
         DhApiModule.forRoot(),
         HttpClientModule,
-        DhChargesCreatePricesScam,
+        WattDanishDatetimeModule.forRoot(),
+        DanishLocaleModule,
       ],
     });
 
@@ -41,7 +39,7 @@ describe('DhChargesCreatePrices', () => {
     };
   }
 
-  it('should fetch and render data on submit', async () => {
+  it('should create', async () => {
     const { fixture } = await setup();
     expect(fixture.componentInstance).toBeTruthy();
   });
