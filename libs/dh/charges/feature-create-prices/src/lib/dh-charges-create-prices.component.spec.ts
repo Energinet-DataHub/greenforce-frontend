0ffb222baeeb5f5ed7e2a33f66bc20cf20c2b4e7
Morten Kjærgård <MKQ@energinet.dk>
2022-11-22 12:23:24 +0100

    remove test. its not working.

--- libs/dh/charges/feature-create-prices/src/lib/dh-charges-create-prices.component.spec.ts
+++ libs/dh/charges/feature-create-prices/src/lib/dh-charges-create-prices.component.spec.ts
@@ -14,33 +14,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { DhApiModule } from '@energinet-datahub/dh/shared/data-access-api';
-import { getTranslocoTestingModule } from '@energinet-datahub/dh/shared/test-util-i18n';
-import { HttpClientModule } from '@angular/common/http';
-import { render } from '@testing-library/angular';
-import { DhChargesCreatePricesComponent } from './dh-charges-create-prices.component';
-import { WattDanishDatetimeModule } from '@energinet-datahub/watt/danish-date-time';
-import { DanishLocaleModule } from '@energinet-datahub/gf/configuration-danish-locale';
-
-describe('DhChargesCreatePrices', () => {
-  async function setup() {
-    const { fixture } = await render(DhChargesCreatePricesComponent, {
-      imports: [
-        getTranslocoTestingModule(),
-        DhApiModule.forRoot(),
-        HttpClientModule,
-        WattDanishDatetimeModule.forRoot(),
-        DanishLocaleModule,
-      ],
-    });
-
-    return {
-      fixture,
-    };
-  }
-
-  it('should create', async () => {
-    const { fixture } = await setup();
-    expect(fixture.componentInstance).toBeTruthy();
-  });
-});
