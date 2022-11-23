6678eb448ebf6d28d737760a3c9da76a5d8af5d6
Jonas Michelsen <jvm@energinet.dk>
2022-11-23 15:31:13 +0100

    Add process and process step result to wholesale controller

--- apps/dh/api-dh/source/DataHub.WebApi/Controllers/WholesaleBatchController.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Controllers/WholesaleBatchController.cs
@@ -17,8 +17,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Mime;
 using System.Threading.Tasks;
-using Energinet.DataHub.Wholesale.Application.Batches;
 using Energinet.DataHub.Wholesale.Client;
+using Energinet.DataHub.Wholesale.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Energinet.DataHub.WebApi.Controllers
@@ -63,6 +63,40 @@ namespace Energinet.DataHub.WebApi.Controllers
         {
             var stream = await _client.GetZippedBasisDataStreamAsync(batchId);
             return File(stream, MediaTypeNames.Application.Zip);
+        }
+
+        /// <summary>
+        /// Get a process.
+        /// </summary>
+        [HttpGet("Process")]
+        public async Task<ActionResult> GetAsync(Guid batchId, GridAreaDto gridAreaDto)
+        {
+            var process = new ProcessDto(
+                BatchState.Completed,
+                ProcessType.BalanceFixing,
+                new GridAreaDto("123"),
+                new ProcessStepDto[]
+                {
+                    new(25),
+                });
+            return await Task.FromResult<ActionResult>(Ok(process));
+        }
+
+        /// <summary>
+        /// Get a processStepResult.
+        /// </summary>
+        [HttpGet("ProcessStepResult")]
+        public async Task<ActionResult> GetAsync(Guid batchId, GridAreaDto gridAreaDto, int stepNumber)
+        {
+            var processStepResult = new ProcessStepResultDto(
+                MeteringPointType.Production,
+                new DateTimeOffset(DateTime.Now),
+                new DateTimeOffset(DateTime.Now),
+                new decimal(1.1),
+                new decimal(1.1),
+                new decimal(1.1),
+                new TimeSeriesPointDto[] { new TimeSeriesPointDto(new DateTimeOffset(DateTime.Now), new decimal(1.1)) });
+            return await Task.FromResult<ActionResult>(Ok(processStepResult));
         }
     }
 }
