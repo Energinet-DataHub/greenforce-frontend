69fbbc5654f711322a43c52c0497acdc8d77203d
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2022-12-13 08:17:04 +0000

    Add size type

--- libs/ui-watt/src/lib/components/badge/badge.component.ts
+++ libs/ui-watt/src/lib/components/badge/badge.component.ts
@@ -17,6 +17,7 @@
 import { Component, HostBinding, Input } from '@angular/core';
 
 export type WattBadgeType = 'warning' | 'success' | 'danger' | 'info';
+export type WattBadgeSize = 'normal' | 'large';
 
 /**
  * Usage:
@@ -29,7 +30,7 @@ export type WattBadgeType = 'warning' | 'success' | 'danger' | 'info';
 })
 export class WattBadgeComponent {
   @Input() type: WattBadgeType = 'info';
-  @Input() size = 'normal';
+  @Input() size: WattBadgeSize = 'normal';
 
   @HostBinding('class')
   get badgeType() {
