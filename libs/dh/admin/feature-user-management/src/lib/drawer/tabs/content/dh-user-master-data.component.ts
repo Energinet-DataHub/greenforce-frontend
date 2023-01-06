8fc292f710abadbc2a9e9ca8349a8bf2290b8150
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-01-06 08:23:24 +0100

    Feat/884 drawer user roles (#1235)
    * Added drawer with a tab component, and some basic user data
    
    * Removed unused loco
    
    * Dynamic header name
    
    * chore: add license
    
    * Missing translations
    
    * Removed unused import
    
    * feat(dh): minor fixes
    
    * feat(dh): move user status badge closer to user name
    
    * feat(dh): add styling on row hover
    
    * feat(dh): move user status translations to higher level
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dzhavat Ushev <dzu@energinet.dk>

---
+++ libs/dh/admin/feature-user-management/src/lib/drawer/tabs/content/dh-user-master-data.component.ts
@@ -0,0 +1,53 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component, Input } from '@angular/core';
+import { TranslocoModule } from '@ngneat/transloco';
+
+import { WattCardModule } from '@energinet-datahub/watt/card';
+import { UserOverviewItemDto } from '@energinet-datahub/dh/shared/domain';
+
+@Component({
+  selector: 'dh-user-master-data',
+  standalone: true,
+  templateUrl: './dh-user-master-data.component.html',
+  styles: [
+    `
+      .master-data-list {
+        padding: 0;
+        margin: 0;
+
+        ul {
+          list-style: none;
+        }
+
+        li {
+          display: grid;
+          margin-top: var(--watt-space-s);
+          grid-template-columns: 20% 1fr;
+        }
+
+        li:not(:last-child) {
+          border-bottom: 1px solid var(--watt-color-neutral-grey-300);
+        }
+      }
+    `,
+  ],
+  imports: [WattCardModule, TranslocoModule],
+})
+export class DhUserMasterDataComponent {
+  @Input() user: UserOverviewItemDto | null = null;
+}
