bd14765cf01e18e2385e9a967d732f15a6cf1362
Jesper Justesen <juj@energinet.dk>
2023-01-09 14:55:25 +0100

    Added initial files for create page

---
+++ libs/dh/admin/feature-user-management/src/lib/feature-create-userrole/tabs/create-role-masterdata-tab/dh-create-userrole-masterdata-tab.component.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageEvent } from '@angular/material/paginator';
+import { TranslocoModule } from '@ngneat/transloco';
+
+import { WattCardModule } from '@energinet-datahub/watt/card';
+
+@Component({
+  selector: 'dh-create-userrole-masterdata-tab',
+  templateUrl: './dh-create-userrole-masterdata-tab.component.html',
+  styleUrls: ['./dh-create-userrole-masterdata-tab.component.scss'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    TranslocoModule,
+    WattCardModule
+  ],
+})
+export class DhCreateUserroleMasterdataTabComponent {
+  @Output() roleCreated = new EventEmitter<PageEvent>();
+}
