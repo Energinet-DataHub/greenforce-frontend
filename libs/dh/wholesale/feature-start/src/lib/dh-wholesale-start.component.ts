590fa269588ab53c3c74b4f66423167f7428d9dc
frontend-specialisten <alexander@frontend-specialisten.dk>
2023-01-10 07:03:09 +0100

    Add toast when starting new batch (#1287)
    * feat(wholesale): add toast notification when creating a batch
    
    * style: format
    
    * test(wholesale): fix failing spec for start batch
    
    * refactor(wholesale): order imports
    
    * style: format
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/dh/wholesale/feature-start/src/lib/dh-wholesale-start.component.ts
+++ libs/dh/wholesale/feature-start/src/lib/dh-wholesale-start.component.ts
@@ -14,8 +14,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TranslocoModule } from '@ngneat/transloco';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { first, Subject, takeUntil } from 'rxjs';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PushModule } from '@rx-angular/template';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
+
 import { WattFormFieldModule } from '@energinet-datahub/watt/form-field';
 import { WattRangeValidators } from '@energinet-datahub/watt/validators';
 import { WattDatepickerModule } from '@energinet-datahub/watt/datepicker';
@@ -24,11 +40,19 @@ import {
   WattDropdownOption,
 } from '@energinet-datahub/watt/dropdown';
 import { WattButtonModule } from '@energinet-datahub/watt/button';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { WattToastService } from '@energinet-datahub/watt/toast';
 
 import { DhWholesaleBatchDataAccessApiStore } from '@energinet-datahub/dh/wholesale/data-access-api';
 import { DhFeatureFlagDirectiveModule } from '@energinet-datahub/dh/shared/feature-flags';
 import { CommonModule } from '@angular/common';
+
+interface CreateBatchFormValues {
+  gridAreas: FormControl<string[] | null>;
+  dateRange: FormControl<{
+    start: string;
+    end: string;
+  } | null>;
+}
 
 @Component({
   selector: 'dh-wholesale-start',
@@ -38,51 +62,101 @@ import { CommonModule } from '@angular/common';
   providers: [DhWholesaleBatchDataAccessApiStore],
   standalone: true,
   imports: [
+    CommonModule,
+    DhFeatureFlagDirectiveModule,
+    PushModule,
+    ReactiveFormsModule,
+    TranslocoModule,
     WattButtonModule,
     WattDatepickerModule,
-    WattFormFieldModule,
-    TranslocoModule,
-    DhFeatureFlagDirectiveModule,
-    ReactiveFormsModule,
-    CommonModule,
     WattDropdownModule,
+    WattFormFieldModule,
   ],
 })
-export class DhWholesaleStartComponent {
-  constructor(private store: DhWholesaleBatchDataAccessApiStore) {}
+export class DhWholesaleStartComponent implements OnInit, OnDestroy {
+  private store = inject(DhWholesaleBatchDataAccessApiStore);
+  private toast = inject(WattToastService);
+  private transloco = inject(TranslocoService);
+  private router = inject(Router);
 
-  optionsGridAreas: WattDropdownOption[] = Object.keys({
-    805: '805',
-    806: '806',
-    512: '512',
-    533: '533',
-    543: '543',
-    584: '584',
-    351: '351',
-  }).map((key) => ({
-    displayValue: key,
-    value: key,
+  private destroy$ = new Subject<void>();
+
+  loadingCreatingBatch$ = this.store.loadingCreatingBatch$;
+
+  createBatchForm = new FormGroup<CreateBatchFormValues>({
+    gridAreas: new FormControl(null, { validators: Validators.required }),
+    dateRange: new FormControl(null, {
+      validators: WattRangeValidators.required(),
+    }),
+  });
+
+  optionsGridAreas: WattDropdownOption[] = [
+    '351',
+    '512',
+    '533',
+    '543',
+    '584',
+    '805',
+    '806',
+  ].map((gridAreaCode) => ({
+    displayValue: gridAreaCode,
+    value: gridAreaCode,
   }));
 
-  formControlGridArea = new FormControl<string[] | null>(
-    null,
-    Validators.required
-  );
+  ngOnInit(): void {
+    // Close toast on navigation
+    this.router.events
+      .pipe(first((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.toast.dismiss();
+      });
 
-  formControlRange = new FormControl<{ start: string; end: string } | null>(
-    null,
-    [WattRangeValidators.required()]
-  );
+    this.store.creatingBatchSuccessTrigger$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.onBatchCreatedSuccess());
+
+    this.store.creatingBatchErrorTrigger$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.onBatchCreatedError());
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   createBatch() {
+    const { gridAreas, dateRange } = this.createBatchForm.getRawValue();
     if (
-      this.formControlRange.value != null &&
-      this.formControlGridArea.value != null
-    ) {
-      this.store.createBatch({
-        gridAreas: this.formControlGridArea.value,
-        dateRange: this.formControlRange.value,
-      });
-    }
+      this.createBatchForm.invalid ||
+      gridAreas === null ||
+      dateRange === null
+    )
+      return;
+
+    this.store.createBatch({ gridAreas, dateRange });
+
+    this.toast.open({
+      type: 'loading',
+      message: this.transloco.translate('wholesale.startBatch.creatingBatch'),
+    });
+  }
+
+  private onBatchCreatedSuccess() {
+    this.toast.update({
+      type: 'success',
+      message: this.transloco.translate(
+        'wholesale.startBatch.creatingBatchSuccess'
+      ),
+    });
+  }
+
+  private onBatchCreatedError() {
+    this.toast.update({
+      type: 'danger',
+      message: this.transloco.translate(
+        'wholesale.startBatch.creatingBatchError'
+      ),
+    });
   }
 }
