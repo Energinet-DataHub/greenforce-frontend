c41b765ec59e0a40fbe5a8205bad139f8add9cbb
Dzhavat Ushev <dzu@energinet.dk>
2023-01-16 13:22:34 +0100

    fix(dh): remove custom data source

--- libs/dh/admin/data-access-api/src/index.ts
+++ libs/dh/admin/data-access-api/src/index.ts
@@ -18,4 +18,3 @@ export * from './lib/dh-admin-user-management-data-access-api.store';
 export * from './lib/dh-admin-user-management-audit-logs-data-access-api.store';
 export * from './lib/dh-admin-user-roles-management-data-access-api.store';
 export * from './lib/dh-admin-user-role-permissions-management-data-access-api.store';
-export * from './lib/dh-custom-data-source';
