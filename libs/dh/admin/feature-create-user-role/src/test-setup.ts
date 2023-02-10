b94138491e828707eba9b3f53b97f83a6e16d051
Dzhavat Ushev <dzu@energinet.dk>
2023-02-10 14:18:06 +0100

    refactor(dh): move create user role to new library (#1409)
    * refactor(dh): move create user role to new library
    
    * chore: add license
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/dh/admin/feature-create-user-role/src/test-setup.ts
+++ libs/dh/admin/feature-create-user-role/src/test-setup.ts
@@ -14,4 +14,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './lib/dh-admin-feature-user-management.component';
+import 'jest-preset-angular/setup-jest';
+
+import {
+  setUpTestbed,
+  setUpAngularTestingLibrary,
+} from '@energinet-datahub/gf/test-util-staging';
+import { addDomMatchers } from '@energinet-datahub/gf/test-util-matchers';
+import { setupMSW } from '@energinet-datahub/dh/shared/test-util-msw';
+
+setupMSW();
+addDomMatchers();
+setUpTestbed();
+setUpAngularTestingLibrary();
