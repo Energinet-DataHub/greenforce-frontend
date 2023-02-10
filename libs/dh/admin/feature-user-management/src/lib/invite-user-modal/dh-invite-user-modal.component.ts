38c2363886317230a4e9f2fc34a87c97e2ac94fb
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-02-10 12:37:55 +0100

    Step component with dummy forms

--- libs/dh/admin/feature-user-management/src/lib/invite-user-modal/dh-invite-user-modal.component.ts
+++ libs/dh/admin/feature-user-management/src/lib/invite-user-modal/dh-invite-user-modal.component.ts
@@ -15,22 +15,47 @@
  * limitations under the License.
  */
 
-import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  inject,
+  Output,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import {
   WattModalComponent,
   WattModalModule,
 } from '@energinet-datahub/watt/modal';
 
+import { CommonModule } from '@angular/common';
 import { WattButtonModule } from '@energinet-datahub/watt/button';
-import { MatStepperModule } from '@angular/material/stepper';
+import { MatStepper, MatStepperModule } from '@angular/material/stepper';
 import { TranslocoModule } from '@ngneat/transloco';
 import { MatDividerModule } from '@angular/material/divider';
+import { WattIconModule } from '@energinet-datahub/watt/icon';
+import { WattInputModule } from '@energinet-datahub/watt/input';
+import { WattFormFieldModule } from '@energinet-datahub/watt/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import {
   WattStepperButtonNextDirective,
   WattStepperButtonPreviousDirective,
 } from '@energinet-datahub/watt/stepper';
+import { MatInputModule } from '@angular/material/input';
+import {
+  StepperSelectionEvent,
+  STEPPER_GLOBAL_OPTIONS,
+} from '@angular/cdk/stepper';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 @Component({
   encapsulation: ViewEncapsulation.None,
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
   selector: 'dh-invite-user-modal',
   templateUrl: './dh-invite-user-modal.component.html',
   styleUrls: ['./dh-invite-user-modal.component.scss'],
@@ -43,11 +68,41 @@ import {
     WattStepperButtonPreviousDirective,
     TranslocoModule,
     MatDividerModule,
+    WattIconModule,
+    CommonModule,
+    ReactiveFormsModule,
+    WattInputModule,
+    WattFormFieldModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
 })
-export class DhInviteUserModalComponent {
+export class DhInviteUserModalComponent implements AfterViewInit {
+  private readonly formBuilder = inject(FormBuilder);
   @ViewChild('inviteUserModal') inviteUserModal!: WattModalComponent;
-  open() {
+  @ViewChild('stepper') stepper!: MatStepper;
+  @Output() closed = new EventEmitter<void>();
+  userInfo = this.formBuilder.group({
+    name: ['', Validators.required],
+  });
+  userRoles = this.formBuilder.group({
+    userRole: ['', Validators.required],
+  });
+
+  ngAfterViewInit(): void {
     this.inviteUserModal.open();
+  }
+
+  inviteUser() {
+    if (this.userInfo.valid && this.userRoles.valid) {
+      this.inviteUserModal.close(true);
+    } else {
+      this.userRoles.markAllAsTouched();
+    }
+  }
+
+  closeModal(status: boolean) {
+    this.closed.emit();
+    this.inviteUserModal.close(status);
   }
 }
