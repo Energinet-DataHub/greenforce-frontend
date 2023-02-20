48034b7be554955a00780d94c1359d7ff30231e7
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-02-20 13:23:12 +0100

    Update libs/dh/admin/feature-invite-user-modal/src/test-setup.ts
    Co-authored-by: Dzhavat Ushev <dzu@energinet.dk>

--- libs/dh/admin/feature-invite-user-modal/src/test-setup.ts
+++ libs/dh/admin/feature-invite-user-modal/src/test-setup.ts
@@ -15,3 +15,15 @@
  * limitations under the License.
  */
 import 'jest-preset-angular/setup-jest';
+
+import {
+  setUpTestbed,
+  setUpAngularTestingLibrary,
+} from '@energinet-datahub/gf/test-util-staging';
+import { addDomMatchers } from '@energinet-datahub/gf/test-util-matchers';
+import { setupMSW } from '@energinet-datahub/dh/shared/test-util-msw';
+
+setupMSW();
+addDomMatchers();
+setUpTestbed();
+setUpAngularTestingLibrary();
