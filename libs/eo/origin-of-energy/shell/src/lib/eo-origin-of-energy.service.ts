e2e7eba9c40b3b2d2664da1c7a9baea8be8deeda
Mads Heimdal Thy <mads@heimdal-thy.dk>
2023-02-23 15:35:53 +0100

    Add tz to all relevant calls

--- libs/eo/origin-of-energy/shell/src/lib/eo-origin-of-energy.service.ts
+++ libs/eo/origin-of-energy/shell/src/lib/eo-origin-of-energy.service.ts
@@ -16,10 +16,8 @@
  */
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import {
-  EoApiEnvironment,
-  eoApiEnvironmentToken,
-} from '@energinet-datahub/eo/shared/environments';
+import { EoApiEnvironment, eoApiEnvironmentToken } from '@energinet-datahub/eo/shared/environments';
+import { CalendarDateRange } from '@energinet-datahub/eo/shared/services';
 
 export interface EoOriginOfEnergyResponse {
   energySources: [
@@ -50,10 +48,16 @@ export class EoOriginOfEnergyService {
   #apiBase: string;
 
   getSourcesFor2021() {
+    const dateRange: CalendarDateRange = {
+      start: new Date('2021-01-01').getTime(),
+      end: new Date('2021-01-01').getTime(),
+    };
+    const encodedTimeZone = encodeURIComponent(Intl.DateTimeFormat().resolvedOptions().timeZone);
+
     return this.http.get<EoOriginOfEnergyResponse>(
-      `${
-        this.#apiBase
-      }/sources?dateFrom=1609459200&dateTo=1640995199&aggregation=Total`,
+      `${this.#apiBase}/sources?dateFrom=${dateRange.start}&dateTo=${
+        dateRange.end
+      }&timeZone=${encodedTimeZone}&aggregation=Total`,
       { withCredentials: true }
     );
   }
