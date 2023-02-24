980b55a5b4d24eaf8ddaac9a3b8c3a7fe628616c
Mads Heimdal Thy <mads@heimdal-thy.dk>
2023-02-24 18:53:28 +0100

    Next small batch of fixes

--- libs/dh/message-archive/feature-log-search/src/lib/shared/findLogname.ts
+++ libs/dh/message-archive/feature-log-search/src/lib/shared/findLogname.ts
@@ -14,18 +14,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const regexLogNameWithDateFolder = new RegExp(
-  /\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])\/.*/
-);
-const regexLogNameIsSingleGuid = new RegExp(
-  /[\da-zA-Z]{8}-([\da-zA-Z]{4}-){3}[\da-zA-Z]{12}$/
-);
+const regexLogNameWithDateFolder = new RegExp(/\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])\/.*/);
+const regexLogNameIsSingleGuid = new RegExp(/[\da-zA-Z]{8}-([\da-zA-Z]{4}-){3}[\da-zA-Z]{12}$/);
 
 export function findLogName(logUrl: string): string {
   if (regexLogNameWithDateFolder.test(logUrl)) {
     const match = regexLogNameWithDateFolder.exec(logUrl);
     return match != null ? match[0] : '';
-  } else if (regexLogNameIsSingleGuid.test(logUrl)) {
+  }
+  if (regexLogNameIsSingleGuid.test(logUrl)) {
     const match = regexLogNameIsSingleGuid.exec(logUrl);
     return match != null ? match[0] : '';
   }
