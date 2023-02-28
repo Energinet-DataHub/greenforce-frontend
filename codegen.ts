d7db597da73e5c1f4532f5196b2614bf7075ce92
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2023-02-28 11:34:09 +0000

    Fix linting

--- codegen.ts
+++ codegen.ts
@@ -6,7 +6,13 @@ const config: CodegenConfig = {
   documents: 'libs/**/*.graphql',
   generates: {
     'libs/dh/shared/domain/src/lib/graphql.ts': {
-      plugins: ['typescript', 'typescript-operations', 'typed-document-node', 'typescript-msw'],
+      plugins: [
+        { add: { content: '/* eslint-disable */' } },
+        'typescript',
+        'typescript-operations',
+        'typed-document-node',
+        'typescript-msw',
+      ],
     },
     './graphql.schema.json': {
       plugins: ['introspection'],
