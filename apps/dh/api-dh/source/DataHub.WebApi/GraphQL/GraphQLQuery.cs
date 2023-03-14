93555743681a64b71725200a58546f21c62da3b2
Karsten M <xnetsrak@gmail.com>
2023-03-14 07:49:43 +0100

    adding graphql permissionauditlogs (#1515)
    * adding graphql for permission audit logs
    
    * adding permission audit logs with changed user name
    
    ---------
    
    Co-authored-by: Jesper Justesen <juj@energinet.dk>

--- apps/dh/api-dh/source/DataHub.WebApi/GraphQL/GraphQLQuery.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/GraphQL/GraphQLQuery.cs
@@ -85,6 +85,40 @@ namespace Energinet.DataHub.WebApi.GraphQL
                 .WithService<IMarketParticipantUserRoleClient>()
                 .ResolveAsync(async (context, client) => await client.GetAsync(context.GetArgument<Guid>("id")));
 
+            Field<NonNullGraphType<ListGraphType<NonNullGraphType<PermissionAuditLogDtoType>>>>("permissionlogs")
+                .Argument<IdGraphType>("id", "The id of the permission")
+                .Resolve()
+                .WithScope()
+                .WithService<IMarketParticipantPermissionsClient>()
+                .WithService<IMarketParticipantClient>()
+                .ResolveAsync(async (context, permissionClient, userClient) =>
+                {
+                    var auditLogs = await permissionClient.GetAuditLogsAsync(context.GetArgument<int>("id"));
+                    var userLookup = new Dictionary<Guid, UserDto>();
+                    var auditLogsViewDtos = new List<PermissionAuditLogViewDto>();
+
+                    foreach (var log in auditLogs)
+                    {
+                        var userFoundInCache = userLookup.ContainsKey(log.ChangedByUserId);
+                        if (!userFoundInCache)
+                        {
+                            var user = await userClient.GetUserAsync(log.ChangedByUserId);
+                            userLookup.TryAdd(log.ChangedByUserId, user);
+                        }
+
+                        userLookup.TryGetValue(log.ChangedByUserId, out var userCache);
+
+                        auditLogsViewDtos.Add(new PermissionAuditLogViewDto(
+                            log.PermissionId,
+                            log.ChangedByUserId,
+                            userCache?.Name ?? throw new KeyNotFoundException("User not found"),
+                            log.PermissionChangeType == PermissionChangeType.DescriptionChange ? PermissionAuditLogType.DescriptionChange : PermissionAuditLogType.Unknown,
+                            log.Timestamp));
+                    }
+
+                    return auditLogsViewDtos;
+                });
+
             Field<ListGraphType<OrganizationDtoType>>("organizations")
                 .Resolve()
                 .WithScope()
