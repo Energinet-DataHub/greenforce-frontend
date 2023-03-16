3b2615bfb921407e4be0b867cd64c487e6e7a870
Dzhavat Ushev <dzu@energinet.dk>
2023-03-16 10:55:34 +0100

    feat(dh): generate edit permission modal library

---
+++ libs/dh/admin/feature-edit-permission-modal/src/test-setup.ts
@@ -0,0 +1,10 @@
+import 'jest-preset-angular/setup-jest';
+
+import { setUpTestbed, setUpAngularTestingLibrary } from '@energinet-datahub/gf/test-util-staging';
+import { addDomMatchers } from '@energinet-datahub/gf/test-util-matchers';
+import { setupMSW } from '@energinet-datahub/dh/shared/test-util-msw';
+
+setupMSW();
+addDomMatchers();
+setUpTestbed();
+setUpAngularTestingLibrary();
