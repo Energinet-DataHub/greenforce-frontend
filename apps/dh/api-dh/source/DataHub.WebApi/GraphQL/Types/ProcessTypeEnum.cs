d1e9528a55a1817695eb84c89579e1cdcd1a0402
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2023-03-17 09:23:08 +0100

    Rollback nswag (#1538)

--- apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Types/ProcessTypeEnum.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Types/ProcessTypeEnum.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.WebApi.Clients.Wholesale.v3;
+using Energinet.DataHub.Wholesale.Contracts;
 using GraphQL.Types;
 
 namespace Energinet.DataHub.WebApi.GraphQL
