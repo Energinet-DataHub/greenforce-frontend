6460d8a0e813e2747e5ffc06ff2d89aef230f5d9
Jesper Justesen <juj@energinet.dk>
2023-03-27 13:25:39 +0200

    Add more data to Permission Document

--- libs/dh/shared/data-access-msw/src/lib/mocks/data/admin-get-permissions.ts
+++ libs/dh/shared/data-access-msw/src/lib/mocks/data/admin-get-permissions.ts
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 import { graphql } from '@energinet-datahub/dh/shared/domain';
+import { EicFunction, UserRoleStatus } from 'libs/dh/shared/domain/src/lib/generated/graphql';
 
 export const adminPermissionsMock: graphql.GetPermissionsQuery = {
   permissions: [
@@ -22,11 +23,39 @@ export const adminPermissionsMock: graphql.GetPermissionsQuery = {
       id: 1,
       name: 'organizations:view',
       description: 'Description for OrganizationView',
+      created: '2023-03-07T00:00:00+00:00',
+      assignableTo: [
+        EicFunction.BalanceResponsibleParty,
+        EicFunction.BillingAgent,
+      ],
+      userRoles: [
+        {
+          id: "2ca09c29-ffc9-4155-d925-08db05f27124",
+          name: "Test role 1",
+          description: "Description 1",
+          eicFunction: EicFunction.BillingAgent,
+          status: UserRoleStatus.Active
+        }
+      ]
     },
     {
       id: 2,
       name: 'organizations:manage',
       description: 'Description for OrganizationManage',
+      created: '2023-03-07T00:00:00+00:00',
+      assignableTo: [
+        EicFunction.BalanceResponsibleParty,
+        EicFunction.BillingAgent,
+      ],
+      userRoles: [
+        {
+          id: "4fa09c29-ffc9-4155-d925-08db05f27124",
+          name: "Test role 2",
+          description: "Description 2",
+          eicFunction: EicFunction.BillingAgent,
+          status: UserRoleStatus.Active
+        }
+      ]
     },
   ],
 };
