56bb587cc3161007e412a5c0f500806f46422b4e
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-03-27 14:10:21 +0200

    one step closer

--- fluxior/apps/dh/src/hooks.server.ts
+++ fluxior/apps/dh/src/hooks.server.ts
@@ -10,13 +10,13 @@ export const handle = SvelteKitAuth({
     AzureADB2C({
       // issuer: 'https://devdatahubb2c.b2clogin.com/4a7411ea-ac71-4b63-9647-b8bd4c5a20e0/v2.0/',
       tenantId: 'devdatahubb2c',
-      clientId: 'd91c10bb-1441-4ae5-9bf9-e6845567d018',
-      clientSecret: 'k_58Q~LmGFODpq2PRubGk_nk5EjwZT7-OrMrbalx',
-      primaryUserFlow: 'b2c_1_u001_signin',
+      clientId: '1982d7c0-9b07-4c41-85ac-476967b10101',
+      clientSecret: 'Pzu8Q~Rg5fnk.~mOyzsKbPLWyA6BuMPLesW7IaDD',
+      primaryUserFlow: 'b2c_1_u001_signin'
       // wellKnown: 'https://devdatahubb2c.b2clogin.com/4a7411ea-ac71-4b63-9647-b8bd4c5a20e0/B2C_1_u001_signin/v2.0/.well-known/openid-configuration',
-      authorization: {
-        params: { scope: 'd91c10bb-1441-4ae5-9bf9-e6845567d018 openid profile offline_access' }
-      }
+      // authorization: {
+      //   params: { scope: '1982d7c0-9b07-4c41-85ac-476967b10101 openid profile offline_access' }
+      // }
       // checks: ['pkce', 'state'],
       // client: { token_endpoint_auth_method: 'none' }
     }) as Provider
