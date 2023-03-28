d33c984774afa2c37026db9df772e1d62636984f
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-03-28 14:46:39 +0200

    Get a single detailed permission

--- libs/dh/admin/feature-permissions/src/lib/details/tabs/dh-admin-permission-roles.component.ts
+++ libs/dh/admin/feature-permissions/src/lib/details/tabs/dh-admin-permission-roles.component.ts
@@ -1,15 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { WattSpinnerModule } from '@energinet-datahub/watt/spinner';
 import { WattCardModule } from '@energinet-datahub/watt/card';
 import { WattTableColumnDef, WattTableDataSource, WATT_TABLE } from '@energinet-datahub/watt/table';
 import { WattEmptyStateModule } from '@energinet-datahub/watt/empty-state';
-import { TranslocoModule } from '@ngneat/transloco';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
 import { PermissionDto } from '@energinet-datahub/dh/shared/domain';
-import { Apollo } from 'apollo-angular';
+import { Apollo, QueryRef } from 'apollo-angular';
 import { Subscription } from 'rxjs';
 import { ApolloError } from '@apollo/client';
 import { graphql } from '@energinet-datahub/dh/shared/domain';
+import { LetModule } from '@rx-angular/template/let';
+import type { ResultOf } from '@graphql-typed-document-node/core';
+
+type UserRole = ResultOf<
+  typeof graphql.GetPermissionDetailsDocument
+>['permission']['userRoles'][number];
 
 @Component({
   selector: 'dh-admin-permission-roles',
@@ -23,19 +37,70 @@ import { graphql } from '@energinet-datahub/dh/shared/domain';
     WATT_TABLE,
     WattEmptyStateModule,
     TranslocoModule,
+    LetModule,
   ],
 })
-export class DhAdminPermissionRolesComponent implements OnInit {
+export class DhAdminPermissionRolesComponent implements OnInit, OnChanges, OnDestroy {
   @Input() selectedPermission: PermissionDto | null = null;
   private apollo = inject(Apollo);
+  private trans: TranslocoService = inject(TranslocoService);
 
-  dataSource = new WattTableDataSource<graphql.UserRole>();
+  subscription!: Subscription;
+  userRoles?: UserRole[];
+  loading = false;
+  error?: ApolloError;
 
-  columns: WattTableColumnDef<graphql.UserRole> = {
+  dataSource = new WattTableDataSource<UserRole>();
+
+  columns: WattTableColumnDef<UserRole> = {
     name: { accessor: 'name' },
   };
 
+  private getPermissionQuery?: QueryRef<
+    graphql.GetPermissionDetailsQuery,
+    {
+      id: string;
+    }
+  >;
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.getPermissionQuery = this.apollo.watchQuery({
+      useInitialLoading: true,
+      notifyOnNetworkStatusChange: true,
+      query: graphql.GetPermissionDetailsDocument,
+      variables: { id: this.selectedPermission?.id.toString() ?? '' },
+    });
+
+    this.subscription = this.getPermissionQuery.valueChanges.subscribe({
+      next: (result) => {
+        this.userRoles = result.data?.permission?.userRoles ?? [];
+        this.loading = result.loading;
+        this.error = result.error;
+        this.dataSource.data = this.userRoles;
+      },
+      error: (error) => {
+        this.error = error;
+      },
+    });
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes.selectedPermission.firstChange === false &&
+      changes.selectedPermission.currentValue
+    ) {
+      const id = changes.selectedPermission.currentValue.id.toString();
+
+      this.getPermissionQuery?.refetch({ id });
+    }
+  }
+
+  translateHeader = (columnId: string): string => {
+    const baseKey = 'admin.userManagement.permissionDetail.tabs.userRoles.columns';
+    return this.trans.translate(`${baseKey}.${columnId}`);
+  };
 }
