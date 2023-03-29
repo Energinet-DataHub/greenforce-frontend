eef08cfe22a7390b8b4a1dfbf5a7d53d3a04d684
Alexander HafstadFrontend Speciliasten <alexander@frontend-specialisten.dk>
2023-03-29 10:17:31 +0200

    chore: [nx migration] update-configs-jest-29

--- libs/dh/market-participant/feature-edit-actor/jest.config.ts
+++ libs/dh/market-participant/feature-edit-actor/jest.config.ts
@@ -19,15 +19,16 @@ export default {
   displayName: 'dh-market-participant-feature-edit-actor',
   preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../../../coverage/libs/dh/market-participant/feature-edit-actor',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
