e4097bc2b876303a672b749cfa1ed89ba2ac9241
frontend-specialisten <alexander@frontend-specialisten.dk>
2023-03-31 08:40:01 +0200

    Update NX to latest version (15.8.9) (#1586)
    * chore: add migrations and update package.json
    
    * chore: [nx migration] update-configs-jest-29
    
    * chore: [nx migration] update-angular-cli-version-15-2-0
    
    * chore: update yarn.lock
    
    * chore: Update PRODUCTION_DEPENDENCIES.md
    
    * Chore: bump version to 0.3.210 for build/infrastructure/eo/chart/Chart.yaml
    
    * Chore: bump version to 0.3.210 for build/infrastructure/eo/chart/values.yaml
    
    * fix(eo): add timezone to fix spec
    
    * fix: specs for dh-admin-feature-user-management
    
    * fix(ui-watt): specs
    
    * fix(market-participant): specs
    
    * chore: fix yarn.lock
    
    * revert: change EO spec instead
    
    * Chore: bump version to 0.3.211 for build/infrastructure/eo/chart/values.yaml
    
    * Chore: bump version to 0.3.211 for build/infrastructure/eo/chart/Chart.yaml
    
    * chore: remove migrations.json
    
    * chore: update tsconfig.spec.json to ES2016
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/dh/wholesale/feature-calculation-steps/jest.config.ts
+++ libs/dh/wholesale/feature-calculation-steps/jest.config.ts
@@ -19,15 +19,16 @@ export default {
   displayName: 'dh-wholesale-feature-calculation-steps',
   preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../../../coverage/libs/dh/wholesale/feature-calculation-steps',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
