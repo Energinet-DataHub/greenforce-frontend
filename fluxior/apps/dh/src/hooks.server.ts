32f22b66c0016e12233ecdb5acf1204ad855752a
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-03-31 10:14:42 +0200

    Almost here

--- fluxior/apps/dh/src/hooks.server.ts
+++ fluxior/apps/dh/src/hooks.server.ts
@@ -8,14 +8,14 @@ export const handle = SvelteKitAuth({
   secret: env.secret,
   providers: [
     AzureADB2C({
-      // issuer: 'https://devdatahubb2c.b2clogin.com/4a7411ea-ac71-4b63-9647-b8bd4c5a20e0/v2.0/',
-      tenantId: 'devdatahubb2c',
-      clientId: '1982d7c0-9b07-4c41-85ac-476967b10101',
-      clientSecret: 'Pzu8Q~Rg5fnk.~mOyzsKbPLWyA6BuMPLesW7IaDD',
-      primaryUserFlow: 'b2c_1_u001_signin'
+      //issuer: 'https://fluxiororg.b2clogin.com/fluxiororg.onmicrosoft.com/v2.0',
+      tenantId: 'fluxiororg',
+      clientId: '2047be93-878e-4360-9b43-ca6e7beb9b5b',
+      clientSecret: 'mJK8Q~GcXlnw861P7PbEbApud8k~MaaywYENpcR~',
+      primaryUserFlow: 'B2C_1A_SIGNUP_SIGNIN'
       // wellKnown: 'https://devdatahubb2c.b2clogin.com/4a7411ea-ac71-4b63-9647-b8bd4c5a20e0/B2C_1_u001_signin/v2.0/.well-known/openid-configuration',
       // authorization: {
-      //   params: { scope: '1982d7c0-9b07-4c41-85ac-476967b10101 openid profile offline_access' }
+      //   params: { scope: 'openid profile offline_access' }
       // }
       // checks: ['pkce', 'state'],
       // client: { token_endpoint_auth_method: 'none' }
