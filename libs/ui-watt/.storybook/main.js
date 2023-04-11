2db4f6bd8aa25d06c76856b7162e596f0a46a92b
Alexander HafstadFrontend Speciliasten <alexander@frontend-specialisten.dk>
2023-04-11 10:55:40 +0200

    chore: run @nrwl/storybook:migrate-7

--- libs/ui-watt/.storybook/main.js
+++ libs/ui-watt/.storybook/main.js
@@ -1,16 +1,15 @@
 module.exports = {
-  core: { builder: 'webpack5' },
-  staticDirs: [{ from: '../src/assets', to: 'assets/ui-watt' }],
-  stories: [
-    '../src/lib/intro.stories.mdx',
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-pseudo-states',
-  ],
+  staticDirs: [{
+    from: '../src/assets',
+    to: 'assets/ui-watt'
+  }],
+  stories: ['../src/lib/intro.stories.mdx', '../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-a11y', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-addon-pseudo-states', "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: '@storybook/angular',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
