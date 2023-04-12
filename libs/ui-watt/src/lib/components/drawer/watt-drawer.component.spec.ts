82a385c43e685b6a4d3a119208e2de736d1bfbbf
Dzhavat Ushev <dzu@energinet.dk>
2023-04-12 15:47:53 +0200

    test(watt): make tests pass

--- libs/ui-watt/src/lib/components/drawer/watt-drawer.component.spec.ts
+++ libs/ui-watt/src/lib/components/drawer/watt-drawer.component.spec.ts
@@ -17,13 +17,15 @@
 import { render, screen, waitFor } from '@testing-library/angular';
 import { composeStories, createMountableStoryComponent } from '@storybook/testing-angular';
 import { EventEmitter } from '@angular/core';
-import { Story } from '@storybook/angular';
+import { StoryFn } from '@storybook/angular';
 import userEvent from '@testing-library/user-event';
 
 import { WattDrawerComponent } from './watt-drawer.component';
 import * as drawerStories from './+storybook/watt-drawer.stories';
 
-const { Normal: Drawer, Multiple, Loading } = composeStories(drawerStories);
+// TODO: Remove this when we have a better solution
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const { Normal: Drawer, Multiple, Loading } = composeStories(drawerStories as any) as any;
 
 describe(WattDrawerComponent.name, () => {
   // Queries
@@ -52,7 +54,7 @@ describe(WattDrawerComponent.name, () => {
 
   // Setup
   async function setup(
-    story: Story<Partial<WattDrawerComponent>>,
+    story: StoryFn<Partial<WattDrawerComponent>>,
     args?: Partial<WattDrawerComponent>
   ) {
     const { component, ngModule } = createMountableStoryComponent(
--- libs/ui-watt/src/lib/components/toast/watt-toast.component.spec.ts
+++ libs/ui-watt/src/lib/components/toast/watt-toast.component.spec.ts
@@ -20,7 +20,9 @@ import userEvent from '@testing-library/user-event';
 
 import * as toastStories from './+storybook/watt-toast.stories';
 
-const { Overview } = composeStories(toastStories);
+// TODO: Remove this when we have a better solution
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const { Overview } = composeStories(toastStories as any) as any;
 
 describe('Toast', () => {
   const getOpenToastButton = async () => screen.getByRole('button', { name: /Open toast/ });
