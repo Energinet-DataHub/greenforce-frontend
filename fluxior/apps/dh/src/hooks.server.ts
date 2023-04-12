0285112d1a48278d5bc6ba32b10b53b99aa2b490
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-04-12 12:59:06 +0200

    Fixed Ad login, and updated packages

--- fluxior/apps/dh/src/hooks.server.ts
+++ fluxior/apps/dh/src/hooks.server.ts
@@ -4,51 +4,22 @@ import { env } from '$env/dynamic/private';
 import type { Provider } from '@auth/core/providers';
 
 export const handle = SvelteKitAuth({
-  debug: true,
+  debug: false,
   secret: env.secret,
-  callbacks: {
-    async jwt({ token, account, profile, isNewUser, user }) {
-      console.log({ token, account, profile, isNewUser, user });
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken;
-      return session;
-    }
-  },
   providers: [
     AzureADB2C({
-      issuer:
-        'https://devdatahubb2c.b2clogin.com/devdatahubb2c.onmicrosoft.com/b2c_1_signinflow/v2.0',
+      issuer: 'https://devdatahubb2c.b2clogin.com/4a7411ea-ac71-4b63-9647-b8bd4c5a20e0/v2.0/',
       token:
         'https://devdatahubb2c.b2clogin.com/devdatahubb2c.onmicrosoft.com/b2c_1_signinflow/oauth2/v2.0/token',
       userinfo: 'https://graph.microsoft.com/oidc/userinfo',
       clientId: '1982d7c0-9b07-4c41-85ac-476967b10101',
       clientSecret: 'Pzu8Q~Rg5fnk.~mOyzsKbPLWyA6BuMPLesW7IaDD',
-      primaryUserFlow: 'b2c_1_signinflow',
       authorization: {
-        params: { scope: '1982d7c0-9b07-4c41-85ac-476967b10101 openid profile email' }
-      },
-      profile: (profile) => {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          image: null
-        };
+        url: 'https://devdatahubb2c.b2clogin.com/devdatahubb2c.onmicrosoft.com/b2c_1_signinflow/oauth2/v2.0/authorize',
+        params: {
+          scope: '1982d7c0-9b07-4c41-85ac-476967b10101 openid profile email'
+        }
       }
     }) as Provider
   ]
 });
-
-// tenantId: 'fluxiororg',
-//       clientId: '2047be93-878e-4360-9b43-ca6e7beb9b5b',
-//       clientSecret: 'mJK8Q~GcXlnw861P7PbEbApud8k~MaaywYENpcR~',
-//       primaryUserFlow: 'B2C_1A_SIGNUP_SIGNIN',
-//       authorization: {
-//         params: { scope: '2047be93-878e-4360-9b43-ca6e7beb9b5b openid profile email' }
-//       },
