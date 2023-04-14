aadfe487edf63d1ca678deeb3db21614e6111c36
Dzhavat Ushev <dzu@energinet.dk>
2023-04-14 08:01:47 +0200

    feat: include full package name in generated file

--- .github/actions/update-production-dependencies-list/update-production-dependencies-list.mjs
+++ .github/actions/update-production-dependencies-list/update-production-dependencies-list.mjs
@@ -18,9 +18,10 @@ function writeProductionDependencyLicenses(licenses) {
   `;
   const content = Object.entries(parsedLicenses)
     .map(([key, value]) => {
-      const splittedKey = key.split('@');
-      const name = splittedKey[splittedKey.length - 2];
-      const version = key.split('@')[splittedKey.length - 1];
+      const atSymbolLastIndex = key.lastIndexOf('@');
+
+      const name = key.slice(0, atSymbolLastIndex);
+      const version = key.slice(atSymbolLastIndex + 1);
 
       return `| [${name}](${value.repository}) | ${version} | ${value.licenses} |`;
     })
