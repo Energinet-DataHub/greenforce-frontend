5695627489ce197dbb5f81e0cdda7190f398aab8
Alexander HafstadFrontend Speciliasten <alexander@frontend-specialisten.dk>
2023-04-27 09:02:14 +0200

    initial commit

---
+++ libs/dh/wholesale/feature-start/src/lib/dh-wholesale-start.cy.ts
@@ -0,0 +1,39 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { WattDanishDatetimeModule } from '@energinet-datahub/watt/danish-date-time';
+
+import { WattToastModule } from '@energinet-datahub/watt/toast';
+import { DhApiModule } from '@energinet-datahub/dh/shared/data-access-api';
+import { DhGraphQLModule } from '@energinet-datahub/dh/shared/data-access-graphql';
+import {
+  DhConfigurationLocalizationModule,
+  DhTranslocoModule,
+} from '@energinet-datahub/dh/globalization/configuration-localization';
+
+import { DhWholesaleStartComponent } from './dh-wholesale-start.component';
+
+beforeEach(() => {
+  cy.request('/__cypress/src/assets/msw/mockServiceWorker.js').then((res) => {
+    cy.intercept('/mockServiceWorker.js', { headers: {'content-type': 'text/javascript'}, body: res.body });
+  });
+
+  cy.intercept('/assets/i18n/da.json', { hostname: 'localhost' }, (req) => {
+    req.redirect('/__cypress/src/assets/i18n/da.json');
+  });
+});
+
+
+it('mounts', () => {
+  cy.mount(DhWholesaleStartComponent, {
+    imports: [
+      BrowserAnimationsModule,
+      DhApiModule.forRoot(),
+      DhConfigurationLocalizationModule.forRoot(),
+      DhGraphQLModule.forRoot(),
+      DhTranslocoModule.forRoot(),
+      HttpClientModule,
+      WattDanishDatetimeModule.forRoot(),
+      WattToastModule.forRoot(),
+    ]
+  });
+});
