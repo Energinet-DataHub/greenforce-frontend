61d8c0644ef3ffd0710336ec44c1df80173a3036
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-05-04 15:19:21 +0200

    Fixed feedback

--- tools/github/create-pr.ts
+++ tools/github/create-pr.ts
@@ -15,38 +15,12 @@
  * limitations under the License.
  */
 import { exec } from 'child_process';
-import inquirer from 'inquirer';
 
-exec(`git branch`, (err, stdout, stderr) => {
+exec(`gh pr create --web`, (err, stdout) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
 
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'branch',
-        message: 'Make PR to:',
-        choices: [
-          'main',
-          new inquirer.Separator(),
-          ...stdout
-            .split('\n')
-            .map((branch) => branch.trim())
-            .filter((branch) => branch !== '' && branch !== 'main'),
-        ],
-      },
-    ])
-    .then((answers) => {
-      exec(`gh pr create --base "${answers.branch}" --web`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          process.exit(1);
-        }
-
-        console.log(stdout);
-      });
-    });
+  console.log(stdout);
 });
