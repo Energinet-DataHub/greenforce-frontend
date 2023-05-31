24cf22e82332d31879ed6cd772a0ce76349544e7
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2023-05-31 17:13:29 +0200

    Add new watt-date-chip

---
+++ libs/ui-watt/src/lib/components/input/datepicker/watt-date-chip.component.ts
@@ -0,0 +1,68 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { WattDatePipe } from '../../../configuration/watt-date.pipe';
+import { WattIconModule } from '../../../foundations/icon/icon.module';
+import { WattMenuChipComponent } from '../../chip/watt-menu-chip.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, MatDatepickerModule, WattMenuChipComponent, WattDatePipe, WattIconModule],
+  selector: 'watt-date-chip',
+  styles: [
+    `
+      input {
+        top: 0;
+        bottom: 0;
+        height: auto;
+        pointer-events: none;
+      }
+    `,
+  ],
+  template: `
+    <mat-datepicker #picker />
+    <watt-menu-chip
+      role="dialog"
+      [disabled]="disabled"
+      [selected]="!!value"
+      [opened]="picker.opened"
+      (toggle)="picker.open()"
+    >
+      <input
+        tabindex="-1"
+        class="cdk-visually-hidden"
+        type="date"
+        [value]="value"
+        [matDatepicker]="picker"
+        (dateChange)="value = $event.value"
+        (dateChange)="selectionChange.emit($event.value)"
+      />
+      <ng-content *ngIf="!value" />
+      <ng-container *ngIf="value">{{ value | wattDate }}</ng-container>
+    </watt-menu-chip>
+  `,
+})
+export class WattDateChipComponent {
+  @Input() disabled = false;
+  @Input() label?: string;
+  @Input() value?: string;
+  @Output() selectionChange = new EventEmitter<string>();
+}
