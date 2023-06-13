9261c54946ec35346539c3effa813348f394b1dc
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-06-13 11:40:55 +0200

    Fixed wrong import

--- libs/eo/core/shell/src/lib/eo-core-shell.providers.ts
+++ libs/eo/core/shell/src/lib/eo-core-shell.providers.ts
@@ -16,7 +16,7 @@
  */
 import { browserConfigurationProviders } from '@energinet-datahub/gf/util-browser';
 import { eoAuthorizationInterceptorProvider } from '@energinet-datahub/eo/shared/services';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
 import { importProvidersFrom } from '@angular/core';
 
 export const eoCoreShellProviders = [
