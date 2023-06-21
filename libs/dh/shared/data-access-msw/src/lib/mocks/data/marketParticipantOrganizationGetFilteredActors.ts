7dc9b88a7995999c03c2bdf4d272f0ae54bce1dd
Dzhavat Ushev <dzu@energinet.dk>
2023-06-21 11:08:46 +0200

    chore(dh): mock request

---
+++ libs/dh/shared/data-access-msw/src/lib/mocks/data/marketParticipantOrganizationGetFilteredActors.ts
@@ -0,0 +1,15 @@
+import { MarketParticipantFilteredActorDto } from '@energinet-datahub/dh/shared/domain';
+
+export const marketParticipantOrganizationGetFilteredActors: MarketParticipantFilteredActorDto[] = [
+  {
+    actorId: '00000000-0000-0000-0000-000000000001',
+    actorNumber: {
+      value: '5790001330583',
+    },
+    name: {
+      value: 'Energinet DataHub A/S (DataHub systemadministrator)',
+    },
+    marketRoles: ['DataHubAdministrator'],
+    gridAreaCodes: [],
+  },
+];
