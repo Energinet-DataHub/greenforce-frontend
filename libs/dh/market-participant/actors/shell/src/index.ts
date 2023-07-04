73172dcc3f82001bd4e44e3cf050844c65b609db
Dzhavat Ushev <dzu@energinet.dk>
2023-07-04 17:17:09 +0200

    feat(dh): generate actors shell library

---
+++ libs/dh/market-participant/actors/shell/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/dh-market-participant-actors-shell.component';
---
+++ libs/dh/market-participant/actors/shell/src/lib/dh-market-participant-actors-shell.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'dh-market-participant-actors-shell',
+  standalone: true,
+  template: `DhMarketParticipantActorsShellComponent`,
+  imports: [CommonModule],
+})
+export class DhMarketParticipantActorsShellComponent {}
