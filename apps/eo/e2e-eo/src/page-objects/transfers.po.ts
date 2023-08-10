3c83b971bb100ad10db850055e1deec1b06581d5
sahma19 <saiuan@msn.com>
2023-08-10 17:09:48 +0200

    Adjusted e2e test

--- apps/eo/e2e-eo/src/page-objects/transfers.po.ts
+++ apps/eo/e2e-eo/src/page-objects/transfers.po.ts
@@ -26,6 +26,8 @@ export class TransfersPo {
   private newAgreementDateRangeStartInput = '[data-testid="new-agreement-start-date-input"]';
   private newAgreementDateRangeEndInput = '[data-testid="new-agreement-end-date-input"]';
   private newAgreementReceiverInput = '[data-testid="new-agreement-receiver-input"]';
+  private newAgreementWalletDepositEndpointInput = '[data-testid="new-agreement-wallet-deposit-endpoint-input"]';
+  private testBase64EncodedWalletDepositEndpoint = "eyJFbmRwb2ludCI6Imh0dHA6Ly9sb2NhbGhvc3Q6Nzg5MC8iLCJQdWJsaWNLZXkiOiJBVTBWVFVzQUFBQUJ5aE5KRmxENlZhVUZPajRGRzcybmVkSmxVbDRjK0xVejdpV0tRNEkzM1k0Q2J5OVBQTm5SdXRuaWUxT1NVRS9ud0RWTWV3bW14TnFFTkw5a0RZeHdMQWs9IiwiVmVyc2lvbiI6MX0=";
   private paginator = '[data-testid="table-paginator"]';
   private testReceiverId = '11111111';
   private testStartDate = '12052023';
@@ -53,6 +55,7 @@ export class TransfersPo {
     cy.get(this.newAgreementDateRangeStartInput).type(this.testStartDate);
     cy.get(this.newAgreementHasEndDate).click();
     cy.get(this.newAgreementDateRangeEndInput).type(this.testEndDate);
+    cy.get(this.newAgreementWalletDepositEndpointInput).type(this.testBase64EncodedWalletDepositEndpoint);
   }
 
   clickCreateAgreementButton() {
@@ -65,6 +68,7 @@ export class TransfersPo {
         senderName: 'Producent A/S',
         senderTin: '11223344',
         receiverTin: '11111111',
+        Base64EncodedWalletDepositEndpoint: 'eyJFbmRwb2ludCI6Imh0dHA6Ly9sb2NhbGhvc3Q6Nzg5MC8iLCJQdWJsaWNLZXkiOiJBVTBWVFVzQUFBQUJ5aE5KRmxENlZhVUZPajRGRzcybmVkSmxVbDRjK0xVejdpV0tRNEkzM1k0Q2J5OVBQTm5SdXRuaWUxT1NVRS9ud0RWTWV3bW14TnFFTkw5a0RZeHdMQWs9IiwiVmVyc2lvbiI6MX0='
       },
     });
     cy.get(this.createNewAgreementButton).click();
