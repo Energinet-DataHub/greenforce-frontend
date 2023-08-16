b58001e00b60ea4758c9c14af0d6f15f8fa9267f
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-08-16 12:45:18 +0200

    New watt-spinner

--- libs/ui-watt/src/lib/components/spinner/watt-spinner.component.ts
+++ libs/ui-watt/src/lib/components/spinner/watt-spinner.component.ts
@@ -14,8 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, Input } from '@angular/core';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
+import { Component, HostBinding, Input } from '@angular/core';
 /**
  * Usage:
  * `import { WattSpinnerComponent } from '@energinet-datahub/watt/spinner';`
@@ -23,9 +22,27 @@ import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@ang
 @Component({
   selector: 'watt-spinner',
   standalone: true,
-  template: `<mat-spinner [diameter]="diameter"></mat-spinner>`,
-  imports: [MatProgressSpinnerModule],
+  styleUrls: ['./watt-spinner.component.scss'],
+  template: `<svg
+    preserveAspectRatio="xMidYMid meet"
+    focusable="false"
+    aria-hidden="true"
+    style="width:{{ diameter }}px; height: {{ diameter }}px;"
+    viewBox="0 0 38.4 38.4"
+  >
+    <circle
+      cx="50%"
+      cy="50%"
+      style="animation-name:circleRotate; stroke-dasharray: 106.814px; stroke-width: 10%; transform-origin: 50% 50% 0px;"
+      r="17"
+    ></circle>
+  </svg>`,
 })
 export class WattSpinnerComponent {
+  @HostBinding('attr.role') role = 'progressbar';
+  @HostBinding('style')
+  get style(): string {
+    return `--watt-spinner-diameter: ${this.diameter}px`;
+  }
   @Input() diameter = 44;
 }
