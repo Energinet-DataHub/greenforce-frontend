510c6e1bf765973fb92f4c264768defe342997ce
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2023-08-18 16:59:27 +0200

    Minor clean up

--- libs/gf/util-calendar/src/calendar.ts
+++ libs/gf/util-calendar/src/calendar.ts
@@ -15,15 +15,8 @@
  * limitations under the License.
  */
 import { Calendar } from './types';
-import * as dateFns from './date-fns';
 import * as Month from './month';
 
-/**
- *
- */
 export const make = (today: Date): Calendar => ({ today });
 
-/**
- *
- */
 export const getCurrentMonth = (calendar: Calendar) => Month.make(calendar, calendar.today);
--- libs/gf/util-calendar/src/date-fns.ts
+++ libs/gf/util-calendar/src/date-fns.ts
@@ -22,14 +22,9 @@ export { default as endOfISOWeek } from 'date-fns/endOfISOWeek';
 export { default as getDate } from 'date-fns/getDate';
 export { default as getDay } from 'date-fns/getDay';
 export { default as getISOWeek } from 'date-fns/getISOWeek';
-export { default as getMonth } from 'date-fns/getMonth';
-export { default as getYear } from 'date-fns/getYear';
-export { default as intlFormat } from 'date-fns/intlFormat';
 export { default as isSameDay } from 'date-fns/isSameDay';
 export { default as isSameMonth } from 'date-fns/isSameMonth';
 export { default as isWeekend } from 'date-fns/isWeekend';
 export { default as startOfISOWeek } from 'date-fns/startOfISOWeek';
 export { default as startOfMonth } from 'date-fns/startOfMonth';
 export { default as subMonths } from 'date-fns/subMonths';
-
-export { default as nextSunday } from 'date-fns/nextSunday';
