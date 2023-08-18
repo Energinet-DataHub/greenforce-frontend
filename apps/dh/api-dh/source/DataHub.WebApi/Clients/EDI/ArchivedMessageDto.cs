39cd38a0b0ff2dbd1998d1e7fdc0b7c7d4bdb925
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2023-08-18 08:21:12 +0200

    added id to archivedMessages

--- apps/dh/api-dh/source/DataHub.WebApi/Clients/EDI/ArchivedMessageDto.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Clients/EDI/ArchivedMessageDto.cs
@@ -15,6 +15,7 @@
 namespace Energinet.DataHub.WebApi.Clients.EDI
 {
     public sealed record ArchivedMessageDto(
+        string Id,
         string MessageId,
         string DocumentType,
         string CreatedAt,
