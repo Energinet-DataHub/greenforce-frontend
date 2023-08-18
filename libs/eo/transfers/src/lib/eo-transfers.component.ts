f3624ce20b9101d76948d1fd6a1e6fcd762cc00e
sahma19 <saiuan@msn.com>
2023-08-18 15:10:59 +0200

    change info text

--- libs/eo/transfers/src/lib/eo-transfers.component.ts
+++ libs/eo/transfers/src/lib/eo-transfers.component.ts
@@ -31,12 +31,13 @@ import { WattCardComponent } from '@energinet-datahub/watt/card';
   template: `
     <eo-popup-message *ngIf="error$ | push"></eo-popup-message>
     <watt-card class="watt-space-stack-l">
-      <h3 class="watt-space-stack-m">This is the beginning</h3>
+      <h3 class="watt-space-stack-m">Welcome to the beta test of Energy Origin</h3>
+      <h4 class="watt-space-stack-m">Notice of change:</h4>
       <p class="watt-space-stack-m">
-        This page is based on real data and is working towards the coming solution regarding
-        certificates. So it is not just a test, though these data cannot yet be used in a legal
-        sense. It will be communicated, when it is out of beta and can be used legally. So you can
-        try this without any consequences.
+        Due to changes in the solution regarding certificates, some data has been removed or is planned to be removed. <br>
+        This means that metering points that have been activated for issuance of certificates before August 17th, 2023, must be re-activated.
+        Furthermore, certificates that have been issued before August 23rd, 2023, will be removed. <br> <br>
+        This page is based on real data and is working towards the coming solution regarding certificates. So it is not just a test, though these data cannot yet be used in a legal sense. It will be communicated, when it is out of beta and can be used legally. So you can try this without any consequences.
       </p>
     </watt-card>
     <watt-card class="watt-space-stack-m">
