92c1d81452fd62ab19b3bea4d9a054843710b97a
Dan Stenrøjl <XDAST@energinet.dk>
2023-08-29 13:49:42 +0200

    Remove experiment with auth_token

--- apps/dh/e2e-dh/src/support/commands.ts
+++ apps/dh/e2e-dh/src/support/commands.ts
@@ -69,9 +69,6 @@ Cypress.Commands.add('login', (email: string, password: string) => {
         cy.findByRole('heading', {
           name: new RegExp(daTranslations.meteringPoint.search.title, 'i'),
         });
-
-        // cy.document().its('cookie').should('contain', 'ai_session');
-
         // cy.visit(Cypress.config('baseUrl'));
         // cy.get('h1').contains(daTranslations.meteringPoint.search.title);
       },
