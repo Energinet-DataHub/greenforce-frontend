e40bec601bb0aed87393b28c1a540cf1291618e6
Dzhavat Ushev <dzu@energinet.dk>
2023-08-31 12:05:27 +0200

    chore(dh): remove eslint comment

--- libs/dh/shared/feature-flags/src/lib/feature-flags.ts
+++ libs/dh/shared/feature-flags/src/lib/feature-flags.ts
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* eslint-disable sonarjs/no-duplicate-string */
 import { DhAppEnvironment } from '@energinet-datahub/dh/shared/environments';
 
 export type DhFeatureFlag = {
