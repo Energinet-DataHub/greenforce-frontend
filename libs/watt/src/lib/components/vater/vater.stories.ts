1cdc12d9c995949ec810b58d81bffa6f60d70f64
Bjørn Hoffmann <ManBearTM@users.noreply.github.com>
2023-09-01 14:39:43 +0200

    feat(watt): VaterSpacer and VaterUtility (#2005)
    * Add even more vater components
    
    * chore: add license
    
    * Chore: bump version to 0.3.338 for build/infrastructure/eo/chart/values.yaml
    
    * Chore: bump version to 0.3.338 for build/infrastructure/eo/chart/Chart.yaml
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/watt/src/lib/components/vater/vater.stories.ts
+++ libs/watt/src/lib/components/vater/vater.stories.ts
@@ -18,6 +18,7 @@ import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 
 import { VaterStackComponent } from './vater-stack.component';
 import { VaterFlexComponent } from './vater-flex.component';
+import { VaterSpacerComponent } from './vater-spacer.component';
 
 const meta: Meta = {
   /* 👇 The title prop is optional.
@@ -27,7 +28,7 @@ const meta: Meta = {
   title: 'Components/Vater',
   decorators: [
     moduleMetadata({
-      imports: [VaterStackComponent, VaterFlexComponent],
+      imports: [VaterStackComponent, VaterFlexComponent, VaterSpacerComponent],
     }),
   ],
 };
@@ -76,6 +77,32 @@ export const Flex: StoryObj<VaterFlexComponent> = {
           <div style="background-color: var(--watt-color-primary); width: 60px;"></div>
           <div style="background-color: var(--watt-color-primary-light); width: 60px;"></div>
           <div style="background-color: var(--watt-color-primary-ultralight); width: 60px;"></div>
+        </vater-flex>
+      </vater-flex>
+    `,
+  }),
+};
+
+export const Spacer: StoryObj<VaterFlexComponent> = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <vater-flex direction="row" gap="m" style="height: 400px; padding-top: var(--watt-space-l)">
+        <vater-flex gap="s" style="width: 50%; padding: var(--watt-space-s); border: 1px dashed var(--watt-color-neutral-grey-500)">
+          <code style="position: absolute; align-self: flex-start; transform: translateY(calc(-100% - var(--watt-space-m)))">direction="column"</code>
+          <div style="background-color: var(--watt-color-primary-dark); min-height: 60px;"></div>
+          <div style="background-color: var(--watt-color-primary); min-height: 60px;"></div>
+          <div style="background-color: var(--watt-color-primary-light); min-height: 60px;"></div>
+          <vater-spacer />
+          <div style="background-color: var(--watt-color-primary-ultralight); min-height: 60px;"></div>
+        </vater-flex>
+        <vater-flex direction="row" gap="s" style="width: 50%; padding: var(--watt-space-s); border: 1px dashed var(--watt-color-neutral-grey-500)">
+          <code style="position: absolute; align-self: flex-start; transform: translateY(calc(-100% - var(--watt-space-m)))">direction="row"</code>
+          <div style="background-color: var(--watt-color-primary-dark); min-width: 60px;"></div>
+          <div style="background-color: var(--watt-color-primary); min-width: 60px;"></div>
+          <div style="background-color: var(--watt-color-primary-light); min-width: 60px;"></div>
+          <vater-spacer />
+          <div style="background-color: var(--watt-color-primary-ultralight); min-width: 60px;"></div>
         </vater-flex>
       </vater-flex>
     `,
