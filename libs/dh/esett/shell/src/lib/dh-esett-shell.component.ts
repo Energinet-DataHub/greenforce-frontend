e0a3510c4f96644335ffa18e5d73226e353a8065
Dzhavat Ushev <dzu@energinet.dk>
2023-09-04 10:06:20 +0200

    feat(dh): add Outgoing messages tab to eSett

--- libs/dh/esett/shell/src/lib/dh-esett-shell.component.ts
+++ libs/dh/esett/shell/src/lib/dh-esett-shell.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@ngneat/transloco';
+
+import { WATT_TABS } from '@energinet-datahub/watt/tabs';
 
 @Component({
   selector: 'dh-esett-shell',
   standalone: true,
-  template: `eSett`,
+  template: `<watt-tabs *transloco="let t; read: 'eSett.tabs'">
+    <watt-tab [label]="t('outgoingMessages.tabLabel')">
+      <div>Tab 1</div>
+    </watt-tab>
+  </watt-tabs>`,
+  imports: [TranslocoDirective, WATT_TABS],
 })
 export class DhESettShellComponent {}
