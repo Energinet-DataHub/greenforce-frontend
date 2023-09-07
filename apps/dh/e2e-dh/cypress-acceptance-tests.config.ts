9ed21103ac0add5135293a91b7340d85fe96eddb
Alexander Hafstad <alexander@frontend-specialisten.dk>
2023-09-07 21:15:30 +0200

    chore: make acceptance test workflow and release package

---
+++ apps/dh/e2e-dh/cypress-acceptance-tests.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    // videosFolder: 'cypress/videos',
+    // screenshotsFolder: 'cypress/screenshots',
+    video: true,
+    chromeWebSecurity: false,
+    supportFile: `${__dirname}/src/support/e2e.ts`,
+    specPattern: `${__dirname}/src/**/*.cy.ts`,
+    fixturesFolder: `${__dirname}/src/fixtures`,
+    excludeSpecPattern: `${__dirname}/src/e2e/b2c-healthchecks.cy.ts`,
+  },
+});
+
+
+
