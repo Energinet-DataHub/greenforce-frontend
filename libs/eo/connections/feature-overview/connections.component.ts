8bc9fcf58d4bb25faf62d6349eefeb58124df82c
Alexander Hafstad <alexander@frontend-specialisten.dk>
2023-09-11 11:55:15 +0200

    test(eo, connections): add spec for creating invitation link

--- libs/eo/connections/feature-overview/connections.component.ts
+++ libs/eo/connections/feature-overview/connections.component.ts
@@ -76,7 +76,7 @@ import { EoConnection, EoConnectionsService } from '../data-access-api/connectio
           </div>
           <vater-spacer />
           <watt-search label="Search" (search)="search = $event" />
-          <watt-button variant="secondary" icon="plus" (click)="inviteConnection.open()"
+          <watt-button variant="secondary" icon="plus" (click)="inviteConnection.open()" data-testid="new-invitation-button"
             >New invitation link</watt-button
           >
         </vater-stack>
