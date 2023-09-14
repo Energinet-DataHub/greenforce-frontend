d4afb34b4c4b1eedceffd32efb9f5e317a5bee7b
Alexander Hafstad <alexander@frontend-specialisten.dk>
2023-09-14 14:26:52 +0200

    refactor(eo): remove unused component eo-line-chart

--- libs/eo/shared/atomic-design/ui-atoms/src/index.ts
+++ libs/eo/shared/atomic-design/ui-atoms/src/index.ts
@@ -18,7 +18,6 @@ export * from './lib/eo-idle-timer/idle-timer-countdown.component';
 export * from './lib/eo-idle-timer/idle-timer-logged-out.component';
 export * from './lib/eo-info-box/eo-info-box.component';
 export * from './lib/eo-inline-message/eo-inline-message.component';
-export * from './lib/eo-line-chart/eo-line-chart.component';
 export * from './lib/eo-media/eo-media-align';
 export * from './lib/eo-media/eo-media.component';
 export * from './lib/eo-pie-chart/eo-pie-chart.component';
