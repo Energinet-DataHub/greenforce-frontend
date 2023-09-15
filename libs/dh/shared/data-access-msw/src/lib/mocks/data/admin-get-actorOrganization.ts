917c102ea8d7ea29ce8c38e14efb73228ec74832
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-09-15 11:38:06 +0200

    mock data, and fixing styles in admin

---
+++ libs/dh/shared/data-access-msw/src/lib/mocks/data/admin-get-actorOrganization.ts
@@ -0,0 +1,11 @@
+import { MarketParticipantOrganizationDto } from '@energinet-datahub/dh/shared/domain';
+
+export const marketParticipantOrganization: MarketParticipantOrganizationDto = {
+  organizationId: '47a76eb8-3814-4bc1-96a9-a9a5b9adf849',
+  name: 'Energinet DataHub A/S',
+  businessRegisterIdentifier: '39315041',
+  domain: 'energinet.dk',
+  comment: '',
+  status: 'Active',
+  address: { streetName: null, number: null, zipCode: null, city: null, country: 'DK' },
+};
