68433880645ff2b40fc9e01ea815fe21e0f3aa3a
Alexander Hafstad <alexander@frontend-specialisten.dk>
2023-10-04 08:26:59 +0200

    fix(eo, transfers): add formControl for wallet deposit enpoint modal

--- libs/eo/transfers/src/lib/eo-transfers-wallet-modal-component.ts
+++ libs/eo/transfers/src/lib/eo-transfers-wallet-modal-component.ts
@@ -6,17 +6,15 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-(walletDepositEndpointError$ | async) === falsee law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 import { ChangeDetectorRef, Component, ViewChild, ViewEncapsulation, inject } from '@angular/core';
-import { NgIf } from '@angular/common';
-import { RxPush } from '@rx-angular/template/push';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { FormControl } from '@angular/forms';
 
 import { WATT_MODAL, WattModalComponent } from '@energinet-datahub/watt/modal';
 import { WattButtonComponent } from '@energinet-datahub/watt/button';
@@ -25,17 +23,18 @@ import { WattCopyToClipboardDirective } from '@energinet-datahub/watt/clipboard'
 
 import { EoTransfersStore } from './eo-transfers.store';
 import { WattEmptyStateComponent } from '@energinet-datahub/watt/empty-state';
+
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'eo-transfers-wallet-modal',
   imports: [
     NgIf,
-    RxPush,
     WATT_MODAL,
     WattButtonComponent,
     WattCopyToClipboardDirective,
     WattEmptyStateComponent,
     WattTextFieldComponent,
+    AsyncPipe,
   ],
   standalone: true,
   template: `
@@ -49,7 +48,7 @@ import { WattEmptyStateComponent } from '@energinet-datahub/watt/empty-state';
       (closed)="onClosed()"
       *ngIf="opened"
     >
-      <form *ngIf="!(walletDepositEndpointError$ | push); else error">
+      <form *ngIf="(walletDepositEndpointError$ | async) !== undefined; else error">
         <p>
           To receive granular certificates the sender must create a transfer agreement. They need
           this key to identify the recipient (you).
@@ -58,7 +57,7 @@ import { WattEmptyStateComponent } from '@energinet-datahub/watt/empty-state';
           <strong>The key is one-time use only</strong>
         </p>
         <div style="display: flex; align-items: center; margin: var(--watt-space-xl) 0;">
-          <watt-text-field label="KEY" [value]="(walletDepositEndpoint$ | push) || ''" #key />
+          <watt-text-field label="KEY" [formControl]="walletDepositEndpoint" [value]="(walletDepositEndpoint$ | async) || ''" #key />
           <watt-button
             style="margin-top:28px;"
             variant="text"
@@ -97,6 +96,7 @@ export class EoTransfersWalletModalComponent {
   private store = inject(EoTransfersStore);
 
   protected opened = false;
+  protected walletDepositEndpoint = new FormControl('');
   protected walletDepositEndpoint$ = this.store.walletDepositEndpoint$;
   protected walletDepositEndpointError$ = this.store.walletDepositEndpointError$;
   protected walletDepositEndpointLoading$ = this.store.walletDepositEndpointLoading$;
@@ -110,6 +110,7 @@ export class EoTransfersWalletModalComponent {
 
     this.walletDepositEndpointLoading$.subscribe((loading) => {
       this.loading = loading;
+      this.cd.detectChanges();
     });
   }
 
