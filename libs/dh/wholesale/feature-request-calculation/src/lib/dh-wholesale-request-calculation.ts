e70b521215172db64fa5873c584858bdeedd4f2e
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-10-05 10:51:36 +0200

    added dropdown

--- libs/dh/wholesale/feature-request-calculation/src/lib/dh-wholesale-request-calculation.ts
+++ libs/dh/wholesale/feature-request-calculation/src/lib/dh-wholesale-request-calculation.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { WATT_CARD } from '@energinet-datahub/watt/card';
+import { WattDropdownComponent, WattDropdownOptions } from '@energinet-datahub/watt/dropdown';
+import { VaterStackComponent } from '@energinet-datahub/watt/vater';
+import { TranslocoDirective } from '@ngneat/transloco';
 
 @Component({
   selector: 'dh-wholesale-request-calculation',
   templateUrl: './dh-wholesale-request-calculation.html',
   standalone: true,
-  imports: [WATT_CARD],
+  imports: [
+    WATT_CARD,
+    WattDropdownComponent,
+    VaterStackComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    TranslocoDirective,
+  ],
 })
-export class DhWholesaleRequestCalculationComponent {}
+export class DhWholesaleRequestCalculationComponent {
+  form = new FormGroup({
+    processType: new FormControl(''),
+  });
+
+  progressTypeOptions: WattDropdownOptions = [
+    { value: '1', displayValue: 'Process type 1' },
+    { value: '2', displayValue: 'Process type 2' },
+    { value: '3', displayValue: 'Process type 3' },
+  ];
+}
