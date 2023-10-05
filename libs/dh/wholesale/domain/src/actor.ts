849694eb3be84b02f3ba63534b8d15f0f79c1d08
Alex <77341673+defectiveAi@users.noreply.github.com>
2023-10-05 14:28:51 +0200

    Update reference.

--- libs/dh/wholesale/domain/src/actor.ts
+++ libs/dh/wholesale/domain/src/actor.ts
@@ -18,4 +18,4 @@ import { ResultOf } from 'apollo-angular';
 
 import { GetActorsForSettlementReportDocument } from '@energinet-datahub/dh/shared/domain/graphql';
 
-export type Actor = ResultOf<typeof GetActorsForSettlementReportDocument>['actors'][0];
+export type Actor = ResultOf<typeof GetActorsForSettlementReportDocument>['actorsForEicFunction'][0];
