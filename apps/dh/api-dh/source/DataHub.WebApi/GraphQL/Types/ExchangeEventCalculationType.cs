e2aada904401a860059bde9887b8e7c929236099
KarstenM <xnetsrak@gmail.com>
2023-10-11 13:47:02 +0200

    graphql update

--- apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Types/ExchangeEventCalculationType.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Types/ExchangeEventCalculationType.cs
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.WebApi.Clients.ESettExchange.v1;
 using HotChocolate.Types;
+using ExchangeCalculationTypeExchange = Energinet.DataHub.WebApi.Clients.ESettExchange.v1.CalculationType;
 
 namespace Energinet.DataHub.WebApi.GraphQL
 {
-    public class ExchangeEventCalculationType : EnumType<CalculationType>
+    public class ExchangeEventCalculationType : EnumType<ExchangeCalculationTypeExchange>
     {
-        protected override void Configure(IEnumTypeDescriptor<CalculationType> descriptor)
+        protected override void Configure(IEnumTypeDescriptor<ExchangeCalculationTypeExchange> descriptor)
         {
             descriptor.Name("ExchangeEventCalculationType");
         }
