4cea934fff185327b1ce01845f3ff69326e0a5d1
Alexander Hafstad <alexander@frontend-specialisten.dk>
2023-10-12 14:17:48 +0200

    fix(watt, modal): add missing module in story

--- libs/watt/src/lib/components/modal/+storybook/watt-modal-from-class.component.ts
+++ libs/watt/src/lib/components/modal/+storybook/watt-modal-from-class.component.ts
@@ -19,16 +19,15 @@ import { WattButtonComponent } from '../../button/watt-button.component';
 import { WATT_MODAL } from '../watt-modal.component';
 import { WattModalService } from '../watt-modal.service';
 import { WattTextFieldComponent } from '../../text-field/watt-text-field.component';
-import { FormControl } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  selector: 'watt-modal-from-class',
-  imports: [WATT_MODAL, WattTextFieldComponent, WattButtonComponent],
+  imports: [WATT_MODAL, WattTextFieldComponent, WattButtonComponent, ReactiveFormsModule],
   template: `
     <watt-modal #modal [title]="title" closeLabel="Close modal" [loading]="isLoading">
-      <watt-text-field [formControl]="exampleFormControl" label="Username" />
-      <watt-text-field [formControl]="exampleFormControl" label="Password" type="password" />
+      <watt-text-field [formControl]="usernameControl" label="Username" />
+      <watt-text-field [formControl]="passwordControl" label="Password" type="password" />
       <watt-modal-actions>
         <watt-button variant="secondary" (click)="modal.close(false)">Cancel</watt-button>
         <watt-button (click)="modal.close(true)">Save</watt-button>
@@ -38,7 +37,8 @@ import { FormControl } from '@angular/forms';
 })
 export class WattModalComponent implements OnInit {
   title = 'This is a modal opened from a class';
-  exampleFormControl = new FormControl('');
+  usernameControl = new FormControl('');
+  passwordControl = new FormControl('');
   isLoading = false;
 
   ngOnInit(): void {
