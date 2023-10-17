eb02730b7ca183cc5272ba76f208bd61bafface6
Michael <52096428+michaelmajgaard@users.noreply.github.com>
2023-10-17 14:40:55 +0200

    feat: organization audit log - drawer (#2154)

--- apps/dh/api-dh/source/DataHub.WebApi/Generated/HotChocolate.Types.Analyzers/HotChocolate.Types.Analyzers.TypeModuleGenerator/HotChocolateTypeModule.g.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Generated/HotChocolate.Types.Analyzers/HotChocolate.Types.Analyzers.TypeModuleGenerator/HotChocolateTypeModule.g.cs
@@ -27,6 +27,7 @@ namespace Microsoft.Extensions.DependencyInjection
             builder.AddType<global::Energinet.DataHub.WebApi.GraphQL.EicFunctionType>();
             builder.AddType<global::Energinet.DataHub.WebApi.GraphQL.EsettExchangeEventType>();
             builder.AddType<global::Energinet.DataHub.WebApi.GraphQL.ExchangeEventCalculationType>();
+            builder.AddType<global::Energinet.DataHub.WebApi.GraphQL.OrganizationAuditLogType>();
             builder.AddType<global::Energinet.DataHub.WebApi.GraphQL.OrganizationType>();
             builder.AddType<global::Energinet.DataHub.WebApi.GraphQL.PermissionType>();
             builder.AddType<global::Energinet.DataHub.WebApi.GraphQL.ProcessStatusType>();
