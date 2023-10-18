8f3ad5a6b54562324590a354d629c45bedc7d107
Morten Vestergaard Hansen <mimo@mimo-design.com>
2023-10-18 16:58:45 +0200

    unused using

--- apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Query.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Query.cs
@@ -23,7 +23,6 @@ using Energinet.DataHub.WebApi.Clients.Wholesale.v3;
 using Energinet.DataHub.WebApi.Controllers.MarketParticipant.Dto;
 using Energinet.DataHub.WebApi.Extensions;
 using HotChocolate;
-using HotChocolate.Authorization;
 using Microsoft.AspNetCore.Http;
 using NodaTime;
 using ProcessType = Energinet.DataHub.WebApi.Clients.Wholesale.v3.ProcessType;
