77d91bfdda466e19f4c9c2b29521f11c65c49ef3
Jeppe Langhoff Sørensen <jeppelanghoff@gmail.com>
2023-10-20 08:06:47 +0200

    feat(watt): optimized stepper on smaller screens

---
+++ libs/watt/src/lib/components/stepper/+storybook/stepper.modal.example.component.ts
@@ -0,0 +1,43 @@
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { WATT_STEPPER } from '..';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { WattButtonComponent } from '../../button';
+import { WattIconComponent } from '../../../foundations/icon';
+import { WattTextFieldComponent } from '../../text-field/watt-text-field.component';
+import { WattFieldErrorComponent } from '../../field/watt-field-error.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { WattModalComponent } from '../../modal/watt-modal.component';
+import { StepperExampleComponent } from './stepper.example.component';
+
+@Component({
+  selector: 'watt-stepper-modal-example',
+  standalone: true,
+  templateUrl: './stepper.modal.example.component.html',
+  imports: [
+    WATT_STEPPER,
+    ReactiveFormsModule,
+    WattButtonComponent,
+    WattIconComponent,
+    WattTextFieldComponent,
+    WattFieldErrorComponent,
+    WattModalComponent,
+    StepperExampleComponent,
+    MatButtonModule,
+    CommonModule,
+  ],
+})
+export class StepperModalExampleComponent {
+  user = new FormBuilder().group({
+    firstname: ['', Validators.required],
+    lastname: [''],
+  });
+  address = new FormBuilder().group({ street: [''], city: [''] });
+  email = new FormBuilder().group({ email: [''] });
+  @ViewChild('modal')
+  modal!: TemplateRef<Element>;
+
+  complete(): void {
+    console.log('completed');
+  }
+}
--- libs/watt/src/lib/components/stepper/+storybook/stepper.stories.ts
+++ libs/watt/src/lib/components/stepper/+storybook/stepper.stories.ts
@@ -15,18 +15,34 @@
  * limitations under the License.
  */
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { applicationConfig, Meta, StoryFn } from '@storybook/angular';
+import { applicationConfig, Meta, moduleMetadata, StoryFn } from '@storybook/angular';
 
 import { StepperExampleComponent } from './stepper.example.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { WattButtonComponent } from '../../button/watt-button.component';
+import { WATT_MODAL } from '../../modal/watt-modal.component';
+import { WattTooltipDirective } from '../../tooltip/watt-tooltip.directive';
+import { WattTextFieldComponent } from '../../text-field/watt-text-field.component';
+import { StepperModalExampleComponent } from './stepper.modal.example.component';
 
 const meta: Meta<StepperExampleComponent> = {
   title: 'Components/Stepper',
+  component: StepperExampleComponent,
   decorators: [
     applicationConfig({
       providers: [provideAnimations()],
     }),
+    moduleMetadata({
+      imports: [
+        ReactiveFormsModule,
+        WattButtonComponent,
+        WATT_MODAL,
+        WattTooltipDirective,
+        WattTextFieldComponent,
+        StepperModalExampleComponent,
+      ],
+    }),
   ],
-  component: StepperExampleComponent,
 };
 
 export default meta;
@@ -37,6 +53,11 @@ export const Stepper: StoryFn = (args) => ({
   props: args,
   template,
 });
+
+export const Modal: StoryFn = (args) => ({
+  props: args,
+  template: `<watt-stepper-modal-example></watt-stepper-modal-example>`
+})
 
 Stepper.parameters = {
   docs: {
