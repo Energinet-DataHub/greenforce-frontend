3e7c9cc6da73c3fb1134e5078b32af1f41c0cdf3
Dzhavat Ushev <dzu@energinet.dk>
2023-11-01 09:42:49 +0100

    feat(dh): implement upload of certificate

--- libs/dh/market-participant/actors/feature-actors/src/lib/drawer/tabs/dh-certificate.component.ts
+++ libs/dh/market-participant/actors/feature-actors/src/lib/drawer/tabs/dh-certificate.component.ts
@@ -14,8 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
 import { TranslocoDirective } from '@ngneat/transloco';
+import { NgIf } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 import { WATT_CARD } from '@energinet-datahub/watt/card';
 import {
@@ -23,33 +25,119 @@ import {
   WattDescriptionListItemComponent,
 } from '@energinet-datahub/watt/description-list';
 import { DhEmDashFallbackPipe } from '@energinet-datahub/dh/shared/ui-util';
+import { WattButtonComponent } from '@energinet-datahub/watt/button';
+import { VaterFlexComponent, VaterStackComponent } from '@energinet-datahub/watt/vater';
+import { WattValidationMessageComponent } from '@energinet-datahub/watt/validation-message';
+import { DhMarketParticipantCertificateStore } from '@energinet-datahub/dh/market-participant/actors/data-access-api';
+import { WattSpinnerComponent } from '@energinet-datahub/watt/spinner';
 
 @Component({
   selector: 'dh-certificate',
   standalone: true,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+
+      .certificate-input {
+        display: none;
+      }
+    `,
+  ],
   template: `
-    <watt-card
-      variant="solid"
+    <ng-container
       *transloco="let t; read: 'marketParticipant.actorsOverview.drawer.tabs.certificate'"
     >
-      <watt-description-list variant="stack">
-        <watt-description-list-item
-          [label]="t('thumbprint')"
-          [value]="undefined | dhEmDashFallback"
+      <input
+        type="file"
+        class="certificate-input"
+        [accept]="certificateExt"
+        (change)="onFileSelected(fileUpload.files)"
+        #fileUpload
+      />
+
+      <vater-flex gap="m">
+        <watt-validation-message
+          *ngIf="isInvalidFileType()"
+          type="warning"
+          [message]="t('invalidFileType')"
         />
-        <watt-description-list-item
-          [label]="t('expiryDate')"
-          [value]="undefined | dhEmDashFallback"
-        />
-      </watt-description-list>
-    </watt-card>
+
+        <vater-flex direction="row" justify="center" *ngIf="isUploadInProgress()">
+          <watt-spinner />
+        </vater-flex>
+
+        <watt-card variant="solid" *ngIf="certificateExists()">
+          <watt-description-list variant="stack">
+            <watt-description-list-item
+              [label]="t('thumbprint')"
+              [value]="undefined | dhEmDashFallback"
+            />
+            <watt-description-list-item
+              [label]="t('expiryDate')"
+              [value]="undefined | dhEmDashFallback"
+            />
+          </watt-description-list>
+        </watt-card>
+
+        <vater-stack direction="row" justify="flex-end" gap="m">
+          <watt-button *ngIf="certificateExists()">{{ t('removeCertificate') }}</watt-button>
+          <watt-button (click)="fileUpload.click()">{{ t('uploadCertificate') }}</watt-button>
+        </vater-stack>
+      </vater-flex>
+    </ng-container>
   `,
   imports: [
+    NgIf,
     TranslocoDirective,
-    DhEmDashFallbackPipe,
+
+    WattButtonComponent,
     WATT_CARD,
     WattDescriptionListComponent,
     WattDescriptionListItemComponent,
+    VaterFlexComponent,
+    VaterStackComponent,
+    WattValidationMessageComponent,
+    WattSpinnerComponent,
+
+    DhEmDashFallbackPipe,
   ],
+  viewProviders: [DhMarketParticipantCertificateStore],
 })
-export class DhCertificateComponent {}
+export class DhCertificateComponent {
+  private readonly httpClient = inject(DhMarketParticipantCertificateStore);
+
+  certificateExt = '.pfx';
+
+  certificateExists = signal(false);
+  isInvalidFileType = signal(false);
+
+  isUploadInProgress = toSignal(this.httpClient.uploadInProgress$, { initialValue: false });
+
+  @Input({ required: true }) actorId = '';
+
+  onFileSelected(files: FileList | null): void {
+    if (files == null) {
+      return;
+    }
+
+    const file = files[0];
+
+    if (this.isValidFileType(file)) {
+      this.isInvalidFileType.set(false);
+
+      return this.startUpload(this.actorId, file);
+    } else {
+      this.isInvalidFileType.set(true);
+    }
+  }
+
+  private startUpload(actorId: string, file: File): void {
+    this.httpClient.uploadCertificate({ actorId, file });
+  }
+
+  private isValidFileType(file: File): boolean {
+    return file.type === 'application/x-pkcs12';
+  }
+}
