c724a13e56c2c28f83f2e40b4bb2a3e9267ce7f1
Dzhavat Ushev <dzu@energinet.dk>
2023-11-01 11:34:33 +0100

    fix(dh): fix path in jest config file

--- libs/dh/market-participant/actors/data-access-api/jest.config.ts
+++ libs/dh/market-participant/actors/data-access-api/jest.config.ts
@@ -17,10 +17,10 @@
 /* eslint-disable */
 export default {
   displayName: 'dh-market-participant-actors-data-access-api',
-  preset: '../../../../jest.preset.js',
+  preset: '../../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {},
-  coverageDirectory: '../../../../coverage/libs/dh/market-participant/actors/data-access-api',
+  coverageDirectory: '../../../../../coverage/libs/dh/market-participant/actors/data-access-api',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
