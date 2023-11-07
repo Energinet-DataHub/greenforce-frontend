11f6187f932d742569e09e1909755b37b7aba63f
Alexander Hafstad <alexander@frontend-specialisten.dk>
2023-11-07 10:17:53 +0100

    feat(eo): add consumption chart to dashboard

--- libs/eo/dashboard/shell/src/lib/eo-dashboard-consumption.component.ts
+++ libs/eo/dashboard/shell/src/lib/eo-dashboard-consumption.component.ts
@@ -15,29 +15,122 @@
  * limitations under the License.
  */
 
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+import { BaseChartDirective, NgChartsModule, ThemeService } from 'ng2-charts';
 
 import { WATT_CARD } from '@energinet-datahub/watt/card';
+import { DatePipe } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [WATT_CARD],
+  imports: [WATT_CARD, NgChartsModule],
+  providers: [ThemeService],
   selector: 'eo-dashboard-consumption',
-  template: `<watt-card>
-    <watt-card-title>
-      <h3>Title</h3>
-    </watt-card-title>
-  </watt-card>`,
   styles: [
     `
+      @use '@energinet-datahub/watt/utils' as watt;
+
       :host {
         display: block;
-      }
+        max-width: 100%;
 
-      .link {
-        text-decoration: none;
+        h5 {
+          // TODO: NEW FONT STYLES
+          font-size: 30px;
+          line-height: 28px;
+          color: var(--watt-on-light-high-emphasis);
+          font-weight: 400;
+        }
+
+        small {
+          color: var(--watt-on-light-low-emphasis);
+        }
+
+        .chart-container {
+          position: relative;
+          width: calc(100vw - 80px);
+          height: 196px;
+          padding-top: var(--watt-space-m);
+          max-width: 100%;
+
+          @include watt.media('>=Large') {
+            width: calc(100vw - 372px);
+          }
+        }
       }
     `,
   ],
+  template: `<watt-card>
+    <watt-card-title>
+      <h4>Green consumption</h4>
+    </watt-card-title>
+
+    <h5>0 Wh</h5>
+    <small>Out of 0 Wh total consumption</small>
+
+    <div class="chart-container">
+      <canvas
+        baseChart
+        [data]="barChartData"
+        [options]="barChartOptions"
+        [legend]="false"
+        [type]="'bar'"
+      >
+      </canvas>
+    </div>
+  </watt-card>`,
 })
-export class EoDashboardConsumptionComponent {}
+export class EoDashboardConsumptionComponent {
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
+  protected isLoading = true;
+  protected barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: this.generateLabels(),
+    datasets: [
+      {
+        data: [80, 30, 40, 20, 0, 0, 100],
+        label: 'Claimed',
+        borderRadius: Number.MAX_VALUE,
+        maxBarThickness: 8,
+        backgroundColor: '#00C898',
+      },
+      {
+        data: [20, 40, 50, 80, 80, 30, 0],
+        label: 'Consumption',
+        borderRadius: Number.MAX_VALUE,
+        maxBarThickness: 8,
+        backgroundColor: '#02525E',
+      },
+    ],
+  };
+
+  protected barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        stacked: true,
+        grid: { display: false },
+        ticks: {
+          maxRotation: 0,
+          autoSkipPadding: 12,
+        }
+      },
+      y: {
+        stacked: true,
+        title: { display: true, text: 'Wh', align: 'end' },
+      },
+    },
+  };
+
+  private generateLabels() {
+    // TODO: Should be localized when translations are available
+    const datePipe = new DatePipe('en-US');
+    const dates = Array.from({ length: 30 }, (_, i) => {
+      const d = new Date();
+      d.setDate(d.getDate() - i);
+      return datePipe.transform(d, 'd MMM');
+    });
+    return dates.reverse();
+  }
+}
--- libs/eo/dashboard/shell/src/lib/eo-dashboard-shell.component.ts
+++ libs/eo/dashboard/shell/src/lib/eo-dashboard-shell.component.ts
@@ -15,61 +15,17 @@
  * limitations under the License.
  */
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { EoInlineMessageComponent } from '@energinet-datahub/eo/shared/atomic-design/ui-atoms';
-import { WattIconComponent } from '@energinet-datahub/watt/icon';
-import { EoDashboardGetDataComponent } from './eo-dashboard-get-data.component';
-import { EoDashboardHourlyDeclarationComponent } from './eo-dashboard-hourly-declaration.component';
-import { EoDashboardLinksComponent } from './eo-dashboard-links.component';
+import { EoDashboardConsumptionComponent } from './eo-dashboard-consumption.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [
-    WattIconComponent,
-    EoInlineMessageComponent,
-    EoDashboardLinksComponent,
-    EoDashboardGetDataComponent,
-    EoDashboardHourlyDeclarationComponent,
+    EoDashboardConsumptionComponent,
   ],
   selector: 'eo-dashboard-shell',
-  styles: [
-    `
-      :host {
-        display: block;
-      }
-
-      .shell-container {
-        display: grid;
-        grid-template-columns: 375px 375px;
-        gap: var(--watt-space-l);
-      }
-    `,
-  ],
   template: `
-    <div class="shell-container">
-      <div>
-        <eo-dashboard-links class="watt-space-stack-l" />
-      </div>
-      <div>
-        <eo-dashboard-hourly-declaration class="watt-space-stack-l" />
-        <eo-dashboard-get-data />
-      </div>
-    </div>
-    <eo-inline-message type="warning">
-      <watt-icon name="custom-primary-info" size="l" />
-      <p>
-        The Energy Origin Platform is <strong>under development</strong> and new functionalities
-        will be released continuously. The first release of the platform only offers
-        <strong>data for companies</strong>. Data for private users is intended to form part of one
-        of the next releases. If you want to influence the new functionality, join us at our
-        <a
-          href="https://www.linkedin.com/groups/12643238/"
-          target="_blank"
-          rel="noopener noreferrer"
-          >LinkedIn group</a
-        >.
-      </p>
-    </eo-inline-message>
+    <eo-dashboard-consumption />
   `,
 })
 export class EoDashboardShellComponent {}
