5be13ba9a7f096d07b74b988d42d42c0063b1299
Alexander Hafstad <alexander@frontend-specialisten.dk>
2023-11-13 10:41:42 +0100

    chore: rename refs to legacy-workspace-plugin

--- tools/legacy-workspace-plugin/jest.config.ts
+++ tools/legacy-workspace-plugin/jest.config.ts
@@ -16,11 +16,11 @@
  */
 /* eslint-disable */
 export default {
-  displayName: 'workspace-plugin',
+  displayName: 'legacy-workspace-plugin',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/tools/workspace-plugin',
+  coverageDirectory: '../../coverage/tools/legacy-workspace-plugin',
 };
