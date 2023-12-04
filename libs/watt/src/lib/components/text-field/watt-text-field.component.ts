9d5a1cf237f4464ba46b7730c04adb8743e64c42
Alexander Hafstad <alexander@frontend-specialisten.dk>
2023-12-04 10:18:59 +0100

    fix(watt): add noop function for onTouched, to avoid console errors

--- libs/watt/src/lib/components/text-field/watt-text-field.component.ts
+++ libs/watt/src/lib/components/text-field/watt-text-field.component.ts
@@ -101,7 +101,7 @@ export class WattTextFieldComponent implements ControlValueAccessor, AfterViewIn
   /* @ignore */
   onChange!: (value: string) => void;
 
-  onTouched!: () => void;
+  onTouched: () => void = () => { /* noop */ };
 
   /* @ignore */
   writeValue(value: string): void {
