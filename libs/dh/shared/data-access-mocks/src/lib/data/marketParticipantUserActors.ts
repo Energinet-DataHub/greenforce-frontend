d97fc84cf0205ea88d938df81bbfa1b820af1bb5
Dzhavat Ushev <dzu@energinet.dk>
2023-12-18 10:34:50 +0100

    chore(dh): convert mock to variable

---
+++ libs/dh/shared/data-access-mocks/src/lib/data/marketParticipantUserActors.ts
@@ -0,0 +1,5 @@
+import { MarketParticipantGetAssociatedUserActorsResponseDto } from '@energinet-datahub/dh/shared/domain';
+
+export const marketParticipantUserActors: MarketParticipantGetAssociatedUserActorsResponseDto = {
+  actorIds: ['3ec41d91-fc6d-4364-ade6-b85576a91d04', 'f73d05cd-cb00-4be3-89b2-115c8425b837'],
+};
