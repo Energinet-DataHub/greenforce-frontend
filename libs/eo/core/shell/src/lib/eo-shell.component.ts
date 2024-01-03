b73f4767916fd59b37b88180b78518af87c4e2c5
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-01-03 11:23:14 +0100

    Add MatDateFnsModule to imports

--- libs/eo/core/shell/src/lib/eo-shell.component.ts
+++ libs/eo/core/shell/src/lib/eo-shell.component.ts
@@ -18,6 +18,7 @@ import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnDestroy, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { MatDateFnsModule } from '@angular/material-date-fns-adapter';
 
 import { WattShellComponent } from '@energinet-datahub/watt/shell';
 import { WattButtonComponent } from '@energinet-datahub/watt/button';
@@ -33,6 +34,7 @@ import { EoPrimaryNavigationComponent } from './eo-primary-navigation.component'
   imports: [
     EoFooterComponent,
     EoPrimaryNavigationComponent,
+    MatDateFnsModule,
     NgIf,
     RouterModule,
     VaterSpacerComponent,
