220add72f6f7e8680a865b20d80f99937d1fdedb
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-01-04 09:48:20 +0100

    refac: Update frontend to use new audits. (#2367)

--- apps/dh/api-dh/source/DataHub.WebApi/Controllers/MarketParticipantPermissionController.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Controllers/MarketParticipantPermissionController.cs
@@ -12,12 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Energinet.DataHub.WebApi.Clients.MarketParticipant.v1;
-using Energinet.DataHub.WebApi.Controllers.MarketParticipant.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Energinet.DataHub.WebApi.Controllers
@@ -48,55 +45,6 @@ namespace Energinet.DataHub.WebApi.Controllers
         public Task<ActionResult> UpdateAsync(UpdatePermissionDto permissionDto)
         {
             return HandleExceptionAsync(() => _client.PermissionPutAsync(permissionDto));
-        }
-
-        /// <summary>
-        /// Retrieves permissions auditLogs
-        /// </summary>
-        [HttpGet]
-        [Route("GetPermissionAuditLogs")]
-        public Task<ActionResult<PermissionAuditLogsViewDto>> GetPermissionAuditLogsAsync(int permissionId)
-        {
-            return HandleExceptionAsync(async () =>
-            {
-                var permissionAuditLogs = await _client
-                    .PermissionAuditlogsAsync(permissionId)
-                    .ConfigureAwait(false);
-
-                var permissionAuditLogWithUser = new List<PermissionAuditLogViewDto>();
-                var userLookup = new Dictionary<Guid, GetAuditIdentityResponse>();
-                var permission = (await _client.PermissionGetAsync()).FirstOrDefault(x => x.Id == permissionId) ?? throw new InvalidOperationException($"Permission {permissionId} was not found");
-
-                permissionAuditLogWithUser.Add(new PermissionAuditLogViewDto(
-                    permissionId,
-                    Guid.Empty,
-                    "DataHub",
-                    PermissionAuditLogType.Created,
-                    permission.Created,
-                    string.Empty));
-
-                foreach (var auditLog in permissionAuditLogs)
-                {
-                    var userFoundInCache = userLookup.ContainsKey(auditLog.AuditIdentityId);
-                    if (!userFoundInCache)
-                    {
-                        var auditIdentityResponseDto = await _client.AuditIdentityAsync(auditLog.AuditIdentityId);
-                        userLookup.Add(auditLog.AuditIdentityId, auditIdentityResponseDto);
-                    }
-
-                    userLookup.TryGetValue(auditLog.AuditIdentityId, out var userDtoCache);
-
-                    permissionAuditLogWithUser.Add(new PermissionAuditLogViewDto(
-                        auditLog.PermissionId,
-                        auditLog.AuditIdentityId,
-                        userDtoCache?.DisplayName ?? throw new KeyNotFoundException("User not found"),
-                        auditLog.PermissionChangeType == PermissionChangeType.DescriptionChange ? PermissionAuditLogType.DescriptionChange : PermissionAuditLogType.Unknown,
-                        auditLog.Timestamp,
-                        auditLog.Value));
-                }
-
-                return new PermissionAuditLogsViewDto(permissionAuditLogWithUser);
-            });
         }
     }
 }
--- libs/dh/shared/domain/src/lib/generated/v1/api/market-participant-user-http.service.ts
+++ libs/dh/shared/domain/src/lib/generated/v1/api/market-participant-user-http.service.ts
@@ -23,8 +23,6 @@ import { MarketParticipantGetActorsAssociatedWithExternalUserIdResponse } from '
 // @ts-ignore
 import { MarketParticipantGetActorsAssociatedWithUserResponse } from '../model/market-participant-get-actors-associated-with-user-response';
 // @ts-ignore
-import { MarketParticipantUserAuditLogsDto } from '../model/market-participant-user-audit-logs-dto';
-// @ts-ignore
 import { MarketParticipantUserIdentityUpdateDto } from '../model/market-participant-user-identity-update-dto';
 // @ts-ignore
 import { MarketParticipantUserInvitationDto } from '../model/market-participant-user-invitation-dto';
@@ -292,77 +290,6 @@ export class MarketParticipantUserHttp {
         return this.httpClient.request<MarketParticipantGetActorsAssociatedWithExternalUserIdResponse>('get', `${this.configuration.basePath}${localVarPath}`,
             {
                 context: localVarHttpContext,
-                responseType: <any>responseType_,
-                withCredentials: this.configuration.withCredentials,
-                headers: localVarHeaders,
-                observe: observe,
-                reportProgress: reportProgress
-            }
-        );
-    }
-
-    /**
-     * Retrieves the audit log history for the specified user.
-     * @param userId 
-     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
-     * @param reportProgress flag to report request and response progress.
-     */
-    public v1MarketParticipantUserGetUserAuditLogsGet(userId?: string, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'text/plain' | 'application/json' | 'text/json', context?: HttpContext}): Observable<MarketParticipantUserAuditLogsDto>;
-    public v1MarketParticipantUserGetUserAuditLogsGet(userId?: string, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'text/plain' | 'application/json' | 'text/json', context?: HttpContext}): Observable<HttpResponse<MarketParticipantUserAuditLogsDto>>;
-    public v1MarketParticipantUserGetUserAuditLogsGet(userId?: string, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'text/plain' | 'application/json' | 'text/json', context?: HttpContext}): Observable<HttpEvent<MarketParticipantUserAuditLogsDto>>;
-    public v1MarketParticipantUserGetUserAuditLogsGet(userId?: string, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'text/plain' | 'application/json' | 'text/json', context?: HttpContext}): Observable<any> {
-
-        let localVarQueryParameters = new HttpParams({encoder: this.encoder});
-        if (userId !== undefined && userId !== null) {
-          localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
-            <any>userId, 'userId');
-        }
-
-        let localVarHeaders = this.defaultHeaders;
-
-        let localVarCredential: string | undefined;
-        // authentication (Bearer) required
-        localVarCredential = this.configuration.lookupCredential('Bearer');
-        if (localVarCredential) {
-            localVarHeaders = localVarHeaders.set('Authorization', 'Bearer ' + localVarCredential);
-        }
-
-        let localVarHttpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
-        if (localVarHttpHeaderAcceptSelected === undefined) {
-            // to determine the Accept header
-            const httpHeaderAccepts: string[] = [
-                'text/plain',
-                'application/json',
-                'text/json'
-            ];
-            localVarHttpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
-        }
-        if (localVarHttpHeaderAcceptSelected !== undefined) {
-            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
-        }
-
-        let localVarHttpContext: HttpContext | undefined = options && options.context;
-        if (localVarHttpContext === undefined) {
-            localVarHttpContext = new HttpContext();
-        }
-
-
-        let responseType_: 'text' | 'json' | 'blob' = 'json';
-        if (localVarHttpHeaderAcceptSelected) {
-            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
-                responseType_ = 'text';
-            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
-                responseType_ = 'json';
-            } else {
-                responseType_ = 'blob';
-            }
-        }
-
-        let localVarPath = `/v1/MarketParticipantUser/GetUserAuditLogs`;
-        return this.httpClient.request<MarketParticipantUserAuditLogsDto>('get', `${this.configuration.basePath}${localVarPath}`,
-            {
-                context: localVarHttpContext,
-                params: localVarQueryParameters,
                 responseType: <any>responseType_,
                 withCredentials: this.configuration.withCredentials,
                 headers: localVarHeaders,
