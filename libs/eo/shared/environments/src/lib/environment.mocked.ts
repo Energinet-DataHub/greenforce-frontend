e80fd5ae09b520f931784a2aa42c28632437e5ef
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-01-05 09:33:06 +0100

    feat(eo): re-enable MSW

--- libs/eo/shared/environments/src/lib/environment.mocked.ts
+++ libs/eo/shared/environments/src/lib/environment.mocked.ts
@@ -35,7 +35,6 @@ export const environment: EoEnvironment = {
 /**
  * Mock Service Worker
  */
-/*
 import { setupServiceWorker } from '@energinet-datahub/gf/util-msw';
 import { loadEoApiEnvironment } from './api-environment/load-eo-api-environment';
 
@@ -45,4 +44,3 @@ import { mocks } from '@energinet-datahub/eo/shared/data-access-mocks';
 loadEoApiEnvironment('eo-api-environment.local.json').then((env) => {
   setupServiceWorker(env.apiBase, mocks);
 });
-*/
