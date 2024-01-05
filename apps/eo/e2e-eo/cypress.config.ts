240db65a0a72b9b9ea597981aa60041ac752c6c9
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-01-05 08:57:33 +0100

    feat(eo): Update Cypress configuration

--- apps/eo/e2e-eo/cypress.config.ts
+++ apps/eo/e2e-eo/cypress.config.ts
@@ -15,22 +15,18 @@
  * limitations under the License.
  */
 import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 export default defineConfig({
   e2e: {
+    ...nxE2EPreset(__dirname),
     specPattern: '**/*.feature',
     supportFile: false,
     chromeWebSecurity: false,
-    fileServerFolder: '.',
-    fixturesFolder: './src/fixtures',
     video: true,
-    videosFolder: 'dist/cypress/apps/eo/e2e-eo/videos',
-    viewportHeight: 800,
-    viewportWidth: 1280,
-    screenshotsFolder: 'dist/cypress/apps/eo/e2e-eo/screenshots',
     async setupNodeEvents(
       on: Cypress.PluginEvents,
       config: Cypress.PluginConfigOptions
