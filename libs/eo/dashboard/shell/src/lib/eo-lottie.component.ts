c909fbb176e12a787c2214999beab3a3f84610c7
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-01-08 08:54:15 +0100

    feat(eo): Remove ngx-lottie and add EoLottieComponent

---
+++ libs/eo/dashboard/shell/src/lib/eo-lottie.component.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import lottie, { AnimationItem } from 'lottie-web';
+
+@Component({
+  selector: 'eo-lottie',
+  standalone: true,
+  template: '<div #lottieContainer [style.width]="width" [style.height]="height"></div>'
+})
+export class EoLottieComponent implements OnInit, OnDestroy {
+  @ViewChild('lottieContainer', {static: true}) lottieContainer!: ElementRef;
+  @Input() animationData!: unknown;
+  @Input() width: string = '100%';
+  @Input() height: string = '100%';
+
+  private animationInstance!: AnimationItem;
+
+  ngOnInit(): void {
+    this.animationInstance = lottie.loadAnimation({
+      container: this.lottieContainer.nativeElement,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: this.animationData,
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.animationInstance) {
+      this.animationInstance.destroy();
+    }
+  }
+}
