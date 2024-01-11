ea56c52a930c369036f30f87f7976938b0888183
Morten Hansen <mimo@mimo-design.com>
2024-01-11 14:10:29 +0100

    return promise

--- libs/gf/msw/util-msw/src/lib/setup-service-worker.ts
+++ libs/gf/msw/util-msw/src/lib/setup-service-worker.ts
@@ -20,9 +20,10 @@ import { setupWorker } from 'msw/browser';
 export function setupServiceWorker(apiBase: string, mocks: mocks) {
   try {
     const worker = setupWorker(...handlers(apiBase, mocks));
-    worker.start({ onUnhandledRequest });
+    return worker.start({ onUnhandledRequest });
     // eslint-disable-next-line no-empty
   } catch (error) {
     console.error(error);
   }
+  return null;
 }
