47e978ef9297eb83ec88eb39cb7f82eb3ed896ab
Jeppe Langhoff Sørensen <jeppelanghoff@gmail.com>
2024-01-16 14:18:50 +0100

    use ngClass

--- libs/watt/src/lib/components/expandable-card/watt-expandable-card.component.ts
+++ libs/watt/src/lib/components/expandable-card/watt-expandable-card.component.ts
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { NgIf, NgTemplateOutlet } from '@angular/common';
+import { CommonModule, NgIf, NgTemplateOutlet } from '@angular/common';
 import {
   Component,
   ContentChild,
@@ -41,7 +41,7 @@ export class WattExpandableCardContentDirective {
  */
 @Component({
   standalone: true,
-  imports: [NgIf, NgTemplateOutlet, MatExpansionModule],
+  imports: [NgIf, NgTemplateOutlet, MatExpansionModule, CommonModule],
   encapsulation: ViewEncapsulation.None,
   selector: 'watt-expandable-card',
   styleUrls: ['./watt-expandable-card.component.scss'],
@@ -55,10 +55,7 @@ export class WattExpandableCardComponent {
   _content?: WattExpandableCardContentDirective;
   /** Whether the card is elevated or has solid border */
   @Input() variant: 'solid' | 'elevation' = 'elevation';
-  @HostBinding('class')
-  get cssClass() {
-    return `watt-expandable-card watt-${this.variant}`;
-  }
+  variantClass = `watt-${this.variant}`;
 }
 
 @Component({
