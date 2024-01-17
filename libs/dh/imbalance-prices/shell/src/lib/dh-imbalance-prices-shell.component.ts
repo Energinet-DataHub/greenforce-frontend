289f0b00dd2b1c99a7fe1b7165119bc07a9680d8
Dzhavat Ushev <dzu@energinet.dk>
2024-01-17 13:09:43 +0100

    feat(dh): upload imbalance prices (#2422)
    * chore(dh): generate data-access-api library
    
    * feat(dh): expose imbalance prices store
    
    * fix(dh): rename upload method
    
    * chore(dh): mock upload endpoint
    
    * feat(dh): add upload file button
    
    * feat(dh): add permission to upload button
    
    * chore: add license
    
    * refactor(dh): move method to constructor
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/dh/imbalance-prices/shell/src/lib/dh-imbalance-prices-shell.component.ts
+++ libs/dh/imbalance-prices/shell/src/lib/dh-imbalance-prices-shell.component.ts
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { TranslocoDirective } from '@ngneat/transloco';
 import { Apollo } from 'apollo-angular';
@@ -22,15 +22,18 @@ import { Apollo } from 'apollo-angular';
 import { WATT_CARD } from '@energinet-datahub/watt/card';
 import {
   VaterFlexComponent,
+  VaterSpacerComponent,
   VaterStackComponent,
   VaterUtilityDirective,
 } from '@energinet-datahub/watt/vater';
 import { WattTableDataSource } from '@energinet-datahub/watt/table';
 import { WattPaginatorComponent } from '@energinet-datahub/watt/paginator';
 import { GetImbalancePricesOverviewDocument } from '@energinet-datahub/dh/shared/domain/graphql';
+import { DhPermissionRequiredDirective } from '@energinet-datahub/dh/shared/feature-authorization';
 
 import { DhImbalancePricesTableComponent } from './table/dh-table.component';
 import { DhImbalancePrice } from './dh-imbalance-prices';
+import { DhImbalancePricesUploaderComponent } from './file-uploader/dh-imbalance-prices-uploader.component';
 
 @Component({
   selector: 'dh-imbalance-prices-shell',
@@ -62,18 +65,21 @@ import { DhImbalancePrice } from './dh-imbalance-prices';
     VaterFlexComponent,
     VaterStackComponent,
     VaterUtilityDirective,
+    VaterSpacerComponent,
     WattPaginatorComponent,
 
     DhImbalancePricesTableComponent,
+    DhImbalancePricesUploaderComponent,
+    DhPermissionRequiredDirective,
   ],
 })
-export class DhImbalancePricesShellComponent implements OnInit {
+export class DhImbalancePricesShellComponent {
   private readonly apollo = inject(Apollo);
   private readonly destroyRef = inject(DestroyRef);
 
-  private readonly getImbalancePricesOverview$ = this.apollo.watchQuery({
-    useInitialLoading: true,
+  private readonly getImbalancePricesOverview$ = this.apollo.query({
     notifyOnNetworkStatusChange: true,
+    fetchPolicy: 'network-only',
     query: GetImbalancePricesOverviewDocument,
   });
 
@@ -83,20 +89,29 @@ export class DhImbalancePricesShellComponent implements OnInit {
   isLoading = false;
   hasError = false;
 
-  ngOnInit(): void {
-    this.getImbalancePricesOverview$.valueChanges
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: (result) => {
-          this.isLoading = result.loading;
+  constructor() {
+    this.fetchData();
+  }
 
-          this.tableDataSource.data = result.data?.imbalancePricesOverview.pricePeriods ?? [];
-          this.totalCount = result.data?.imbalancePricesOverview.pricePeriods.length ?? 0;
-        },
-        error: () => {
-          this.hasError = true;
-          this.isLoading = false;
-        },
-      });
+  onUploadSuccess(): void {
+    this.tableDataSource.data = [];
+    this.fetchData();
+  }
+
+  private fetchData(): void {
+    this.isLoading = true;
+
+    this.getImbalancePricesOverview$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
+      next: ({ loading, data }) => {
+        this.isLoading = loading;
+
+        this.tableDataSource.data = data.imbalancePricesOverview.pricePeriods;
+        this.totalCount = data.imbalancePricesOverview.pricePeriods.length;
+      },
+      error: () => {
+        this.hasError = true;
+        this.isLoading = false;
+      },
+    });
   }
 }
