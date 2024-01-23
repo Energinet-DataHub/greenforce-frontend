6be3922775993077c280128db6c6b9052038c9b4
Dzhavat Ushev <dzu@energinet.dk>
2024-01-23 09:11:00 +0100

    chore(dh): update mock data

--- libs/dh/imbalance-prices/shell/src/lib/drawer/dh-month-view.mock.ts
+++ libs/dh/imbalance-prices/shell/src/lib/drawer/dh-month-view.mock.ts
@@ -1,33 +1,48 @@
 import {
+  ImbalancePrice,
   ImbalancePriceDaily,
   ImbalancePriceDailyCompletenessStatus,
   PriceAreaCode,
 } from '@energinet-datahub/dh/shared/domain/graphql';
+
+const imbalancePrices: ImbalancePrice[] = [
+  {
+    __typename: 'ImbalancePrice',
+    priceAreaCode: PriceAreaCode.Dk1,
+    price: 109.000451,
+    timestamp: new Date('2024-01-01T00:00:00.000+01:00'),
+  },
+  {
+    __typename: 'ImbalancePrice',
+    priceAreaCode: PriceAreaCode.Dk1,
+    price: 76.410051,
+    timestamp: new Date('2024-01-01T01:00:00.000+01:00'),
+  },
+  {
+    __typename: 'ImbalancePrice',
+    priceAreaCode: PriceAreaCode.Dk1,
+    price: -100.041549,
+    timestamp: new Date('2024-01-01T02:00:00.000+01:00'),
+  },
+];
 
 export const monthViewMock: ImbalancePriceDaily[] = [
   {
     __typename: 'ImbalancePriceDaily',
     timeStamp: new Date('2024-01-01'),
     status: ImbalancePriceDailyCompletenessStatus.Complete,
-    imbalancePrices: [
-      {
-        __typename: 'ImbalancePrice',
-        priceAreaCode: PriceAreaCode.Dk1,
-        price: 109.000451,
-        timestamp: new Date('2024-01-01T00:00:00.000+01:00'),
-      },
-    ],
+    imbalancePrices,
   },
   {
     __typename: 'ImbalancePriceDaily',
     timeStamp: new Date('2024-01-02'),
     status: ImbalancePriceDailyCompletenessStatus.InComplete,
-    imbalancePrices: [],
+    imbalancePrices,
   },
   {
     __typename: 'ImbalancePriceDaily',
     timeStamp: new Date('2024-01-03'),
     status: ImbalancePriceDailyCompletenessStatus.NoData,
-    imbalancePrices: [],
+    imbalancePrices,
   },
 ];
