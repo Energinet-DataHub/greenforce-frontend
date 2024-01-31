d011ec1c9ffa37052e03b46e88ca16e3dc941a9e
Morten Hansen <mimo@mimo-design.com>
2024-01-31 13:17:03 +0100

    Use worker to speedup and provide feedback for parsing documents

--- libs/watt/src/lib/components/code/watt-code.component.ts
+++ libs/watt/src/lib/components/code/watt-code.component.ts
@@ -17,35 +17,51 @@
 import {
   Component,
   ChangeDetectionStrategy,
-  Input,
   ViewEncapsulation,
   OnInit,
   signal,
+  input,
 } from '@angular/core';
+
+import { WattSpinnerComponent } from '../spinner';
+import { VaterStackComponent } from '../vater';
 
 @Component({
   selector: 'watt-code',
   template: `
-    <pre>
-        <code>{{ formattedCode() }}</code>
+    @if (loading()) {
+      <vater-stack [fill]="'horizontal'" [align]="'center'"><watt-spinner /></vater-stack>
+    } @else {
+      <pre>
+        <code class="hljs" [innerHTML]="formattedCode()"></code>
       </pre>
+    }
   `,
   styleUrls: ['./watt-code.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
+  imports: [WattSpinnerComponent, VaterStackComponent],
   standalone: true,
 })
 export class WattCodeComponent implements OnInit {
-  @Input({ required: true }) code: string | null = null;
-  @Input() lineNumbers = true;
+  code = input.required<string | null>();
 
+  /** @ignore */
   formattedCode = signal<string>('');
+  /** @ignore */
+  loading = signal<boolean>(false);
 
+  /** @ignore */
   ngOnInit(): void {
+    console.log(this.code());
+    if (this.code() === null) return;
+
     const worker = new Worker(new URL('./watt-code.worker.ts', import.meta.url));
     worker.onmessage = (event) => {
       this.formattedCode.set(event.data);
+      this.loading.set(false);
     };
-    worker.postMessage(this.code);
+    worker.postMessage(this.code());
+    this.loading.set(true);
   }
 }
--- libs/watt/src/lib/components/code/watt-code.worker.ts
+++ libs/watt/src/lib/components/code/watt-code.worker.ts
@@ -1,13 +1,6 @@
 import hljs from 'highlight.js';
-import json from 'highlight.js/lib/languages/json';
-import xml from 'highlight.js/lib/languages/xml';
-
-hljs.registerLanguage('json', json);
-hljs.registerLanguage('xml', xml);
 
 onmessage = (event) => {
-  console.log(event.data);
-  const result = hljs.highlightAuto(event.data);
-  console.log(result.value);
+  const result = hljs.highlightAuto(event.data, ['xml', 'json']);
   postMessage(result.value);
 };
