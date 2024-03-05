d2960d15324f838edeb95108551eb679f424e4a1
Morten Vestergaard Hansen <mimo@mimo-design.com>
2024-03-05 15:27:49 +0100

    Update libs/dh/market-participant/actors/feature-delegation/src/index.ts
    Co-authored-by: Dzhavat Ushev <dzu@energinet.dk>

--- libs/dh/market-participant/actors/feature-delegation/src/index.ts
+++ libs/dh/market-participant/actors/feature-delegation/src/index.ts
@@ -15,4 +15,3 @@
  * limitations under the License.
  */
 export { DhDelegationTabComponent } from './lib/dh-delegation-tab.component';
-export { DhDelegationCreateModalComponent as DhCreateDelegationComponent } from './lib/dh-delegation-create-modal.component';
