70e954eb2c1a00d7509214ed0d79c6a20d03984c
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-03-18 16:07:10 +0100

    Fix import statement in watt-datepicker-v2.component.ts

--- libs/watt/src/lib/picker/datepicker/watt-datepicker-v2.component.ts
+++ libs/watt/src/lib/picker/datepicker/watt-datepicker-v2.component.ts
@@ -14,8 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { NgIf } from '@angular/common';
-import { FormatWidth, getLocaleDateFormat } from '@angular/common';
+import { FormatWidth, getLocaleDateFormat, NgIf } from '@angular/common';
 import {
   ChangeDetectorRef,
   Component,
