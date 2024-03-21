32ef0015d292a1c4d01adf0aa07579d09b2776d9
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-03-21 10:56:33 +0100

    trigger ci

--- libs/gf/msw/e2e-util-msw/cy-custom-mount.ts
+++ libs/gf/msw/e2e-util-msw/cy-custom-mount.ts
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { Type } from '@angular/core';
 import { MountConfig, mount } from 'cypress/angular';
 
