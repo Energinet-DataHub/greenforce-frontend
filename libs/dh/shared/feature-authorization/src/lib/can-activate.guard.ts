9b2419385189665cc808b4ec5a012bc45c57d85e
Morten Hansen <mimo@mimo-design.com>
2024-03-25 11:48:40 +0100

    Only redirect to login on local

--- libs/dh/shared/feature-authorization/src/lib/can-activate.guard.ts
+++ libs/dh/shared/feature-authorization/src/lib/can-activate.guard.ts
@@ -1,10 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
+import { DhFeatureFlagsService } from '@energinet-datahub/dh/shared/feature-flags';
 
 export const AuthGuard: CanActivateFn = (): boolean => {
   const router = inject(Router);
   const msalService = inject(MsalService);
+  const featureFlagService = inject(DhFeatureFlagsService);
+
+  if (!featureFlagService.isEnabled('new-login-flow')) return true;
 
   if (msalService.instance.getAllAccounts().length > 0) return true;
 
