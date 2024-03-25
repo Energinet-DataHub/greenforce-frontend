72a340e49f65092f02cbdd25cdf7f11b7689e55f
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-03-25 09:02:08 +0100

    fix: linting

--- libs/eo/shared/data-access-mocks/src/lib/metering-points.ts
+++ libs/eo/shared/data-access-mocks/src/lib/metering-points.ts
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* eslint-disable sonarjs/no-duplicated-branches */
 import { http, HttpResponse } from 'msw';
 
 export function meteringPointsMocks(apiBase: string) {
