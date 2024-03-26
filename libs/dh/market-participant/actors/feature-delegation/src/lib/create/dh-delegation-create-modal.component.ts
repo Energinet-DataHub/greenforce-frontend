f56cf9185ec82939dffd409aa96f3b27009f58bd
Dzhavat Ushev <dzu@energinet.dk>
2024-03-26 14:42:34 +0100

    chore(dh): remove unused type

--- libs/dh/market-participant/actors/feature-delegation/src/lib/create/dh-delegation-create-modal.component.ts
+++ libs/dh/market-participant/actors/feature-delegation/src/lib/create/dh-delegation-create-modal.component.ts
@@ -56,30 +56,6 @@ import { parseGraphQLErrorResponse } from '@energinet-datahub/dh/shared/data-acc
 import { DhActorExtended } from '@energinet-datahub/dh/market-participant/actors/domain';
 import { readApiErrorResponse } from '@energinet-datahub/dh/market-participant/data-access-api';
 
-/** TODO: Remove when Typescript 5.4 lands with support for groupBy  */
-declare global {
-  interface ObjectConstructor {
-    /**
-     * Groups members of an iterable according to the return value of the passed callback.
-     * @param items An iterable.
-     * @param keySelector A callback which will be invoked for each item in items.
-     */
-    groupBy<K extends PropertyKey, T>(
-      items: Iterable<T>,
-      keySelector: (item: T, index: number) => K
-    ): Partial<Record<K, T[]>>;
-  }
-
-  interface MapConstructor {
-    /**
-     * Groups members of an iterable according to the return value of the passed callback.
-     * @param items An iterable.
-     * @param keySelector A callback which will be invoked for each item in items.
-     */
-    groupBy<K, T>(items: Iterable<T>, keySelector: (item: T, index: number) => K): Map<K, T[]>;
-  }
-}
-
 @Component({
   selector: 'dh-create-delegation',
   standalone: true,
