06f0776aab16880d1f836eae12d9e9d594562a99
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-04-05 10:08:32 +0200

    Remove unnecessary imports from picker shared module

--- libs/watt/src/lib/picker/shared/index.ts
+++ libs/watt/src/lib/picker/shared/index.ts
@@ -16,7 +16,5 @@
  */
 export * from './maskito-time-range-mask';
 export * from './placeholder-mask/watt-placeholder-mask.component';
-export * from './watt-input-mask.service';
 export * from './watt-picker-base';
 export * from './watt-picker-value';
-export * from './watt-range-input.service';
