7ced2831355e1d04c89b19a01b37165df5aeba31
Morten Hansen <mimo@mimo-design.com>
2024-04-24 10:56:44 +0200

    Add defaultCommandTimeout to cypress.config.ts

--- apps/dh/e2e-dh/cypress.config.ts
+++ apps/dh/e2e-dh/cypress.config.ts
@@ -20,4 +20,5 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 export default defineConfig({
   e2e: nxE2EPreset(__dirname),
   video: true,
+  defaultCommandTimeout: 6000,
 });
