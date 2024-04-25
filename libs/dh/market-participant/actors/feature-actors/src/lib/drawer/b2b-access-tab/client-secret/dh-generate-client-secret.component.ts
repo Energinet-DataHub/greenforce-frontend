c6ca8d34fe656e39e4d0d5da8197bebc03bce98a
Morten Hansen <mimo@mimo-design.com>
2024-04-25 08:13:22 +0200

    Fixed lint

--- libs/dh/market-participant/actors/feature-actors/src/lib/drawer/b2b-access-tab/client-secret/dh-generate-client-secret.component.ts
+++ libs/dh/market-participant/actors/feature-actors/src/lib/drawer/b2b-access-tab/client-secret/dh-generate-client-secret.component.ts
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, Input, inject, output } from '@angular/core';
+import { Component, input, inject, output } from '@angular/core';
 import { TranslocoDirective, TranslocoService } from '@ngneat/transloco';
 import { toSignal } from '@angular/core/rxjs-interop';
 
@@ -22,7 +22,6 @@ import { WattButtonComponent } from '@energinet-datahub/watt/button';
 import { WattToastService } from '@energinet-datahub/watt/toast';
 import { DhMarketPartyB2BAccessStore } from '@energinet-datahub/dh/market-participant/actors/data-access-api';
 import { DhActorAuditLogService } from '../../dh-actor-audit-log.service';
-import { input } from '@angular/core';
 
 @Component({
   selector: 'dh-generate-client-secret',
