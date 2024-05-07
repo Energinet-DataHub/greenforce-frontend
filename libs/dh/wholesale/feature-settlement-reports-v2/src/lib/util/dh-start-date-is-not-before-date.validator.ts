cfc3c9af02c876962bfba0aa1decb1c8488b9152
Dzhavat Ushev <dzu@energinet.dk>
2024-05-07 14:52:09 +0200

    feat(dh): improvements to request settlement report modal (#2863)
    * chore(dh): format file
    
    * chore(dh): format file
    
    * feat(dh): improvements to request settlement report modal
    
    * chore(dh): debug CI
    
    * fix(dh): use valueOf() when comparing dates
    
    * fix(dh): add missing translation
    
    * fix(dh): remove flaky test
    
    * refactor(dh): use function to convert enum to dropdown options

---
+++ libs/dh/wholesale/feature-settlement-reports-v2/src/lib/util/dh-start-date-is-not-before-date.validator.ts
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+import { WattRange, dayjs } from '@energinet-datahub/watt/date';
+
+export const dhStartDateIsNotBeforeDateValidator =
+  (date: Date) =>
+  (control: AbstractControl<WattRange<string> | null>): ValidationErrors | null => {
+    const range = control.value;
+
+    if (range === null) {
+      return null;
+    }
+
+    if (dayjs(range.start).isBefore(date)) {
+      return { startDateIsBeforeMinDate: true };
+    }
+
+    return null;
+  };
