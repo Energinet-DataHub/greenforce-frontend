7c8e1107bc02652cb5b61f3239d2db4d8ce8331d
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2024-05-14 11:11:02 +0200

    Add comment explaining 30 days condition

--- libs/dh/wholesale/feature-request-calculation/src/lib/dh-wholesale-request-calculation-validators.ts
+++ libs/dh/wholesale/feature-request-calculation/src/lib/dh-wholesale-request-calculation-validators.ts
@@ -21,5 +21,7 @@ type rangeControl = AbstractControl<WattRange<string>>;
 
 export const max31DaysDateRangeValidator: ValidatorFn = ({ value }: rangeControl) => {
   if (!value?.end || !value?.start) return null;
+  // Since the date range does not include the last millisecond (ends at 23:59:59.999),
+  // this condition checks for 30 days, not 31 days (as the diff is in whole days only).
   return dayjs(value.end).diff(value.start, 'days') > 30 ? { max31DaysDateRange: true } : null;
 };
