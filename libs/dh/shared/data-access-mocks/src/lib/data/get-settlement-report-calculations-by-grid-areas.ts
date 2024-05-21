e34249f613842f0bca23246db1e9f40ff7b1c72a
Dzhavat Ushev <dzu@energinet.dk>
2024-05-21 09:08:43 +0200

    feat(dh): select calculation for period and grid area (#2904)
    * feat(dh): add GraphQL query and mock data
    
    * feat(dh): select calculation for period and grid area
    
    * chore(dh): revert delay
    
    * chore(dh): add English translations
    
    ---------
    
    Co-authored-by: Morten Vestergaard Hansen <mimo@mimo-design.com>

---
+++ libs/dh/shared/data-access-mocks/src/lib/data/get-settlement-report-calculations-by-grid-areas.ts
@@ -0,0 +1,66 @@
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { GetSettlementReportCalculationsByGridAreasQuery } from '@energinet-datahub/dh/shared/domain/graphql';
+
+export const mockSettlementReportCalculationsByGridAreas: GetSettlementReportCalculationsByGridAreasQuery =
+  {
+    __typename: 'Query',
+    settlementReportGridAreaCalculationsForPeriod: [
+      {
+        __typename: 'KeyValuePairOfStringAndListOfRequestSettlementReportGridAreaCalculation',
+        key: '001',
+        value: [
+          {
+            __typename: 'RequestSettlementReportGridAreaCalculation',
+            calculationDate: new Date('2021-12-01T23:00:00Z'),
+            calculationId: '911d0c33-3232-49e1-a0ef-bcef313d1098',
+            gridAreaWithName: {
+              __typename: 'GridAreaDto',
+              code: '001',
+              displayName: '001 • Sort netvirksomhed',
+            },
+          },
+        ],
+      },
+      {
+        __typename: 'KeyValuePairOfStringAndListOfRequestSettlementReportGridAreaCalculation',
+        key: '002',
+        value: [
+          {
+            __typename: 'RequestSettlementReportGridAreaCalculation',
+            calculationDate: new Date('2022-12-09T23:00:00Z'),
+            calculationId: '8ff516a1-95b0-4f07-9b58-3fb94791casd',
+            gridAreaWithName: {
+              __typename: 'GridAreaDto',
+              code: '002',
+              displayName: '002 • Gul netvirksomhed',
+            },
+          },
+          {
+            __typename: 'RequestSettlementReportGridAreaCalculation',
+            calculationDate: new Date('2022-12-01T23:00:00Z'),
+            calculationId: '911d0c33-3232-49e1-00aa-bcef313d1098',
+            gridAreaWithName: {
+              __typename: 'GridAreaDto',
+              code: '002',
+              displayName: '002 • Gul netvirksomhed',
+            },
+          },
+        ],
+      },
+    ],
+  };
