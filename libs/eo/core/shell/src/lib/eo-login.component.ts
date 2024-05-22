5c45d821e79cf7f8540fb19a091bb30c5db571dc
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-05-22 14:38:15 +0200

    chore: Update redirection path in eo-login.component.ts

--- libs/eo/core/shell/src/lib/eo-login.component.ts
+++ libs/eo/core/shell/src/lib/eo-login.component.ts
@@ -67,8 +67,8 @@ export class EoLoginComponent {
         }
 
         if (scope.includes('dashboard')) {
-          const path = redirectionPath
-            ? redirectionPath && redirectionPath !== '/'
+          const path = redirectionPath && redirectionPath !== '/'
+            ? redirectionPath
             : `/${this.transloco.getActiveLang()}/dashboard`;
           this.router.navigate([path]);
 
