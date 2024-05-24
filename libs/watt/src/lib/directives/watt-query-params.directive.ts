f02eee195120721a5d1f71d7c5d100ead5b2a2e9
Morten Hansen <mimo@mimo-design.com>
2024-05-24 14:34:38 +0200

    Store filter state with directive

--- libs/watt/src/lib/directives/watt-query-params.directive.ts
+++ libs/watt/src/lib/directives/watt-query-params.directive.ts
@@ -1,6 +1,7 @@
 import { DestroyRef, Directive, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormGroupDirective } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Directive({
   standalone: true,
@@ -9,13 +10,37 @@ import { FormGroupDirective } from '@angular/forms';
 export class WattQueryParamsDirective implements OnInit {
   private formGroup = inject(FormGroupDirective);
   private destoryRef = inject(DestroyRef);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
   ngOnInit(): void {
-    console.log(this.formGroup.valueChanges);
     this.formGroup.valueChanges
       ?.pipe(takeUntilDestroyed(this.destoryRef))
       .subscribe((formValues) => {
-        console.log({ formValues });
+        for (const key in formValues) {
+          if (formValues[key] === null || formValues[key] === undefined) {
+            delete formValues[key];
+          }
+        }
+
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: {
+            state: JSON.stringify(formValues),
+          },
+          queryParamsHandling: 'merge',
+        });
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: { state: JSON.stringify(formValues) },
+          queryParamsHandling: 'merge',
+        });
       });
+
+    if (location.search) {
+      const queryParams = new URLSearchParams(location.search);
+      const value = JSON.parse(queryParams.get('state') ?? '');
+      this.formGroup.control.patchValue(value);
+    }
   }
 }
