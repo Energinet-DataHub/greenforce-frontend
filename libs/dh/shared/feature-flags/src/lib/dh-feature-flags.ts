e8e2546daf805a5c658e0beb2d8d9350b3fa6274
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-05-27 13:53:24 +0200

    feat: Enable settlement reports everywhere except prod.

--- libs/dh/shared/feature-flags/src/lib/dh-feature-flags.ts
+++ libs/dh/shared/feature-flags/src/lib/dh-feature-flags.ts
@@ -49,9 +49,6 @@ export const dhFeatureFlagsConfig = {
   'settlement-reports-v2': {
     created: '30-04-2024',
     disabledEnvironments: [
-      DhAppEnvironment.test_001,
-      DhAppEnvironment.test_002,
-      DhAppEnvironment.preprod,
       DhAppEnvironment.prod,
     ],
   },
