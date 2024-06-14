121e4d6e76db7db6de425293b55a4cd41b7aa027
Dzhavat Ushev <dzu@energinet.dk>
2024-06-14 08:55:57 +0200

    fix(dh): disable settlement reports v2 on preprod

--- libs/dh/shared/feature-flags/src/lib/dh-feature-flags.ts
+++ libs/dh/shared/feature-flags/src/lib/dh-feature-flags.ts
@@ -48,7 +48,7 @@ export const dhFeatureFlagsConfig = {
   },
   'settlement-reports-v2': {
     created: '30-04-2024',
-    disabledEnvironments: [],
+    disabledEnvironments: [DhAppEnvironment.preprod],
   },
   // This feature flag should be removed in favor of injected environment variables
   // from terraform, whenever the new web application setup is ready (outlaws).
