52f277376f3b8f0bfddd824f9bde5e87c7a7d6a1
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2024-06-26 09:56:53 +0200

    Add mutation wrapper

--- libs/dh/shared/data-access-graphql/src/index.ts
+++ libs/dh/shared/data-access-graphql/src/index.ts
@@ -18,4 +18,4 @@ export { graphQLProviders } from './lib/dh-graphql.providers';
 export { parseGraphQLErrorResponse } from './lib/error-handler';
 export { getActorOptions } from './lib/graphql/get-actors-by-eic-function-for-dropdowns';
 export { getGridAreaOptions } from './lib/graphql/get-grid-area-options';
-export { query } from './lib/signal';
+export { query, mutation } from './lib/signal';
--- libs/dh/shared/data-access-graphql/src/lib/signal.ts
+++ libs/dh/shared/data-access-graphql/src/lib/signal.ts
@@ -20,19 +20,35 @@ import {
   OperationVariables,
   SubscribeToMoreOptions as ApolloSubscribeToMoreOptions,
   NetworkStatus,
+  ApolloQueryResult,
+  FetchResult,
 } from '@apollo/client/core';
 import { Apollo, WatchQueryOptions } from 'apollo-angular';
 import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import {
   BehaviorSubject,
+  Observable,
+  catchError,
+  filter,
+  firstValueFrom,
   map,
+  of,
   shareReplay,
   startWith,
   switchMap,
+  take,
   takeWhile,
+  tap,
   withLatestFrom,
 } from 'rxjs';
 import { exists } from '@energinet-datahub/dh/shared/util-operators';
+import { MutationOptions as ApolloMutationOptions } from 'apollo-angular/types';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+/** Create an observable of ApolloQueryResult from an ApolloError. */
+function fromApolloError<T>(error: ApolloError): Observable<ApolloQueryResult<T>> {
+  return of({ error, data: undefined as T, loading: false, networkStatus: NetworkStatus.error });
+}
 
 // Since the `query` function is a wrapper around Apollo's `watchQuery`, the API is almost
 // exactly the same, with the exception the query field, which is now a separate argument.
@@ -51,7 +67,7 @@ export interface SubscribeToMoreOptions<TData, TSubscriptionData, TSubscriptionV
 export function query<TResult, TVariables extends OperationVariables>(
   // Limited to TypedDocumentNode to ensure the query is statically typed
   query: TypedDocumentNode<TResult, TVariables>,
-  options: QueryOptions<TVariables>
+  options?: QueryOptions<TVariables>
 ) {
   // Inject dependencies
   const client = inject(Apollo);
@@ -60,7 +76,7 @@ export function query<TResult, TVariables extends OperationVariables>(
   // The `refetch` function on Apollo's `QueryRef` does not properly handle backpressure by
   // cancelling the previous request. As a workaround, the `valueChanges` is unsubscribed to and
   // a new `watchQuery` (with optionally new variables) is executed each time `refetch` is called.
-  const variables$ = new BehaviorSubject(options.variables);
+  const variables$ = new BehaviorSubject(options?.variables);
   const ref$ = variables$.pipe(map((v) => client.watchQuery({ ...options, query, variables: v })));
 
   // The inner observable will be recreated each time the `variables$` emits
@@ -78,28 +94,26 @@ export function query<TResult, TVariables extends OperationVariables>(
       )
     ),
     shareReplay(1), // Make sure the ref is available to late subscribers (in `subscribeToMore`)
-    exists() // Only emit the replayed ref if it is not null
+    exists(), // Only emit the replayed ref if it is not null
+    takeUntilDestroyed(destroyRef) // Shared observables need to be completed manually
   );
 
+  // Signals holding the result values
   const data = signal<TResult | undefined>(undefined);
   const error = signal<ApolloError | undefined>(undefined);
   const loading = signal(true);
   const networkStatus = signal<NetworkStatus>(NetworkStatus.loading);
 
   // Update the signal values based on the result of the query
-  const subscription = result$.subscribe({
-    next(result) {
-      data.set(result.data);
+  const subscription = result$
+    .pipe(catchError((error: ApolloError) => fromApolloError<TResult>(error)))
+    .subscribe((result) => {
+      // The `data` field is wrongly typed and can actually be empty
+      data.set(result.data ?? undefined);
       error.set(result.error);
       loading.set(result.loading);
       networkStatus.set(result.networkStatus);
-    },
-    error(err: ApolloError) {
-      error.set(err);
-      loading.set(false);
-      networkStatus.set(NetworkStatus.error);
-    },
-  });
+    });
 
   // Clean up when the component is destroyed
   destroyRef.onDestroy(() => {
@@ -128,12 +142,14 @@ export function query<TResult, TVariables extends OperationVariables>(
         .subscribe({ query, variables, context })
         .pipe(
           map(({ data }) => data),
-          exists(), // The data should generally be available, but type system says otherwise
-          withLatestFrom(refReplay$) // Ensure the ref (and cache) is ready
+          exists(), // The data should generally be available, but types says otherwise
+          withLatestFrom(refReplay$), // Ensure the ref (and cache) is ready,
+          takeUntilDestroyed(destroyRef) // Stop the subscription when the component is destroyed
         )
         .subscribe({
           next([data, ref]) {
             if (!updateQuery) return;
+
             // The update query callback has a second argument named `variables` which contains the
             // variables used by the original query. But the types for `updateQuery` seem to expect
             // subscription variables, which is why that argument is unused. Might be incorrect.
@@ -145,11 +161,69 @@ export function query<TResult, TVariables extends OperationVariables>(
           },
         });
 
-      // Stop the subscription when the component is destroyed
-      destroyRef.onDestroy(() => subscription.unsubscribe());
-
       // Allow the caller to terminate the subscription manually
       return () => subscription.unsubscribe();
+    },
+  };
+}
+
+export interface MutationOptions<TResult, TVariables>
+  extends Omit<ApolloMutationOptions<TResult, TVariables>, 'mutation'> {
+  onCompleted?: (data: TResult, clientOptions?: MutationOptions<TResult, TVariables>) => void;
+  onError?: (error: ApolloError, clientOptions?: MutationOptions<TResult, TVariables>) => void;
+}
+
+/** Signal-based wrapper around Apollo's `mutate` function, made to align with `useMutation`. */
+export function mutation<TResult, TVariables>(
+  // Limited to TypedDocumentNode to ensure the query is statically typed
+  mutation: TypedDocumentNode<TResult, TVariables>,
+  options?: MutationOptions<TResult, TVariables>
+) {
+  // Inject dependencies
+  const client = inject(Apollo);
+  const destroyRef = inject(DestroyRef);
+
+  // Signals holding the result values
+  const data = signal<TResult | undefined>(undefined);
+  const error = signal<ApolloError | undefined>(undefined);
+  const loading = signal(true);
+
+  return {
+    // Upcast to prevent writing to signals
+    data: data as Signal<TResult | undefined>,
+    error: error as Signal<ApolloError | undefined>,
+    loading: loading as Signal<boolean>,
+    mutate(innerOptions: MutationOptions<TResult, TVariables>) {
+      const mergedOptions = { ...options, ...innerOptions };
+      const { onCompleted, onError, ...mutationOptions } = mergedOptions;
+      return firstValueFrom(
+        client.mutate({ ...mutationOptions, mutation }).pipe(
+          // The MutationResult type is different from QueryResult in several ways
+          map(({ errors, ...result }) => ({
+            ...result,
+            error: errors?.length ? new ApolloError({ graphQLErrors: errors }) : undefined,
+          })),
+          catchError((error: ApolloError) => of({ error, data: undefined, loading: false })),
+          // Update the signal values based on the result of the mutation
+          tap((result) => {
+            data.set(result.data ?? undefined);
+            error.set(result.error);
+            loading.set(result.loading);
+          }),
+          // Since this observable returns a promise, it should only emit the final result
+          filter((result) => !result.loading),
+          // Trigger the optional callbacks once the mutation has completed
+          tap((result) => {
+            if (result.error) {
+              onError?.(result.error, mergedOptions);
+            } else if (result.data) {
+              onCompleted?.(result.data, mergedOptions);
+            }
+          }),
+          take(1), // Complete the observable when result is available
+          takeUntilDestroyed(destroyRef) // ...or when the component is destroyed
+        )
+      );
     },
   };
 }
