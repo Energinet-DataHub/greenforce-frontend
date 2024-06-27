2daf2ff3453ffd8721f678dc44463e1892852bb9
Dzhavat Ushev <dzu@energinet.dk>
2024-06-27 12:28:50 +0200

    refactor(dh): use graphql to get a list of user roles

--- libs/dh/admin/data-access-api/src/lib/dh-admin-assignable-user-roles.store.ts
+++ libs/dh/admin/data-access-api/src/lib/dh-admin-assignable-user-roles.store.ts
@@ -14,20 +14,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Observable, Subject, switchMap, tap } from 'rxjs';
 import { ComponentStore } from '@ngrx/component-store';
 import { tapResponse } from '@ngrx/operators';
+import { Apollo } from 'apollo-angular';
+import type { ResultOf } from '@graphql-typed-document-node/core';
 
 import { ErrorState, LoadingState } from '@energinet-datahub/dh/shared/data-access-api';
-import {
-  MarketParticipantUserRoleHttp,
-  MarketParticipantUserRoleDto,
-} from '@energinet-datahub/dh/shared/domain';
+import { GetUserRolesByActorIdDocument } from '@energinet-datahub/dh/shared/domain/graphql';
+
+export type UserRoleItem = ResultOf<typeof GetUserRolesByActorIdDocument>['userRolesByActorId'][0];
 
 interface State {
   readonly requestState: LoadingState | ErrorState;
-  readonly assignableUserRoles: MarketParticipantUserRoleDto[];
+  readonly assignableUserRoles: UserRoleItem[];
 }
 
 const initialState: State = {
@@ -37,13 +38,15 @@ const initialState: State = {
 
 @Injectable()
 export class DhAdminAssignableUserRolesStore extends ComponentStore<State> {
+  private readonly apollo = inject(Apollo);
+
   isInit$ = this.select((state) => state.requestState === LoadingState.INIT);
   isLoading$ = this.select((state) => state.requestState === LoadingState.LOADING);
   hasGeneralError$ = new Subject<void>();
 
   assignableUserRoles$ = this.select((state) => state.assignableUserRoles);
 
-  constructor(private marketParticipantUserRoleHttp: MarketParticipantUserRoleHttp) {
+  constructor() {
     super(initialState);
   }
 
@@ -53,27 +56,25 @@ export class DhAdminAssignableUserRolesStore extends ComponentStore<State> {
         this.resetState();
         this.setLoading(LoadingState.LOADING);
       }),
-      switchMap((actorId) => {
-        return this.marketParticipantUserRoleHttp
-          .v1MarketParticipantUserRoleGetAssignableGet(actorId)
-          .pipe(
-            tapResponse(
-              (userRoles) => {
-                this.setLoading(LoadingState.LOADED);
-                this.updateRoles(userRoles);
-              },
-              () => {
-                this.setLoading(ErrorState.GENERAL_ERROR);
-                this.handleError();
-              }
-            )
-          );
-      })
+      switchMap((actorId) =>
+        this.apollo.query({ query: GetUserRolesByActorIdDocument, variables: { actorId } }).pipe(
+          tapResponse(
+            (response) => {
+              this.setLoading(LoadingState.LOADED);
+              this.updateRoles(response.data.userRolesByActorId);
+            },
+            () => {
+              this.setLoading(ErrorState.GENERAL_ERROR);
+              this.handleError();
+            }
+          )
+        )
+      )
     )
   );
 
   private updateRoles = this.updater(
-    (state: State, assignableUserRoles: MarketParticipantUserRoleDto[]): State => ({
+    (state: State, assignableUserRoles: UserRoleItem[]): State => ({
       ...state,
       assignableUserRoles,
     })
