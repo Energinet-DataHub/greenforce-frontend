cb67cd3305e7b0c1cb25a275001ddd1e1d4917a3
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-07-01 12:39:28 +0200

    Update auth interceptor to handle API requests and include Authorization header

--- libs/eo/shared/services/src/auth/auth.interceptor.ts
+++ libs/eo/shared/services/src/auth/auth.interceptor.ts
@@ -17,54 +17,57 @@
 import {
   HTTP_INTERCEPTORS,
   HttpErrorResponse,
-  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
   HttpStatusCode,
 } from '@angular/common/http';
 import { ClassProvider, Injectable, inject } from '@angular/core';
-import { tap } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 import { TranslocoService } from '@ngneat/transloco';
 
 import { EoAuthService } from './auth.service';
 import { EoAuthStore } from './auth.store';
 import { WattToastService } from '@energinet-datahub/watt/toast';
+import { eoApiEnvironmentToken } from '@energinet-datahub/eo/shared/environments';
 @Injectable()
 export class EoAuthorizationInterceptor implements HttpInterceptor {
+
+  private apiBase: string = inject(eoApiEnvironmentToken).apiBase;
   private authService: EoAuthService = inject(EoAuthService);
   private authStore: EoAuthStore = inject(EoAuthStore);
   private toastService: WattToastService = inject(WattToastService);
   private transloco: TranslocoService = inject(TranslocoService);
-
   private tokenRefreshCalls = ['PUT', 'POST', 'DELETE'];
   private ignoreTokenRefreshUrls = ['/api/auth/token', '/api/authorization/consent/grant'];
 
   intercept(req: HttpRequest<unknown>, handler: HttpHandler) {
-    console.log('INTERCEPT - AUTH', req);
-    if (!req.url.includes('/api')) return handler.handle(req);
+    // Only requests to the API should be handled by this interceptor
+    if (!req.url.includes(this.apiBase)) return handler.handle(req);
 
-    //if (this.#shouldRefreshToken(req)) {
-    //console.log('AUTH - refreshToken');
-    //this.authService.refreshToken().subscribe();
-    //}
+    if (this.shouldRefreshToken(req)) {
+      console.log('AUTH - refreshToken');
+      this.authService.refreshToken().subscribe();
+    }
 
     console.log('ADD AUTH HEADER');
     const authorizedRequest = req.clone({
       headers: req.headers.append('Authorization', `Bearer ${this.authStore.token.getValue()}`),
     });
     return handler.handle(authorizedRequest).pipe(
-      tap((event) => {
-        if (event.type !== HttpEventType.Response) return;
-        if (this.#is403ForbiddenResponse(event)) this.#displayPermissionError();
-        if (this.#is401UnauthorizedResponse(event)) this.authService.logout();
+      catchError((error) => {
+        console.log('AUTH - catchError', error);
+        if (this.is403ForbiddenResponse(error)) this.displayPermissionError();
+        if (this.is401UnauthorizedResponse(error)) {
+          this.authService.logout();
+        }
 
-        console.log('AUTH - nothing triggered');
-      })
+        return throwError(() => error);
+      }),
     );
   }
 
-  #shouldRefreshToken(req: HttpRequest<unknown>): boolean {
+  private shouldRefreshToken(req: HttpRequest<unknown>): boolean {
     const path = new URL(req.urlWithParams).pathname;
     console.log('AUTH - shouldRefreshToken', path, req);
     return (
@@ -72,7 +75,7 @@ export class EoAuthorizationInterceptor implements HttpInterceptor {
     );
   }
 
-  #displayPermissionError() {
+  private displayPermissionError() {
     console.log('AUTH - displayPermissionError');
     this.toastService.open({
       message: this.transloco.translate('You do not have permission to perform this action.'),
@@ -80,12 +83,12 @@ export class EoAuthorizationInterceptor implements HttpInterceptor {
     });
   }
 
-  #is403ForbiddenResponse(error: unknown): boolean {
+  private is403ForbiddenResponse(error: unknown): boolean {
     console.log('AUTH - is403ForbiddenResponse', error);
     return error instanceof HttpErrorResponse && error.status === HttpStatusCode.Forbidden;
   }
 
-  #is401UnauthorizedResponse(error: unknown): boolean {
+  private is401UnauthorizedResponse(error: unknown): boolean {
     console.log('AUTH - is401UnauthorizedResponse', error);
     return error instanceof HttpErrorResponse && error.status === HttpStatusCode.Unauthorized;
   }
