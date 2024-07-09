329ddeb44c678388e46c90cf9dfe4e79ea22799f
sahma19 <saiuan@msn.com>
2024-07-09 12:39:52 +0200

    try redirect again

--- libs/eo/terms/src/lib/eo-terms.component.ts
+++ libs/eo/terms/src/lib/eo-terms.component.ts
@@ -149,19 +149,18 @@ export class EoTermsComponent {
     if (this.startedAcceptFlow) return;
     this.startedAcceptFlow = true;
 
-    this.termsService
-      .acceptTerms()
-      .pipe(switchMap(() => this.authService.refreshToken()))
-      .subscribe({
-        next: () => {
-          this.router.navigate([this.transloco.getActiveLang(), 'login']);
-        },
-        error: () => {
-          this.router.navigate([this.transloco.getActiveLang()]);
-        },
-        complete: () => {
-          this.startedAcceptFlow = false;
-        },
-      });
+    this.termsService.acceptTerms().pipe(
+      switchMap(() => this.authService.refreshToken())
+    ).subscribe({
+      next: () => {
+        this.router.navigate([this.transloco.getActiveLang(), 'login']);
+      },
+      error: () => {
+        this.router.navigate([this.transloco.getActiveLang()]);
+      },
+      complete: () => {
+        this.startedAcceptFlow = false;
+      }
+    });
   }
 }
