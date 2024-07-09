3b7ea8841867a5cafddd716b0cef59ac7c287f48
frontend-specialisten <alexander@frontend-specialisten.dk>
2024-07-09 10:59:41 +0200

    Vcluster/fe ett devportal (#3351)
    * feat(ett, documentation): add markdown documentation area
    
    * feat(ett, nginx): make sure app-eo-landing-page is served when accessing /documentation
    
    * style: format
    
    * fix(eo, landing-page): temp adjust the maximum budget of initial bundle
    
    * chore: Update PRODUCTION_DEPENDENCIES.md
    
    * trigger ci
    
    * fix(ett): linting
    
    * Trying fix 404 for /da|en/documentation/*
    
    * fix nginx config
    
    * prerender /en/documentation/3rd-party-api-usage
    
    * feat(nginx): serve app-eo-landing-page for /documentation
    
    * fix nginx config and prerendered routes
    
    * Add note for adding documentation to the config
    
    * reorder imports
    
    * chore: Update EO API environment documentation configuration URLs
    
    * feat(nginx): update documentation route handling
    
    This commit updates the nginx configuration to properly handle the routes for documentation pages. It fixes the issue with serving the specific file or directory for `/da/documentation/*` and `/en/documentation/*` routes. The updated configuration now tries to serve the specific file or directory at `/app-eo-landing-page/browser/$1/documentation/$2/index.html` for these routes.
    
    Note: This commit is based on recent user commits and repository commits.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/eo/api-documentation/shell/index.ts
+++ libs/eo/api-documentation/shell/index.ts
@@ -14,18 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import { Routes } from '@angular/router';
-import { EoLandingPageShellComponent } from './shell.component';
-
-export const eoLandingPageRoutes: Routes = [
-  { path: '', component: EoLandingPageShellComponent },
-  /**
-   * API documentation
-   */
-  {
-    path: 'documentation',
-    loadChildren: () =>
-      import('@energinet-datahub/eo/api-documentation').then((m) => m.eoApiDocumentationRoutes),
-  },
-];
+export * from './routes';
