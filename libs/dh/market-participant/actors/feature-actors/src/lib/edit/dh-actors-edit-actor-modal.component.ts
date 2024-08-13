9f97adff7a2f295ace0d17169e9f3674a627218f
Dzhavat Ushev <dzu@energinet.dk>
2024-08-13 13:09:06 +0200

    feat(dh): set validation to market participant name (#3414)
    * feat(dh): set length validation to market participant name
    
    * fix(dh): set edit modal to large size
    
    * fix(dh): rename variables

--- libs/dh/market-participant/actors/feature-actors/src/lib/edit/dh-actors-edit-actor-modal.component.ts
+++ libs/dh/market-participant/actors/feature-actors/src/lib/edit/dh-actors-edit-actor-modal.component.ts
@@ -36,6 +36,10 @@ import {
   UpdateActorDocument,
   UpdateActorMutation,
 } from '@energinet-datahub/dh/shared/domain/graphql';
+import {
+  dhMarketParticipantNameMaxLength,
+  dhMarketParticipantNameMaxLengthValidatorFn,
+} from '../dh-market-participant-name-max-length.validator';
 
 @Component({
   standalone: true,
@@ -85,10 +89,11 @@ export class DhActorsEditActorModalComponent {
   actorEditableFieldsQuery = lazyQuery(GetActorEditableFieldsDocument);
   updateActorMutation = mutation(UpdateActorDocument);
 
+  nameMaxLength = dhMarketParticipantNameMaxLength;
   departmentNameMaxLength = 250;
 
   actorForm = this.formBuilder.group({
-    name: ['', Validators.required],
+    name: ['', [Validators.required, dhMarketParticipantNameMaxLengthValidatorFn]],
     departmentName: ['', [Validators.required, Validators.maxLength(this.departmentNameMaxLength)]],
     departmentEmail: ['', [Validators.required, Validators.email]],
     departmentPhone: ['', Validators.required],
