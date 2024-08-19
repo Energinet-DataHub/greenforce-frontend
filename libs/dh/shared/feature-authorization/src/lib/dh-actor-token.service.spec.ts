1911fa266015c765b45bd02fa27ff207c6976269
Morten Vestergaard Hansen <mimo@mimo-design.com>
2024-08-19 13:08:01 +0200

    Reworked token service not to use swagger clients (#3438)
    * Reworked token service not to use swagger clients
    
    * Fixed test

--- libs/dh/shared/feature-authorization/src/lib/dh-actor-token.service.spec.ts
+++ libs/dh/shared/feature-authorization/src/lib/dh-actor-token.service.spec.ts
@@ -16,14 +16,20 @@
  */
 
 import { DhActorTokenService } from './dh-actor-token.service';
-import { HttpHandler, HttpRequest, HttpHeaders, HttpResponse } from '@angular/common/http';
+import {
+  HttpHandler,
+  HttpRequest,
+  HttpHeaders,
+  HttpResponse,
+  provideHttpClient,
+} from '@angular/common/http';
 import { firstValueFrom, of } from 'rxjs';
 
-import { MarketParticipantUserHttp, TokenHttp } from '@energinet-datahub/dh/shared/domain';
 import { LocalStorageFake, SessionStorageFake } from '@energinet-datahub/gf/test-util';
 
 import { DhActorStorage } from './dh-actor-storage';
-import { MsalService } from '@azure/msal-angular';
+import { TestBed } from '@angular/core/testing';
+import { MsalServiceMock } from '@energinet-datahub/dh/shared/test-util-auth';
 
 describe(DhActorTokenService, () => {
   const createActorsRequest = () =>
@@ -35,188 +41,179 @@ describe(DhActorTokenService, () => {
       }
     );
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        MsalServiceMock,
+        DhActorStorage,
+        LocalStorageFake,
+        SessionStorageFake,
+        provideHttpClient(),
+        provideHttpClient(),
+      ],
+    });
+  });
+
   test('should return true if request is for getting actors', async () => {
-    // arrange
-    const request = createActorsRequest();
+    TestBed.runInInjectionContext(() => {
+      // arrange
+      const request = createActorsRequest();
 
-    const target = new DhActorTokenService(
-      {} as MarketParticipantUserHttp,
-      {} as TokenHttp,
-      new DhActorStorage(new LocalStorageFake(), new SessionStorageFake()),
-      {} as MsalService
-    );
+      const target = new DhActorTokenService();
 
-    // act
-    const actual = target.isPartOfAuthFlow(request);
+      // act
+      const actual = target.isPartOfAuthFlow(request);
 
-    // assert
-    expect(actual).toEqual(true);
+      // assert
+      expect(actual).toEqual(true);
+    });
   });
 
   test('should return true if request is for getting token', async () => {
-    // arrange
-    const request = new HttpRequest<string>('GET', 'https://localhost:5000/v1/Token');
+    TestBed.runInInjectionContext(() => {
+      // arrange
+      const request = new HttpRequest<string>('GET', 'https://localhost:5000/v1/Token');
 
-    const target = new DhActorTokenService(
-      {} as MarketParticipantUserHttp,
-      {} as TokenHttp,
-      new DhActorStorage(new LocalStorageFake(), new SessionStorageFake()),
-      {} as MsalService
-    );
+      const target = new DhActorTokenService();
+      // act
+      const actual = target.isPartOfAuthFlow(request);
 
-    // act
-    const actual = target.isPartOfAuthFlow(request);
-
-    // assert
-    expect(actual).toEqual(true);
+      // assert
+      expect(actual).toEqual(true);
+    });
   });
 
   test('should return false if request is not for authentication flow', async () => {
-    // arrange
-    const request = new HttpRequest<string>('GET', 'https://localhost:5000/v1/Not/Relevant');
+    TestBed.runInInjectionContext(() => {
+      // arrange
+      const request = new HttpRequest<string>('GET', 'https://localhost:5000/v1/Not/Relevant');
 
-    const target = new DhActorTokenService(
-      {} as MarketParticipantUserHttp,
-      {} as TokenHttp,
-      new DhActorStorage(new LocalStorageFake(), new SessionStorageFake()),
-      {} as MsalService
-    );
+      const target = new DhActorTokenService();
 
-    // act
-    const actual = target.isPartOfAuthFlow(request);
+      // act
+      const actual = target.isPartOfAuthFlow(request);
 
-    // assert
-    expect(actual).toEqual(false);
+      // assert
+      expect(actual).toEqual(false);
+    });
   });
 
   test('should return false if request is not for internal authentication flow', async () => {
-    // arrange
-    const request = new HttpRequest<string>(
-      'GET',
-      'https://b2cshresdevwe002.b2clogin.com/b2cshresdevwe002.onmicrosoft.com/b2c_some_policy/oauth2/v2.0/token'
-    );
+    TestBed.runInInjectionContext(() => {
+      // arrange
+      const request = new HttpRequest<string>(
+        'GET',
+        'https://b2cshresdevwe002.b2clogin.com/b2cshresdevwe002.onmicrosoft.com/b2c_some_policy/oauth2/v2.0/token'
+      );
 
-    const target = new DhActorTokenService(
-      {} as MarketParticipantUserHttp,
-      {} as TokenHttp,
-      new DhActorStorage(new LocalStorageFake(), new SessionStorageFake()),
-      {} as MsalService
-    );
+      const target = new DhActorTokenService();
 
-    // act
-    const actual = target.isPartOfAuthFlow(request);
+      // act
+      const actual = target.isPartOfAuthFlow(request);
 
-    // assert
-    expect(actual).toEqual(false);
+      // assert
+      expect(actual).toEqual(false);
+    });
   });
 
   test('should call endpoint when cache is empty', async () => {
-    // arrange
-    const response = new HttpResponse<string>({
-      status: 200,
-      body: 'response',
+    await TestBed.runInInjectionContext(async () => {
+      // arrange
+      const response = new HttpResponse<string>({
+        status: 200,
+        body: 'response',
+      });
+      const request = createActorsRequest();
+
+      const handler: HttpHandler = {
+        handle: jest.fn(() => of(response)),
+      };
+
+      const target = new DhActorTokenService();
+
+      // act
+      const actual = (await firstValueFrom(
+        target.handleAuthFlow(request, handler)
+      )) as HttpResponse<string>;
+
+      // assert
+      expect(actual.body).toEqual(response.body);
+      expect(handler.handle).toHaveBeenCalledTimes(1);
     });
-    const request = createActorsRequest();
-
-    const handler: HttpHandler = {
-      handle: jest.fn(() => of(response)),
-    };
-
-    const target = new DhActorTokenService(
-      {} as MarketParticipantUserHttp,
-      {} as TokenHttp,
-      new DhActorStorage(new LocalStorageFake(), new SessionStorageFake()),
-      {} as MsalService
-    );
-
-    // act
-    const actual = (await firstValueFrom(
-      target.handleAuthFlow(request, handler)
-    )) as HttpResponse<string>;
-
-    // assert
-    expect(actual.body).toEqual(response.body);
-    expect(handler.handle).toBeCalledTimes(1);
   });
 
   test('should not call endpoint when cache is current', async () => {
-    // arrange
-    const response = new HttpResponse<string>({
-      status: 200,
-      body: 'response',
+    await TestBed.runInInjectionContext(async () => {
+      // arrange
+      const response = new HttpResponse<string>({
+        status: 200,
+        body: 'response',
+      });
+      const request = createActorsRequest();
+
+      const handler: HttpHandler = {
+        handle: jest.fn(() => of(response)),
+      };
+
+      const target = new DhActorTokenService();
+
+      // act
+      const actualA = (await firstValueFrom(
+        target.handleAuthFlow(request, handler)
+      )) as HttpResponse<string>;
+      const actualB = (await firstValueFrom(
+        target.handleAuthFlow(request, handler)
+      )) as HttpResponse<string>;
+
+      // assert
+      expect(actualA.body).toEqual(response.body);
+      expect(actualB.body).toEqual(response.body);
+      expect(handler.handle).toHaveBeenCalledTimes(1);
     });
-    const request = createActorsRequest();
-
-    const handler: HttpHandler = {
-      handle: jest.fn(() => of(response)),
-    };
-
-    const target = new DhActorTokenService(
-      {} as MarketParticipantUserHttp,
-      {} as TokenHttp,
-      new DhActorStorage(new LocalStorageFake(), new SessionStorageFake()),
-      {} as MsalService
-    );
-
-    // act
-    const actualA = (await firstValueFrom(
-      target.handleAuthFlow(request, handler)
-    )) as HttpResponse<string>;
-    const actualB = (await firstValueFrom(
-      target.handleAuthFlow(request, handler)
-    )) as HttpResponse<string>;
-
-    // assert
-    expect(actualA.body).toEqual(response.body);
-    expect(actualB.body).toEqual(response.body);
-    expect(handler.handle).toBeCalledTimes(1);
   });
 
   test('should call endpoint when cache is invalidated', async () => {
-    // arrange
-    const responseA = new HttpResponse<string>({
-      status: 200,
-      body: 'responseA',
+    await TestBed.runInInjectionContext(async () => {
+      // arrange
+      const responseA = new HttpResponse<string>({
+        status: 200,
+        body: 'responseA',
+      });
+      const responseB = new HttpResponse<string>({
+        status: 200,
+        body: 'responseB',
+      });
+      const requestA = createActorsRequest();
+      const requestB = new HttpRequest<string>(
+        'GET',
+        'https://localhost:5000/v1/MarketParticipantUser/GetUserActors',
+        {
+          headers: new HttpHeaders({ Authorization: 'Bearer abc' }),
+        }
+      );
+
+      const handlerA: HttpHandler = {
+        handle: jest.fn(() => of(responseA)),
+      };
+      const handlerB: HttpHandler = {
+        handle: jest.fn(() => of(responseB)),
+      };
+
+      const target = new DhActorTokenService();
+
+      // act
+      const actualA = (await firstValueFrom(
+        target.handleAuthFlow(requestA, handlerA)
+      )) as HttpResponse<string>;
+      const actualB = (await firstValueFrom(
+        target.handleAuthFlow(requestB, handlerB)
+      )) as HttpResponse<string>;
+
+      // assert
+      expect(actualA.body).toEqual(responseA.body);
+      expect(actualB.body).toEqual(responseB.body);
+      expect(handlerA.handle).toHaveBeenCalledTimes(1);
+      expect(handlerB.handle).toHaveBeenCalledTimes(1);
     });
-    const responseB = new HttpResponse<string>({
-      status: 200,
-      body: 'responseB',
-    });
-    const requestA = createActorsRequest();
-    const requestB = new HttpRequest<string>(
-      'GET',
-      'https://localhost:5000/v1/MarketParticipantUser/GetUserActors',
-      {
-        headers: new HttpHeaders({ Authorization: 'Bearer abc' }),
-      }
-    );
-
-    const handlerA: HttpHandler = {
-      handle: jest.fn(() => of(responseA)),
-    };
-    const handlerB: HttpHandler = {
-      handle: jest.fn(() => of(responseB)),
-    };
-
-    const target = new DhActorTokenService(
-      {} as MarketParticipantUserHttp,
-      {} as TokenHttp,
-      new DhActorStorage(new LocalStorageFake(), new SessionStorageFake()),
-      {} as MsalService
-    );
-
-    // act
-    const actualA = (await firstValueFrom(
-      target.handleAuthFlow(requestA, handlerA)
-    )) as HttpResponse<string>;
-    const actualB = (await firstValueFrom(
-      target.handleAuthFlow(requestB, handlerB)
-    )) as HttpResponse<string>;
-
-    // assert
-    expect(actualA.body).toEqual(responseA.body);
-    expect(actualB.body).toEqual(responseB.body);
-    expect(handlerA.handle).toBeCalledTimes(1);
-    expect(handlerB.handle).toBeCalledTimes(1);
   });
 });
