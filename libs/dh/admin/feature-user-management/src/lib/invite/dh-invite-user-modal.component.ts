28a4c51ad2d49f2a4933a6739c84c0a4fab6470b
Dzhavat Ushev <dzu@energinet.dk>
2024-09-02 14:21:17 +0200

    chore(dh): format file

--- libs/dh/admin/feature-user-management/src/lib/invite/dh-invite-user-modal.component.ts
+++ libs/dh/admin/feature-user-management/src/lib/invite/dh-invite-user-modal.component.ts
@@ -118,6 +118,7 @@ export class DhInviteUserModalComponent extends WattTypedModal {
   inOrganizationMailDomain = computed(() => {
     const email = this.emailChanged();
     const domain = this.domain();
+
     return !!email && !!domain && email.toUpperCase().endsWith(domain.toUpperCase());
   });
 
