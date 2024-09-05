2b37fccda25c3c39dcda26a06dcfc2c349285a8c
Dzhavat Ushev <dzu@energinet.dk>
2024-09-05 13:09:31 +0200

    chore(dh): format file

--- libs/dh/shared/util-application-insights/src/lib/dh-shared-util-application-insights.providers.ts
+++ libs/dh/shared/util-application-insights/src/lib/dh-shared-util-application-insights.providers.ts
@@ -16,6 +16,7 @@
  */
 import { ErrorHandler, makeEnvironmentProviders } from '@angular/core';
 import { ApplicationinsightsAngularpluginErrorService } from '@microsoft/applicationinsights-angularplugin-js';
+
 import { applicationInsightsInitializer } from './dh-application-insights.initializer';
 
 export const applicationInsightsProviders = makeEnvironmentProviders([
