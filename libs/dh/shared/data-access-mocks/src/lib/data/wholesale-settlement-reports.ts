447ccb3b84387068a2cbfb8004dc61f4c983df67
Dzhavat Ushev <dzu@energinet.dk>
2024-09-13 10:42:53 +0200

    feat(dh): create basic settlement report drawer (#3532)
    * chore(dh): move file to util folder
    
    * feat(dh): support seconds in duration component
    
    * fix(dh): remove execution time column from table
    
    * chore(dh): update mock data
    
    * chore(dh): move component to util folder
    
    * feat(dh): create basic drawer component
    
    * feat(dh): open drawer when settlement report row is clicked
    
    * chore(dh): update mock

--- libs/dh/shared/data-access-mocks/src/lib/data/wholesale-settlement-reports.ts
+++ libs/dh/shared/data-access-mocks/src/lib/data/wholesale-settlement-reports.ts
@@ -26,8 +26,9 @@ const periodEnd = new Date('2021-12-02T23:00:00Z');
 const executionTimeStart = new Date('2024-09-11T06:12:00Z');
 
 const executionTimeEnd_Days = new Date('2024-09-12T17:15:00Z');
-const executionTimeStart_Hours = new Date('2024-09-11T14:44:00Z');
-const executionTimeStart_Minutes = new Date('2024-09-11T06:48:00Z');
+const executionTimeEnd_Hours = new Date('2024-09-11T14:44:00Z');
+const executionTimeEnd_Minutes = new Date('2024-09-11T06:48:00Z');
+const executionTimeEnd_Seconds = new Date('2024-09-11T06:12:42Z');
 
 export const wholesaleSettlementReportsQueryMock = (
   apiBase: string
@@ -71,7 +72,7 @@ export const wholesaleSettlementReportsQueryMock = (
       },
       executionTime: {
         start: executionTimeStart,
-        end: executionTimeStart_Hours,
+        end: executionTimeEnd_Hours,
       },
       settlementReportDownloadUrl: `${apiBase}/v1/WholesaleSettlementReport/DownloadReport`,
       fromApi: true,
@@ -92,7 +93,7 @@ export const wholesaleSettlementReportsQueryMock = (
       },
       executionTime: {
         start: executionTimeStart,
-        end: executionTimeStart_Minutes,
+        end: executionTimeEnd_Minutes,
       },
       settlementReportDownloadUrl: `${apiBase}/v1/WholesaleSettlementReport/DownloadReport`,
       fromApi: true,
@@ -100,6 +101,27 @@ export const wholesaleSettlementReportsQueryMock = (
     {
       __typename: 'SettlementReport',
       id: '85d1798474654be1b8c2f5bc543ed444',
+      calculationType: CalculationType.ThirdCorrectionSettlement,
+      period: { start: periodStart, end: periodEnd },
+      numberOfGridAreasInReport: 15,
+      includesBasisData: false,
+      statusType: SettlementReportStatusType.Error,
+      progress: 50,
+      actor: {
+        __typename: 'Actor',
+        id: '3',
+        name: 'Blå Strøm',
+      },
+      executionTime: {
+        start: executionTimeStart,
+        end: executionTimeEnd_Seconds,
+      },
+      settlementReportDownloadUrl: `${apiBase}/v1/WholesaleSettlementReport/DownloadReport`,
+      fromApi: true,
+    },
+    {
+      __typename: 'SettlementReport',
+      id: '85d1798474654be1b8c2f5bc543ed555',
       calculationType: CalculationType.FirstCorrectionSettlement,
       period: { start: periodStart, end: null },
       numberOfGridAreasInReport: 42,
