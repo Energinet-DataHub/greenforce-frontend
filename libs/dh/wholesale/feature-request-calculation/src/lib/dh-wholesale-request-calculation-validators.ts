139e1cc50769a2e0cfa5b6d6718ff69c56aa3fd6
Bjørn Hoffmann <bhoffmann@BearBook.local>
2024-09-16 14:38:12 +0200

    Undo change

--- libs/dh/wholesale/feature-request-calculation/src/lib/dh-wholesale-request-calculation-validators.ts
+++ libs/dh/wholesale/feature-request-calculation/src/lib/dh-wholesale-request-calculation-validators.ts
@@ -17,9 +17,9 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 import { WattRange, dayjs } from '@energinet-datahub/watt/date';
 
-type RangeControl = AbstractControl<WattRange<string>>;
+type rangeControl = AbstractControl<WattRange<string>>;
 
-export const max31DaysDateRangeValidator: ValidatorFn = ({ value }: RangeControl) => {
+export const max31DaysDateRangeValidator: ValidatorFn = ({ value }: rangeControl) => {
   if (!value?.end || !value?.start) return null;
   // Since the date range does not include the last millisecond (ends at 23:59:59.999),
   // this condition checks for 30 days, not 31 days (as the diff is in whole days only).
