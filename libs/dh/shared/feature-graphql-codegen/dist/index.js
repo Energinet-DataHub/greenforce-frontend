10dd972161bedaf82dc2dfe28b5aad8312e1bac4
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2024-09-20 09:23:21 +0200

    Ignore feature-graphql-codegen dist files from license and prettier

--- libs/dh/shared/feature-graphql-codegen/dist/index.js
+++ libs/dh/shared/feature-graphql-codegen/dist/index.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
 /**
  * @license
  * Copyright 2020 Energinet DataHub A/S
@@ -14,23 +17,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const tslib_1 = require('tslib');
-/**
- * @license
- * Copyright 2020 Energinet DataHub A/S
- *
- * Licensed under the Apache License, Version 2.0 (the "License2");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-tslib_1.__exportStar(require('./apollo-data-source'), exports);
+tslib_1.__exportStar(require("./apollo-data-source"), exports);
