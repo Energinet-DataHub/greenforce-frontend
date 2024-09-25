e0279ec0532d76db097b2c700e0cb49ca3ae9415
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2024-09-25 15:10:18 +0200

    Update DataSource to better handle initial search state

--- libs/dh/shared/util-apollo/src/lib/dataSource.ts
+++ libs/dh/shared/util-apollo/src/lib/dataSource.ts
@@ -21,11 +21,10 @@ import {
   debounceTime,
   filter,
   map,
-  merge,
+  mergeWith,
   Observable,
   ReplaySubject,
   skip,
-  startWith,
   Subscription,
   switchMap,
   take,
@@ -68,6 +67,10 @@ export class ApolloDataSource<TResult, TVariables extends ConnectionVariables, T
 
   get loading() {
     return this._query.loading();
+  }
+
+  get called() {
+    return this._query.called();
   }
 
   get error() {
@@ -117,19 +120,27 @@ export class ApolloDataSource<TResult, TVariables extends ConnectionVariables, T
     );
 
     if (!options?.skip) this._inputChange.next(options?.variables);
+
+    this._updateChangeSubscription();
   }
 
   refetch = (variables?: TVariables) => this._inputChange.next(variables);
   subscribeToMore: QueryResult<TResult, TVariables>['subscribeToMore'] = (options) =>
     this._query.subscribeToMore(options);
 
+  reset() {
+    // Reset subject so it does not have an initial value
+    this._inputChange = new ReplaySubject(1);
+    this._updateChangeSubscription();
+    this._totalCount.set(0);
+    this._data.set([]);
+    this._query.reset();
+    if (this.paginator) this.paginator.length = 0;
+    if (this.sort) this._configureSort(this.sort);
+  }
+
   connect() {
-    return this._connection.pipe(
-      tap((connection) => this._totalCount.set(connection.totalCount)),
-      map((connection) => connection.nodes ?? []),
-      tap((nodes) => this._data.set(nodes)),
-      startWith([] as TNode[])
-    );
+    return toObservable(this._data);
   }
 
   disconnect() {
@@ -151,11 +162,11 @@ export class ApolloDataSource<TResult, TVariables extends ConnectionVariables, T
 
     // Update MatSort to reflect the initial sort
     const [id, direction] = Object.entries(initialSort)[0];
-    sort.sort({
-      id,
-      start: (direction?.toLowerCase() ?? '') as SortDirection,
-      disableClear: true,
-    });
+    const start = (direction?.toLowerCase() ?? '') as SortDirection;
+
+    // Setting the sort to the same values will toggle direction
+    if (sort.active === id && sort.direction === start) return;
+    sort.sort({ id, start, disableClear: true });
   }
 
   _updateChangeSubscription() {
@@ -164,54 +175,52 @@ export class ApolloDataSource<TResult, TVariables extends ConnectionVariables, T
     // Sorting and pagination are required for this data source
     if (!sort || !paginator) return;
 
-    const filterChange = this._filter.pipe(
+    const inputChange = this._filter.pipe(
       skip(1),
       debounceTime(100),
-      map((filter) => ({ filter }))
+      map((filter) => ({ filter })),
+      mergeWith(this._inputChange),
+      map((variables) => ({ variables }))
     );
 
     const sortChange = sort.sortChange.pipe(
       filter((s) => Boolean(s.direction)),
-      map((s) => ({ ...firstPage(paginator), order: { [s.active]: s.direction.toUpperCase() } })),
-      tap(() => paginator.firstPage())
+      map((s) => ({ [s.active]: s.direction.toUpperCase() })),
+      map((order) => ({ skip: this.totalCount <= 1, variables: { order } }))
     );
 
     // Create observables that emits just before the sort and input changes, in order
     // to prevent the paginator observable from running in response to `firstPage()`.
+    const beforeInputChange = connectable(inputChange);
     const beforeSortChange = connectable(sort.sortChange);
-    const beforeInputChange = connectable(this._inputChange);
     const pageChange = this._connection.pipe(
+      tap((connection) => this._totalCount.set(connection.totalCount)),
       tap((connection) => (paginator.length = connection.totalCount)),
       tap(() => (paginator.disabled = false)),
       switchMap((connection) =>
         paginator.page.pipe(
           take(1),
-          takeUntil(beforeSortChange),
           takeUntil(beforeInputChange),
+          takeUntil(beforeSortChange),
           tap(() => (paginator.disabled = true)),
-          map((event) => navigate(paginator, event, connection.pageInfo))
+          map((event) => navigate(paginator, event, connection.pageInfo)),
+          map((variables) => ({ variables }))
         )
       )
     );
 
-    const inputChange = this._inputChange.pipe(
+    const dataChange = this._connection.pipe(map((connection) => connection.nodes ?? []));
+    const optionsChange = inputChange.pipe(
+      mergeWith(sortChange),
       tap(() => paginator.firstPage()),
-      map((variables) => ({
-        ...firstPage(paginator),
-        ...variables,
-      }))
-    );
-
-    const variablesChange = merge(
-      filterChange,
-      sortChange,
-      pageChange,
-      inputChange
-    ) as Observable<TVariables>;
+      map((opts) => ({ ...opts, variables: { ...firstPage(paginator), ...opts.variables } })),
+      mergeWith(pageChange)
+    ) as Observable<QueryOptions<TVariables>>;
 
     this._subscription?.unsubscribe();
-    this._subscription = beforeSortChange.connect();
+    this._subscription = dataChange.subscribe((nodes) => this._data.set(nodes));
     this._subscription.add(beforeInputChange.connect());
-    this._subscription.add(variablesChange.subscribe((v) => this._query.refetch(v)));
+    this._subscription.add(beforeSortChange.connect());
+    this._subscription.add(optionsChange.subscribe((opts) => this._query.setOptions(opts)));
   }
 }
