11356098574147c0028ea3ec6002a8b702a8ba1c
Dzhavat Ushev <dzu@energinet.dk>
2024-09-26 13:38:37 +0200

    fix(watt): alignment of drawer action items

--- libs/watt/src/lib/components/drawer/watt-drawer-actions.component.ts
+++ libs/watt/src/lib/components/drawer/watt-drawer-actions.component.ts
@@ -18,8 +18,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'watt-drawer-actions',
-  template: `<ng-content select="watt-button" />`,
-  styleUrls: ['./watt-drawer-actions.component.scss'],
   standalone: true,
+  styles: `
+    :host {
+      flex-shrink: 0;
+      display: flex;
+      justify-content: flex-end;
+      align-items: flex-start;
+      gap: var(--watt-space-m);
+      margin-left: auto;
+      margin-right: var(--watt-space-ml);
+    }
+  `,
+  template: `<ng-content select="watt-button" />`,
 })
 export class WattDrawerActionsComponent {}
