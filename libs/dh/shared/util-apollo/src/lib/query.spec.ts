2d62112e3ca1dfbf39dcc186d529be67522267f5
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2024-10-14 13:53:03 +0200

    Add tests for subscribeToMore + result from refetch/setOptions

--- libs/dh/shared/util-apollo/src/lib/query.spec.ts
+++ libs/dh/shared/util-apollo/src/lib/query.spec.ts
@@ -29,6 +29,14 @@ const TEST_QUERY = gql`
   }
 `;
 
+const TEST_SUBSCRIPTION = gql`
+  subscription TestSubscription {
+    calculationUpdated {
+      id
+    }
+  }
+`;
+
 describe('query', () => {
   let controller: ApolloTestingController;
 
@@ -54,6 +62,7 @@ describe('query', () => {
       expect(result.error()).toBeUndefined();
       expect(result.loading()).toBe(true);
       expect(result.networkStatus()).toBe(NetworkStatus.loading);
+      expect(result.called()).toBe(false);
     }));
 
   it('should respond correctly on success', fakeAsync(() =>
@@ -67,6 +76,7 @@ describe('query', () => {
       expect(result.error()).toBeUndefined();
       expect(result.loading()).toBe(false);
       expect(result.networkStatus()).toBe(NetworkStatus.ready);
+      expect(result.called()).toBe(true);
     })));
 
   it('should respond correctly on error', fakeAsync(() =>
@@ -79,6 +89,7 @@ describe('query', () => {
       expect(result.error()).toBeInstanceOf(ApolloError);
       expect(result.loading()).toBe(false);
       expect(result.networkStatus()).toBe(NetworkStatus.error);
+      expect(result.called()).toBe(true);
     })));
 
   it('should respond correctly when skipped', () =>
@@ -88,6 +99,7 @@ describe('query', () => {
       expect(result.error()).toBeUndefined();
       expect(result.loading()).toBe(false);
       expect(result.networkStatus()).toBe(NetworkStatus.ready);
+      expect(result.called()).toBe(false);
     }));
 
   it('should start a new query on refetch', fakeAsync(() =>
@@ -105,6 +117,7 @@ describe('query', () => {
       expect(result.error()).toBeUndefined();
       expect(result.loading()).toBe(false);
       expect(result.networkStatus()).toBe(NetworkStatus.ready);
+      expect(result.called()).toBe(true);
     })));
 
   it('should start a new query on refetch after it has errored', fakeAsync(() =>
@@ -125,7 +138,30 @@ describe('query', () => {
       tick();
 
       expect(result.data()).toEqual(data);
+      expect(result.error()).toBeUndefined();
       expect(result.loading()).toBe(false);
+      expect(result.called()).toBe(true);
+    })));
+
+  it('should resolve with ApolloQueryResult on refetch', fakeAsync(() =>
+    TestBed.runInInjectionContext(async () => {
+      const result = query(TEST_QUERY);
+      const queryOp = controller.expectOne(TEST_QUERY);
+      const queryData = { __type: { name: 'Query' } };
+      queryOp.flush({ data: queryData });
+      tick();
+
+      const promise = result.refetch();
+      const mutationOp = controller.expectOne(TEST_QUERY);
+      const mutationData = { __type: { name: 'Mutation' } };
+      mutationOp.flush({ data: mutationData });
+      tick();
+
+      expect(await promise).toEqual({
+        data: mutationData,
+        loading: false,
+        networkStatus: NetworkStatus.ready,
+      });
     })));
 
   it('should start a new query on setOptions', fakeAsync(() =>
@@ -140,6 +176,28 @@ describe('query', () => {
       expect(result.error()).toBeUndefined();
       expect(result.loading()).toBe(false);
       expect(result.networkStatus()).toBe(NetworkStatus.ready);
+      expect(result.called()).toBe(true);
+    })));
+
+  it('should resolve with ApolloQueryResult on setOptions', fakeAsync(() =>
+    TestBed.runInInjectionContext(async () => {
+      const result = query(TEST_QUERY);
+      const queryOp = controller.expectOne(TEST_QUERY);
+      const queryData = { __type: { name: 'Query' } };
+      queryOp.flush({ data: queryData });
+      tick();
+
+      const promise = result.setOptions({ fetchPolicy: 'network-only' });
+      const mutationOp = controller.expectOne(TEST_QUERY);
+      const mutationData = { __type: { name: 'Mutation' } };
+      mutationOp.flush({ data: mutationData });
+      tick();
+
+      expect(await promise).toEqual({
+        data: mutationData,
+        loading: false,
+        networkStatus: NetworkStatus.ready,
+      });
     })));
 
   it('should clear result after reset', fakeAsync(() =>
@@ -154,5 +212,121 @@ describe('query', () => {
       expect(result.error()).toBeUndefined();
       expect(result.loading()).toBe(false);
       expect(result.networkStatus()).toBe(NetworkStatus.ready);
+      expect(result.called()).toBe(false);
+    })));
+
+  it('should not call updateQuery while query is loading', fakeAsync(() =>
+    TestBed.runInInjectionContext(() => {
+      const onUpdateQuery = jest.fn((x) => x);
+      const result = query(TEST_QUERY);
+      controller.expectOne(TEST_QUERY);
+
+      result.subscribeToMore({ document: TEST_SUBSCRIPTION, updateQuery: onUpdateQuery });
+      const subscriptionOp = controller.expectOne(TEST_SUBSCRIPTION);
+      const subscriptionData = { data: { calculationUpdated: { id: '1' } } };
+      subscriptionOp.flush(subscriptionData);
+      tick();
+
+      expect(result.loading()).toBe(true);
+      expect(onUpdateQuery).not.toHaveBeenCalled();
+    })));
+
+  it('should not call updateQuery while query is refetching', fakeAsync(() =>
+    TestBed.runInInjectionContext(() => {
+      const onUpdateQuery = jest.fn((x) => x);
+      const result = query(TEST_QUERY);
+      result.subscribeToMore({ document: TEST_SUBSCRIPTION, updateQuery: onUpdateQuery });
+
+      const initialQueryOp = controller.expectOne(TEST_QUERY);
+      const queryData = { __type: { name: 'Query' } };
+      initialQueryOp.flush({ data: queryData });
+      tick();
+
+      result.refetch();
+      controller.expectOne(TEST_QUERY);
+      tick();
+
+      const subscriptionOp = controller.expectOne(TEST_SUBSCRIPTION);
+      const subscriptionData = { data: { calculationUpdated: { id: '1' } } };
+      subscriptionOp.flush(subscriptionData);
+      tick();
+
+      expect(result.loading()).toBe(true);
+      expect(onUpdateQuery).not.toHaveBeenCalled();
+    })));
+
+  it('should call updateQuery when initial query is complete', fakeAsync(() =>
+    TestBed.runInInjectionContext(() => {
+      const onUpdateQuery = jest.fn((x) => x);
+      const result = query(TEST_QUERY);
+      result.subscribeToMore({ document: TEST_SUBSCRIPTION, updateQuery: onUpdateQuery });
+
+      const subscriptionOp = controller.expectOne(TEST_SUBSCRIPTION);
+      const subscriptionData = { data: { calculationUpdated: { id: '1' } } };
+      subscriptionOp.flush(subscriptionData);
+      tick();
+
+      const queryOp = controller.expectOne(TEST_QUERY);
+      const queryData = { __type: { name: 'Query' } };
+      queryOp.flush({ data: queryData });
+      tick();
+
+      expect(result.loading()).toBe(false);
+      expect(onUpdateQuery).toHaveBeenCalledWith(queryData, {
+        subscriptionData,
+        variables: undefined,
+      });
+    })));
+
+  it('should call updateQuery when refetch is complete', fakeAsync(() =>
+    TestBed.runInInjectionContext(() => {
+      const onUpdateQuery = jest.fn((x) => x);
+      const result = query(TEST_QUERY, { skip: true });
+      result.subscribeToMore({ document: TEST_SUBSCRIPTION, updateQuery: onUpdateQuery });
+
+      const subscriptionOp = controller.expectOne(TEST_SUBSCRIPTION);
+      const subscriptionData = { data: { calculationUpdated: { id: '1' } } };
+      subscriptionOp.flush(subscriptionData);
+      tick();
+
+      result.refetch();
+      const queryOp = controller.expectOne(TEST_QUERY);
+      const queryData = { __type: { name: 'Query' } };
+      queryOp.flush({ data: queryData });
+      tick();
+
+      expect(result.loading()).toBe(false);
+      expect(onUpdateQuery).toHaveBeenCalledWith(queryData, {
+        subscriptionData,
+        variables: undefined,
+      });
+    })));
+
+  it('should only call updateQuery once per data emitted', fakeAsync(() =>
+    TestBed.runInInjectionContext(() => {
+      const onUpdateQuery = jest.fn((x) => x);
+      const result = query(TEST_QUERY);
+      const initialQueryOp = controller.expectOne(TEST_QUERY);
+      const queryData = { __type: { name: 'Query' } };
+      initialQueryOp.flush({ data: queryData });
+      tick();
+
+      result.subscribeToMore({ document: TEST_SUBSCRIPTION, updateQuery: onUpdateQuery });
+      const subscriptionOp = controller.expectOne(TEST_SUBSCRIPTION);
+      const subscriptionData = { data: { calculationUpdated: { id: '1' } } };
+      subscriptionOp.flush(subscriptionData);
+      tick();
+
+      result.refetch();
+      const refetchQueryOp = controller.expectOne(TEST_QUERY);
+      refetchQueryOp.flush({ data: queryData });
+      tick();
+
+      expect(result.loading()).toBe(false);
+      expect(onUpdateQuery).toHaveBeenCalledTimes(1);
+      expect(onUpdateQuery).toHaveBeenCalledWith(queryData, {
+        subscriptionData,
+        variables: undefined,
+      });
     })));
 });
