ce6f706a7a82398630d387652177a9a43e50ea54
Dzhavat Ushev <dzu@energinet.dk>
2024-10-28 15:31:23 +0100

    feat(dh): click-to-navigate in notifications (#3669)
    * feat(dh): click-to-navigate in notifications
    
    * fix(dh): remove unused import

--- libs/dh/core/feature-notifications/src/lib/dh-notification.ts
+++ libs/dh/core/feature-notifications/src/lib/dh-notification.ts
@@ -16,5 +16,6 @@
  */
 import { GetNotificationsDocument } from '@energinet-datahub/dh/shared/domain/graphql';
 import type { ResultOf } from '@graphql-typed-document-node/core';
+
 export type DhNotifications = ResultOf<typeof GetNotificationsDocument>['notifications'];
 export type DhNotification = DhNotifications[0];
