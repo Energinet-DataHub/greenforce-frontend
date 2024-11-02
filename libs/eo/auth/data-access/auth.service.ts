43651fb79c96f9dd61da0031421328865dc58f43
sahma19 <saiuan@msn.com>
2024-11-02 19:57:21 +0100

    try

--- libs/eo/auth/data-access/auth.service.ts
+++ libs/eo/auth/data-access/auth.service.ts
@@ -185,14 +185,31 @@ export class EoAuthService {
 
   signinCallback(): Promise<User | null> {
     return this.userManager
-      ? this.userManager?.signinCallback().then((user) => {
-          if (user) {
-            this.user.set((user as EoUser) ?? null);
+      ? this.userManager.signinRedirectCallback().then((user) => {
+        if (user) {
+          this.user.set(user as EoUser);
+
+          // Retrieve the nonce from sessionStorage
+          const storedNonce = sessionStorage.getItem('auth_nonce');
+
+          // Decode the state parameter
+          const stateString = atob(user.state as string);
+          const state = JSON.parse(stateString);
+
+          // Validate the nonce
+          if (!storedNonce || storedNonce !== state.nonce) {
+            console.error('Invalid nonce in state parameter.');
+            return Promise.reject('Invalid state parameter.');
           }
-          return Promise.resolve(user ?? null);
-        })
+
+          // Clear the nonce from storage
+          sessionStorage.removeItem('auth_nonce');
+        }
+        return Promise.resolve(user ?? null);
+      })
       : Promise.resolve(null);
   }
+
 
   renewToken(): Promise<User | null> {
     return this.userManager?.signinSilent() ?? Promise.resolve(null);
