10602d650180f7f46cc30a80d636931a5e92b177
Dan Stenrøjl <XDAST@energinet.dk>
2024-11-08 10:00:05 +0100

    Map to calculation dto

--- apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Extensions/ProcessManagerCalculationClientV1Extensions.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Extensions/ProcessManagerCalculationClientV1Extensions.cs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 using Energinet.DataHub.ProcessManager.Api.Model;
+using Energinet.DataHub.ProcessManager.Api.Model.OrchestrationInstance;
 using Energinet.DataHub.ProcessManager.Client.Processes.BRS_023_027.V1;
 using Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_023_027.V1.Model;
 using Energinet.DataHub.WebApi.Clients.Wholesale.v3;
@@ -36,7 +37,7 @@ public static class ProcessManagerCalculationClientV1Extensions
         var periodStart = input.Period?.Start.ToDateTimeOffset();
         var periodEnd = input.Period?.End.ToDateTimeOffset();
 
-        var calculations = await client.SearchCalculationsAsync(
+        var processManagerCalculations = await client.SearchCalculationsAsync(
             lifecycleState: null,
             terminationState: null,
             startedAtOrLater: minExecutionTime,
@@ -48,13 +49,14 @@ public static class ProcessManagerCalculationClientV1Extensions
             isInternalCalculation: isInternal,
             CancellationToken.None);
 
+        var calculations = processManagerCalculations
+            .Select(x => x.MapToCalculationDto());
+
         return calculations
-            .OrderByDescending(x => x?.Lifecycle?.ScheduledToRunAt ?? DateTimeOffset.MinValue)
-            ////.Where(x => states.Length == 0 || states.Contains(x.OrchestrationState))
-            ////.Where(x => calculationTypes.Length == 0 || calculationTypes.Contains(x.CalculationType))
-            .Where(x => input.ExecutionType == null || x?.ParameterValue?.IsInternalCalculation == isInternal)
-            .Select(x => x.MapToCalculationDto())
-            .ToList();
+            .OrderByDescending(x => x.ScheduledAt)
+            .Where(x => states.Length == 0 || states.Contains(x.OrchestrationState))
+            .Where(x => calculationTypes.Length == 0 || calculationTypes.Contains(x.CalculationType))
+            .Where(x => input.ExecutionType == null || x.IsInternalCalculation == isInternal);
     }
 
     internal static async Task<CalculationDto> GetCalculationAsync(
@@ -72,17 +74,94 @@ public static class ProcessManagerCalculationClientV1Extensions
         // TODO: Can we create a new type to be used in UI so we avoid a tight coupling to external types?
         return new CalculationDto
         {
-            RunId = null, // Deprecate
-            Resolution = null, // Deprecate
-            Unit = null, // Deprecate
+            RunId = null, // Deprecated
+            Resolution = null, // Deprecated
+            Unit = null, // Deprecated
+            AreSettlementReportsCreated = false, // Deprecated in current context
 
             CalculationId = instanceDto.Id,
-            PeriodStart = instanceDto?.ParameterValue?.PeriodStartDate ?? DateTimeOffset.MinValue,
-            PeriodEnd = instanceDto?.ParameterValue?.PeriodEndDate ?? DateTimeOffset.MinValue,
-            ScheduledAt = instanceDto?.Lifecycle?.ScheduledToRunAt ?? DateTimeOffset.MinValue,
+            CreatedByUserId = Guid.Empty, // TODO: Missing in Process Manager
+            ScheduledAt = instanceDto.Lifecycle?.ScheduledToRunAt ?? DateTimeOffset.MinValue,
 
-            ExecutionTimeStart = instanceDto?.Lifecycle?.StartedAt ?? DateTimeOffset.MinValue,
-            ExecutionTimeEnd = instanceDto?.Lifecycle?.TerminatedAt,
+            CalculationType = Enum
+                .TryParse<Clients.Wholesale.v3.CalculationType>(
+                    instanceDto.ParameterValue.CalculationType.ToString(),
+                    ignoreCase: true,
+                    out var calculationTypeResult)
+                ? calculationTypeResult
+                : throw new InvalidOperationException($"Invalid CalculationType '{instanceDto.ParameterValue.CalculationType}'; cannot be mapped."),
+            GridAreaCodes = instanceDto.ParameterValue.GridAreaCodes.ToArray(),
+            PeriodStart = instanceDto.ParameterValue.PeriodStartDate,
+            PeriodEnd = instanceDto.ParameterValue.PeriodEndDate,
+            IsInternalCalculation = instanceDto.ParameterValue.IsInternalCalculation,
+
+            ExecutionTimeStart = instanceDto.Lifecycle?.StartedAt,
+            ExecutionTimeEnd = null, // Not used as far as I can tell; instead 'CompletedTime' is used and mapped to 'executionTimeEnd'
+            CompletedTime = instanceDto.Lifecycle?.TerminatedAt,
+
+            OrchestrationState = instanceDto.MapToOrchestrationState(),
         };
     }
+
+#pragma warning disable IDE0011 // Add braces
+    internal static CalculationOrchestrationState MapToOrchestrationState(
+        this OrchestrationInstanceTypedDto<NotifyAggregatedMeasureDataInputV1> instanceDto)
+    {
+        var calculationStep = instanceDto.Steps.Where(step => step.Sequence == 0).Single();
+        var messagesEnqueuedStep = instanceDto.Steps.Where(step => step.Sequence == 1).Single();
+
+        switch (instanceDto.Lifecycle.State)
+        {
+            case OrchestrationInstanceLifecycleStates.Pending:
+            case OrchestrationInstanceLifecycleStates.Queued:
+                return CalculationOrchestrationState.Scheduled;
+
+            case OrchestrationInstanceLifecycleStates.Running:
+                switch (calculationStep.Lifecycle.State)
+                {
+                    case OrchestrationStepLifecycleStates.Pending:
+                        return CalculationOrchestrationState.Started;
+
+                    case OrchestrationStepLifecycleStates.Running:
+                        return CalculationOrchestrationState.Calculating;
+                }
+
+                switch (messagesEnqueuedStep.Lifecycle.State)
+                {
+                    case OrchestrationStepLifecycleStates.Pending:
+                        return CalculationOrchestrationState.Calculated;
+
+                    case OrchestrationStepLifecycleStates.Running:
+                        return CalculationOrchestrationState.ActorMessagesEnqueuing;
+                }
+
+                return CalculationOrchestrationState.ActorMessagesEnqueued;
+
+            case OrchestrationInstanceLifecycleStates.Terminated:
+                switch (instanceDto.Lifecycle.TerminationState)
+                {
+                    case OrchestrationInstanceTerminationStates.Succeeded:
+                        return CalculationOrchestrationState.Completed;
+
+                    case OrchestrationInstanceTerminationStates.Failed:
+                        if (calculationStep.Lifecycle.TerminationState == OrchestrationStepTerminationStates.Failed)
+                            return CalculationOrchestrationState.CalculationFailed;
+
+                        if (messagesEnqueuedStep.Lifecycle.TerminationState == OrchestrationStepTerminationStates.Failed)
+                            return CalculationOrchestrationState.ActorMessagesEnqueuingFailed;
+
+                        throw new InvalidOperationException($"Unexpected combination of orchestration instance Lifecycle.TerminationState and steps Lifecycle.TerminationState.");
+
+                    case OrchestrationInstanceTerminationStates.UserCanceled:
+                        return CalculationOrchestrationState.Canceled;
+
+                    default:
+                        throw new InvalidOperationException($"Invalid Lifecycle.TerminationState '{instanceDto.Lifecycle.TerminationState}'; cannot be mapped.");
+                }
+
+            default:
+                throw new InvalidOperationException($"Invalid Lifecycle.State '{instanceDto.Lifecycle.State}'; cannot be mapped.");
+        }
+    }
+#pragma warning restore IDE0011 // Add braces
 }
