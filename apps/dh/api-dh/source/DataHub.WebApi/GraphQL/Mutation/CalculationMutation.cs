c8b48e6304a7eecf227b1f726867a6dced09a2cf
Dan Stenrøjl <XDAST@energinet.dk>
2024-11-13 14:41:31 +0100

    Start and Cancel

--- apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Mutation/CalculationMutation.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/GraphQL/Mutation/CalculationMutation.cs
@@ -12,11 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.ProcessManager.Api.Model;
+using Energinet.DataHub.ProcessManager.Client;
+using Energinet.DataHub.ProcessManager.Client.Processes.BRS_023_027.V1;
+using Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_023_027.V1.Model;
 using Energinet.DataHub.WebApi.Clients.Wholesale.Orchestrations;
 using Energinet.DataHub.WebApi.Clients.Wholesale.Orchestrations.Dto;
+using Energinet.DataHub.WebApi.Clients.Wholesale.ProcessManager;
+using Energinet.DataHub.WebApi.Clients.Wholesale.SettlementReports.Dto;
 using Energinet.DataHub.WebApi.Clients.Wholesale.v3;
+using Energinet.DataHub.WebApi.Common;
 using Energinet.DataHub.WebApi.GraphQL.Enums;
 using HotChocolate.Subscriptions;
+using Microsoft.FeatureManagement;
 using NodaTime;
 
 namespace Energinet.DataHub.WebApi.GraphQL.Mutation;
@@ -29,6 +37,8 @@ public partial class Mutation
         string[] gridAreaCodes,
         CalculationType calculationType,
         DateTimeOffset? scheduledAt,
+        [Service] IFeatureManager featureManager,
+        [Service] INotifyAggregatedMeasureDataClientV1 processManagerCalculationClient,
         [Service] IWholesaleOrchestrationsClient client,
         [Service] ITopicEventSender sender,
         CancellationToken cancellationToken)
@@ -38,16 +48,33 @@ public partial class Mutation
             throw new Exception("Period cannot be open-ended");
         }
 
-        var requestDto = new StartCalculationRequestDto(
-            StartDate: period.Start.ToDateTimeOffset(),
-            EndDate: period.End.ToDateTimeOffset(),
-            ScheduledAt: scheduledAt ?? DateTimeOffset.UtcNow,
-            GridAreaCodes: gridAreaCodes,
-            CalculationType: calculationType,
-            IsInternalCalculation: executionType == CalculationExecutionType.Internal);
+        Guid calculationId;
+        var useProcessManager = await featureManager.IsEnabledAsync(nameof(FeatureFlags.Names.UseProcessManager));
+        if (useProcessManager)
+        {
+            var requestDto = new ScheduleOrchestrationInstanceDto<NotifyAggregatedMeasureDataInputV1>(
+                RunAt: scheduledAt ?? DateTimeOffset.UtcNow,
+                InputParameter: new NotifyAggregatedMeasureDataInputV1(
+                    CalculationType: calculationType.MapToCalculationType(),
+                    GridAreaCodes: gridAreaCodes,
+                    PeriodStartDate: period.Start.ToDateTimeOffset(),
+                    PeriodEndDate: period.End.ToDateTimeOffset(),
+                    IsInternalCalculation: executionType == CalculationExecutionType.Internal));
 
-        var calculationId = await client
-            .StartCalculationAsync(requestDto, cancellationToken);
+            calculationId = await processManagerCalculationClient.ScheduleNewCalculationAsync(requestDto, cancellationToken);
+        }
+        else
+        {
+            var requestDto = new StartCalculationRequestDto(
+                StartDate: period.Start.ToDateTimeOffset(),
+                EndDate: period.End.ToDateTimeOffset(),
+                ScheduledAt: scheduledAt ?? DateTimeOffset.UtcNow,
+                GridAreaCodes: gridAreaCodes,
+                CalculationType: calculationType,
+                IsInternalCalculation: executionType == CalculationExecutionType.Internal);
+
+            calculationId = await client.StartCalculationAsync(requestDto, cancellationToken);
+        }
 
         await sender.SendAsync(nameof(CreateCalculationAsync), calculationId, cancellationToken);
 
@@ -56,12 +83,21 @@ public partial class Mutation
 
     public async Task<bool> CancelScheduledCalculationAsync(
         Guid calculationId,
+        [Service] IFeatureManager featureManager,
+        [Service] IProcessManagerClient processManagerClient,
         [Service] IWholesaleOrchestrationsClient client,
         CancellationToken cancellationToken)
     {
-        var requestDto = new CancelScheduledCalculationRequestDto(calculationId);
-
-        await client.CancelScheduledCalculationAsync(requestDto, cancellationToken);
+        var useProcessManager = await featureManager.IsEnabledAsync(nameof(FeatureFlags.Names.UseProcessManager));
+        if (useProcessManager)
+        {
+            await processManagerClient.CancelScheduledOrchestrationInstanceAsync(calculationId, cancellationToken);
+        }
+        else
+        {
+            var requestDto = new CancelScheduledCalculationRequestDto(calculationId);
+            await client.CancelScheduledCalculationAsync(requestDto, cancellationToken);
+        }
 
         return true;
     }
