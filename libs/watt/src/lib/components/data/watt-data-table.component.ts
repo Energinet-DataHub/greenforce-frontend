90d965feee6da7a43106e6f0bd034682bc2a9f0a
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-11-13 09:54:08 +0100

    feat(watt, data-table): add support for retry buton in empty state

--- libs/watt/src/lib/components/data/watt-data-table.component.ts
+++ libs/watt/src/lib/components/data/watt-data-table.component.ts
@@ -119,9 +119,13 @@ import { WattButtonComponent } from '../button';
                 [message]="error() ? intl.errorText : ready() ? intl.emptyText : intl.defaultText"
               >
                 @if (enableRetry()) {
+<<<<<<< HEAD
                   <watt-button variant="secondary" (click)="retry.emit()">{{
                     intl.emptyRetry
                   }}</watt-button>
+=======
+                  <watt-button variant="secondary" (click)="retry.emit()">{{ intl.emptyRetry }}</watt-button>
+>>>>>>> 234409cb0 (feat(watt, data-table): add support for retry buton in empty state)
                 }
               </watt-empty-state>
             </div>
