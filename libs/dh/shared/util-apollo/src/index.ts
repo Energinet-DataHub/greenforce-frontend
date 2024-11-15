9c284c25e75fa237bca099936205ae129c249ded
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2024-11-15 14:02:19 +0100

    Do not export internal abstract class

--- libs/dh/shared/util-apollo/src/index.ts
+++ libs/dh/shared/util-apollo/src/index.ts
@@ -18,6 +18,5 @@ export * from './lib/query';
 export * from './lib/lazyQuery';
 export * from './lib/mutation';
 export * from './lib/subscription';
-export * from './lib/data-source/ApolloDataSource';
 export * from './lib/data-source/CollectionSegmentDataSource';
 export * from './lib/data-source/ConnectionDataSource';
