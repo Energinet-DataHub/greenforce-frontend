a8dc0c5cda09e0b0fc171fa4831d23e08222a4d9
Dzhavat Ushev <dzu@energinet.dk>
2024-11-19 11:01:18 +0100

    refactor(dh): use Signals in components

--- libs/dh/market-participant/actors/feature-delegation/src/lib/dh-delegation-tab.component.ts
+++ libs/dh/market-participant/actors/feature-delegation/src/lib/dh-delegation-tab.component.ts
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 import {
+  ChangeDetectionStrategy,
   Component,
   DestroyRef,
   computed,
@@ -54,6 +55,7 @@ import { DhDelegations, DhDelegationsByType } from './dh-delegations';
 import { DhDelegationsOverviewComponent } from './overview/dh-delegations-overview.component';
 import { DhDelegationCreateModalComponent } from './create/dh-delegation-create-modal.component';
 import { dhGroupDelegations } from './util/dh-group-delegations';
+import { lazyQuery } from '@energinet-datahub/dh/shared/util-apollo';
 
 @Component({
   selector: 'dh-delegation-tab',
@@ -66,6 +68,7 @@ import { dhGroupDelegations } from './util/dh-group-delegations';
     `,
   ],
   templateUrl: './dh-delegation-tab.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     TranslocoDirective,
     ReactiveFormsModule,
@@ -84,15 +87,18 @@ import { dhGroupDelegations } from './util/dh-group-delegations';
   ],
 })
 export class DhDelegationTabComponent {
-  private readonly _modalService = inject(WattModalService);
-  private readonly _apollo = inject(Apollo);
-  private readonly _destroyRef = inject(DestroyRef);
+  private readonly modalService = inject(WattModalService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  private delegationsForActorQuery = lazyQuery(GetDelegationsForActorDocument);
 
   actor = input.required<DhActorExtended>();
-  isLoading = signal(false);
-  isError = signal(false);
+  isLoading = this.delegationsForActorQuery.loading;
+  hasError = computed(() => this.delegationsForActorQuery.error() !== undefined);
 
-  delegationsRaw = signal<DhDelegations>([]);
+  private delegationsRaw = computed<DhDelegations>(
+    () => this.delegationsForActorQuery.data()?.delegationsForActor ?? []
+  );
   delegationsByType = signal<DhDelegationsByType>([]);
 
   isEmpty = computed(() => this.delegationsRaw().length === 0);
@@ -101,15 +107,23 @@ export class DhDelegationTabComponent {
   statusOptions = dhEnumToWattDropdownOptions(ActorDelegationStatus);
 
   constructor() {
-    effect(() => this.fetchData(this.actor().id), { allowSignalWrites: true });
+    effect(() => {
+      this.statusControl.reset();
 
-    this.statusControl.valueChanges.subscribe((value) => {
-      this.filterDelegations(value);
+      this.delegationsForActorQuery.refetch({ actorId: this.actor().id });
     });
+
+    effect(() => this.delegationsByType.set(dhGroupDelegations(this.delegationsRaw())), {
+      allowSignalWrites: true,
+    });
+
+    this.statusControl.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((value) => this.filterDelegations(value));
   }
 
   onSetUpDelegation() {
-    this._modalService.open({ component: DhDelegationCreateModalComponent, data: this.actor() });
+    this.modalService.open({ component: DhDelegationCreateModalComponent, data: this.actor() });
   }
 
   private filterDelegations(filter: ActorDelegationStatus[] | null) {
@@ -119,37 +133,10 @@ export class DhDelegationTabComponent {
       return this.delegationsByType.set(dhGroupDelegations(delegations));
     }
 
-    const delegationsFiltered = delegations.filter((delegation) => {
-      return filter.includes(delegation.status);
-    });
+    const delegationsFiltered = delegations.filter((delegation) =>
+      filter.includes(delegation.status)
+    );
 
     this.delegationsByType.set(dhGroupDelegations(delegationsFiltered));
-  }
-
-  private fetchData(actorId: string) {
-    this.isLoading.set(true);
-    this.isError.set(false);
-    this.delegationsRaw.set([]);
-    this.statusControl.reset();
-
-    this._apollo
-      .watchQuery({
-        query: GetDelegationsForActorDocument,
-        variables: { actorId },
-      })
-      .valueChanges.pipe(takeUntilDestroyed(this._destroyRef))
-      .subscribe({
-        next: (result) => {
-          this.isLoading.set(result.loading);
-
-          this.delegationsRaw.set(result.data?.delegationsForActor ?? []);
-
-          this.delegationsByType.set(dhGroupDelegations(this.delegationsRaw()));
-        },
-        error: () => {
-          this.isLoading.set(false);
-          this.isError.set(true);
-        },
-      });
   }
 }
