cf2cfb4596d27c31abfc67734250976a3fef7129
Alexander Hafstad <alexander@frontend-specialisten.dk>
2024-11-19 10:53:16 +0100

    revert unrelated changes

--- libs/watt/src/lib/components/data/watt-data-table.component.ts
+++ libs/watt/src/lib/components/data/watt-data-table.component.ts
@@ -119,16 +119,9 @@ import { WattButtonComponent } from '../button';
                 [message]="error() ? intl.errorText : ready() ? intl.emptyText : intl.defaultText"
               >
                 @if (enableRetry()) {
-                  <<<<<<< HEAD
                   <watt-button variant="secondary" (click)="retry.emit()">{{
                     intl.emptyRetry
                   }}</watt-button>
-                  =======
-                  <watt-button variant="secondary" (click)="retry.emit()">{{
-                    intl.emptyRetry
-                  }}</watt-button>
-                  >>>>>>> 234409cb0 (feat(watt, data-table): add support for retry buton in empty
-                  state)
                 }
               </watt-empty-state>
             </div>
