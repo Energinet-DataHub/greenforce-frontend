749d3b9eabe99472961a1cb01e4966ea872c14d7
Morten Hansen <mimo@mimo-design.com>
2024-11-21 09:05:50 +0100

    Fixed type

--- libs/dh/admin/data-access-api/src/lib/types/user.type.ts
+++ libs/dh/admin/data-access-api/src/lib/types/user.type.ts
@@ -17,5 +17,5 @@
 import { GetUsersDocument } from '@energinet-datahub/dh/shared/domain/graphql';
 import type { ResultOf } from '@graphql-typed-document-node/core';
 
-export type DhUsers = NonNullable<ResultOf<typeof GetUsersDocument>['users']>['nodes'];
+export type DhUsers = NonNullable<ResultOf<typeof GetUsersDocument>['users']>['items'];
 export type DhUser = NonNullable<DhUsers>[0];
