7564c7a4ca3d1dc79ea5febe8428a654a8518068
Mathias Sejrup Plougmann <masep@energinet.dk>
2024-11-26 12:58:51 +0100

    chore update injection

--- libs/eo/auth/data-access/terms.service.ts
+++ libs/eo/auth/data-access/terms.service.ts
@@ -15,27 +15,23 @@
  * limitations under the License.
  */
 import { HttpClient } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { EoApiEnvironment, eoApiEnvironmentToken } from '@energinet-datahub/eo/shared/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EoTermsService {
-  #apiBase: string;
   private currentVersion = -1;
-  constructor(
-    private http: HttpClient,
-    @Inject(eoApiEnvironmentToken) apiEnvironment: EoApiEnvironment
-  ) {
-    this.#apiBase = `${apiEnvironment.apiBase}`;
-  }
+  #apiEnvironment: EoApiEnvironment = inject(eoApiEnvironmentToken);
+  #http: HttpClient = inject(HttpClient);
+  #apiBase = this.#apiEnvironment.apiBase;
 
   setVersion(version: number) {
     this.currentVersion = version;
   }
 
   acceptTerms() {
-    return this.http.put(`${this.#apiBase}/terms/user/accept/${this.currentVersion}`, null);
+    return this.#http.put(`${this.#apiBase}/terms/user/accept/${this.currentVersion}`, null);
   }
 }
