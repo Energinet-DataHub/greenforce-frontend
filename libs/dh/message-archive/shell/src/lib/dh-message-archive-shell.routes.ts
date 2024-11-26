58cff40d919b0015028c99517812351ca5c6526b
Dzhavat Ushev <dzu@energinet.dk>
2024-11-26 15:16:14 +0100

    refactor(dh): use default export with routes

--- libs/dh/message-archive/shell/src/lib/dh-message-archive-shell.routes.ts
+++ libs/dh/message-archive/shell/src/lib/dh-message-archive-shell.routes.ts
@@ -19,10 +19,7 @@ import { Routes } from '@angular/router';
 export const dhMessageArchiveShellRoutes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('@energinet-datahub/dh/message-archive/feature-search').then(
-        (m) => m.DhMessageArchiveSearchPageComponent
-      ),
+    loadComponent: () => import('@energinet-datahub/dh/message-archive/feature-search'),
     pathMatch: 'full',
     data: {
       titleTranslationKey: 'messageArchive.topBarTitle',
