4b6adca293d5b58e62db333e9a84bcef109bfae1
Dzhavat Ushev <dzu@energinet.dk>
2024-11-29 10:16:23 +0100

    fix(dh): export type the right way

--- libs/dh/market-participant/grid-areas/domain/src/dh-grid-area-row.ts
+++ libs/dh/market-participant/grid-areas/domain/src/dh-grid-area-row.ts
@@ -21,7 +21,7 @@ import type {
 } from '@energinet-datahub/dh/shared/domain/graphql';
 import type { WattRange } from '@energinet-datahub/watt/date';
 
-export type DhGridAreaRow {
+export type DhGridAreaRow = {
   id: string;
   code: string;
   actor: string;
