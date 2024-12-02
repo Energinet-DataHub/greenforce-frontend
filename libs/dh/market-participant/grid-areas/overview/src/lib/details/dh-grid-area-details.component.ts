2b4dea3c433d5a7e386c72752a06efd8ae09d3d5
Dzhavat Ushev <dzu@energinet.dk>
2024-12-02 14:37:31 +0100

    refactor(dh): use vater-stack in view

--- libs/dh/market-participant/grid-areas/overview/src/lib/details/dh-grid-area-details.component.ts
+++ libs/dh/market-participant/grid-areas/overview/src/lib/details/dh-grid-area-details.component.ts
@@ -18,7 +18,7 @@ import { Component, effect, input, output, viewChild } from '@angular/core';
 import { TranslocoDirective } from '@ngneat/transloco';
 
 import { WattDatePipe } from '@energinet-datahub/watt/date';
-import { VaterFlexComponent } from '@energinet-datahub/watt/vater';
+import { VaterFlexComponent, VaterStackComponent } from '@energinet-datahub/watt/vater';
 import { WATT_DRAWER, WattDrawerComponent } from '@energinet-datahub/watt/drawer';
 import { DhGridAreaRow } from '@energinet-datahub/dh/market-participant/grid-areas/domain';
 
@@ -39,6 +39,7 @@ import { DhAuditLogComponent } from './dh-audit-log.component';
     TranslocoDirective,
 
     VaterFlexComponent,
+    VaterStackComponent,
     WattDatePipe,
     WATT_DRAWER,
     DhGridAreaStatusBadgeComponent,
@@ -62,31 +63,31 @@ import { DhAuditLogComponent } from './dh-audit-log.component';
         <watt-drawer-heading>
           <h2 class="watt-space-stack-s">{{ gridAreaView.code }}</h2>
 
-          <vater-flex direction="row" justify="flex-start" wrap="wrap" gap="ml">
-            <vater-flex direction="row" gap="s" grow="0">
+          <vater-flex direction="row" wrap="wrap" gap="ml" grow="0">
+            <vater-stack direction="row" gap="s">
               <span class="watt-label">{{ t('columns.actor') }}</span>
               <span>{{ gridAreaView.actor }}</span>
-            </vater-flex>
+            </vater-stack>
 
-            <vater-flex direction="row" gap="s" grow="0">
+            <vater-stack direction="row" gap="s">
               <span class="watt-label">{{ t('columns.organization') }}</span>
               <span>{{ gridAreaView.organization }}</span>
-            </vater-flex>
+            </vater-stack>
 
-            <vater-flex direction="row" gap="s" grow="0">
+            <vater-stack direction="row" gap="s">
               <span class="watt-label">{{ t('columns.priceArea') }}</span>
               <span>{{ gridAreaView.priceArea }}</span>
-            </vater-flex>
+            </vater-stack>
 
-            <vater-flex direction="row" gap="s" grow="0">
+            <vater-stack direction="row" gap="s">
               <span class="watt-label">{{ t('columns.type') }}</span>
               <span>{{ t('types.' + gridAreaView.type) }}</span>
-            </vater-flex>
+            </vater-stack>
 
-            <vater-flex direction="row" gap="s" grow="0">
+            <vater-stack direction="row" gap="s">
               <span class="watt-label">{{ t('columns.period') }}</span>
               <span>{{ gridAreaView.period | wattDate }}</span>
-            </vater-flex>
+            </vater-stack>
           </vater-flex>
         </watt-drawer-heading>
       }
