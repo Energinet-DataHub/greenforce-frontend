680a581554935cb6b98e4eb74343792d974d92b6
Morten Hansen <mimo@mimo-design.com>
2024-12-05 14:06:58 +0100

    reset

--- apps/dh/api-dh/source/app-host/Program.cs
+++ apps/dh/api-dh/source/app-host/Program.cs
@@ -1,8 +1,16 @@
+using HotChocolate.Fusion.Aspire;
+using Microsoft.Extensions.Options;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var webapi = builder.AddProject<Projects.DataHub_WebApi>("bff", launchProfileName: "DataHub.WebApi");
 
 builder.AddFusionGateway<Projects.DataHub_Gateway>("gateway")
-.WithSubgraph(webapi);
+    .WithSubgraph(webapi)
+    .WithOptions(new FusionCompositionOptions
+    {
+        // equivalent to `--enable-nodes` CLI option
+        EnableGlobalObjectIdentification = true
+    });
 
 builder.Build().Compose().Run();
