db92711e5a71332ef10101e5c0137425dca119b0
Morten Vestergaard Hansen <mimo@mimo-design.com>
2024-12-10 14:45:55 +0100

    Refactor/simplify file structur (#3831)
    * moving stuff around
    
    * Getting working again
    
    * chore: add license
    
    * style: format
    
    * poke
    
    * forgot a file
    
    * Try on CI
    
    * lint
    
    * Fixed path
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dzhavat Ushev <dzu@energinet.dk>

--- libs/dh/market-participant/grid-areas/src/index.ts
+++ libs/dh/market-participant/grid-areas/src/index.ts
@@ -16,20 +16,4 @@
  * limitations under the License.
  */
 //#endregion
-import type {
-  GridAreaStatus,
-  GridAreaType,
-  PriceAreaCode,
-} from '@energinet-datahub/dh/shared/domain/graphql';
-import type { WattRange } from '@energinet-datahub/watt/date';
-
-export type DhGridAreaRow = {
-  id: string;
-  code: string;
-  actor: string;
-  organization: string;
-  status: GridAreaStatus;
-  type: GridAreaType;
-  priceArea: PriceAreaCode;
-  period: WattRange<Date>;
-};
+export { DhGridAreasComponent as default } from './table.component';
