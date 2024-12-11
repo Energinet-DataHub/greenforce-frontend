e4b1c49d127447ac0c5a43e9e31ea3f084cb6bdd
Dzhavat Ushev <dzu@energinet.dk>
2024-12-11 14:27:05 +0100

    fix(dh): rename translation properties and fix markup

--- libs/dh/market-participant/grid-areas/src/audit-log.component.ts
+++ libs/dh/market-participant/grid-areas/src/audit-log.component.ts
@@ -65,7 +65,7 @@ type AuditLog = NonNullable<GridArea['auditLog']>[0];
               [innerHTML]="
                 t('changeTypes.CONSOLIDATION_REQUESTED', {
                   auditedBy: entry.auditedBy,
-                  marketParticipant: entry.previousOwner,
+                  previousOwner: entry.previousOwner,
                   gridArea: this.gridArea()?.code,
                   gridAreaStopsAt: this.gridArea()?.validTo | wattDate,
                 })
@@ -75,7 +75,7 @@ type AuditLog = NonNullable<GridArea['auditLog']>[0];
             <div
               [innerHTML]="
                 t('changeTypes.CONSOLIDATION_COMPLETED', {
-                  marketParticipant: entry.currentOwner,
+                  previousOwner: entry.previousOwner,
                 })
               "
             ></div>
