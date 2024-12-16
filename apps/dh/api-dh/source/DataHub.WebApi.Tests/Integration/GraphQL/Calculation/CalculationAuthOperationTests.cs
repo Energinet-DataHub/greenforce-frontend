b93579fdb4c2c2fab7523ca5c487be18c5c706e4
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2024-12-16 21:38:01 +0100

    Use Theory for tests + add subscription tests

--- apps/dh/api-dh/source/DataHub.WebApi.Tests/Integration/GraphQL/Calculation/CalculationAuthOperationTests.cs
+++ apps/dh/api-dh/source/DataHub.WebApi.Tests/Integration/GraphQL/Calculation/CalculationAuthOperationTests.cs
@@ -13,11 +13,18 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using Energinet.DataHub.WebApi.Clients.Wholesale.v3;
+using Energinet.DataHub.WebApi.Common;
+using Energinet.DataHub.WebApi.GraphQL.Types.Calculation;
 using Energinet.DataHub.WebApi.Tests.Extensions;
-using Energinet.DataHub.WebApi.Tests.Fixtures;
+using Energinet.DataHub.WebApi.Tests.Mocks;
 using Energinet.DataHub.WebApi.Tests.TestServices;
+using HotChocolate;
 using HotChocolate.Execution;
+using Moq;
 using Xunit;
 
 namespace Energinet.DataHub.WebApi.Tests.Integration.GraphQL.Calculation;
@@ -73,47 +80,88 @@ public class CalculationAuthOperationTests
     }
     """;
 
-    [Fact]
-    public async Task ExecuteCalculationQueries_AnonymousAsync()
+    private static readonly string _calculationSubscriptions =
+    $$"""
+    subscription {
+      calculationUpdated {
+        id
+      }
+    }
+    """;
+
+    [Theory]
+    [InlineData(UserIdentity.Authenticated)]
+    [InlineData(UserIdentity.Anonymous)]
+    public async Task ExecuteCalculationQueriesAsync(UserIdentity userIdentity)
     {
         var server = new GraphQLTestService();
         var result = await server.ExecuteRequestAsync(b => b
             .SetDocument(_calculationQueries)
-            .SetUser(ClaimsPrincipalFactory.CreateAnonymous()));
+            .SetUser(ClaimsPrincipalMocks.Create(userIdentity)));
 
-        await result.MatchSnapshotAsync();
+        await result.MatchSnapshotAsync($"ExecuteCalculationQueries_{userIdentity}");
     }
 
-    [Fact]
-    public async Task ExecuteCalculationQueries_AuthenticatedAsync()
-    {
-        var server = new GraphQLTestService();
-        var result = await server.ExecuteRequestAsync(b => b
-            .SetDocument(_calculationQueries)
-            .SetUser(ClaimsPrincipalFactory.CreateAuthenticated()));
-
-        await result.MatchSnapshotAsync();
-    }
-
-    [Fact]
-    public async Task ExecuteCalculationMutations_AnonymousAsync()
+    [Theory]
+    [InlineData(UserIdentity.Authenticated)]
+    [InlineData(UserIdentity.Anonymous)]
+    public async Task ExecuteCalculationMutationsAsync(UserIdentity userIdentity)
     {
         var server = new GraphQLTestService();
         var result = await server.ExecuteRequestAsync(b => b
             .SetDocument(_calculationMutations)
-            .SetUser(ClaimsPrincipalFactory.CreateAnonymous()));
+            .SetUser(ClaimsPrincipalMocks.Create(userIdentity)));
 
-        await result.MatchSnapshotAsync();
+        await result.MatchSnapshotAsync($"ExecuteCalculationMutations_{userIdentity}");
     }
 
-    [Fact]
-    public async Task ExecuteCalculationMutations_AuthenticatedAsync()
+    [Theory]
+    [InlineData(UserIdentity.Administrator)]
+    [InlineData(UserIdentity.Authenticated)]
+    [InlineData(UserIdentity.Anonymous)]
+    public async Task ExecuteCalculationSubscriptionsAsync(UserIdentity userIdentity)
     {
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
         var server = new GraphQLTestService();
-        var result = await server.ExecuteRequestAsync(b => b
-            .SetDocument(_calculationMutations)
-            .SetUser(ClaimsPrincipalFactory.CreateAuthenticated()));
 
-        await result.MatchSnapshotAsync();
+        // It seems like the test has to return actual data in order for the Authorize attribute
+        // to block access. Perhaps the authorization works per emit rather than per request.
+        server.FeatureManagerMock
+            .Setup(x => x.IsEnabledAsync(nameof(FeatureFlags.Names.UseProcessManager)))
+            .ReturnsAsync(true);
+
+        server.ProcessManagerCalculationClientMock
+            .Setup(x => x.GetCalculationAsync(_calculationId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new CalculationDto()
+            {
+                CalculationId = _calculationId,
+                OrchestrationState = CalculationOrchestrationState.Calculating,
+            });
+
+        server.ProcessManagerCalculationClientMock
+            .Setup(x => x.QueryCalculationsAsync(It.IsAny<CalculationQueryInput>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(
+            [
+                new CalculationDto()
+                {
+                    CalculationId = _calculationId,
+                    OrchestrationState = CalculationOrchestrationState.Calculating,
+                }
+            ]);
+
+        var stream = (IResponseStream)await server.ExecuteRequestAsync(
+            b => b
+                .SetDocument(_calculationSubscriptions)
+                .SetUser(ClaimsPrincipalMocks.Create(userIdentity)),
+            cts.Token);
+
+        var results = new List<string>();
+
+        await foreach (var result in stream.ReadResultsAsync().WithCancellation(cts.Token))
+        {
+            results.Add(result.ToJson());
+        }
+
+        await results.MatchSnapshotAsync($"ExecuteCalculationSubscriptions_{userIdentity}");
     }
 }
