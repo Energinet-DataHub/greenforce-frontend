8a7dd6045cdbe10bd38df28912129329a9b4c883
Morten Hansen <mimo@mimo-design.com>
2024-12-17 12:45:56 +0100

    Fixed stepper in EO

--- libs/watt/src/lib/components/stepper/watt-stepper.component.ts
+++ libs/watt/src/lib/components/stepper/watt-stepper.component.ts
@@ -31,7 +31,7 @@ import {
 } from '@angular/core';
 import { MatStepper, MatStepperModule } from '@angular/material/stepper';
 import { NgTemplateOutlet } from '@angular/common';
-import { StepperSelectionEvent, STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { StepperSelectionEvent, STEPPER_GLOBAL_OPTIONS, CdkStepper } from '@angular/cdk/stepper';
 import { RxPush } from '@rx-angular/template/push';
 import { from, map, Observable, of, startWith, withLatestFrom } from 'rxjs';
 
@@ -51,6 +51,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
       provide: STEPPER_GLOBAL_OPTIONS,
       useValue: { showError: true, displayDefaultIndicatorType: false },
     },
+    { provide: CdkStepper, useExisting: WattStepperComponent },
     { provide: MatStepper, useExisting: WattStepperComponent },
   ],
 })
