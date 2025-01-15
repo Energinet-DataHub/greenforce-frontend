a03ddc59edd6fd2d87ad47c714640cc922c2f5da
Morten Hansen <mimo@mimo-design.com>
2025-01-15 08:53:43 +0100

    still not working

--- libs/watt/.storybook/main.ts
+++ libs/watt/.storybook/main.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import type { UserConfig } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -38,22 +39,39 @@ const config = {
         ],
       },
       resolve: {
-        alias: {
-          '@energinet-datahub/watt': path.resolve(__dirname, '_index.scss'),
-        },
+        alias: [
+          {
+            find: /^@energinet-datahub\/watt/,
+            replacement: path.join(
+              __dirname,
+              '../src/lib/styles/@energinet-datahub/watt/_index.scss'
+            ),
+          },
+          {
+            find: /^@energinet-datahub\/watt\/utils/,
+            replacement: path.join(
+              __dirname,
+              '../src/lib/styles/@energinet-datahub/watt/_utils.scss'
+            ),
+          },
+        ],
       },
-      css: {
-        preprocessorOptions: {
-          // scss: {
-          //   includePaths: ['libs/watt/src/lib/styles'],
-          // },
-        },
-      },
+      // css: {
+      //   preprocessorOptions: {
+      //     scss: {
+      //       loadPaths: [
+      //         'libs/watt/src/lib/styles/_index.scss',
+      //         'libs/watt/src/lib/styles/_utils.scss',
+      //       ],
+      //     },
+      //   },
+      // },
       plugins: [
         angular({
           jit: true,
           tsconfig: './.storybook/tsconfig.json',
         }),
+        nxViteTsPaths(),
       ],
     });
   },
