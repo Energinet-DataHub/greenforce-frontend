0dd61099fb4f6170f71b608b3c3d31f4242c0a7e
Dzhavat Ushev <dzu@energinet.dk>
2025-01-16 13:01:17 +0100

    fix(dh): fix API error translation logic  (#3886)

--- libs/dh/market-participant/data-access-api/src/lib/dh-market-participant-error-handling.ts
+++ libs/dh/market-participant/data-access-api/src/lib/dh-market-participant-error-handling.ts
@@ -31,10 +31,12 @@ export const readApiErrorResponse = (errors: ApiErrorCollection[]) => {
 
 const translateApiError = (errorDescriptor: ApiErrorDescriptor) => {
   const translationKey = `marketParticipant.${errorDescriptor.code}`;
+
   const translation = translate(
     translationKey,
     flatten(translateArgs(errorDescriptor.args, translationKey))
   );
+
   return translationKey === translation
     ? translate(`marketParticipant.market_participant.error_fallback`, {
         message: errorDescriptor.message,
@@ -42,11 +44,27 @@ const translateApiError = (errorDescriptor: ApiErrorDescriptor) => {
     : translation;
 };
 
-const translateArgs = (args: Record<string, string>, code: string) =>
-  Object.entries(args).reduce((acc, [key, value]) => {
+const translateArgs = (args: Record<string, string>, code: string) => {
+  if (translationWithArgsExists(args, code)) {
+    return args;
+  }
+
+  return Object.entries(args).reduce((acc, [key, value]) => {
     const translationPath = code.split('.');
     translationPath.pop();
+
     const translationKey = `${translationPath.join('.')}.args.${key}.${value}`;
     const translation = translate(translationKey);
-    return { ...acc, [key]: translationKey === translation ? translationKey : translation };
+
+    return {
+      ...acc,
+      [key]: translationKey === translation ? translationKey : translation,
+    };
   }, {});
+};
+
+function translationWithArgsExists(args: Record<string, string>, code: string) {
+  const translationWithArgs = translate(code, flatten(args));
+
+  return translationWithArgs !== code;
+}
