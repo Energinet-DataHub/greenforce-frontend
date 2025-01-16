249f65fab061a8f368d2fe7ab73cc86c53f4d3ab
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-01-16 14:21:55 +0100

    Add RequestsClient

---
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/ProcessManager/Requests/Client/RequestsClient.cs
@@ -0,0 +1,41 @@
+// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.ProcessManager.Client;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_026.V1.Model;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.Shared.BRS_026_028;
+using Energinet.DataHub.WebApi.Extensions;
+
+namespace Energinet.DataHub.WebApi.Modules.ProcessManager.Requests.Client;
+
+public class RequestsClient(
+    IHttpContextAccessor httpContextAccessor,
+    IProcessManagerClient client)
+    : IRequestsClient
+{
+    public async Task<IEnumerable<IActorRequestQueryResult>> GetRequestsAsync(CancellationToken ct = default)
+    {
+        var userIdentity = httpContextAccessor.CreateUserIdentity();
+        var customQuery = new ActorRequestQuery(userIdentity, DateTimeOffset.Now, DateTimeOffset.Now);
+        return await client.SearchOrchestrationInstancesByCustomQueryAsync(customQuery, ct);
+    }
+
+    public async Task<bool> RequestCalculatedEnergyTimeSeriesAsync(
+        RequestCalculatedEnergyTimeSeriesInputV1 input,
+        CancellationToken ct = default)
+    {
+        await Task.CompletedTask;
+        return true;
+    }
+}
