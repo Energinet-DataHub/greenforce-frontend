0cffa4c0a6505070fda8d541eaf9519fde94596e
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-01-23 13:21:48 +0100

    Formatting

--- apps/dh/api-dh/source/DataHub.WebApi.Tests/Fixtures/CalculationFactory.cs
+++ apps/dh/api-dh/source/DataHub.WebApi.Tests/Fixtures/CalculationFactory.cs
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System;
-using System.Collections.Generic;
 using Energinet.DataHub.ProcessManager.Abstractions.Api.Model;
 using Energinet.DataHub.ProcessManager.Abstractions.Api.Model.OrchestrationInstance;
 using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_023_027.V1.Model;
@@ -22,13 +21,12 @@ namespace Energinet.DataHub.WebApi.Tests.Fixtures;
 
 public static class CalculationFactory
 {
-    private static CalculationInputV1 calculation =
-        new CalculationInputV1(
-            CalculationType.Aggregation,
-            [],
-            new(2024, 12, 3, 23, 0, 0, TimeSpan.Zero),
-            new(2024, 12, 20, 23, 0, 0, TimeSpan.Zero),
-            false);
+    private static CalculationInputV1 calculation = new CalculationInputV1(
+        CalculationType.Aggregation,
+        [],
+        new(2024, 12, 3, 23, 0, 0, TimeSpan.Zero),
+        new(2024, 12, 20, 23, 0, 0, TimeSpan.Zero),
+        false);
 
     public static OrchestrationInstanceTypedDto<CalculationInputV1> Create(
         OrchestrationInstanceLifecycleState lifecycleState = OrchestrationInstanceLifecycleState.Pending,
@@ -36,82 +34,84 @@ public static class CalculationFactory
         Guid? id = null,
         string[]? gridAreaCodes = null,
         bool isInternalCalculation = false) =>
-            OrchestrationInstanceFactory.CreateOrchestrationInstance(
-                calculation with { IsInternalCalculation = isInternalCalculation, GridAreaCodes = gridAreaCodes ?? [] },
-                lifecycleState,
-                terminationState,
-                [
-                    CreateCalculateStep(MapStateToStepLifecycle(lifecycleState, terminationState)),
-                    CreateEnqueueStep(
-                        !isInternalCalculation
-                            ? CreateStepLifecycle(StepInstanceLifecycleState.Pending)
-                            : CreateStepLifecycle(StepInstanceLifecycleState.Terminated, OrchestrationStepTerminationState.Skipped))
-                ],
-                id);
+        OrchestrationInstanceFactory.CreateOrchestrationInstance(
+            calculation with { IsInternalCalculation = isInternalCalculation, GridAreaCodes = gridAreaCodes ?? [] },
+            lifecycleState,
+            terminationState,
+            [
+                CreateCalculateStep(MapStateToStepLifecycle(lifecycleState, terminationState)),
+                !isInternalCalculation
+                    ? CreateEnqueueStep(CreateStepLifecycle(StepInstanceLifecycleState.Pending))
+                    : CreateEnqueueStep(
+                        CreateStepLifecycle(
+                            StepInstanceLifecycleState.Terminated,
+                            OrchestrationStepTerminationState.Skipped)),
+            ],
+            id);
 
     public static OrchestrationInstanceTypedDto<CalculationInputV1> CreateEnqueuing(
         OrchestrationInstanceLifecycleState lifecycleState = OrchestrationInstanceLifecycleState.Running,
         OrchestrationInstanceTerminationState? terminationState = null,
         string[]? gridAreaCodes = null) =>
-            OrchestrationInstanceFactory.CreateOrchestrationInstance(
-                calculation with { GridAreaCodes = gridAreaCodes ?? [] },
-                lifecycleState,
-                terminationState,
-                [
-                    CreateCalculateStep(
-                        CreateStepLifecycle(
-                            StepInstanceLifecycleState.Terminated,
-                            OrchestrationStepTerminationState.Succeeded)),
-                    CreateEnqueueStep(MapStateToStepLifecycle(lifecycleState, terminationState))
-                ]);
+        OrchestrationInstanceFactory.CreateOrchestrationInstance(
+            calculation with { GridAreaCodes = gridAreaCodes ?? [] },
+            lifecycleState,
+            terminationState,
+            [
+                CreateCalculateStep(
+                    CreateStepLifecycle(
+                        StepInstanceLifecycleState.Terminated,
+                        OrchestrationStepTerminationState.Succeeded)),
+                CreateEnqueueStep(MapStateToStepLifecycle(lifecycleState, terminationState))
+            ]);
 
     private static StepInstanceLifecycleDto MapStateToStepLifecycle(
         OrchestrationInstanceLifecycleState lifecycleState,
         OrchestrationInstanceTerminationState? terminationState) =>
-            lifecycleState switch
+        lifecycleState switch
+        {
+            OrchestrationInstanceLifecycleState.Pending or
+            OrchestrationInstanceLifecycleState.Queued =>
+                CreateStepLifecycle(StepInstanceLifecycleState.Pending, null),
+            OrchestrationInstanceLifecycleState.Running =>
+                CreateStepLifecycle(StepInstanceLifecycleState.Running, null),
+            OrchestrationInstanceLifecycleState.Terminated => terminationState switch
             {
-                OrchestrationInstanceLifecycleState.Pending or
-                OrchestrationInstanceLifecycleState.Queued =>
+                OrchestrationInstanceTerminationState.UserCanceled =>
                     CreateStepLifecycle(StepInstanceLifecycleState.Pending, null),
-                OrchestrationInstanceLifecycleState.Running =>
-                    CreateStepLifecycle(StepInstanceLifecycleState.Running, null),
-                OrchestrationInstanceLifecycleState.Terminated => terminationState switch
-                {
-                    OrchestrationInstanceTerminationState.UserCanceled =>
-                        CreateStepLifecycle(StepInstanceLifecycleState.Pending, null),
-                    OrchestrationInstanceTerminationState.Succeeded =>
-                        CreateStepLifecycle(
-                            StepInstanceLifecycleState.Terminated,
-                            OrchestrationStepTerminationState.Succeeded),
-                    OrchestrationInstanceTerminationState.Failed =>
-                        CreateStepLifecycle(
-                            StepInstanceLifecycleState.Terminated,
-                            OrchestrationStepTerminationState.Failed),
-                },
-            };
+                OrchestrationInstanceTerminationState.Succeeded =>
+                    CreateStepLifecycle(
+                        StepInstanceLifecycleState.Terminated,
+                        OrchestrationStepTerminationState.Succeeded),
+                OrchestrationInstanceTerminationState.Failed =>
+                    CreateStepLifecycle(
+                        StepInstanceLifecycleState.Terminated,
+                        OrchestrationStepTerminationState.Failed),
+            },
+        };
 
     private static StepInstanceLifecycleDto CreateStepLifecycle(
         StepInstanceLifecycleState lifecycleState,
         OrchestrationStepTerminationState? terminationState = null) =>
-            new StepInstanceLifecycleDto(
-                lifecycleState,
-                terminationState,
-                lifecycleState == StepInstanceLifecycleState.Pending ? null : DateTimeOffset.Now,
-                lifecycleState != StepInstanceLifecycleState.Terminated ? null : DateTimeOffset.Now);
+        new StepInstanceLifecycleDto(
+            lifecycleState,
+            terminationState,
+            lifecycleState == StepInstanceLifecycleState.Pending ? null : DateTimeOffset.Now,
+            lifecycleState != StepInstanceLifecycleState.Terminated ? null : DateTimeOffset.Now);
 
     private static StepInstanceDto CreateCalculateStep(StepInstanceLifecycleDto lifecycle) =>
-            new StepInstanceDto(
-                new("a246b5a3-4a06-42f4-877e-1e1122f092d0"),
-                lifecycle,
-                "TestCalculationStep",
-                1,
-                string.Empty);
+        new StepInstanceDto(
+            new("a246b5a3-4a06-42f4-877e-1e1122f092d0"),
+            lifecycle,
+            "TestCalculationStep",
+            1,
+            string.Empty);
 
     private static StepInstanceDto CreateEnqueueStep(StepInstanceLifecycleDto lifecycle) =>
-            new StepInstanceDto(
-                new("e2d18a00-77d0-4160-bbda-10b6b6f309d2"),
-                lifecycle,
-                "TestEnqueueStep",
-                2,
-                string.Empty);
+        new StepInstanceDto(
+            new("e2d18a00-77d0-4160-bbda-10b6b6f309d2"),
+            lifecycle,
+            "TestEnqueueStep",
+            2,
+            string.Empty);
 }
