a91731e2f671e84b5e11eabbc3598199e009ad24
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-01-27 21:27:32 +0100

    Move ObjectType out of node

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/SettlementReports/Types/SettlementReportType.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/SettlementReports/Types/SettlementReportType.cs
@@ -12,24 +12,72 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_023_027.V1.Model;
+using Energinet.DataHub.WebApi.Clients.MarketParticipant.v1;
+using Energinet.DataHub.WebApi.Clients.Wholesale.SettlementReports.Dto;
+using Energinet.DataHub.WebApi.Modules.Common.DataLoaders;
 using Energinet.DataHub.WebApi.Modules.SettlementReports.Enums;
 using NodaTime;
+using NodaTime.Extensions;
 
-namespace Energinet.DataHub.WebApi.Modules.SettlementReports.Types;
+namespace Energinet.DataHub.WebApi.Modules.SettlementReports;
 
-public sealed record SettlementReport(
-    string Id,
-    Guid RequestedByActorId,
-    CalculationType CalculationType,
-    Interval Period,
-    int NumberOfGridAreasInReport,
-    bool IncludesBasisData,
-    string StatusMessage,
-    double Progress,
-    SettlementReportStatusType StatusType,
-    Interval ExecutionTime,
-    bool FromApi,
-    bool CombineResultInASingleFile,
-    bool IncludeMonthlyAmount,
-    string[] GridAreas);
+[ObjectType<RequestedSettlementReportDto>]
+public static partial class SettlementReportType
+{
+    public static Interval Period([Parent] RequestedSettlementReportDto r) =>
+        new Interval(r.PeriodStart.ToInstant(), r.PeriodEnd.ToInstant());
+
+    public static SettlementReportStatusType StatusType([Parent] RequestedSettlementReportDto r) =>
+        r.Status switch
+        {
+            SettlementReportStatus.InProgress => SettlementReportStatusType.InProgress,
+            SettlementReportStatus.Completed => SettlementReportStatusType.Completed,
+            SettlementReportStatus.Failed => SettlementReportStatusType.Error,
+            SettlementReportStatus.Canceled => SettlementReportStatusType.Canceled,
+            _ => SettlementReportStatusType.Error,
+        };
+
+    public static Interval ExecutionTime([Parent] RequestedSettlementReportDto r) =>
+        new Interval(r.CreatedDateTime.ToInstant(), r.EndedDateTime?.ToInstant());
+
+    public static string[] GridAreas([Parent] RequestedSettlementReportDto r) =>
+        r.GridAreas.Select(ga => ga.Key).ToArray();
+
+    public static string? SettlementReportDownloadUrl(
+        [Parent] RequestedSettlementReportDto r,
+        [Service] LinkGenerator linkGenerator,
+        IHttpContextAccessor httpContextAccessor) => linkGenerator.GetUriByAction(
+            httpContextAccessor.HttpContext!,
+            "DownloadReport",
+            "WholesaleSettlementReport",
+            new { settlementReportId = r.RequestId.Id, FromApi = r.JobId != null });
+
+    public static async Task<ActorDto?> ActorAsync(
+        [Parent] RequestedSettlementReportDto r,
+        ActorByIdBatchDataLoader dataLoader) =>
+        await dataLoader.LoadAsync(r.RequestedByActorId);
+
+    static partial void Configure(
+        IObjectTypeDescriptor<RequestedSettlementReportDto> descriptor)
+    {
+        descriptor
+            .Name("SettlementReport")
+            .BindFieldsExplicitly();
+
+        descriptor.Field(f => f.RequestId.Id).Name("id");
+        descriptor.Field(f => f.RequestedByActorId);
+        descriptor.Field(f => f.CalculationType);
+        descriptor.Field(f => f.GridAreaCount).Name("numberOfGridAreasInReport");
+        descriptor.Field(f => f.ContainsBasisData).Name("includesBasisData");
+        descriptor.Field(f => f.Progress);
+        descriptor.Field(f => f.JobId != null).Name("fromApi");
+        descriptor.Field(f => !f.SplitReportPerGridArea).Name("combineResultInASingleFile");
+        descriptor.Field(f => f.IncludeMonthlyAmount);
+
+        // WTF IS THIS???
+        descriptor
+            .Field("statusMessage")
+            .Type<NonNullType<StringType>>()
+            .Resolve(_ => string.Empty);
+    }
+}
