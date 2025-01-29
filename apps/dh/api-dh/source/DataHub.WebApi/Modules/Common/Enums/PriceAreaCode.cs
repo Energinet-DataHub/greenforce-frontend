a3ee975970c4ec46dfa3391921f20b6346c9b5ce
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-01-29 12:47:13 +0100

    Move PriceAreaCode into Modules/Common

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/Common/Enums/PriceAreaCode.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/Common/Enums/PriceAreaCode.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.WebApi.GraphQL.Enums;
+namespace Energinet.DataHub.WebApi.Modules.Common.Enums;
 
 public enum PriceAreaCode
 {
