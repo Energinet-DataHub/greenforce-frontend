c929ee1d2dd54068766eff1e2b5b6e368f0c4558
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-01-30 16:44:05 +0100

    Remove obsolete code

--- libs/dh/wholesale/domain/src/grid-areas.ts
+++ libs/dh/wholesale/domain/src/grid-areas.ts
@@ -17,28 +17,6 @@
  */
 //#endregion
 import type { ResultOf } from '@graphql-typed-document-node/core';
-
-import { Range } from '@energinet-datahub/dh/shared/domain';
 import { GetGridAreasDocument } from '@energinet-datahub/dh/shared/domain/graphql';
 
 export type GridArea = ResultOf<typeof GetGridAreasDocument>['gridAreas'][0];
-
-export function filterValidGridAreas(
-  gridAreas: GridArea[],
-  dateRange: Range<string> | null
-): GridArea[] {
-  if (dateRange === null) return gridAreas;
-  return gridAreas.filter((gridArea) => {
-    const { validTo, validFrom } = gridArea;
-
-    // Is valid from before the end of the date range
-    const isValidFrom = new Date(validFrom).getTime() <= new Date(dateRange.end).getTime();
-
-    // Is valid to after the start of the date range
-    const isValidTo = !validTo
-      ? true
-      : new Date(validTo).getTime() >= new Date(dateRange.start).getTime();
-
-    return isValidTo && isValidFrom;
-  });
-}
