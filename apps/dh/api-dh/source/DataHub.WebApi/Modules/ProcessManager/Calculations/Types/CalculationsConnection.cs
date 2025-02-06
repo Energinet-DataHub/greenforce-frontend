40e674642b7314d3028186b5d06a52afb710e778
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-02-06 16:51:18 +0100

    Fix namespace

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/ProcessManager/Calculations/Types/CalculationsConnection.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/ProcessManager/Calculations/Types/CalculationsConnection.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.WebApi.GraphQL.Types.Calculation;
+namespace Energinet.DataHub.WebApi.Modules.ProcessManager.Calculations.Types;
 
 [ExtendObjectType("CalculationsConnection")]
 public class CalculationsConnection
