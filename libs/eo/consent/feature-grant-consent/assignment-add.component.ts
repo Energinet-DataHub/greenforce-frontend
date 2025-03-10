9225bda991e4a0394fd26e1fc139fdbd1feead73
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-03-10 15:22:46 +0100

    Move assignement-add into eo (it was very app specific)

---
+++ libs/eo/consent/feature-grant-consent/assignment-add.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eo-assignment-add-icon',
+  templateUrl: './assignment-add.svg',
+})
+export class EoAssignmentAddIconComponent {}
