31e6695b5c702c61686bfe49975815efedd11732
Dzhavat Ushev <dzu@energinet.dk>
2025-04-01 09:26:17 +0200

    fix(gf): force JSDOM to use different browser APIs

--- scripts/gf/test-util/jsdom-lax-ssl-environment.ts
+++ scripts/gf/test-util/jsdom-lax-ssl-environment.ts
@@ -52,13 +52,15 @@ class JsdomLaxSslEnvironment extends JSDOMEnvironment {
       options
     );
 
-    // Force Jest/JSDOM to use structuredClone
+    // Force Jest/JSDOM to use:
     // See https://github.com/jsdom/jsdom/issues/3363#issuecomment-1467894943
     this.global.structuredClone = structuredClone;
 
-    // Force Jest/JSDOM to use BroadcastChannel
     // See https://github.com/mswjs/jest-fixed-jsdom/blob/main/index.js
     this.global.BroadcastChannel = BroadcastChannel;
+    this.global.TransformStream = TransformStream;
+    this.global.Request = Request;
+    this.global.Response = Response;
   }
 }
 
