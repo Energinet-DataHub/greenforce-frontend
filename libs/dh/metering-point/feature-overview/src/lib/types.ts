5e6050a69821cdf93d4cd764f2c1b2160f2dcbb8
Morten Hansen <mimo@mimo-design.com>
2025-04-03 15:00:07 +0200

    Lint

--- libs/dh/metering-point/feature-overview/src/lib/types.ts
+++ libs/dh/metering-point/feature-overview/src/lib/types.ts
@@ -22,7 +22,6 @@ import {
   GetMeteringPointByIdDocument,
 } from '@energinet-datahub/dh/shared/domain/graphql';
 
-import { ExtractNodeType } from '@energinet-datahub/dh/shared/util-apollo';
 import type { ResultOf } from '@graphql-typed-document-node/core';
 
 export type MeteringPointDetails = ResultOf<typeof GetMeteringPointByIdDocument>['meteringPoint'];
