37d9b4705a02e70af9957b729283bd98e0a4dfe3
sahma19 <shd@energinet.dk>
2025-04-23 09:34:42 +0200

    Update libs/eo/core/globalization/assets-localization/i18n/en.ts
    Co-authored-by: Michael Gimm Holdensen <michaelgimm@gmail.com>

--- libs/eo/core/globalization/assets-localization/i18n/en.ts
+++ libs/eo/core/globalization/assets-localization/i18n/en.ts
@@ -240,7 +240,9 @@ export const EN_TRANSLATIONS: TranslationKeys = {
   shared: {
     notWhitelistedError: {
       title: 'No access',
-      message: `You do not yet have access to issue Granular Guarantees of Origin.<br /> Contact support at <a href="tel:+4570222810">+45 70 22 28 10</a> or <a href="mailto:datahub@energinet.dk">datahub@energinet.dk</a>.`,
+      message: `You do not yet have access to issue Granular Guarantees of Origin.<br /> Contact support at <a href="tel:+4570222810">+45 70 22 28 10</a> or <a href="mailto:datahub@energinet.dk?cc=mil@energinet.dk">datahub@energinet.dk</a>.`,
+      
+      
     },
     notMitIDErhvervError: {
       title: 'An error occured',
