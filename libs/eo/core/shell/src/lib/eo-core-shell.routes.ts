1aa994c303b19a440594aa41c8c3feca9ec7fdc0
sahma19 <saiuan@msn.com>
2025-04-24 11:26:52 +0200

    remove redundant route

--- libs/eo/core/shell/src/lib/eo-core-shell.routes.ts
+++ libs/eo/core/shell/src/lib/eo-core-shell.routes.ts
@@ -60,10 +60,6 @@ const routes: Routes = [
     redirectTo: 'onboarding/signin-callback',
   },
   {
-    path: 'contact-support',
-    component: ContactSupportComponent,
-  },
-  {
     path: eoOnboardingRoutePath,
     loadChildren: () =>
       import('@energinet-datahub/eo/onboarding/shell').then(
