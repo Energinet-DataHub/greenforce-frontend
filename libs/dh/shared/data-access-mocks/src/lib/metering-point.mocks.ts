744baf14b005f13c35c058bc726247945c600d98
Dzhavat Ushev <dzu@energinet.dk>
2025-04-25 12:52:03 +0200

    fix(dh): show circle when data quality has changed

--- libs/dh/shared/data-access-mocks/src/lib/metering-point.mocks.ts
+++ libs/dh/shared/data-access-mocks/src/lib/metering-point.mocks.ts
@@ -266,7 +266,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 1,
-              hasQuantityChanged: false,
+              hasQuantityOrQualityChanged: false,
               historic: measurementPoints.toSpliced(0, 1),
               observationTime: new Date('2023-01-01T23:59:59.99999Z'),
               current: measurementPoints[0],
@@ -274,7 +274,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 2,
-              hasQuantityChanged: true,
+              hasQuantityOrQualityChanged: true,
               historic: measurementPoints.toSpliced(0, 1),
               observationTime: new Date('2023-01-01T00:00:00Z'),
               current: measurementPoints[0],
@@ -282,7 +282,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 3,
-              hasQuantityChanged: false,
+              hasQuantityOrQualityChanged: false,
               historic: measurementPoints.toSpliced(0, 1).toSpliced(0, 1),
               observationTime: new Date('2023-01-01T01:00:00Z'),
               current: measurementPoints.toSpliced(0, 1)[0],
@@ -290,7 +290,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 4,
-              hasQuantityChanged: false,
+              hasQuantityOrQualityChanged: false,
               historic: measurementPoints.toSpliced(2, 4).toSpliced(0, 1),
               observationTime: new Date('2023-01-01T02:00:00Z'),
               current: measurementPoints.toSpliced(2, 4)[0],
@@ -298,7 +298,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 5,
-              hasQuantityChanged: false,
+              hasQuantityOrQualityChanged: false,
               historic: measurementPoints.toSpliced(1, 3).toSpliced(0, 1),
               observationTime: new Date('2023-01-01T03:00:00Z'),
               current: measurementPoints.toSpliced(1, 3)[0],
@@ -306,7 +306,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 6,
-              hasQuantityChanged: false,
+              hasQuantityOrQualityChanged: false,
               historic: measurementPoints.toSpliced(1, 4).toSpliced(0, 1),
               observationTime: new Date('2023-01-01T04:00:00Z'),
               current: measurementPoints.toSpliced(1, 4)[0],
@@ -314,7 +314,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 7,
-              hasQuantityChanged: false,
+              hasQuantityOrQualityChanged: false,
               historic: measurementPoints.toSpliced(2, 3).toSpliced(0, 1),
               observationTime: new Date('2023-01-01T05:00:00Z'),
               current: measurementPoints.toSpliced(2, 3)[0],
@@ -322,7 +322,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 8,
-              hasQuantityChanged: false,
+              hasQuantityOrQualityChanged: false,
               historic: measurementPoints.toSpliced(0, 3).toSpliced(0, 1),
               observationTime: new Date('2023-01-01T06:00:00Z'),
               current: measurementPoints.toSpliced(0, 3)[0],
@@ -330,7 +330,7 @@ function getMeasurements() {
             {
               __typename: 'MeasurementPositionDto',
               index: 9,
-              hasQuantityChanged: true,
+              hasQuantityOrQualityChanged: true,
               historic: measurementPoints.toSpliced(0, 3).toSpliced(0, 1),
               observationTime: new Date('2023-01-01T07:00:00Z'),
               current: measurementPoints.toSpliced(0, 3)[0],
