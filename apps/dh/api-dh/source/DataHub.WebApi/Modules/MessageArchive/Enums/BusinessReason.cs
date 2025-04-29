6c99436a48118b6a5f850b94d43539b27ab30b45
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-04-29 16:32:43 +0200

    Move BusinessReason enum to modules structure

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/MessageArchive/Enums/BusinessReason.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/MessageArchive/Enums/BusinessReason.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.WebApi.GraphQL.Enums;
+namespace Energinet.DataHub.WebApi.Modules.MessageArchive.Enums;
 
 public enum BusinessReason
 {
