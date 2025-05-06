6ee8ee30f9628fd7f500eff7d97e3d4672abd885
sahma19 <saiuan@msn.com>
2025-05-06 20:20:07 +0200

    Capital M

--- libs/eo/core/globalization/assets-localization/i18n/en.ts
+++ libs/eo/core/globalization/assets-localization/i18n/en.ts
@@ -489,7 +489,7 @@ export const EN_TRANSLATIONS: TranslationKeys = {
     fuelCodeLabel: 'Fuel code',
     backToCertificatesLink: '<< Back to Certificates',
     biddingZoneHeadline: 'Bidding Zone',
-    municipalityLabel: 'municipality',
+    municipalityLabel: 'Municipality',
     energyTag: {
       headline: 'EnergyTag',
       connectedGridIdentification: 'Grid',
