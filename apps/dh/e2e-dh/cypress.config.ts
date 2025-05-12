bcd7359e421121ad0df2c30df1e6d21cc2949aaf
Morten Hansen <mimo@mimo-design.com>
2025-05-12 09:01:14 +0200

    Trying

--- apps/dh/e2e-dh/cypress.config.ts
+++ apps/dh/e2e-dh/cypress.config.ts
@@ -20,7 +20,12 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname, { bundler: 'vite' }),
+  e2e: {
+    ...nxE2EPreset(__dirname, { bundler: 'vite' }),
+    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
+    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
+    injectDocumentDomain: true,
+  },
   video: true,
   defaultCommandTimeout: 6000,
   viewportWidth: 1280,
