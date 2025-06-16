cb63aa6a0db7b411ebbafa3ce4050c55d961a2cd
Mathias Sejrup Plougmann <masep@energinet.dk>
2025-06-16 14:49:37 +0200

    Start report generation button (#4372)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/eo/core/globalization/assets-localization/i18n/translation-keys.ts
+++ libs/eo/core/globalization/assets-localization/i18n/translation-keys.ts
@@ -124,6 +124,7 @@ export interface TranslationKeys {
     transfers: string;
     activityLog: string;
     consent: string;
+    reports: string;
   };
   footer: {
     poweredBy: string;
@@ -480,6 +481,31 @@ export interface TranslationKeys {
           EndDateChanged: string;
           Expired: string;
         };
+      };
+    };
+  };
+  reports: {
+    title: string;
+    overview: {
+      title: string;
+      newReport: string;
+      table: {
+        createdAtTitle: string;
+        statusTitle: string;
+        status: {
+          pending: string;
+          completed: string;
+          failed: string;
+        };
+      };
+      modal: {
+        title: string;
+        disclaimer: string;
+        startDateLabel: string;
+        endDateLabel: string;
+        startDateAfterEndDateErrorMessage: string;
+        cancel: string;
+        start: string;
       };
     };
   };
