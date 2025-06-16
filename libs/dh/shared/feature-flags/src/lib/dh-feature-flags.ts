76a2857dfad165dbd80f4f2bd8b79b4dd87eba58
Jesper Justesen <jesper.justesen@gmail.com>
2025-06-16 09:01:01 +0200

    Update dh-feature-flags.ts

--- libs/dh/shared/feature-flags/src/lib/dh-feature-flags.ts
+++ libs/dh/shared/feature-flags/src/lib/dh-feature-flags.ts
@@ -80,7 +80,6 @@ export const dhFeatureFlagsConfig = {
     created: latestBump,
     disabledEnvironments: [
       DhAppEnvironment.local,
-      DhAppEnvironment.test_001,
       DhAppEnvironment.test_002,
       DhAppEnvironment.preprod,
       DhAppEnvironment.prod,
