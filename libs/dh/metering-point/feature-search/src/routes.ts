17948429c46c360013c1b851050832c6f12224c7
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-06-18 12:09:57 +0200

    Guard route with permission and release toggle

--- libs/dh/metering-point/feature-search/src/routes.ts
+++ libs/dh/metering-point/feature-search/src/routes.ts
@@ -34,11 +34,13 @@ import {
   MeasurementsSubPaths,
   MeteringPointSubPaths,
 } from '@energinet-datahub/dh/core/routing';
+import { EicFunction } from '@energinet-datahub/dh/shared/domain/graphql';
+import { dhReleaseToggleGuard } from '@energinet-datahub/dh/shared/release-toggle';
 
 import { DhSearchComponent } from './components/dh-search.component';
 import { dhMeteringPointIdParam } from './components/dh-metering-point-id-param';
 import { dhCanActivateMeteringPointOverview } from './components/dh-can-activate-metering-point-overview';
-import { EicFunction } from '@energinet-datahub/dh/shared/domain/graphql';
+
 import { inject } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
@@ -143,6 +145,10 @@ export const dhMeteringPointRoutes: Routes = [
               },
               {
                 path: getPath<MeasurementsSubPaths>('upload'),
+                canActivate: [
+                  PermissionGuard(['measurements:manage']),
+                  dhReleaseToggleGuard('PM96-SHAREMEASUREDATA'),
+                ],
                 loadComponent: () =>
                   import('@energinet-datahub/dh/metering-point/feature-overview').then(
                     (m) => m.DhMeasurementsUploadComponent
