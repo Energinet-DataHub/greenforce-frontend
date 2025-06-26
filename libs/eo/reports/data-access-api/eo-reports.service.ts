f36d16d4279d70e726433c49d3fcfc73a7087f35
Mathias Sejrup Plougmann <masep@energinet.dk>
2025-06-26 08:48:55 +0200

    chore: fixes lint errors

--- libs/eo/reports/data-access-api/eo-reports.service.ts
+++ libs/eo/reports/data-access-api/eo-reports.service.ts
@@ -19,11 +19,10 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Inject, Injectable, OnDestroy, signal } from '@angular/core';
 import { EoApiEnvironment, eoApiEnvironmentToken } from '@energinet-datahub/eo/shared/environments';
-import { EoReportRequest, EoReport, EoReportResponse } from './report.types';
+import { EoReport, EoReportRequest, EoReportResponse } from './report.types';
 import { catchError, EMPTY, exhaustMap, retry, Subject, takeUntil, timer } from 'rxjs';
 import { EoActorService } from '@energinet-datahub/eo/auth/data-access';
 import { formatDate } from '@angular/common';
-import { WattToastService } from '@energinet/watt/toast';
 
 @Injectable({
   providedIn: 'root',
