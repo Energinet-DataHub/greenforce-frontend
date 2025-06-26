f763e6984388d8b56425c4e5f473b17f6e8c8261
Dzhavat Ushev <dzu@energinet.dk>
2025-06-26 13:34:24 +0200

    chore: upgrade msal to v4.0.7

--- libs/dh/auth/feature-msal/src/lib/dh-b2c-config.ts
+++ libs/dh/auth/feature-msal/src/lib/dh-b2c-config.ts
@@ -49,7 +49,7 @@ export function MSALInstanceFactory(config: DhB2CEnvironment): IPublicClientAppl
         logLevel: LogLevel.Error,
         piiLoggingEnabled: false,
       },
-      allowNativeBroker: false,
+      allowPlatformBroker: false,
       // This value should be higher than the interval of the timer in DhSseLink,
       // which is currently set to 30 seconds. If the value is less than that
       // interval, GraphQL subscriptions may briefly be using an expired token.
