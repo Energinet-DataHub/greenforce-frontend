b03a4f6cb1c96a38f452539f9a87f5ea29abf103
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-07-01 09:16:12 +0200

    Remove obsolete code + move some code around

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/RevisionLog/Middleware/RevisionLogMiddleware.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/RevisionLog/Middleware/RevisionLogMiddleware.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.WebApi.Modules.RevisionLog.Client;
 using HotChocolate.Language;
 using HotChocolate.Resolvers;
 
