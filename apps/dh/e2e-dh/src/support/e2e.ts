71f10e4ce007c6b60c59f85ae925a8b2de6f8e32
Dzhavat Ushev <dzu@energinet.dk>
2025-08-04 20:45:14 +0200

    test(e2e-dh): add some more e2e tests

--- apps/dh/e2e-dh/src/support/e2e.ts
+++ apps/dh/e2e-dh/src/support/e2e.ts
@@ -33,11 +33,7 @@
 import './commands';
 
 beforeEach(() => {
-  if (
-    !['b2c-healthchecks.cy.ts', 'redirect-to-same-url-after-login.cy.ts'].includes(
-      Cypress.spec.name
-    )
-  ) {
+  if (!['b2c-healthchecks.cy.ts', 'redirect-to-initial-url.cy.ts'].includes(Cypress.spec.name)) {
     cy.login(Cypress.env('DH_E2E_USERNAME'), Cypress.env('DH_E2E_PASSWORD'));
   }
 });
