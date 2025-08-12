e72c0d1a9f47a7a59407d55046c7b449e20709a4
Dzhavat Ushev <dzu@energinet.dk>
2025-08-12 14:20:07 +0200

    feat(dh): allow large text files in measurements reports (#4560)

--- libs/dh/reports/feature-measurements-reports/src/components/request-report/request-report-modal.component.ts
+++ libs/dh/reports/feature-measurements-reports/src/components/request-report/request-report-modal.component.ts
@@ -48,6 +48,7 @@ import { WattRange } from '@energinet-datahub/watt/date';
 import { WattFieldErrorComponent, WattFieldHintComponent } from '@energinet-datahub/watt/field';
 import { WattToastService } from '@energinet-datahub/watt/toast';
 import { WattRangeValidators } from '@energinet-datahub/watt/validators';
+import { WattCheckboxComponent } from '@energinet-datahub/watt/checkbox';
 
 import {
   getActorOptionsSignal,
@@ -63,6 +64,7 @@ import {
   RequestMeasurementsReportMutation,
 } from '@energinet-datahub/dh/shared/domain/graphql';
 import { mutation } from '@energinet-datahub/dh/shared/util-apollo';
+
 import {
   DhDropdownTranslatorDirective,
   dhEnumToWattDropdownOptions,
@@ -80,6 +82,7 @@ type DhFormType = FormGroup<{
   meteringPointTypes: FormControl<MeasurementsReportMeteringPointType[] | null>;
   energySupplier?: FormControl<string | null>;
   resolution: FormControl<AggregatedResolution>;
+  allowLargeTextFiles: FormControl<boolean>;
 }>;
 
 type MeasurementsReportRequestedBy = {
@@ -102,6 +105,7 @@ type MeasurementsReportRequestedBy = {
     WattButtonComponent,
     WattFieldErrorComponent,
     WattFieldHintComponent,
+    WattCheckboxComponent,
     DhDropdownTranslatorDirective,
   ],
   styles: `
@@ -126,7 +130,6 @@ export class DhRequestReportModal extends WattTypedModal<MeasurementsReportReque
   private readonly destroyRef = inject(DestroyRef);
 
   private readonly toastService = inject(WattToastService);
-  private readonly datepicker = viewChild.required(WattDatepickerComponent);
 
   private readonly requestReportMutation = mutation(RequestMeasurementsReportDocument);
 
@@ -141,6 +144,7 @@ export class DhRequestReportModal extends WattTypedModal<MeasurementsReportReque
     resolution: new FormControl<AggregatedResolution>(AggregatedResolution.ActualResolution, {
       nonNullable: true,
     }),
+    allowLargeTextFiles: new FormControl<boolean>(false, { nonNullable: true }),
   });
 
   private gridAreaChanges = toSignal(this.form.controls.gridAreas.valueChanges);
@@ -202,8 +206,14 @@ export class DhRequestReportModal extends WattTypedModal<MeasurementsReportReque
       return;
     }
 
-    const { meteringPointTypes, resolution, energySupplier, period, gridAreas } =
-      this.form.getRawValue();
+    const {
+      meteringPointTypes,
+      resolution,
+      energySupplier,
+      period,
+      gridAreas,
+      allowLargeTextFiles,
+    } = this.form.getRawValue();
 
     if (period == null || gridAreas == null) {
       return;
@@ -217,6 +227,7 @@ export class DhRequestReportModal extends WattTypedModal<MeasurementsReportReque
             end: period.end ? period.end : null,
           },
           gridAreaCodes: gridAreas,
+          preventLargeTextFiles: !allowLargeTextFiles,
           meteringPointTypes:
             meteringPointTypes ?? Object.values(MeasurementsReportMeteringPointType),
           energySupplier: energySupplier === ALL_ENERGY_SUPPLIERS ? null : energySupplier,
