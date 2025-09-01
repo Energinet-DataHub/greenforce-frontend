62d239257df62a5ac962d04f9cff81bf672402a5
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-09-01 15:01:55 +0200

    Use composite add for expanding multiple "rows" at once

--- libs/watt/package/table/watt-table-expand-animation.ts
+++ libs/watt/package/table/watt-table-expand-animation.ts
@@ -51,6 +51,7 @@ export const animateExpandableCells = (
             {
               duration: 300,
               easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+              composite: 'add',
             }
           );
         });
