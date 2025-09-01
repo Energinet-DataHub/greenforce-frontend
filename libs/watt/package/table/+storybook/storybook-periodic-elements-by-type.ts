6747155276b3479bed0689fcac1f9b93b3f576ad
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-09-01 14:59:53 +0200

    Add expand/collapse all buttons to story

--- libs/watt/package/table/+storybook/storybook-periodic-elements-by-type.ts
+++ libs/watt/package/table/+storybook/storybook-periodic-elements-by-type.ts
@@ -16,7 +16,13 @@
  * limitations under the License.
  */
 //#endregion
-import { Component, computed, input, linkedSignal } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
+import {
+  VaterFlexComponent,
+  VaterStackComponent,
+  VaterUtilityDirective,
+} from '@energinet-datahub/watt/vater';
+import { WattButtonComponent } from '@energinet-datahub/watt/button';
 import { WATT_TABLE, WattTableColumnDef } from '../watt-table.component';
 import { WattTableDataSource } from '../watt-table-data-source';
 import { StorybookPeriodicElements } from './storybook-periodic-elements';
@@ -24,38 +30,53 @@ import { PeriodicElementsByType } from './storybook-periodic-elements-data';
 
 @Component({
   selector: 'storybook-periodic-elements-by-type',
-  imports: [StorybookPeriodicElements, WATT_TABLE],
+  imports: [
+    StorybookPeriodicElements,
+    VaterFlexComponent,
+    VaterStackComponent,
+    VaterUtilityDirective,
+    WattButtonComponent,
+    WATT_TABLE,
+  ],
   styles: `
     :host {
       display: contents;
     }
-
-    watt-table {
-      height: 100%;
-    }
   `,
   template: `
-    <watt-table
-      #table
-      description="Atomic Elements"
-      sortBy="position"
-      sortDirection="asc"
-      variant="zebra"
-      [dataSource]="dataSource()"
-      [columns]="columns"
-    >
-      <ng-container *wattTableCell="table.columns.type; let category">
-        {{ category.title }}
-      </ng-container>
-      <ng-container *wattTableCell="table.columns.elements; let category">
-        <storybook-periodic-elements [data]="category.elements" [hideColumnHeaders]="true" />
-      </ng-container>
-    </watt-table>
+    <vater-flex fill="vertical" gap="m">
+      <vater-stack fill="horizontal" justify="end" direction="row" gap="m">
+        <watt-button variant="secondary" (click)="expand()">Expand all</watt-button>
+        <watt-button variant="secondary" (click)="collapse()">Collapse all</watt-button>
+      </vater-stack>
+      <watt-table
+        #table
+        vater
+        fill="vertical"
+        description="Atomic Elements"
+        sortBy="position"
+        sortDirection="asc"
+        variant="zebra"
+        [dataSource]="dataSource()"
+        [columns]="columns"
+        [expanded]="expanded()"
+      >
+        <ng-container *wattTableCell="table.columns.type; let category">
+          {{ category.title }}
+        </ng-container>
+        <ng-container *wattTableCell="table.columns.elements; let category">
+          <storybook-periodic-elements [data]="category.elements" [hideColumnHeaders]="true" />
+        </ng-container>
+      </watt-table>
+    </vater-flex>
   `,
 })
 export class StorybookPeriodicElementsByType {
+  expanded = signal<PeriodicElementsByType[]>([]);
   data = input.required<PeriodicElementsByType[]>();
   dataSource = computed(() => new WattTableDataSource(this.data()));
+  expand = () => this.expanded.set(this.dataSource().data);
+  collapse = () => this.expanded.set([]);
   columns = {
     type: { accessor: (row) => row.title },
     elements: { accessor: (row) => row.elements, expandable: true },
