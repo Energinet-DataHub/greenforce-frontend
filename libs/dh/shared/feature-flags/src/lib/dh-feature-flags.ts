41685ed60d9ada403bd7d78569d99b1c00145bf1
Dzhavat Ushev <dzu@energinet.dk>
2025-09-09 12:12:59 +0200

    feat(dh): enable new security model in prod (#4662)

--- libs/dh/shared/feature-flags/src/lib/dh-feature-flags.ts
+++ libs/dh/shared/feature-flags/src/lib/dh-feature-flags.ts
@@ -62,7 +62,7 @@ export const dhFeatureFlagsConfig = {
   },
   'new-security-model': {
     created: latestBump,
-    disabledEnvironments: [DhAppEnvironment.prod, DhAppEnvironment.local],
+    disabledEnvironments: [DhAppEnvironment.local],
   },
 } satisfies FeatureFlagConfig;
 
