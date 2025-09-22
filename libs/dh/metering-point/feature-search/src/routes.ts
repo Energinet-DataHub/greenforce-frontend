e8398bae8fd3f59644b17b58f1b68681cde5db98
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-09-22 09:24:15 +0200

    Remove import

--- libs/dh/metering-point/feature-search/src/routes.ts
+++ libs/dh/metering-point/feature-search/src/routes.ts
@@ -37,7 +37,6 @@ import { dhReleaseToggleGuard } from '@energinet-datahub/dh/shared/release-toggl
 import { DhSearchComponent } from './components/dh-search.component';
 import { dhMeteringPointIdParam } from './components/dh-metering-point-id-param';
 import { dhCanActivateMeteringPointOverview } from './components/dh-can-activate-metering-point-overview';
-import { FeatureFlagGuard } from '@energinet-datahub/dh/shared/feature-flags';
 
 const marketRolesWithDataAccess = [
   EicFunction.EnergySupplier,
