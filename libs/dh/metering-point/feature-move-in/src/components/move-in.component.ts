8cc76d838b983cc227f263f14cc658ba17e242f7
Mathias Sejrup Plougmann <masep@energinet.dk>
2025-09-24 10:17:37 +0200

    First stab at contact details form (#4722)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/dh/metering-point/feature-move-in/src/components/move-in.component.ts
+++ libs/dh/metering-point/feature-move-in/src/components/move-in.component.ts
@@ -24,12 +24,19 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { WATT_MODAL, WattTypedModal } from '@energinet-datahub/watt/modal';
 import { WATT_STEPPER } from '@energinet-datahub/watt/stepper';
 
-import { MoveInCustomerDetailsFormType } from '../types';
+import { MoveInContactDetailsFormType, MoveInCustomerDetailsFormType } from '../types';
 import { DhCustomerDetailsComponent } from './customer-details.component';
+import { DhContactDetailsFormComponent } from './dh-contact-details-form.component';
 
 @Component({
   selector: 'dh-move-in',
-  imports: [TranslocoDirective, WATT_MODAL, WATT_STEPPER, DhCustomerDetailsComponent],
+  imports: [
+    TranslocoDirective,
+    WATT_MODAL,
+    WATT_STEPPER,
+    DhCustomerDetailsComponent,
+    DhContactDetailsFormComponent,
+  ],
   templateUrl: './move-in.component.html',
 })
 export class DhMoveInComponent extends WattTypedModal {
@@ -50,8 +57,47 @@ export class DhMoveInComponent extends WattTypedModal {
     isProtectedAddress: this.fb.control<boolean>(false),
   });
 
-  contactDetailsForm = this.fb.group({
-    // Define form controls and validation here
+  contactDetailsForm = this.fb.group<MoveInContactDetailsFormType>({
+    legalContactSameAsCustomer: this.fb.control<boolean>(true),
+    legalContactName: this.fb.control<string>('', [Validators.required]),
+    legalContactTitle: this.fb.control<string>(''),
+    legalContactPhone: this.fb.control<string>(''),
+    legalContactMobile: this.fb.control<string>(''),
+    legalContactEmail: this.fb.control<string>(''),
+    legalAddressSameAsMeteringPoint: this.fb.control<boolean>(true),
+    legalAddressStreet: this.fb.control<string>('', [Validators.required]),
+    legalAddressNumber: this.fb.control<string>(''),
+    legalAddressFloor: this.fb.control<string>(''),
+    legalAddressDoor: this.fb.control<string>(''),
+    legalAddressPostalCode: this.fb.control<string>('', [Validators.required]),
+    legalAddressCity: this.fb.control<string>('', [Validators.required]),
+    legalAddressCountry: this.fb.control<string>(''),
+    legalAddressRoadCode: this.fb.control<string>(''),
+    legalAddressPostalDistrict: this.fb.control<string>(''),
+    legalAddressPostBox: this.fb.control<string>(''),
+    legalAddressMunicipalityCode: this.fb.control<string>(''),
+    legalAddressDarReference: this.fb.control<string>(''),
+    legalNameAddressProtection: this.fb.control<boolean>(false),
+    technicalContactSameAsCustomer: this.fb.control<boolean>(true),
+    technicalContactName: this.fb.control<string>(''),
+    technicalContactTitle: this.fb.control<string>(''),
+    technicalContactPhone: this.fb.control<string>(''),
+    technicalContactMobile: this.fb.control<string>(''),
+    technicalContactEmail: this.fb.control<string>(''),
+    technicalAddressSameAsMeteringPoint: this.fb.control<boolean>(true),
+    technicalAddressStreet: this.fb.control<string>('', [Validators.required]),
+    technicalAddressNumber: this.fb.control<string>(''),
+    technicalAddressFloor: this.fb.control<string>(''),
+    technicalAddressDoor: this.fb.control<string>(''),
+    technicalAddressPostalCode: this.fb.control<string>('', [Validators.required]),
+    technicalAddressCity: this.fb.control<string>('', [Validators.required]),
+    technicalAddressCountry: this.fb.control<string>(''),
+    technicalAddressRoadCode: this.fb.control<string>(''),
+    technicalAddressPostalDistrict: this.fb.control<string>(''),
+    technicalAddressPostBox: this.fb.control<string>(''),
+    technicalAddressMunicipalityCode: this.fb.control<string>(''),
+    technicalAddressDarReference: this.fb.control<string>(''),
+    technicalNameAddressProtection: this.fb.control<boolean>(false),
   });
 
   private customerTypeChanged = toSignal(
