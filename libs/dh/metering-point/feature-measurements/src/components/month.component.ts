83f6e4087e4d18d5a7e17cddf5896a91ff5c41cf
Dzhavat Ushev <dzu@energinet.dk>
2025-09-24 10:31:22 +0200

    feat: add support for startView on yearmonth component

--- libs/dh/metering-point/feature-measurements/src/components/month.component.ts
+++ libs/dh/metering-point/feature-measurements/src/components/month.component.ts
@@ -95,7 +95,11 @@ import { dhFormatMeasurementNumber } from '../utils/dh-format-measurement-number
       <watt-data-filters *transloco="let t; read: 'meteringPoint.measurements.filters'">
         <form wattQueryParams [formGroup]="form">
           <vater-stack direction="row" gap="ml" align="baseline">
-            <watt-yearmonth-field [formControl]="form.controls.yearMonth" canStepThroughMonths />
+            <watt-yearmonth-field
+              [formControl]="form.controls.yearMonth"
+              startView="year"
+              canStepThroughMonths
+            />
             <!-- <watt-slide-toggle [formControl]="form.controls.showOnlyChangedValues">
               {{ t('showOnlyChangedValues') }}
             </watt-slide-toggle> -->
