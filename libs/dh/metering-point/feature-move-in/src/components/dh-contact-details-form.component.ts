b3bc15e81a94439e6c3692519f90baf536f828fb
Mathias Sejrup Plougmann <masep@energinet.dk>
2025-09-24 09:23:34 +0200

    chore: alters translation strings to use correct format, removes unnecessary declarations, and implements country options in contact details form - suggestions from PR

--- libs/dh/metering-point/feature-move-in/src/components/dh-contact-details-form.component.ts
+++ libs/dh/metering-point/feature-move-in/src/components/dh-contact-details-form.component.ts
@@ -16,9 +16,8 @@
  * limitations under the License.
  */
 //#endregion
-import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, input } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, input, } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, } from '@angular/forms';
 import { TranslocoDirective } from '@jsverse/transloco';
 
 import { WattDropdownComponent, WattDropdownOptions } from '@energinet-datahub/watt/dropdown';
@@ -26,19 +25,18 @@ import { WattCheckboxComponent } from '@energinet-datahub/watt/checkbox';
 import { WattTextFieldComponent } from '@energinet-datahub/watt/text-field';
 import { WattPhoneFieldComponent } from '@energinet-datahub/watt/phone-field';
 import { MoveInContactDetailsFormType } from '../types';
+import { DhDropdownTranslatorDirective } from '@energinet-datahub/dh/shared/ui-util';
 
 @Component({
   selector: 'dh-contact-details-form',
-  standalone: true,
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
-    CommonModule,
     ReactiveFormsModule,
     TranslocoDirective,
     WattDropdownComponent,
     WattCheckboxComponent,
     WattTextFieldComponent,
     WattPhoneFieldComponent,
+    DhDropdownTranslatorDirective,
   ],
   template: `
     @let form = contactDetailsForm();
@@ -106,8 +104,10 @@ import { MoveInContactDetailsFormType } from '../types';
           </div>
 
           <watt-dropdown
+            translateKey="shared.countries"
+            dhDropdownTranslator
             [formControl]="form.controls.legalAddressCountry"
-            [options]="countries()"
+            [options]="countryOptions"
             [label]="t('country')"
           />
 
@@ -167,18 +167,18 @@ import { MoveInContactDetailsFormType } from '../types';
             [label]="t('mobile')"
           />
 
-          <div>
-            <watt-text-field
-              [formControl]="form.controls.technicalContactEmail"
-              [label]="t('email')"
-            />
-          </div>
+          <watt-text-field
+            [formControl]="form.controls.technicalContactEmail"
+            [label]="t('email')"
+          />
           <h4>
             {{ t('addressDetails') }}
           </h4>
+
           <watt-checkbox [formControl]="form.controls.technicalAddressSameAsMeteringPoint">
             {{ t('addressSameAsMeteringPoint') }}
           </watt-checkbox>
+
           <watt-text-field
             [formControl]="form.controls.technicalAddressStreet"
             [label]="t('street')"
@@ -211,8 +211,10 @@ import { MoveInContactDetailsFormType } from '../types';
           </div>
 
           <watt-dropdown
+            translateKey="shared.countries"
+            dhDropdownTranslator
             [formControl]="form.controls.technicalAddressCountry"
-            [options]="countries()"
+            [options]="countryOptions"
             [label]="t('country')"
           />
           <watt-text-field
@@ -260,5 +262,14 @@ import { MoveInContactDetailsFormType } from '../types';
 })
 export class DhContactDetailsFormComponent {
   contactDetailsForm = input.required<FormGroup<MoveInContactDetailsFormType>>();
-  countries = input.required<WattDropdownOptions>();
+  countryOptions: WattDropdownOptions = [
+    { value: 'DK', displayValue: 'DK' },
+    { value: 'SE', displayValue: 'SE' },
+    { value: 'NO', displayValue: 'NO' },
+    { value: 'DE', displayValue: 'DE' },
+    { value: 'FI', displayValue: 'FI' },
+    { value: 'PL', displayValue: 'PL' },
+    { value: 'NL', displayValue: 'NL' },
+    { value: 'CH', displayValue: 'CH' },
+  ];
 }
