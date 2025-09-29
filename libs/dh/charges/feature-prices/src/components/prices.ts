dd7cfdd114901f842b3a6d18ea01f53f86441b03
Morten Hansen <mimo@mimo-design.com>
2025-09-29 13:08:10 +0200

    Added on push

--- libs/dh/charges/feature-prices/src/components/prices.ts
+++ libs/dh/charges/feature-prices/src/components/prices.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'dh-prices',
   template: ` <h1>Prices</h1> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DhPricesComponent {}
