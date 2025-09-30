b9f77a161a28cade112aba537040007a86dd4483
Morten Vestergaard Hansen <mimo@mimo-design.com>
2025-09-30 08:58:00 +0200

    Added table for charges (#4752)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/dh/charges/feature-charges/src/components/charges.component.ts
+++ libs/dh/charges/feature-charges/src/components/charges.component.ts
@@ -16,11 +16,101 @@
  * limitations under the License.
  */
 //#endregione';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DhNavigationService } from '@energinet-datahub/dh/shared/navigation';
+import { WattDataFiltersComponent, WattDataTableComponent } from '@energinet-datahub/watt/data';
+
+import {
+  WattTableCellDirective,
+  WattTableColumnDef,
+  WattTableComponent,
+  WattTableDataSource,
+} from '@energinet-datahub/watt/table';
+import { VaterUtilityDirective } from '@energinet-datahub/watt/vater';
+import { TranslocoDirective } from '@jsverse/transloco';
+
+type Charge = {
+  id: string;
+  type: string;
+  name: string;
+  owner: string;
+  status: string;
+};
 
 @Component({
   selector: 'dh-charges',
-  template: ` <h1>Charges</h1> `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    RouterOutlet,
+    TranslocoDirective,
+    WattTableComponent,
+    WattTableCellDirective,
+    WattDataTableComponent,
+    WattDataFiltersComponent,
+
+    VaterUtilityDirective,
+  ],
+  providers: [DhNavigationService],
+  template: `
+    <watt-data-table
+      [enableCount]="false"
+      vater
+      inset="ml"
+      *transloco="let t; prefix: 'charges.charges.table'"
+    >
+      <watt-data-filters />
+
+      <watt-table
+        *transloco="let resolveHeader; prefix: 'charges.charges.table.columns'"
+        [dataSource]="dataSource"
+        [columns]="columns"
+        [resolveHeader]="resolveHeader"
+        [activeRow]="selection()"
+        (rowClick)="navigation.navigate('details', $event.id)"
+      >
+        <ng-container *wattTableCell="columns.type; let element">
+          {{ element.type }}
+        </ng-container>
+        <ng-container *wattTableCell="columns.id; let element">
+          {{ element.id }}
+        </ng-container>
+        <ng-container *wattTableCell="columns.name; let element">
+          {{ element.name }}
+        </ng-container>
+        <ng-container *wattTableCell="columns.owner; let element">
+          {{ element.owner }}
+        </ng-container>
+        <ng-container *wattTableCell="columns.status; let element">
+          {{ element.status }}
+        </ng-container>
+      </watt-table>
+    </watt-data-table>
+    <router-outlet />
+  `,
 })
-export class DhChargesComponent {}
+export class DhChargesComponent {
+  protected readonly navigation = inject(DhNavigationService);
+
+  dataSource = new WattTableDataSource<Charge>([
+    {
+      id: '1',
+      type: 'Standard',
+      name: 'Standard Charge',
+      owner: 'EnergyCo',
+      status: 'Active',
+    },
+  ]);
+
+  columns: WattTableColumnDef<Charge> = {
+    type: { accessor: 'type' },
+    id: { accessor: 'id' },
+    name: { accessor: 'name' },
+    owner: { accessor: 'owner' },
+    status: { accessor: 'status' },
+  };
+
+  selection = () => {
+    return this.dataSource.filteredData.find((row) => row.id === this.navigation.id());
+  };
+}
