0bd2c082fefff589d51796d6707d8a81995c140d
Bjørn Hoffmann <ManBearTM@users.noreply.github.com>
2025-10-09 10:50:44 +0200

    formatting

--- libs/dh/reports/feature-settlement-reports/src/components/report-status.component.ts
+++ libs/dh/reports/feature-settlement-reports/src/components/report-status.component.ts
@@ -35,7 +35,7 @@ import { WattButtonComponent } from '@energinet-datahub/watt/button';
       }
       @case ('COMPLETED') {
         <watt-button
-         variant="text"
+          variant="text"
           size="small"
           icon="fileDownload"
           (click)="download.emit($event)"
