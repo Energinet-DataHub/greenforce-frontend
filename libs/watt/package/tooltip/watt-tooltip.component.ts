f96be6d05074c2fd005e7983fb0b7744af9952aa
Mathias Sejrup Plougmann <masep@energinet.dk>
2025-10-31 12:54:30 +0100

    chore: updates to signal standards

--- libs/watt/package/tooltip/watt-tooltip.component.ts
+++ libs/watt/package/tooltip/watt-tooltip.component.ts
@@ -17,103 +17,105 @@
  */
 //#endregion
 import {
-  AfterViewInit,
   Component,
+  computed,
+  DestroyRef,
+  effect,
   ElementRef,
-  HostBinding,
   inject,
-  Input,
-  OnDestroy,
+  input,
+
   Renderer2,
-  ViewChild,
+  viewChild,
 } from '@angular/core';
 import { createPopper, Instance } from '@popperjs/core';
 import { Platform } from '@angular/cdk/platform';
 
 import { wattTooltipPosition, wattTooltipVariant } from './watt-tooltip.directive';
 import { FocusMonitor } from '@angular/cdk/a11y';
-import { Subject, takeUntil } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 type unlistenerFunction = () => void;
 
 @Component({
   template: `
-    {{ text }}
-    <div class="arrow"></div>
+    {{ text() }}
+    <div #arrow class="arrow"></div>
   `,
   selector: 'watt-tooltip',
   styleUrls: ['./watt-tooltip.component.scss'],
+  host: {
+    '[id]': 'id',
+    '[attr.role]': '"tooltip"',
+    '[class]': 'hostClass()'
+  }
 })
-export class WattTooltipComponent implements AfterViewInit, OnDestroy {
+export class WattTooltipComponent {
   static nextId = 0;
 
-  @Input() text!: string;
-  @Input() target!: HTMLElement;
-  @Input() position!: wattTooltipPosition;
-  @Input() variant!: wattTooltipVariant;
+  text = input.required<string>();
+  target = input.required<HTMLElement>();
+  position = input.required<wattTooltipPosition>();
+  variant = input.required<wattTooltipVariant>();
 
-  @ViewChild('arrow') arrow!: ElementRef<HTMLElement>;
+  arrow = viewChild<ElementRef<HTMLElement>>('arrow');
 
-  @HostBinding() id = `watt-tooltip-${WattTooltipComponent.nextId++}`; // used by aria-describedby
-  @HostBinding('attr.role') role = 'tooltip';
-  @HostBinding('class') get hostClass() {
-    return `tooltip-${this.variant}`;
-  }
+  readonly id = `watt-tooltip-${WattTooltipComponent.nextId++}`; // used by aria-describedby
+  hostClass = computed(() => `tooltip-${this.variant()}`);
 
   private element: HTMLElement = inject(ElementRef).nativeElement;
   private platform = inject(Platform);
   private renderer: Renderer2 = inject(Renderer2);
   private focusMonitor: FocusMonitor = inject(FocusMonitor);
+  private destroyRef = inject(DestroyRef);
 
-  /** Emits when the component is destroyed. */
-  private readonly destroyed = new Subject<void>();
   private listeners: unlistenerFunction[] = [];
   private showClass = 'show';
-
   private popper: Instance | null = null;
 
-  ngAfterViewInit(): void {
-    this.setupEventListeners();
+  constructor() {
+    this.destroyRef.onDestroy(() => {
+      this.popper?.destroy();
+      this.listeners.forEach(listener => listener());
+    });
 
-    this.focusMonitor
-      .monitor(this.target, true)
-      .pipe(takeUntil(this.destroyed))
-      .subscribe((origin) => {
-        if (!origin) {
-          this.hide();
-        } else if (origin === 'keyboard') {
-          this.show();
-        }
-      });
+    effect(() => {
+      this.setupEventListeners();
+
+      this.focusMonitor
+        .monitor(this.target(), true)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe((origin) => {
+          if (!origin) {
+            this.hide();
+          } else if (origin === 'keyboard') {
+            this.show();
+          }
+        });
+    });
   }
 
-  ngOnDestroy(): void {
-    this.popper?.destroy();
-    this.listeners.forEach((listener) => listener());
-    this.destroyed.next();
-    this.destroyed.complete();
-  }
 
   private setupEventListeners(): void {
     // The mouse events shouldn't be bound on mobile devices, because they can prevent the
     // first tap from firing its click event or can cause the tooltip to open for clicks.
     if (this.platformSupportsMouseEvents()) {
-      const mouseEnter = this.renderer.listen(this.target, 'mouseenter', this.show.bind(this));
-      const mouseLeave = this.renderer.listen(this.target, 'mouseleave', this.hide.bind(this));
-      const wheel = this.renderer.listen(this.target, 'wheel', this.hide.bind(this));
+      const mouseEnter = this.renderer.listen(this.target(), 'mouseenter', this.show.bind(this));
+      const mouseLeave = this.renderer.listen(this.target(), 'mouseleave', this.hide.bind(this));
+      const wheel = this.renderer.listen(this.target(), 'wheel', this.hide.bind(this));
       this.listeners.push(mouseEnter, mouseLeave, wheel);
     } else {
-      const touchStart = this.renderer.listen(this.target, 'touchstart', this.show.bind(this));
-      const touchEnd = this.renderer.listen(this.target, 'touchend', this.hide.bind(this));
-      const touchCancel = this.renderer.listen(this.target, 'touchcancel', this.hide.bind(this));
+      const touchStart = this.renderer.listen(this.target(), 'touchstart', this.show.bind(this));
+      const touchEnd = this.renderer.listen(this.target(), 'touchend', this.hide.bind(this));
+      const touchCancel = this.renderer.listen(this.target(), 'touchcancel', this.hide.bind(this));
       this.listeners.push(touchStart, touchEnd, touchCancel);
     }
   }
 
   private show(): void {
     if (!this.popper) {
-      this.popper = createPopper(this.target, this.element, {
-        placement: this.position,
+      this.popper = createPopper(this.target(), this.element, {
+        placement: this.position(),
       });
     } else {
       this.popper.forceUpdate();
