bed9c41779cda1591634edbb6588cab14f103cde
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-11-03 10:51:11 +0100

    Make day table handle all res

--- libs/dh/charges/feature-prices/src/components/series/tables/day.ts
+++ libs/dh/charges/feature-prices/src/components/series/tables/day.ts
@@ -16,23 +16,34 @@
  * limitations under the License.
  */
 //#endregion
-import { ChangeDetectionStrategy, Component, effect, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, input } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { VaterStackComponent } from '@energinet/watt/vater';
 import { WattDatepickerComponent } from '@energinet/watt/datepicker';
+import { dayjs } from '@energinet/watt/date';
 import { WattDataFiltersComponent, WattDataTableComponent } from '@energinet/watt/data';
 import { WATT_TABLE, WattTableColumnDef, WattTableDataSource } from '@energinet/watt/table';
-import { ChargeSeries, GetChargeSeriesDocument } from '@energinet-datahub/dh/shared/domain/graphql';
+import {
+  ChargeResolution,
+  ChargeSeries,
+  GetChargeResolutionDocument,
+  GetChargeSeriesDocument,
+} from '@energinet-datahub/dh/shared/domain/graphql';
+import { DhCircleComponent } from '@energinet-datahub/dh/shared/ui-util';
 import { query } from '@energinet-datahub/dh/shared/util-apollo';
+import { capitalize } from '@energinet-datahub/dh/shared/util-text';
 
 @Component({
   selector: 'dh-prices',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     TranslocoDirective,
+    VaterStackComponent,
     WattDatepickerComponent,
     WattDataFiltersComponent,
     WattDataTableComponent,
     WATT_TABLE,
+    DhCircleComponent,
   ],
   template: `
     <watt-data-table
@@ -46,15 +57,25 @@ import { query } from '@energinet-datahub/dh/shared/util-apollo';
         <watt-datepicker />
       </watt-data-filters>
       <watt-table
-        *transloco="let resolveHeader; read: 'charge.series.columns'"
-        [resolveHeader]="resolveHeader"
+        *transloco="let t; read: 'charges.series.columns'"
+        [resolveHeader]="t"
         [columns]="columns"
         [dataSource]="dataSource"
         [loading]="series.loading()"
         [stickyFooter]="true"
       >
-        <ng-container *wattTableCell="columns.hour; let series">
-          {{ series.totalAmount }}
+        <ng-container *wattTableCell="columns.date; header: t(resolution()); let _; let i = index">
+          {{ formatTime(i) }}
+        </ng-container>
+        <ng-container *wattTableCell="columns.history; header: ''; let series">
+          <vater-stack direction="row" gap="xl">
+            @if (series.points.length > 1) {
+              <dh-circle />
+            }
+            @for (point of series.points; track $index) {
+              <span class="watt-on-light--medium-emphasis">{{ point.price }}</span>
+            }
+          </vater-stack>
         </ng-container>
       </watt-table>
     </watt-data-table>
@@ -62,6 +83,8 @@ import { query } from '@energinet-datahub/dh/shared/util-apollo';
 })
 export class DhChargeSeriesDay {
   id = input.required<string>();
+  charge = query(GetChargeResolutionDocument, () => ({ variables: { id: this.id() } }));
+  resolution = computed(() => this.charge.data()?.chargeById?.resolution ?? 'Unknown');
   series = query(GetChargeSeriesDocument, () => ({
     variables: {
       interval: { start: new Date(), end: new Date() },
@@ -70,9 +93,30 @@ export class DhChargeSeriesDay {
   }));
 
   dataSource = new WattTableDataSource<ChargeSeries>();
-  columns = {
-    hour: { accessor: (row) => row.totalAmount },
-  } satisfies WattTableColumnDef<ChargeSeries>;
+  columns: WattTableColumnDef<ChargeSeries> = {
+    date: { accessor: null },
+    price: {
+      accessor: (row) => row.points.find((r) => r.toDateTime > new Date())?.price,
+      align: 'right',
+    },
+    history: { accessor: null, tooltip: 'What', size: '1fr' },
+  };
+
+  formatTime = (index: number) => {
+    const today = dayjs();
+    switch (this.resolution()) {
+      case 'QuarterHourly':
+        return `${today.minute(index * 15).format('mm')} — ${today.minute((index + 1) * 15).format('mm')}`;
+      case 'Hourly':
+        return `${today.hour(index).format('HH')} — ${today.hour(index + 1).format('HH')}`;
+      case 'Daily':
+        return today.date(index + 1).format('DD');
+      case 'Monthly':
+        return capitalize(today.month(index).format('MMMM'));
+      case 'Unknown':
+        return index + 1;
+    }
+  };
 
   constructor() {
     effect(() => {
