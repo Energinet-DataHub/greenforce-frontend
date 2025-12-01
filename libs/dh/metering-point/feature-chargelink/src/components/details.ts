b16e1ac82f9e9ba61db687ee579fffe916cfd4ef
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-12-01 11:18:09 +0100

    Undo rename in charge link

--- libs/dh/metering-point/feature-chargelink/src/components/details.ts
+++ libs/dh/metering-point/feature-chargelink/src/components/details.ts
@@ -40,7 +40,7 @@ import { DhMeteringPointEditChargeLink } from './edit';
 import { DhMeteringPointCancelChargeLink } from './cancel';
 
 @Component({
-  selector: 'dh-charges-link-details',
+  selector: 'dh-charge-link-details',
   imports: [
     TranslocoDirective,
 
@@ -100,7 +100,7 @@ import { DhMeteringPointCancelChargeLink } from './cancel';
     </watt-drawer>
   `,
 })
-export default class DhChargesLinkDetails {
+export default class DhChargeLinkDetails {
   private readonly modalService = inject(WattModalService);
   query = query(GetChargeLinkHistoryDocument, () => ({
     variables: { chargeLinkId: this.id(), meteringPointId: this.meteringPointId() },
