3bcd47d12dc83f4a80b8f5243c636c0b4e275917
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-12-04 14:32:01 +0100

    Add upload-series modal

---
+++ libs/dh/charges/feature-charges/src/components/actions/upload-series.ts
@@ -0,0 +1,114 @@
+//#region License
+/**
+ * @license
+ * Copyright 2020 Energinet DataHub A/S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License2");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//#endregion
+import { Component, computed, input, signal } from '@angular/core';
+import { ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { TranslocoDirective } from '@jsverse/transloco';
+
+import { WattButtonComponent } from '@energinet/watt/button';
+import { WattDropZone } from '@energinet/watt/dropzone';
+import { WATT_MODAL } from '@energinet/watt/modal';
+
+import { dhMakeFormControl, injectRelativeNavigate } from '@energinet-datahub/dh/shared/ui-util';
+import {
+  parseChargeSeries,
+  ChargeSeriesResult,
+} from '@energinet-datahub/dh/charges/feature-parse-series';
+import { GetChargeByIdDocument } from '@energinet-datahub/dh/shared/domain/graphql';
+import { query } from '@energinet-datahub/dh/shared/util-apollo';
+import { assertIsDefined } from '@energinet-datahub/dh/shared/util-assert';
+import { WattFieldErrorComponent } from '@energinet/watt/field';
+import { WattFieldHintComponent } from '../../../../../../watt/package/field/watt-field-hint.component';
+
+@Component({
+  selector: 'dh-charges-upload-series',
+  imports: [
+    ReactiveFormsModule,
+    TranslocoDirective,
+    WattButtonComponent,
+    WattDropZone,
+    WattFieldErrorComponent,
+    WATT_MODAL,
+    WattFieldHintComponent,
+  ],
+  template: `
+    <watt-modal
+      #modal
+      *transloco="let t; prefix: 'charges.actions.uploadSeries'"
+      autoOpen
+      size="small"
+      [title]="t('title')"
+      (closed)="navigate('..')"
+    >
+      <watt-dropzone
+        accept="text/csv"
+        [formControl]="file"
+        [progress]="progress()"
+        [showProgressBar]="progress() > 0"
+        [loadingMessage]="progress() === 100 ? t('success') : t('loading')"
+      >
+        <watt-field-hint>{{ t('hint') }}</watt-field-hint>
+        @if (file.errors?.multiple) {
+          <watt-field-error>
+            {{ t('errors.multiple') }}
+          </watt-field-error>
+        } @else if (file.errors?.type) {
+          <watt-field-error>
+            {{ t('errors.type') }}
+          </watt-field-error>
+        } @else if (file.errors) {
+          <watt-field-error>
+            {{ t('csvErrors.' + file.errors[0]?.key, { row: file.errors[0]?.index + 2 }) }}
+          </watt-field-error>
+        }
+      </watt-dropzone>
+      <watt-modal-actions>
+        <watt-button variant="secondary" (click)="modal.close(false)">
+          {{ t('close') }}
+        </watt-button>
+        <watt-button variant="primary" [disabled]="!file.valid" (click)="save()">
+          {{ t('submit') }}
+        </watt-button>
+      </watt-modal-actions>
+    </watt-modal>
+  `,
+})
+export default class DhChargesUploadSeries {
+  navigate = injectRelativeNavigate();
+  id = input.required<string>();
+  query = query(GetChargeByIdDocument, () => ({ variables: { id: this.id() } }));
+  resolution = computed(() => this.query.data()?.chargeById?.resolution);
+
+  private validate = async (): Promise<ValidationErrors | null> => {
+    if (!this.file.value) return null;
+    const [file] = this.file.value;
+    const resolution = this.resolution();
+    assertIsDefined(resolution);
+    await parseChargeSeries(file, resolution).forEach(this.chargeSeries.set);
+    const errors = this.chargeSeries()?.errors;
+    return errors?.length ? errors : null;
+  };
+
+  file = dhMakeFormControl<File[]>(null, Validators.required, this.validate);
+  chargeSeries = signal<ChargeSeriesResult | null>(null, { equal: () => false });
+  progress = computed(() => this.chargeSeries()?.progress ?? 0);
+
+  save() {
+    console.log(this.chargeSeries(), 'saving charge series');
+  }
+}
