d9bfbce958df40bf270913951233c61b7be3283b
Dzhavat Ushev <dzu@energinet.dk>
2025-12-09 09:38:11 +0100

    fix(dh): improvements to connection state change (#5042)

--- libs/dh/metering-point/feature-overview/src/components/connection-state-manage/connection-state-manage.ts
+++ libs/dh/metering-point/feature-overview/src/components/connection-state-manage/connection-state-manage.ts
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 //#endregion
-import { Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Validators, FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 
@@ -24,6 +24,9 @@ import { WattButtonComponent } from '@energinet/watt/button';
 import { WattTypedModal, WATT_MODAL } from '@energinet/watt/modal';
 import { WattDropdownComponent } from '@energinet/watt/dropdown';
 import { WattDatepickerComponent } from '@energinet/watt/datepicker';
+import { WattIconComponent } from '@energinet/watt/icon';
+import { WattTooltipDirective } from '@energinet/watt/tooltip';
+import { VaterStackComponent } from '@energinet/watt/vater';
 import { dayjs } from '@energinet/watt/date';
 
 import { ConnectionState } from '@energinet-datahub/dh/shared/domain/graphql';
@@ -36,39 +39,57 @@ import { dhEnumToWattDropdownOptions } from '@energinet-datahub/dh/shared/ui-uti
     TranslocoDirective,
     ReactiveFormsModule,
 
+    VaterStackComponent,
     WATT_MODAL,
     WattButtonComponent,
     WattDropdownComponent,
     WattDatepickerComponent,
+    WattIconComponent,
+    WattTooltipDirective,
   ],
+  styles: `
+    form {
+      width: 50%;
+    }
+  `,
   template: `
-    <watt-modal
-      #modal
-      [title]="t('modalTitle')"
-      *transloco="let t; prefix: 'meteringPoint.changeConnectionState'"
-    >
-      <watt-dropdown
-        [label]="t('connectionStateLabel')"
-        [formControl]="stateControl"
-        [showResetOption]="false"
-        [options]="stateControlOptions"
-      />
+    <watt-modal #modal *transloco="let t; prefix: 'meteringPoint.changeConnectionState'">
+      <h2 vater-stack direction="row" gap="s">
+        {{ t('modalTitle') }}
+        <watt-icon [style.color]="'black'" name="info" [wattTooltip]="t('tooltip')" />
+      </h2>
 
-      @if (modalData.currentConnectionState !== stateControl.value) {
-        <watt-datepicker
-          [label]="t('validityDate')"
-          [formControl]="validityDateControl"
-          [min]="minDate"
-          [max]="today"
+      @let isCurrentStatusSameAsNew =
+        modalData.currentConnectionState === form.controls.state.value;
+
+      <form [formGroup]="form" id="change-connection-state-form" (ngSubmit)="save()">
+        <watt-dropdown
+          [label]="t('connectionStateLabel')"
+          [formControl]="form.controls.state"
+          [showResetOption]="false"
+          [options]="stateControlOptions"
         />
-      }
+
+        @if (!isCurrentStatusSameAsNew) {
+          <watt-datepicker
+            [label]="t('validityDate')"
+            [formControl]="form.controls.validityDate"
+            [min]="minDate"
+            [max]="today"
+          />
+        }
+      </form>
 
       <watt-modal-actions>
         <watt-button variant="secondary" (click)="modal.close(false)">
           {{ t('cancel') }}
         </watt-button>
 
-        <watt-button (click)="save()">
+        <watt-button
+          formId="change-connection-state-form"
+          [disabled]="isCurrentStatusSameAsNew"
+          type="submit"
+        >
           {{ t('save') }}
         </watt-button>
       </watt-modal-actions>
@@ -81,16 +102,22 @@ export class DhConnectionStateManageComponent extends WattTypedModal<{
   today = new Date();
   minDate = dayjs(this.today).subtract(7, 'days').toDate();
 
-  stateControl = new FormControl<ConnectionState>(this.modalData.currentConnectionState);
-  validityDateControl = new FormControl<Date>(this.today, {
-    validators: [Validators.required],
+  form = new FormGroup({
+    state: new FormControl<ConnectionState>(this.modalData.currentConnectionState),
+    validityDate: new FormControl<Date>(this.today, {
+      validators: [Validators.required],
+    }),
   });
 
-  stateControlOptions = dhEnumToWattDropdownOptions(ConnectionState, [
-    ConnectionState.NotUsed,
-    ConnectionState.ClosedDown,
-    ConnectionState.Disconnected,
-  ]);
+  stateControlOptions = dhEnumToWattDropdownOptions(ConnectionState, this.statesToExclude());
+
+  private statesToExclude(): ConnectionState[] | undefined {
+    if (this.modalData.currentConnectionState === ConnectionState.New) {
+      return [ConnectionState.NotUsed, ConnectionState.ClosedDown, ConnectionState.Disconnected];
+    }
+
+    return undefined;
+  }
 
   save() {
     console.log('Saving connection state changes...');
