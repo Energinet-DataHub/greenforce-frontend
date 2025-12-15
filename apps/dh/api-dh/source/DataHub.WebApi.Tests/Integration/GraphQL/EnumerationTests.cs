15c240815d11324cfa8cad1385b2c8312fb633c3
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2025-12-15 16:33:43 +0100

    Add Enumeration tests

---
+++ apps/dh/api-dh/source/DataHub.WebApi.Tests/Integration/GraphQL/EnumerationTests.cs
@@ -0,0 +1,47 @@
+// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Energinet.DataHub.WebApi.Modules.Common.Models;
+using Energinet.DataHub.WebApi.Modules.Processes.Requests.Models;
+using Energinet.DataHub.WebApi.Tests.Helpers;
+using Xunit;
+using ChargeResolution = Energinet.DataHub.Charges.Abstractions.Shared.Resolution;
+using RequestChangeOfPriceListResolution = Energinet.DataHub.EDI.B2CClient.Abstractions.RequestChangeOfPriceList.V1.Models.ResolutionV1;
+
+namespace Energinet.DataHub.WebApi.Tests.Integration.GraphQL;
+
+public class EnumerationTests
+{
+    [Theory]
+    [InlineData(typeof(ChargeResolution))]
+    public void ResolutionFromNameTests(Type type) =>
+        EnumerationTestHelper.TestEnumerationFrom(type, Resolution.FromName);
+
+    [Theory]
+    [InlineData(typeof(RequestChangeOfPriceListResolution))]
+    public void ResolutionFromDurationTests(Type type) =>
+        EnumerationTestHelper.TestEnumerationFrom(type, Resolution.FromDuration);
+
+    [Theory]
+    [InlineData(typeof(ChargeResolution))]
+    public void ResolutionCastTests(Type type) =>
+        EnumerationTestHelper.TestEnumerationCast<Resolution>(type, r => r.Cast(type));
+
+    [Fact]
+    public void RequestChangeOfPriceListResolutionCastFromDurationTests() =>
+        EnumerationTestHelper.TestEnumerationCast<Resolution>(
+            typeof(RequestChangeOfPriceListResolution),
+            r => r.CastFromDuration<RequestChangeOfPriceListResolution>());
+}
