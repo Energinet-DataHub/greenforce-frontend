fb5cd40541c8871987b3b31b544bf8acb7d5edc2
Morten Hansen <mimo@mimo-design.com>
2025-12-21 19:56:54 +0100

    Removed

--- libs/dh/charges/feature-charges/src/components/actions/edit.ts
+++ libs/dh/charges/feature-charges/src/components/actions/edit.ts
@@ -40,7 +40,6 @@ import {
 } from '@energinet-datahub/dh/shared/ui-util';
 
 import {
-  VatClassificationDto,
   UpdateChargeDocument,
   GetChargeByIdDocument,
 } from '@energinet-datahub/dh/shared/domain/graphql';
