a43727f1419cd254b34068aee2cbff6806dd6e23
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2026-01-12 15:16:16 +0100

    Remove using

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/Charges/Models/ChargeSeriesPointChange.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/Charges/Models/ChargeSeriesPointChange.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using NodaTime;
-
 namespace Energinet.DataHub.WebApi.Modules.Charges.Models;
 
 public record ChargeSeriesPointChange(decimal Price, bool IsCurrent, string? MessageId);
