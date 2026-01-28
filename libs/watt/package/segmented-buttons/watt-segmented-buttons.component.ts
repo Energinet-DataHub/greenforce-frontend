38f4c66af6b56ee9cc7ea161b442c882814e6205
Morten Hansen <mimo@mimo-design.com>
2026-01-28 10:17:25 +0100

    Added queryparam merge

--- libs/watt/package/segmented-buttons/watt-segmented-buttons.component.ts
+++ libs/watt/package/segmented-buttons/watt-segmented-buttons.component.ts
@@ -96,6 +96,7 @@ import { WattSegmentedButtonComponent } from './watt-segmented-button.component'
       @for (segmentedButton of segmentedButtonElements(); track segmentedButton) {
         <mat-button-toggle
           [routerLink]="segmentedButton.link()"
+          [queryParamsHandling]="'merge'"
           routerLinkActive="mat-button-toggle-checked"
           [disableRipple]="true"
           [value]="segmentedButton.value()"
