c291f9b47325b9cf6a2c3c8c39b296560a3e9d2d
Mathias Sejrup Plougmann <masep@energinet.dk>
2026-01-29 23:57:36 +0100

    chore: adds buttons

--- libs/dh/metering-point/feature-actor-conversation/src/components/actor-conversation-selected-conversation.component.ts
+++ libs/dh/metering-point/feature-actor-conversation/src/components/actor-conversation-selected-conversation.component.ts
@@ -20,11 +20,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { WattIconComponent } from '@energinet/watt/icon';
 import { VaterStackComponent } from '@energinet/watt/vater';
 import { WattBadgeComponent } from '@energinet/watt/badge';
+import { WattButtonComponent } from '@energinet/watt/button';
+import { WattMenuComponent, WattMenuItemComponent, WattMenuTriggerDirective } from '@energinet/watt/menu';
 
 @Component({
   selector: 'dh-actor-conversation-selected-conversation',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [WattIconComponent, VaterStackComponent, WattBadgeComponent],
+  imports: [
+    WattIconComponent,
+    VaterStackComponent,
+    WattBadgeComponent,
+    WattButtonComponent,
+    WattMenuComponent,
+    WattMenuItemComponent,
+    WattMenuTriggerDirective,
+  ],
   styles: `
     .no-margin {
       margin: 0;
@@ -36,25 +46,42 @@ import { WattBadgeComponent } from '@energinet/watt/badge';
   `,
   template: `
     <!-- Header -->
-    <vater-stack gap="s" class="inset-stretch-inverted" align="start">
-      <vater-stack direction="row" gap="xs">
-        <span class="watt-text-s">Sort Strøm</span>
-        <watt-icon name="right" size="xs" />
-        <span class="watt-text-s">Netvirksomhed</span>
+    <vater-stack direction="row" justify="space-between" class="inset-stretch-inverted">
+      <vater-stack gap="s" align="start">
+        <vater-stack direction="row" gap="xs">
+          <span class="watt-text-s">Sort Strøm</span>
+          <watt-icon name="right" size="xs" />
+          <span class="watt-text-s">Netvirksomhed</span>
+        </vater-stack>
+        <vater-stack direction="row" gap="s">
+          <h3 class="no-margin">Måledata</h3>
+          <watt-badge type="neutral">Afsluttet</watt-badge>
+        </vater-stack>
+        <vater-stack direction="row" gap="m">
+          <vater-stack direction="row" gap="xs">
+            <label>ID</label>
+            <span class="watt-text-s">521532</span>
+          </vater-stack>
+          <vater-stack direction="row" gap="xs">
+            <label>Intern note</label>
+            <span class="watt-text-s">CS00123645</span>
+          </vater-stack>
+        </vater-stack>
       </vater-stack>
-      <vater-stack direction="row" gap="s">
-        <h3 class="no-margin">Måledata</h3>
-        <watt-badge type="neutral">Afsluttet</watt-badge>
-      </vater-stack>
+
       <vater-stack direction="row" gap="m">
-        <vater-stack direction="row" gap="xs">
-          <label>ID</label>
-          <span class="watt-text-s">521532</span>
-        </vater-stack>
-        <vater-stack direction="row" gap="xs">
-          <label>Intern note</label>
-          <span class="watt-text-s">CS00123645</span>
-        </vater-stack>
+        <watt-button [disabled]="true" variant="secondary">Afslut sag</watt-button>
+        <watt-button variant="secondary" [wattMenuTriggerFor]="menu">
+          <watt-icon name="moreVertical" />
+        </watt-button>
+        <watt-menu #menu>
+          <watt-menu-item>
+            Intern note
+          </watt-menu-item>
+          <watt-menu-item>
+            Marker som ulæst
+          </watt-menu-item>
+        </watt-menu>
       </vater-stack>
     </vater-stack>
     <hr class="watt-divider no-margin" />
