cd41a7f753ae0883615d27ccb35998ceb79758c2
Mathias Sejrup Plougmann <masep@energinet.dk>
2026-02-03 14:37:09 +0100

    chore: adds todo

--- libs/dh/metering-point/feature-actor-conversation/src/components/actor-conversation-shell.component.ts
+++ libs/dh/metering-point/feature-actor-conversation/src/components/actor-conversation-shell.component.ts
@@ -168,6 +168,7 @@ export class DhActorConversationShellComponent {
   private toastService = inject(WattToastService);
 
   async startConversation(formValue: StartConversationFormValue) {
+    // TODO: MASEP Remove when the API takes actorId and UserId
     const actorName = this.selectedMarketParticipant()?.actorName ?? '';
     const userName = (this.userProfile()?.firstName ?? '') + ' ' + (this.userProfile()?.lastName ?? '');
 
