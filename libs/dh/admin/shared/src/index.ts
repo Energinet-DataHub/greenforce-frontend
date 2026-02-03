4642c8faa65d5964054d56204c9b7175f0dcdd19
Dzhavat Ushev <dzu@energinet.dk>
2026-02-03 08:29:12 +0100

    fix: minor refactoring (#5230)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/dh/admin/shared/src/index.ts
+++ libs/dh/admin/shared/src/index.ts
@@ -18,6 +18,5 @@
 //#endregion
 export { DhRoleStatusComponent } from './dh-role-status.component';
 export { DhUserStatusComponent } from './dh-user-status.component';
-export { DhTabDataGeneralErrorComponent } from './dh-tab-data-general-error.component';
 export { DhPermissionsTableComponent } from './dh-permissions-table.component';
 export * from './types/dh-update-user-role.type';
--- libs/watt/package/description-list/watt-description-list-item.component.ts
+++ libs/watt/package/description-list/watt-description-list-item.component.ts
@@ -17,21 +17,19 @@
  */
 //#endregion
 import { ChangeDetectionStrategy, Component, input, TemplateRef, viewChild } from '@angular/core';
-import { NgClass } from '@angular/common';
 
 @Component({
-  imports: [NgClass],
   selector: 'watt-description-list-item',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  hostDirectives: [NgClass],
   template: `<ng-template #templateRef
-    ><div [ngClass]="{ 'force-new-row': forceNewRow() }">
+    ><div [class.force-new-row]="forceNewRow()">
       <dt class="watt-label watt-on-light--high-emphasis">{{ label() }}</dt>
       <dd class="watt-text-s">{{ value() }}<ng-content /></dd></div
   ></ng-template>`,
 })
 export class WattDescriptionListItemComponent<T> {
   templateRef = viewChild.required<TemplateRef<unknown | null>>('templateRef');
+
   label = input<string>('');
   value = input<T | null>(null);
   forceNewRow = input(false);
