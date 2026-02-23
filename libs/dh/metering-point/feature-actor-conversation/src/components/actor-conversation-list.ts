d29ce4757a52c2cec6a3dac0dff3c7febd9d9387
Mathias Sejrup Plougmann <masep@energinet.dk>
2026-02-23 22:49:53 +0100

    chore: Adds component for filters

--- libs/dh/metering-point/feature-actor-conversation/src/components/actor-conversation-list.ts
+++ libs/dh/metering-point/feature-actor-conversation/src/components/actor-conversation-list.ts
@@ -20,19 +20,20 @@ import { ChangeDetectionStrategy, Component, computed, input, output } from '@an
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { WattButtonComponent } from '@energinet/watt/button';
 import { VATER, VaterUtilityDirective } from '@energinet/watt/vater';
-import { TranslocoDirective } from '@jsverse/transloco';
+import { TranslocoDirective, translateObjectSignal } from '@jsverse/transloco';
 import { DhActorConversationListItemComponent } from './actor-conversation-list-item';
 import { Conversation, NewConversation } from '../types';
 import { WattHeadingComponent } from '@energinet/watt/heading';
 import { QueryResult } from '@energinet-datahub/dh/shared/util-apollo';
 import {
+  ConversationSubject,
   GetConversationsQuery,
   GetConversationsQueryVariables,
 } from '@energinet-datahub/dh/shared/domain/graphql';
-import { DhResultComponent } from '@energinet-datahub/dh/shared/ui-util';
+import { dhEnumToWattDropdownOptions, DhResultComponent } from '@energinet-datahub/dh/shared/ui-util';
 import { dayjs } from '@energinet/watt/core/date';
 import { WattSimpleSearchComponent } from '@energinet/watt/search';
-import { WattIconComponent } from '@energinet/watt/icon';
+import { WattDropdownComponent, WattDropdownOptionGroup } from '@energinet/watt/dropdown';
 
 @Component({
   selector: 'dh-actor-conversation-list',
@@ -46,7 +47,7 @@ import { WattIconComponent } from '@energinet/watt/icon';
     DhResultComponent,
     ReactiveFormsModule,
     WattSimpleSearchComponent,
-    WattIconComponent,
+    WattDropdownComponent,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   styles: `
@@ -65,7 +66,7 @@ import { WattIconComponent } from '@energinet/watt/icon';
   `,
   template: `
     <vater-stack sticky="top" *transloco="let t; prefix: 'meteringPoint.actorConversation'">
-      <vater-stack fill="horizontal" class="new-conversation watt-space-inset-m" gap="m">
+      <vater-stack fill="horizontal" class="new-conversation watt-space-inset-m" gap="m" align="start">
         <vater-stack fill="horizontal" direction="row" justify="space-between" align="center">
           <h3 watt-heading>{{ t('cases') }}</h3>
           <watt-button
@@ -77,7 +78,6 @@ import { WattIconComponent } from '@energinet/watt/icon';
             {{ t('newCaseButton') }}
           </watt-button>
         </vater-stack>
-        <vater-stack fill="horizontal" direction="row" gap="s">
           <watt-simple-search
             vater
             fill="horizontal"
@@ -85,10 +85,13 @@ import { WattIconComponent } from '@energinet/watt/icon';
             [formControl]="searchControl"
             (search)="filter.emit($event)"
           />
-          <watt-button variant="secondary">
-            <watt-icon name="filter" />
-          </watt-button>
-        </vater-stack>
+          <watt-dropdown
+            [formControl]="formControl"
+            [options]="options()"
+            [multiple]="true"
+            [chipMode]="true"
+            [placeholder]="t('filters.placeholder')"
+          />
       </vater-stack>
       <hr class="watt-divider thick-divider" />
     </vater-stack>
@@ -119,6 +122,8 @@ import { WattIconComponent } from '@energinet/watt/icon';
   `,
 })
 export class DhActorConversationListComponent {
+  private translations = translateObjectSignal('meteringPoint.actorConversation');
+
   conversationsQuery = input<QueryResult<GetConversationsQuery, GetConversationsQueryVariables>>();
   conversations = computed(
     () => this.conversationsQuery()?.data()?.conversationsForMeteringPoint?.conversations ?? []
@@ -129,6 +134,36 @@ export class DhActorConversationListComponent {
   filter = output<string>();
   selectConversation = output<Conversation>();
   searchControl = new FormControl('');
+  formControl = new FormControl<string | ''>('');
+  conversationSubjectOptions = dhEnumToWattDropdownOptions(ConversationSubject);
+
+  options = computed<WattDropdownOptionGroup[]>(() => {
+    const t = this.translations();
+
+    return [
+      {
+        label: t['filters']['general'],
+        options: [
+          { value: 'myCases', displayValue: t['filters']['myCases'] },
+          { value: 'showOnlyUnread', displayValue: t['filters']['showOnlyUnread'] },
+        ],
+      },
+      {
+        label: t['filters']['status'],
+        options: [
+          { value: 'active', displayValue: t['active'] },
+          { value: 'closed', displayValue: t['closed'] },
+        ],
+      },
+      {
+        label: t['subjectLabel'],
+        options: this.conversationSubjectOptions.map((option) => ({
+          ...option,
+          displayValue: t['subjects'][option.value],
+        })),
+      },
+    ];
+  });
 
   newConversation: NewConversation = {
     __typename: 'ConversationInfo',
