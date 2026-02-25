419a6cdc7c8eed67cab1ea58a4c1109dcf93bd44
Mathias Sejrup Plougmann <masep@energinet.dk>
2026-02-25 20:31:48 +0100

    feat: adds sent anonymous

--- libs/dh/metering-point/feature-actor-conversation/src/components/actor-conversation-message.ts
+++ libs/dh/metering-point/feature-actor-conversation/src/components/actor-conversation-message.ts
@@ -55,15 +55,20 @@ import { WattSeparatorComponent } from '@energinet/watt/separator';
           <span>{{ t('receivers.' + message().senderType) }}</span>
           <span>{{ message().createdTime | wattDate: 'short' }}</span>
         </vater-stack>
-        @if (message().actorName && message().userName) {
-          <span>{{ message().actorName + ', ' + message().userName }}</span>
-        }
+        <vater-stack direction="row" gap="s">
+          @if (message().actorName && message().userName) {
+            <span>{{ message().actorName + ', ' + message().userName }}</span>
+          }
+          @if (message().actorName && message().userName && message().anonymous) {
+            <span>{{ t('sentAnonymously') }}</span>
+          }
+        </vater-stack>
       </vater-stack>
       <watt-separator />
       @if (message().messageType === 'USER_MESSAGE') {
         <span vater fill="horizontal" class="watt-space-inset-m">{{
-          message().userMessage?.content
-        }}</span>
+            message().userMessage?.content
+          }}</span>
       }
       @if (message().messageType === 'CLOSING_MESSAGE') {
         <span vater fill="horizontal" class="watt-space-inset-m">{{ t('closingMessage') }}</span>
