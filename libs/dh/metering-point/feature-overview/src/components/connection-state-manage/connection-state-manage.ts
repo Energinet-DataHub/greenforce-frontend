7331d37fb00019d20af6bda68d9adf36d42ba46d
Dzhavat Ushev <dzu@energinet.dk>
2026-02-26 14:04:57 +0100

    fix: add state param to mutation

--- libs/dh/metering-point/feature-overview/src/components/connection-state-manage/connection-state-manage.ts
+++ libs/dh/metering-point/feature-overview/src/components/connection-state-manage/connection-state-manage.ts
@@ -140,6 +140,7 @@ export class DhConnectionStateManageComponent extends WattTypedModal<{
       variables: {
         input: {
           meteringPointId: this.modalData.meteringPointId,
+          toState: this.form.controls.state.value,
           validityDate,
         },
       },
