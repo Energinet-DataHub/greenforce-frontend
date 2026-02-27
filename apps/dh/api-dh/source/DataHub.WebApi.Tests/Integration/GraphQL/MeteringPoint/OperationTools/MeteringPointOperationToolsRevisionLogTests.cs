46a4e1cbf95a98b6b77029513c8d066e007e0be5
Ebbe Knudsen <xedkn@energinet.dk>
2026-02-27 11:58:57 +0100

    Add revision log to operation tools metering point

---
+++ apps/dh/api-dh/source/DataHub.WebApi.Tests/Integration/GraphQL/MeteringPoint/OperationTools/MeteringPointOperationToolsRevisionLogTests.cs
@@ -0,0 +1,60 @@
+// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.ElectricityMarket.Abstractions.Features.MeteringPoint.GetMeteringPointDebug.V1;
+using Energinet.DataHub.ElectricityMarket.Abstractions.Framework;
+using Energinet.DataHub.WebApi.Tests.Helpers;
+using Energinet.DataHub.WebApi.Tests.TestServices;
+using Energinet.DataHub.WebApi.Tests.Traits;
+using Moq;
+using Xunit;
+
+namespace Energinet.DataHub.WebApi.Tests.Integration.GraphQL.MeteringPoint.OperationTools;
+
+public class MeteringPointOperationToolsRevisionLogTests
+{
+    [Fact]
+    [RevisionLogTest("ElectricityMarketOperationToolsNode.GetOperationToolsMeteringPointAsync")]
+    public async Task GetOperationToolsMeteringPoint()
+    {
+        var operation =
+            $$"""
+                query($id: String!) {
+                  operationToolsMeteringPoint(id: $id) {
+                    meteringPointJson
+                  }
+                }
+              """;
+
+        var server = new GraphQLTestService();
+
+        var result = Result<GetMeteringPointDebugResultDtoV1>.Success(
+            new GetMeteringPointDebugResultDtoV1(
+                MeteringPointJson: "{\"id\":\"570000000000000008\"}",
+                MeteringPointWithRelationsJson: null,
+                Events: []));
+
+        server.ElectricityMarketClientMock.Setup(
+                c => c.SendAsync(It.IsAny<GetMeteringPointDebugQueryV1>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(result);
+
+        await RevisionLogTestHelper.ExecuteAndAssertAsync(
+            server,
+            operation,
+            new() { { "id", "570000000000000008" } });
+    }
+}
