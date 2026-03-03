8005e229a83190c34e6d8c5958b1efe61be971e6
Dzhavat Ushev <dzu@energinet.dk>
2026-03-03 14:39:18 +0100

    fix: disable lint rule

--- libs/dh/metering-point/feature-overview/src/components/connection-state-manage/connection-state-manage.ts
+++ libs/dh/metering-point/feature-overview/src/components/connection-state-manage/connection-state-manage.ts
@@ -170,6 +170,7 @@ export class DhConnectionStateManageComponent extends WattTypedModal<{
     }
   }
 
+  // eslint-disable-next-line sonarjs/cognitive-complexity
   private findMinDate(newConnectionState: ElectricityMarketViewConnectionState): Date {
     const daysSinceCreated = dayjs(this.today).diff(
       dayjs(this.modalData.currentCreatedDate),
