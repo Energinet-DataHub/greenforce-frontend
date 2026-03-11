362cf4c5474301436d8124e704f84ef363ee1ece
Morten Vestergaard Hansen <mimo@mimo-design.com>
2026-03-11 08:14:27 +0100

    refactor(dh): migrate libs/dh/shared/ domain to implicit libs (#5461)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libs/dh/shared/util-navigation/index.ts
+++ libs/dh/shared/util-navigation/index.ts
@@ -16,8 +16,4 @@
  * limitations under the License.
  */
 //#endregion
-import * as dhLocalApiEnvironment from './assets/configuration/dh-api-environment.local.json';
-import * as dhLocalB2CEnvironment from './assets/configuration/dh-b2c-environment.local.json';
-import * as dhLocalAppEnvironment from './assets/configuration/dh-app-environment.local.json';
-
-export { dhLocalApiEnvironment, dhLocalB2CEnvironment, dhLocalAppEnvironment };
+export { DhNavigationService } from './src/navigation.service';
