619fdb602f8732a0bf0ed0118c1c341d44b70f81
Kevin Guldager Stampe <kevinstampe@gmail.com>
2026-03-16 10:21:07 +0100

    chore: fixed broken openapi code after .net 10 upgrade, big breaking changes in Microsoft.OpenApi

--- apps/dh/api-dh/source/DataHub.WebApi/RequireNonNullablePropertiesSchemaFilter.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/RequireNonNullablePropertiesSchemaFilter.cs
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Reflection;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Energinet.DataHub.WebApi;
@@ -23,59 +22,20 @@ public class RequireNonNullablePropertiesSchemaFilter : ISchemaFilter
     /// <summary>
     /// Add to model.Required all properties where Nullable is false.
     /// </summary>
-    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    public void Apply(IOpenApiSchema schema, SchemaFilterContext context)
     {
         if (schema.Properties == null || !schema.Properties.Any())
         {
             return;
         }
 
-        FixNullableProperties(schema, context);
-
         var additionalRequiredProps = schema.Properties
-            .Where(x => !x.Value.Nullable && !schema.Required.Contains(x.Key))
+            .Where(x => !(x.Value.Type?.HasFlag(JsonSchemaType.Null) ?? false) && !(schema.Required?.Contains(x.Key) ?? false))
             .Select(x => x.Key);
 
         foreach (var propKey in additionalRequiredProps)
         {
-            schema.Required.Add(propKey);
-        }
-    }
-
-    /// <summary>
-    /// Option "SupportNonNullableReferenceTypes" not working with complex types ({ "type": "object" }),
-    /// so they always have "Nullable = false",
-    /// see method "SchemaGenerator.GenerateSchemaForMember"
-    /// </summary>
-    private static void FixNullableProperties(OpenApiSchema schema, SchemaFilterContext context)
-    {
-        foreach (var (key, value) in schema.Properties)
-        {
-            if (value.Reference == null)
-            {
-                continue;
-            }
-
-            var field = context.Type
-                .GetMembers(BindingFlags.Public | BindingFlags.Instance)
-                .FirstOrDefault(x =>
-                    string.Equals(x.Name, key, StringComparison.InvariantCultureIgnoreCase));
-
-            if (field == null)
-            {
-                continue;
-            }
-
-            var fieldType = field switch
-            {
-                FieldInfo fieldInfo => fieldInfo.FieldType,
-                PropertyInfo propertyInfo => propertyInfo.PropertyType,
-                _ => throw new NotSupportedException(),
-            };
-
-            value.Nullable = fieldType.IsValueType
-                ? Nullable.GetUnderlyingType(fieldType) != null
-                : !field.IsNonNullableReferenceType();
+            schema.Required!.Add(propKey);
         }
     }
 }
