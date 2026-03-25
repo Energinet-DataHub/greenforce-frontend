31825832d3083883cc75e860574ddc6e2bb25edd
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2026-03-25 10:28:24 +0100

    Refactor charge link bff a bit

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/ElectricityMarket/Charges/ChargeLinkOperations.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/ElectricityMarket/Charges/ChargeLinkOperations.cs
@@ -31,20 +31,17 @@ public static partial class ChargeLinkOperations
     [Authorize(Roles = ["metering-point:prices"])]
     public static async Task<IEnumerable<ChargeLinkDto>> GetChargeLinksByMeteringPointIdAsync(
         string meteringPointId,
-        CancellationToken ct,
-        IChargeLinkClient client) =>
-            await client.GetChargeLinksByMeteringPointIdAsync(meteringPointId, ct);
+        IChargeLinkClient client,
+        CancellationToken ct)
+        => await client.GetChargeLinksByMeteringPointIdAsync(meteringPointId, ct);
 
     [Query]
     [Authorize(Roles = ["metering-point:prices"])]
     public static async Task<ChargeLinkDto?> GetChargeLinkByIdAsync(
         ChargeLinkId id,
-        CancellationToken ct,
-        IChargeLinkClient client)
-    {
-        var chargeLinks = await client.GetChargeLinksByMeteringPointIdAsync(id.MeteringPointId, ct);
-        return chargeLinks.FirstOrDefault(cl => cl.ChargeIdentifier == id.ChargeId);
-    }
+        IChargeLinkClient client,
+        CancellationToken ct)
+        => await client.GetChargeLinkByIdAsync(id, ct);
 
     [Mutation]
     [Authorize(Roles = ["metering-point:prices-manage"])]
@@ -52,8 +49,8 @@ public static partial class ChargeLinkOperations
         ChargeLinkId id,
         DateTimeOffset stopDate,
         IChargeLinkClient client,
-        CancellationToken ct) =>
-            await client.StopChargeLinkAsync(id, stopDate, ct);
+        CancellationToken ct)
+        => await client.StopChargeLinkAsync(id, stopDate, ct);
 
     [Mutation]
     [Authorize(Roles = ["metering-point:prices-manage"])]
@@ -61,9 +58,9 @@ public static partial class ChargeLinkOperations
         ChargeLinkId id,
         DateTimeOffset newStartDate,
         int factor,
-        CancellationToken ct,
-        IChargeLinkClient client) =>
-            await client.EditChargeLinkAsync(id, newStartDate, factor, ct);
+        IChargeLinkClient client,
+        CancellationToken ct)
+        => await client.EditChargeLinkAsync(id, newStartDate, factor, ct);
 
     [Mutation]
     [Authorize(Roles = ["metering-point:prices-manage"])]
@@ -72,37 +69,39 @@ public static partial class ChargeLinkOperations
         string meteringPointId,
         DateTimeOffset newStartDate,
         int factor,
-        CancellationToken ct,
-        IChargeLinkClient client) =>
-            await client.CreateChargeLinkAsync(chargeId, meteringPointId, newStartDate, factor, ct);
+        IChargeLinkClient client,
+        CancellationToken ct)
+        => await client.CreateChargeLinkAsync(chargeId, meteringPointId, newStartDate, factor, ct);
 
     [Mutation]
     [Authorize(Roles = ["metering-point:prices-manage"])]
     public static async Task<bool> CancelChargeLinkAsync(
         ChargeLinkId id,
-        CancellationToken ct,
-        IChargeLinkClient client) =>
-            await client.CancelChargeLinkAsync(id, ct);
+        IChargeLinkClient client,
+        CancellationToken ct)
+        => await client.CancelChargeLinkAsync(id, ct);
 
     public static IEnumerable<ChargeLinkHistory> GetHistory(
-        [Parent] ChargeLinkDto chargeLink) =>
-            chargeLink.ChargeLinkPeriods
-                .OrderByDescending(x => x.From)
-                .Select(period => new ChargeLinkHistory(
-                    period.From.ToDateTimeOffset(),
-                    string.Empty,
-                    string.Empty)).ToList();
+        [Parent] ChargeLinkDto chargeLink)
+        => [.. chargeLink.ChargeLinkPeriods
+            .OrderByDescending(x => x.From)
+            .Select(period => new ChargeLinkHistory(
+                period.From.ToDateTimeOffset(),
+                string.Empty,
+                string.Empty))];
 
-    public static async Task<Charge?> GetChargeAsync(
+    public static async Task<Charge> GetChargeAsync(
         [Parent] ChargeLinkDto chargeLink,
         IChargesClient client,
-        CancellationToken ct) =>
-            await client.GetChargeByIdAsync(chargeLink.ChargeIdentifier, ct);
+        CancellationToken ct)
+        => await client.GetChargeByIdAsync(chargeLink.ChargeIdentifier, ct)
+            ?? throw new GraphQLException("Charge not found");
 
-    public static int GetAmount([Parent] ChargeLinkDto chargeLink) => chargeLink.GetPeriod()?.Factor ?? 1;
+    public static int GetAmount([Parent] ChargeLinkDto chargeLink)
+        => chargeLink.GetPeriod().Factor;
 
-    public static ChargeLinkPeriodDto? GetPeriod([Parent] ChargeLinkDto chargeLink) =>
-        chargeLink.GetPeriod();
+    public static ChargeLinkPeriodDto GetPeriod([Parent] ChargeLinkDto chargeLink)
+        => chargeLink.GetPeriod();
 
     static partial void Configure(IObjectTypeDescriptor<ChargeLinkDto> descriptor)
     {
