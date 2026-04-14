fd0e9859238227e7bf7050f675d91ce30415b9b0
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2026-04-14 14:48:31 +0200

    Add SortOrder to ChargeType

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/Charges/Models/ChargeType.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/Charges/Models/ChargeType.cs
@@ -21,12 +21,13 @@ namespace Energinet.DataHub.WebApi.Modules.Charges.Models;
 public record ChargeType(
     string Name,
     ExternalChargeType Type,
-    bool IsTax)
+    bool IsTax,
+    int SortOrder)
 {
-    public static readonly ChargeType Tariff = new(nameof(Tariff), ExternalChargeType.Tariff, false);
-    public static readonly ChargeType TariffTax = new(nameof(TariffTax), ExternalChargeType.Tariff, true);
-    public static readonly ChargeType Subscription = new(nameof(Subscription), ExternalChargeType.Subscription, false);
-    public static readonly ChargeType Fee = new(nameof(Fee), ExternalChargeType.Fee, false);
+    public static readonly ChargeType Tariff = new(nameof(Tariff), ExternalChargeType.Tariff, false, 1);
+    public static readonly ChargeType TariffTax = new(nameof(TariffTax), ExternalChargeType.Tariff, true, 2);
+    public static readonly ChargeType Fee = new(nameof(Fee), ExternalChargeType.Fee, false, 3);
+    public static readonly ChargeType Subscription = new(nameof(Subscription), ExternalChargeType.Subscription, false, 4);
 
     public override string ToString() => Name;
 
