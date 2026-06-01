8747195e16a0a39ad9ea99c453894fb8d2bbb65b
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2026-06-01 10:16:58 +0000

    Use new model + handle month resolution differently

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/Charges/Client/ChargesClient.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/Charges/Client/ChargesClient.cs
@@ -281,10 +281,19 @@ public class ChargesClient(
         ChargeIdentifierDto id,
         DateTimeOffset start,
         DateTimeOffset end,
-        List<ChargePointV2> points,
+        List<ChargeSeriesPointInput> points,
         CancellationToken ct = default)
     {
         var charge = await GetChargeByIdAsync(id, ct) ?? throw new GraphQLException("Charge not found");
+        var resolution = charge.Resolution.CastDurationTo<ResolutionV2>();
+        var seriesPeriods = charge.Resolution == Resolution.Monthly
+            ? BuildMonthlySeriesPeriods(resolution, points, end)
+            : [new ChargeSeriesPointsV2(
+                Resolution: resolution,
+                Start: start,
+                End: end,
+                Points: [.. points.Select(p => new ChargePointV2(p.Position, p.Price))])];
+
         var result = await ediClient.SendAsync(
             new UpsertChargeSeriesCommandV2(new(
                 ChargeId: id.Code,
@@ -292,14 +301,7 @@ public class ChargesClient(
                 ChargeOwnerId: id.Owner,
                 Start: start,
                 End: end,
-                Points:
-                [
-                    new(
-                        Resolution: charge.Resolution.CastDurationTo<ResolutionV2>(),
-                        Start: start,
-                        End: end,
-                        Points: points),
-                ])),
+                Points: [.. seriesPeriods])),
             ct);
 
         return result.IsSuccess;
@@ -442,6 +444,18 @@ public class ChargesClient(
         ChargeTypeDto.Subscription => ChargeTypeV1.Subscription,
         ChargeTypeDto.Fee => ChargeTypeV1.Fee,
     };
+
+    private static IEnumerable<ChargeSeriesPointsV2> BuildMonthlySeriesPeriods(
+        ResolutionV2 resolution,
+        List<ChargeSeriesPointInput> points,
+        DateTimeOffset overallEnd)
+        => points.Zip(
+            points.Skip(1).Select(p => p.StartDate).Append(overallEnd),
+            (point, end) => new ChargeSeriesPointsV2(
+                Resolution: resolution,
+                Start: point.StartDate,
+                End: end,
+                Points: [new ChargePointV2(Position: 1, PriceAmount: point.Price)]));
 
     private static DateTimeZone DanishTimeZone => LocalDateExtensions.DanishTimeZone;
 
