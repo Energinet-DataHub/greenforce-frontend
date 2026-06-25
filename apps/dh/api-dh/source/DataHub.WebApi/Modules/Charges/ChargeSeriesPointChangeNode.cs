ccc842526d6ca12e93ba34e026649db44b4aa0fe
Bjørn Hoffmann <bjorn.id@bhoffmann.com>
2026-06-25 18:47:37 +0000

    Get message ID using PM + dataloader

--- apps/dh/api-dh/source/DataHub.WebApi/Modules/Charges/ChargeSeriesPointChangeNode.cs
+++ apps/dh/api-dh/source/DataHub.WebApi/Modules/Charges/ChargeSeriesPointChangeNode.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.ProcessManager.Client;
+using Energinet.DataHub.WebApi.Extensions;
 using Energinet.DataHub.WebApi.Modules.Charges.Models;
 
 namespace Energinet.DataHub.WebApi.Modules.Charges.Types;
@@ -19,6 +21,28 @@ namespace Energinet.DataHub.WebApi.Modules.Charges.Types;
 [ObjectType<ChargeSeriesPointChange>]
 public static partial class ChargeSeriesPointChangeNode
 {
+    [DataLoader]
+    public static async Task<IReadOnlyDictionary<Guid, string?>> GetActorMessageIdByOrchestrationIdAsync(
+        IReadOnlyList<Guid> keys,
+        IProcessManagerClient client,
+        IHttpContextAccessor httpContextAccessor,
+        CancellationToken ct)
+    {
+        var identity = httpContextAccessor.CreateUserIdentity();
+        return await keys
+            .ToAsyncEnumerable()
+            .Select(async (id, ct) => await client.GetOrchestrationInstanceByIdAsync(new(identity, id), ct))
+            .Select(r => KeyValuePair.Create(r.Id, r.ActorMessageId))
+            .ToDictionaryAsync(cancellationToken: ct);
+    }
+
+    public static async Task<string?> GetMessageIdAsync(
+        [Parent] ChargeSeriesPointChange parent,
+        ActorMessageIdByOrchestrationIdDataLoader dataLoader)
+        => parent.OrchestrationInstanceId is not null
+            ? await dataLoader.LoadAsync(parent.OrchestrationInstanceId.Value)
+            : null;
+
     static partial void Configure(IObjectTypeDescriptor<ChargeSeriesPointChange> descriptor)
     {
         descriptor.Name("ChargeSeriesPointChange");
@@ -26,6 +50,5 @@ public static partial class ChargeSeriesPointChangeNode
         descriptor.Field(f => f.Created);
         descriptor.Field(f => f.Price);
         descriptor.Field(f => f.IsCurrent);
-        descriptor.Field(f => f.OrchestrationInstanceId).Type<StringType>().Name("messageId"); // TODO: Fix
     }
 }
